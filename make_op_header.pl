6f1e5c4cb468d5a2b6cd08b4c611bab721de86d9
Simon Cozens <simon@simon-cozens.org>
2001-09-03 16:38:29 +0000

    Spit out the whole of op.h, not just the bit in the middle.
    git-svn-id: https://svn.parrot.org/parrot/trunk@5 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- make_op_header.pl
+++ make_op_header.pl
@@ -3,9 +3,26 @@
 # rip through opcode_table and spit out a chunk of C header for the
 # functions in it
 while (<>) {
-    next if /^\s*#/;
+    next if /^\s*#/ or /^\s*$/;
     chomp;
     (undef, $name, undef) = split /\t/, $_;
     print "IV *$name(IV *, struct Perl_Interp *);\n";
 }
 
+BEGIN {
+    print q{/* op.h
+ *
+ * Opcode header
+ *
+ */
+
+#if !defined(PARROT_OP_H_GUARD)
+#define PARROT_OP_H_GUARD
+
+typedef IV OP;
+
+#define DEFAULT_OPCODE_TABLE NULL
+}
+}
+
+END { print "\n#endif\n"; }
