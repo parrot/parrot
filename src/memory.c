ebdcb766ee4c48f39babccaf76b7a8d58f610a83
Simon Cozens <simon@simon-cozens.org>
2001-09-15 11:26:31 +0000

    Fixed to match the coding standards.
    Courtesy of: Gibbs Tanton - tgibbs <Tanton.Gibbs@acxiom.com>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@94 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/memory.c
+++ src/memory.c
@@ -1,44 +1,69 @@
 /* Memory.c
- *
- *  Handle memory allocation
- *
+ *  Copyright: (When this is determined...it will go here)
+ *  CVS Info
+ *     $RCSfile$
+ *     $Revision$
+ *     $Date$
+ *  Overview:
+ *     The memory (mem) API handles memory allocation
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References:
  */
 
 #include "parrot/parrot.h"
 
-/* Allocate a chunk of memory aligned on a power-of-2 boundary */
+/*=for api mem mem_allocate_aligned
+   Allocate a chunk of memory aligned on a power-of-2 boundary
+*/
 void *
 mem_allocate_aligned(IV size) {
-  IV max_to_alloc;
-  IV mask;
-  IV i;
-  void *mem = NULL;
-
-  /* Okay, we just brute-force things here. Yeah it's stupid, but it
-     works */
-  for (i = 1; i < 0xffffff; i <<= 1) {
-    if (size > i) {
-      mask = ~(i*2 - 1);
-      max_to_alloc = i*4;
-    } else {
-      break;
-    }
-  }
+    IV max_to_alloc;
+    IV mask;
+    IV i;
+    void *mem = NULL;
     
-  mem = malloc(max_to_alloc);
-  if (((IV)mem & mask) < (IV)mem) {
-    mem = (void *)(((IV)mem & mask) + ~mask + 1);
-  } 
-  return mem;
+    /* Okay, we just brute-force things here. Yeah it's stupid, but it
+       works */
+    for (i = 1; i < 0xffffff; i <<= 1) {
+        if (size > i) {
+            mask = ~(i*2 - 1);
+            max_to_alloc = i*4;
+        }
+        else {
+            break;
+        }
+    }
+    
+    mem = malloc(max_to_alloc);
+    if (((IV)mem & mask) < (IV)mem) {
+        mem = (void *)(((IV)mem & mask) + ~mask + 1);
+    } 
+    return mem;
 }
 
+/*=for api mem mem_sys_allocate
+   uses malloc to allocate system memory
+*/
 void *
 mem_sys_allocate(IV size) {
-  return malloc(size);
+    return malloc(size);
 }
 
+/*=for api mem mem_setup_allocator
+   initializes the allocator
+*/
 void
 mem_setup_allocator(struct Perl_Interp *interpreter) {
 }
 
-  
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil 
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
