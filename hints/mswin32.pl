564a8f5f8f7c4e07111e85f3fefeab5effab0691
Brent Royal-Gordon <brent@brentdax.com>
2001-09-16 22:06:45 +0000

    Finishing the Configure hints
    git-svn-id: https://svn.parrot.org/parrot/trunk@117 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ hints/mswin32.pl
@@ -0,0 +1,17 @@
+{
+	my $is_msvc = grep { $c{cc} eq $_ } ( qw(cl cl.exe) );
+	my $is_mingw = grep { $c{cc} eq $_ } ( qw(gcc gcc.exe) );
+
+	if( $is_msvc ) {
+		$c{o} = '.obj';
+		$c{ld_out} = '-out:';
+		$c{cc_debug} = '-Zi';
+		$c{ld_debug} = '-debug';
+	}
+	elsif( $is_mingw ) {
+		$c{ld} = 'gcc';
+		# if your perl is ActivePerl, then libs are .lib files,
+		# not necessary, and gcc does not like them
+		$c{libs} = ' ' if $c{libs} =~ m/\.lib\s/i;
+	}
+}
