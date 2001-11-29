6282fa4b56e7a5a50e5911f23116d0978774b01d
Alex Gough <alex@earth.li>
2001-11-29 15:21:10 +0000

    This is a hints file for os/2, I've set $c{perl} as $^X lies, which confused bash which make uses to run things when doing its stuff.
    git-svn-id: https://svn.parrot.org/parrot/trunk@457 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ hints/os2.pl
@@ -0,0 +1,6 @@
+$c{libs} = "-lm -lsocket -lcExt -lbsd";
+$c{iv} = "long";
+$c{nv} = "double";
+$c{opcode_t}="long";
+$c{ccflags} = "-Zexe -Zmt -I. -fno-strict-aliasing -Wall -Wno-unused -Wpointer-arith -Wcast-align -mieee-fp -I./include";
+$c{perl} = "perl";
