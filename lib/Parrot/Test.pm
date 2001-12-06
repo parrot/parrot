9c7ea2abf9d9fa0b9a65f371ed3642ee72e73d45
Dan Sugalski <dan@sidhe.org>
2001-12-06 21:23:50 +0000

    Undo twiddles to test.pm
    git-svn-id: https://svn.parrot.org/parrot/trunk@502 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Test.pm
+++ lib/Parrot/Test.pm
@@ -37,7 +37,7 @@ sub _run_command {
     $redir_string .= "open $from, $to;"
   }
 
-  system "mcr $^X -e \"$redir_string;system q{$command};\"";
+  system "$^X -e \"$redir_string;system q{$command};\"";
 }
 
 my $count;
@@ -59,8 +59,8 @@ foreach my $i ( qw(is isnt like) ) {
     print ASSEMBLY $assembly;
     close ASSEMBLY;
 
-    _run_command( "perl assemble.pl $as_f --output $by_f" );
-    _run_command( "[]test_prog $by_f", 'STDOUT' => $out_f, 'STDERR' => $out_f);
+    _run_command( "$PConfig{perl} assemble.pl $as_f --output $by_f" );
+    _run_command( "./test_prog $by_f", 'STDOUT' => $out_f, 'STDERR' => $out_f);
 
     my $prog_output;
     open OUTPUT, "< $out_f";
