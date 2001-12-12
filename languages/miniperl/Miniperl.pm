596850ba588d92ef90c827dd44b73875fbb996db
Jeff Goff <jgoff@speakeasy.net>
2001-12-12 16:17:47 +0000

    Final go-round, hopefully.
    languages/miniperl/Makefile - Used basically to run tests, as there is nothing
                                  here that needs to be dynamically built.
    languages/miniperl/Miniperl.pm - Stub module that calls the real Miniperl/ mods
    languages/miniperl/foo.mpl - Sample miniperl file.
    languages/miniperl/miniperlc - Miniperl compiler stub
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@538 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ languages/miniperl/Miniperl.pm
@@ -0,0 +1,23 @@
+package Miniperl;
+
+use Data::Dumper;
+
+use Miniperl::Tokenizer qw(tokenize);
+use Miniperl::Parser qw(parse);
+use Miniperl::Generator;
+
+sub new {
+  my ($class,$file) = @_;
+  bless { file => $file },$class;
+}
+
+sub compile {
+  my $self = shift;
+  $self->{tokens} = tokenize($self->{file});
+#print Dumper($self->{tokens});exit;
+  $self->{tree}   = parse($self->{tokens});
+#print Dumper($self->{tree});exit;
+  Miniperl::Generator->new($self->{tree})->generate();
+}
+
+1;
---
+++ languages/miniperl/foo.mpl
@@ -0,0 +1,5 @@
+my ($a,$b);
+$b="foo";
+$a=-71.5;
+print $a;
+print $b;
