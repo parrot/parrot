1e0e982a247ca6eb6a78b6a7e5784219af81981f
Robert Spier <robert@perl.org>
2001-12-14 07:41:52 +0000

    - define MAKE macro - topological sort and subgraph order - start of timestamp stuff   (still badly broken)
    git-svn-id: https://svn.parrot.org/parrot/trunk@568 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- build_tools/make.pl
+++ build_tools/make.pl
@@ -9,6 +9,8 @@ use File::Spec;
 
 use Data::Dumper;
 
+use FindBin;
+
 sub new {
   my $proto = shift;
   my $file  = shift || 'Makefile';
@@ -20,6 +22,7 @@ sub new {
   }
   bless $self,$proto;
   $self->_unfold_lines($file);
+  $self->{expansion}{MAKE} = { exp => $FindBin::Bin, line => -1 };
   return $self;
 }
 
@@ -84,33 +87,79 @@ sub parse {
   $self->_validate_macros;
   $self->_expand;
   $self->_unpack_files;
-  $self->_walk($self->{depend});
+  $self->_toposort( $self->_depgraph_edges() );;
+
+
+  #  $self->_walk($self->{depend});
 }
 
-sub _run {
+sub _uptodate {
   my $self = shift;
   my $target = shift;
 
-  for my $file (values %{$target->{files}}) {
-    next unless ref($file) eq 'HASH';
-    $self->_run($file);
+#  print "$target is dependent on...\n";
+#  print "$_\n" for keys %{$self->{depend}{$_}{files}};
+  return 0 unless -e $target;
+
+  my $M = -M $target;
+  for (keys %{$self->{depend}{$target}{files}}) {
+    print "$_ < $target\n";
+    return 0 if (-M $_ > $M);
   }
-  for(@{$target->{actions}}) {
-    print "$_\n";
-  }
+
+  return 1;
 }
 
 sub run {
   my $self   = shift;
   my $target = shift;
 
-  if(defined $self->{depend}{$target}) {
-    $self->_run($self->{depend}{$target});
+  my @order = $self->_order($target);
+  for my $t (@order) {
+#    print "# building $t\n";
+    my $target = $self->{depend}{$t};
+
+    unless ($target->{actions}) {
+      # handle implicit stuff - this should be split out
+      $t =~ /\.([^\.]+)$/;
+      next unless $1;
+      # FIXME: this only handles the first implicit match
+      my ($implicit) = grep  {/^\.\w+\.$1/ } (keys %{$self->{depend}});
+      if ($implicit) {
+	my $src = $t;
+	my ($q,$r) = $implicit =~ /^(\.\w)(\.\w)/ or die;
+	$src =~ s/$r$/$q/;
+	print "HERE $src\n";
+	if (-e $src) {
+	  $target->{actions} = $self->{depend}{$implicit}{actions};
+	  $target->{files} = { $src => 1 };
+	  $target->{selif} = { reverse %{$target->{files}} };
+	}
+      }
+    } # end implicit handler
+
+    if (!$self->_uptodate($t)) {
+      for (@{$target->{actions}}) {
+	# handle special variables
+	{
+	  my $first = $target->{selif}->{1};
+	  $_ =~ s/\$@/$t/g;
+	  $_ =~ s/\$</$first/g;
+	}
+	print "$_\n";
+	system($_);
+      }
+    }
   }
-  else {
-    print STDERR "*** Target '$target' not defined.\n";
-    exit 1;
-  }
+
+
+#  if(defined $self->{depend}{$target}) {
+#    $self->_run($self->{depend}{$target});
+#  }
+#  else {
+#    print STDERR "*** Target '$target' not defined.\n";
+#    exit 1;
+#  }
 }
 
 sub _validate_macros {
@@ -174,13 +223,16 @@ sub _expand {
 sub _unpack_files {
   my $self = shift;
   my $dep = $self->{depend};
-
   for my $dependency (values %$dep) {
+    my $i = 1;
     $dependency->{files} = {
-      map { $_ => 1 }
+      map { $_ => $i++ }
       split /\s+/, $dependency->{files}
     };
+    $dependency->{selif} = { reverse %{$dependency->{files}} };
   }
+  # reversed so we can get the n'th element.
+
 }
 
 sub _walk {
@@ -224,6 +276,80 @@ sub _unfold_lines {
   close FILE;
 }
 
+
+sub _depgraph_edges {
+  my $self = shift;
+  my @out;
+
+  for my $k (keys %{$self->{depend}}) {
+    for my $v (keys %{$self->{depend}{$k}{files}}) {
+      push @out,[$k,$v];
+    }
+  }
+  return @out
+}
+
+sub _toposort {
+  my ($self,@pairs) = @_;               # list of lists
+  my %numpreds = ();
+  my %successors = ();
+
+  foreach (@pairs) {
+    my ($k,$v) = @$_;
+    # make sure every element is in numpreds
+    $numpreds{$k} = 0 unless exists $numpreds{$k};
+    $numpreds{$v} = 0 unless exists $numpreds{$v};
+    # since k < v , second gains a pred
+    $numpreds{$v}++;
+    # and first gains a succ
+    if ( exists $successors{$k} ) {
+      push @{$successors{$k}},$v;
+    } else {
+      $successors{$k} = [$v];
+    }
+  }
+
+  my @answer = grep { $numpreds{$_} == 0} keys %numpreds;
+
+  foreach my $x (@answer) {
+    delete $numpreds{$x};
+    if ( exists $successors{$x} ) {
+      for my $y ( @{$successors{$x}} ) {
+        $numpreds{$y}--;
+        push @answer,$y if $numpreds{$y} == 0;
+      }
+      delete $successors{$x};
+    }
+  }
+
+  if (keys %numpreds) {
+    # can get a more detailed error by printing the remaining values
+    # in numpreds
+    die "Cycle Error!";
+  }
+
+  $self->{tsort} = [@answer];
+}
+
+
+
+sub _order {
+  my ($self, $target) = @_;
+  my @order = @{ $self->{tsort} };
+  my @working = ( $target );
+  my %seen;
+  # walk the graph starting at our target, and mark everything we pass
+  while (@working) {
+    my $c = shift @working;
+    $seen{$c}++;
+    push @working, (grep { !$seen{$_} } (keys %{$self->{depend}{$c}{files}}));
+  }
+  # trim @order (into @deps) with only the stuff we've seen;
+  my (@deps) = grep { $seen{$_} } @order;
+  return reverse @deps;
+}
+
+
 package main;
 
 my $parser = Makefile::Parser->new($ARGV[0]);
@@ -231,4 +357,5 @@ $parser->parse();
 #print Dumper($parser->{expansion});
 #print Dumper($parser->{depend});
 
-$parser->run('test');
+$parser->run($ARGV[1]||'test');
+
