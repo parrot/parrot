ace6992a71744b208fc1a71835cc07d89f5f2460
Dan Sugalski <dan@sidhe.org>
2002-01-04 16:09:01 +0000

    - genclass.pl attempts to put $Id$ into generated files    but the $Id$ string gets mangled when it's committed.    This patch fixes the existing .pmc files and fixes genclass.pl.
     - Makes capitalization in .pmc header match actual filenames
    
     - The command for calling genclass.pl in vtables.pod is incorrect,
       since it assumes you're running in classes/ but genclass.pl assumes
       it's being run from the main directory. I changed genclass.pl to not
       care.
    
    
    Courtesy of Steve Fink <steve@fink.com>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@729 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- classes/genclass.pl
+++ classes/genclass.pl
@@ -1,13 +1,18 @@
+# $Id$
+
+use FindBin;
+use lib "$FindBin::Bin/..";
 use Parrot::Vtable;
-my %vtbl = parse_vtable("vtable.tbl");
+my %vtbl = parse_vtable("$FindBin::Bin/../vtable.tbl");
 my $classname = shift;
 die "No classname given!\n" unless $classname;
 
+my $DOLLAR = '$';
 print <<EOF;
 /* ${classname}.pmc
  *  Copyright: (When this is determined...it will go here)
  *  CVS Info
- *     \$Id$
+ *     ${DOLLAR}Id${DOLLAR}
  *  Overview:
  *     These are the vtable functions for the $classname base class
  *  Data Structure and Algorithms:
