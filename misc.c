cd414a3efa6b2c1021831ce08d890c28d8568018
Dan Sugalski <dan@sidhe.org>
2002-02-12 16:49:55 +0000

    Need standard header/footer comments
    git-svn-id: https://svn.parrot.org/parrot/trunk@1022 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- misc.c
+++ misc.c
@@ -1,3 +1,15 @@
+/* misc.c
+ *  Copyright: (When this is determined...it will go here)
+ *  CVS Info
+ *     $Id$
+ *  Overview:
+ *     Miscellaneous functions
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References:
+ */
+
 #include "parrot/parrot.h"
 
 #include <stdarg.h>
@@ -515,3 +527,12 @@ Parrot_snprintf(struct Parrot_Interp *interpreter, char* targ, UINTVAL len, char
 	va_end(args);
 }
 
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil 
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
