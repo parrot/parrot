a76f87eb16ea1246a5f25b36cbec28f7a8a02f4f
Alex Gough <alex@earth.li>
2002-03-08 21:12:12 +0000

    Added private flags to string.h, mostly so that other GCed stuff can also have it's very own private flags.
    git-svn-id: https://svn.parrot.org/parrot/trunk@1139 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/string.h
+++ include/parrot/string.h
@@ -43,23 +43,32 @@ typedef struct {
 
 /* Buffer flags */
 typedef enum BUFFER_flag {
+    /* bits the GC can keep its dirty mitts off of */
+    BUFFER_private0_FLAG = 1<< 0,
+    BUFFER_private1_FLAG = 1<< 1,
+    BUFFER_private2_FLAG = 1<< 2,
+    BUFFER_private3_FLAG = 1<< 3,
+    BUFFER_private4_FLAG = 1<< 4,
+    BUFFER_private5_FLAG = 1<< 5,
+    BUFFER_private6_FLAG = 1<< 6,
+    BUFFER_private7_FLAG = 1<< 7,
     /* The contents of the buffer can't be moved by the GC */
-    BUFFER_immobile_FLAG = 1 << 0,
+    BUFFER_immobile_FLAG = 1 << 8,
     /* Marks the contents as coming from a non-Parrot source */
-    BUFFER_external_FLAG = 1 << 1,
+    BUFFER_external_FLAG = 1 << 9,
     /* Mark the buffer as pointing to system memory */
-    BUFFER_sysmem_FLAG = 1 << 2,
+    BUFFER_sysmem_FLAG = 1 << 10,
     /* Mark the contents as Copy on write */
-    BUFFER_COW_FLAG = 1 << 3,
+    BUFFER_COW_FLAG = 1 << 11,
     /* Private flag for the GC system. Set if the buffer's in use as
      * far as the GC's concerned */
-    BUFFER_live_FLAG = 1 << 4,
+    BUFFER_live_FLAG = 1 << 12,
     /* Mark the bufffer as needing GC */
-    BUFFER_needs_GC_FLAG = 1 << 5,
+    BUFFER_needs_GC_FLAG = 1 << 13,
     /* Mark the buffer as on the free list */
-    BUFFER_on_free_list_FLAG = 1 << 6,
+    BUFFER_on_free_list_FLAG = 1 << 14,
     /* This is a constant--don't kill it! */
-    BUFFER_constant_FLAG = 1 << 7
+    BUFFER_constant_FLAG = 1 << 15
 } BUFFER_flags;
 
 #define STRING struct parrot_string_t
