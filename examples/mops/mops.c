368e057c848d9e3e067975e7751457570149adb5
Josh Wilmes <josh-perl6@hitchhiker.org>
2002-03-09 02:10:26 +0000

    re-indented
    git-svn-id: https://svn.parrot.org/parrot/trunk@1151 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- examples/mops/mops.c
+++ examples/mops/mops.c
@@ -15,64 +15,67 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-FLOATVAL time_n(void)
+FLOATVAL
+time_n(void)
 {
-  struct timeval t;
-  gettimeofday(&t, NULL);
-  return (FLOATVAL)t.tv_sec + ((FLOATVAL)t.tv_usec / 1000000.0);
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (FLOATVAL)t.tv_sec + ((FLOATVAL)t.tv_usec / 1000000.0);
 }
 
-int main(int argc, char * argv[])
+int
+main(int argc, char *argv[])
 {
-  INTVAL   I1;
-  INTVAL   I2;
-  INTVAL   I3;
-  INTVAL   I4;
-  INTVAL   I5;
-  FLOATVAL N1;
-  FLOATVAL N2;
-  FLOATVAL N5;
+    INTVAL I1;
+    INTVAL I2;
+    INTVAL I3;
+    INTVAL I4;
+    INTVAL I5;
+    FLOATVAL N1;
+    FLOATVAL N2;
+    FLOATVAL N5;
 
-  I2 = 0;                             /* set    I2, 0             */
-  I3 = 1;                             /* set    I3, 1             */
-  I4 = 100000000;                     /* set    I4, 100000000     */
-                                      /*                          */
-  printf("Iterations:    %li\n", I4); /* print  "Iterations:    " */
-                                      /* print  I4                */
-                                      /* print  "\n"              */
-                                      /*                          */
-  I1 = 2;                             /* set    I1, 2             */
-  I5 = I4 * I1;                       /* mul    I5, I4, I1        */
-                                      /*                          */
-  printf("Estimated ops: %li\n", I5); /* print  "Estimated ops: " */
-                                      /* print  I5                */
-                                      /* print  "\n"              */
-                                      /*                          */
-  N1 = time_n();                      /* time   N1                */
-                                      /*                          */
-REDO:                                 /* REDO:                    */
-  I4 = I4 - I3;                       /* sub    I4, I4, I3        */
-  if (I4) goto REDO;                  /* if     I4, REDO          */
-                                      /*                          */
-DONE:                                 /* DONE:                    */
-  N5 = time_n();                      /* time   N5                */
-                                      /*                          */
-  N2 = N5 - N1;                       /* sub    N2, N5, N1        */
-                                      /*                          */
-  printf("Elapsed time:  %f\n", N2);  /* print  "Elapsed time:  " */
-                                      /* print  N2                */
-                                      /* print  "\n"              */
-                                      /*                          */
-  N1 = I5;                            /* iton   N1, I5            */
-  N1 = N1 / N2;                       /* div    N1, N1, N2        */
-  N2 = 1000000.0;                     /* set    N2, 1000000.0     */
-  N1 = N1 / N2;                       /* div    N1, N1, N2        */
-                                      /*                          */
-  printf("M op/s:        %f\n", N1);  /* print  "M op/s:        " */
-                                      /* print  N1                */
-                                      /* print  "\n"              */
-                                      /*                          */
-  return 0;                           /* end                      */
+    I2 = 0;                     /* set    I2, 0             */
+    I3 = 1;                     /* set    I3, 1             */
+    I4 = 100000000;             /* set    I4, 100000000     */
+    /*                          */
+    printf("Iterations:    %li\n", I4); /* print  "Iterations:    " */
+    /* print  I4                */
+    /* print  "\n"              */
+    /*                          */
+    I1 = 2;                     /* set    I1, 2             */
+    I5 = I4 * I1;               /* mul    I5, I4, I1        */
+    /*                          */
+    printf("Estimated ops: %li\n", I5); /* print  "Estimated ops: " */
+    /* print  I5                */
+    /* print  "\n"              */
+    /*                          */
+    N1 = time_n();              /* time   N1                */
+    /*                          */
+  REDO:                        /* REDO:                    */
+    I4 = I4 - I3;               /* sub    I4, I4, I3        */
+    if (I4)
+        goto REDO;              /* if     I4, REDO          */
+    /*                          */
+  DONE:                        /* DONE:                    */
+    N5 = time_n();              /* time   N5                */
+    /*                          */
+    N2 = N5 - N1;               /* sub    N2, N5, N1        */
+    /*                          */
+    printf("Elapsed time:  %f\n", N2);  /* print  "Elapsed time:  " */
+    /* print  N2                */
+    /* print  "\n"              */
+    /*                          */
+    N1 = I5;                    /* iton   N1, I5            */
+    N1 = N1 / N2;               /* div    N1, N1, N2        */
+    N2 = 1000000.0;             /* set    N2, 1000000.0     */
+    N1 = N1 / N2;               /* div    N1, N1, N2        */
+    /*                          */
+    printf("M op/s:        %f\n", N1);  /* print  "M op/s:        " */
+    /* print  N1                */
+    /* print  "\n"              */
+    /*                          */
+    return 0;                   /* end                      */
 }
 
 /*
