479a8509e6ee0208d4d082dca51526af5a3ab4f0
Melvin Smith <melvin.smith@mindspring.com>
2002-03-29 19:26:18 +0000

    Oops, remove empty line.
    git-svn-id: https://svn.parrot.org/parrot/trunk@1288 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/pmc.h
+++ include/parrot/pmc.h
@@ -36,7 +36,6 @@ struct PMC {
         INTVAL int_val;       /* commonly-accessed data, *not* pointers to */
         FLOATVAL num_val;     /* completely different data.  That's why it's */
         DPOINTER *struct_val; /* referred to as a "cache". */
-
     } cache;
     SYNC *synchronize;
     PMC *next_for_GC;         /* Yeah, the GC data should be out of
