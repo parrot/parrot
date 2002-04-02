19cc7727f3996a2de276934ff84307da5e4150d6
Josh Wilmes <josh-perl6@hitchhiker.org>
2002-04-02 02:14:23 +0000

    Ok, here's the necessary fixes to the VMS hints and Makefile.in to get VMS to Configure and have MMS parse the resulting Makefile.  It includes all the stuff I posted eariler today.
        - There *must* be a space between the target, colon and dependencyes
            foo: bar    *wrong*
            foo : bar   *right*
    
        - LD was defined twice
    
        - MMS does not appear to understand .dummy_targets like .test_dummy.
    
        - Anything Case Sensitive on the command line must be in "".
          This includes things like -MSome::Module.
    
        - $(PERL) needs 'MCR' prefixed to it to run.
    From: Michael G Schwern <schwern@pobox.com>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@1299 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- hints/vms.pl
+++ hints/vms.pl
@@ -1,8 +1,12 @@
 use Cwd;
-$c{ccflags} = "/Standard=Relaxed_ANSI/Prefix=All/Obj=.obj/NoList/NOANSI_ALIAS/include=([.include],\"/here\")";
+$c{ccflags} = qq{/Standard=Relaxed_ANSI/Prefix=All/Obj=.obj/NoList/NOANSI_ALIAS/include="./include"};
+
+$c{perl} = "MCR $c{perl}";
+
 if (!defined $c{exe}) {
   $c{exe} = "exe";
 }
+
 {
 	local $^W;		#no warnings on redefinition
 
@@ -10,8 +14,11 @@ if (!defined $c{exe}) {
 	        my $name;
 		$name = shift;
 		$name = "test" unless $name;
-		system("$c{cc} $c{ccflags} $name.c") and die "C compiler died!";
-		system("link/exe=test_siz $name") and die "Link failed!";
+        my $cmd = "$c{cc} $c{ccflags} $name.c";
+		system($cmd) and die "C compiler died!  Command was '$cmd'";
+
+        $cmd = "link/exe=test_siz $name";
+		system($cmd) and die "Link failed!  Command was '$cmd'";
 	};
 
 	*runtestc=sub {
