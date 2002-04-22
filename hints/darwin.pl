cf3d899b9d8c17e60ef2fdbca7e03d683acec7b3
Steve Fink <steve@fink.com>
2002-04-22 17:00:22 +0000

    Make -flat_namespace occur only once in $c{ldflags}.  - From David Hand <davidhand@davidhand.com>
    git-svn-id: https://svn.parrot.org/parrot/trunk@1358 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -1,5 +1,6 @@
 $c{ccflags} .= " -I/sw/include";
 $c{ccflags} =~ s/-flat_namespace\s*//;
+$c{ldflags} =~ s/-flat_namespace\s*//;
 $c{ldflags} .= " -L/sw/lib -flat_namespace ";
 $c{libs} .= " -ldl";
 $c{cc_warn} = "-Wno-shadow"
