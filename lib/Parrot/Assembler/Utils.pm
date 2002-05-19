ee8d7d9fed5462b01afeb05eadf543d490e96907
Jeff Goff <jgoff@speakeasy.net>
2002-05-19 02:26:36 +0000

    This should resolve numeric formats and quoting. Macros will go away, and PMCs will change, but the tests should not fail.
    git-svn-id: https://svn.parrot.org/parrot/trunk@1486 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Assembler/Utils.pm
+++ lib/Parrot/Assembler/Utils.pm
@@ -1,18 +1,26 @@
 package Parrot::Assembler::Utils;
+use POSIX;
 use Text::Balanced qw(extract_bracketed);
 use strict;
 
-my $int_re = qr/([-+]?((0b[01]+)|(0x[0-9a-f]+)|(\d+)))/;
+my $bin_re = qr/([-+]?(0b[01]+))/;
+my $dec_re = qr/([-+]?(\d+))/;
+my $hex_re = qr/([-+]?(0x[0-9a-fA-F]+))/;
 my $num_re = '([-+]?\d+\.\d+([eE][+-]?\d+)?)';
 my $reg_re = '([PSNI]\d+)';
 my $str_re =
-qr/(\"(?:[^\\\"]*(?:\\.[^\\\"]*)*)\")/;
+qr/(\"(?:[^\\\"]*(?:\\.[^\\\"]*)*)\" |
+    \'(?:[^\\\']*(?:\\.[^\\\']*)*)\')/x;
 
 sub ex_arg { # Extract an argument.
     1 while $_[0]=~ s/^\s+//;
     if($_[0] =~ s/^$num_re\s*,?//) {
         return ("nc", $1);
-    } elsif ($_[0] =~ s/^$int_re\s*,?//) {
+    } elsif ($_[0] =~ s/^$bin_re\s*,?//) {
+        return ("ic",(strtol(substr($1,2),2))[0]);
+    } elsif ($_[0] =~ s/^$hex_re\s*,?//) {
+        return ("ic",(strtol(substr($1,2),16))[0]);
+    } elsif ($_[0] =~ s/^$dec_re\s*,?//) {
         return ("ic", 0+$1);
     } elsif($_[0] =~ s/^$str_re\s*,?//) {
         return ("sc", $1);
@@ -26,7 +34,8 @@ sub ex_arg { # Extract an argument.
         return (lc (substr($1,0,1)), $1);
     } elsif($_[0] =~ s/^(\[k:$reg_re\])\s*,?//) {
         return ("r", $1);
-    } elsif($_[0] =~ s/^(\[k:($num_re|$int_re|$str_re)\])\s*,?//) {
+    } elsif($_[0] =~
+        s/^(\[k:($num_re|$bin_re|$dec_re|$hex_re|$str_re)\])\s*,?//) {
         return ("kc", $1);
     } elsif($_[0] =~ s/^(\w+)\s*,?//) {
         return ("label", $1);
