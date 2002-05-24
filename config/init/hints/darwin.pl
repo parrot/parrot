eea37c79063c0392c409893a100738162226f81d
Dan Sugalski <dan@sidhe.org>
2002-05-24 21:41:40 +0000

    Make darwin/OS X work
    git-svn-id: https://svn.parrot.org/parrot/trunk@1535 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/darwin.pl
+++ config/init/hints/darwin.pl
@@ -1,13 +1,13 @@
 my($ccflags, $ldflags, $libs)=Configure::Data->get(qw(ccflags ldflags libs));
 
-$ccflags .= "-I/sw/include";
+$ccflags .= " -I/sw/include ";
 $ccflags =~ s/-flat_namespace\s*//;
 $ldflags =~ s/-flat_namespace\s*//;
 $ldflags .= " -L/sw/lib -flat_namespace ";
-$libs    .= "-ldl";
+$libs    .= " -ldl ";
 
 Configure::Data->set(
   ccflags => $ccflags,
   ldflags => $ldflags,
-  cc_warn => "-Wno-shadow"
+  cc_warn => " -Wno-shadow "
 );
