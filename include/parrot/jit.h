bf7cf397619258f27caca99f736db1680599597e
Daniel Grunblatt <daniel@grunblatt.com.ar>
2002-06-07 01:43:26 +0000

    Indent
    git-svn-id: https://svn.parrot.org/parrot/trunk@1624 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/jit.h
+++ include/parrot/jit.h
@@ -9,7 +9,10 @@
 
 typedef void (*jit_f)(struct Parrot_Interp *);
 
-jit_f build_asm(struct Parrot_Interp *, opcode_t *, opcode_t *, opcode_t *);
+jit_f build_asm(struct Parrot_Interp *,
+                opcode_t *,
+                opcode_t *,
+                opcode_t *);
 
 /* Platform generic fixup information */
 typedef struct Parrot_jit_fixup_t {
@@ -25,14 +28,14 @@ typedef struct Parrot_jit_fixup_t {
 
 /* Hold native code offsets/addresses */
 typedef union {
-    void *ptr;          /* Pointer to native code */
-    ptrdiff_t offset;   /* Offset of native code from arena_start */
+    void *ptr;           /* Pointer to native code */
+    ptrdiff_t offset;    /* Offset of native code from arena_start */
 } Parrot_jit_opmap;
 
 enum {
-    JIT_BRANCH_NO,
-    JIT_BRANCH_TARGET,
-    JIT_BRANCH_SOURCE
+    JIT_BRANCH_NO,      /* The opcode doesn't branch */
+    JIT_BRANCH_TARGET,  /* The opcode is a branch target */
+    JIT_BRANCH_SOURCE   /* The opcode is a branch source */
 };
 
 typedef int Parrot_jit_register_count_t;
@@ -50,7 +53,7 @@ typedef struct Parrot_jit_optimizer_section {
     Parrot_jit_register_count_t int_reg_count[NUM_REGISTERS];
     Parrot_jit_optimizer_section_ptr prev;
     Parrot_jit_optimizer_section_ptr next;
-    char has_jit_op;
+    int jit_op_count; /* Number of jitted ops */
 } Parrot_jit_optimizer_section_t;
 
 typedef struct {
@@ -61,11 +64,11 @@ typedef struct {
 typedef struct {
     opcode_t *cur_op;
     opcode_t op_i;
-    Parrot_jit_opmap *op_map;   /* Maps opcode offsets to native code */
+    Parrot_jit_opmap *op_map;    /* Maps opcode offsets to native code */
     UINTVAL map_size;
-    char *native_ptr;           /* Current pointer to native code */
-    char *arena_start;          /* Start of current native code segment */
-    ptrdiff_t arena_size;       /* in bytes */
+    char *native_ptr;        /* Current pointer to native code */
+    char *arena_start;       /* Start of current native code segment */
+    ptrdiff_t arena_size;    /* in bytes */
     Parrot_jit_fixup *fixups;   /* List of fixupes */
     Parrot_jit_optimizer_t *optimizer; /* Optimizer information */
 } Parrot_jit_info;
@@ -73,7 +76,8 @@ typedef struct {
 #define Parrot_jit_fixup_target(jit_info, fixup) \
     ((jit_info)->arena_start + (fixup)->native_offset)
 
-typedef void (*jit_fn_t)(Parrot_jit_info *jit_info, struct Parrot_Interp * interpreter);
+typedef void (*jit_fn_t)(Parrot_jit_info *jit_info, 
+                         struct Parrot_Interp * interpreter);
 
 typedef struct {
     jit_fn_t fn;
@@ -85,13 +89,20 @@ extern jit_fn_info_t op_jit[];
 
 void Parrot_jit_newfixup(Parrot_jit_info *jit_info);
 
-void Parrot_jit_begin(Parrot_jit_info *jit_info, struct Parrot_Interp * interpreter);
-void Parrot_jit_dofixup(Parrot_jit_info *jit_info, struct Parrot_Interp * interpreter);
-void Parrot_jit_cpcf_op(Parrot_jit_info *jit_info, struct Parrot_Interp * interpreter);
-void Parrot_jit_normal_op(Parrot_jit_info *jit_info, struct Parrot_Interp * interpreter);
+void Parrot_jit_begin(Parrot_jit_info *jit_info, 
+                      struct Parrot_Interp * interpreter);
+void Parrot_jit_dofixup(Parrot_jit_info *jit_info, 
+                        struct Parrot_Interp * interpreter);
+void Parrot_jit_cpcf_op(Parrot_jit_info *jit_info, 
+                        struct Parrot_Interp * interpreter);
+void Parrot_jit_normal_op(Parrot_jit_info *jit_info, 
+                          struct Parrot_Interp * interpreter);
 
 Parrot_jit_optimizer_t *
-optimize_jit(struct Parrot_Interp *, opcode_t *, opcode_t *, opcode_t *);
+optimize_jit(struct Parrot_Interp *, 
+             opcode_t *, 
+             opcode_t *, 
+             opcode_t *);
 
 #endif /* JIT_H_GUARD */
 
