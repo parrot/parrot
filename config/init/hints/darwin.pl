53439a7d1294cb404c96bd3d2478051bf32cd182
Dan Sugalski <dan@sidhe.org>
2002-06-07 19:21:17 +0000

    last fink-ism removed
    git-svn-id: https://svn.parrot.org/parrot/trunk@1634 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/darwin.pl
+++ config/init/hints/darwin.pl
@@ -1,14 +1,12 @@
-my($ccflags, $ldflags, $libs)=Configure::Data->get(qw(ccflags ldflags libs));
+my($ccflags, $ldflags)=Configure::Data->get(qw(ccflags ldflags));
 
 $ccflags .= " -pipe -fno-common ";
 $ccflags =~ s/-flat_namespace\s*//;
 $ldflags =~ s/-flat_namespace\s*//;
 $ldflags .= " -flat_namespace ";
-$libs    .= " -ldl ";
 
 Configure::Data->set(
   ccflags => $ccflags,
   ldflags => $ldflags,
   cc_warn => "-Wno-shadow",
-  libs => $libs
 );
