9465962d3382d43248e59ea41ddd26de990289e4
Josh Wilmes <josh-perl6@hitchhiker.org>
2002-06-08 05:32:43 +0000

    Correct a broken i_fcntl on my system.  Not sure if this was a problem for anyone else..  But anyway, now we can probe for headers ourselves, if so-inclined.
    git-svn-id: https://svn.parrot.org/parrot/trunk@1654 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/headers.pl
+++ config/auto/headers.pl
@@ -2,7 +2,7 @@ package Configure::Step;
 
 use strict;
 use vars qw($description @args);
-use Parrot::Configure::Step;
+use Parrot::Configure::Step ':auto';
 use Config;
 
 $description="Probing for C headers...";
@@ -24,6 +24,31 @@ sub runstep {
 	next unless /^i_/;
 	Configure::Data->set($_, $Config{$_});
     }
+    
+    # some headers may not be probed-for by perl 5, or might not be 
+    # properly reflected in %Config (i_fcntl seems to be wrong on my machine,
+    # for instance).
+    my @extra_headers = qw(fcntl.h);
+    
+    foreach my $header (@extra_headers) {
+        my $flag = "i_$header";
+        $flag =~ s/\.h$//g; $flag =~ s/\///g;
+
+        Configure::Data->set(testheader => $header);
+        cc_gen('config/auto/headers/test_c.in');
+        Configure::Data->set(testheader => undef);
+
+        my $build_ok = 1;
+        eval { cc_build(); };
+        $build_ok = 0 if $@;
+
+        if ($build_ok && (cc_run() =~ /^$header OK$/)) {
+            Configure::Data->set($flag, 'define');
+        } else {
+            Configure::Data->set($flag, undef);
+        }
+        cc_clean();
+    }    
 }
 
 1;
