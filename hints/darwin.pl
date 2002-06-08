d21d4538e054707893138058cef1e5e82ceaa8be
Josh Wilmes <josh-perl6@hitchhiker.org>
2002-06-08 05:38:51 +0000

    These all live under config/ nowadays.
    git-svn-id: https://svn.parrot.org/parrot/trunk@1657 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- hints/darwin.pl
+++
@@ -1,6 +0,0 @@
-$c{ccflags} .= " -I/sw/include";
-$c{ccflags} =~ s/-flat_namespace\s*//;
-$c{ldflags} =~ s/-flat_namespace\s*//;
-$c{ldflags} .= " -L/sw/lib -flat_namespace ";
-$c{libs} .= " -ldl";
-$c{cc_warn} = "-Wno-shadow"
