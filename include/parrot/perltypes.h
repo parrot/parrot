cf2c733f0a51a61eb5ce075788a99f80ec98038a
Sean O'Rourke <educated_foo@yahoo.com>
2002-07-18 02:41:56 +0000

    - Fix type conversion between Perl* scalar PMCs (may be temporary). - Fix comparison direction for PerlInt
    git-svn-id: https://svn.parrot.org/parrot/trunk@1864 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ include/parrot/perltypes.h
@@ -0,0 +1,36 @@
+/* perltypes.h
+ *  Copyright: (When this is determined...it will go here)
+ *  CVS Info
+ *     $Id$
+ *  Overview:
+ *     Header to be shared among Perl PMC classes
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References:
+ */
+
+/*
+ * Change PMC "thing" to scalar type "type", updating vtable and flags
+ * as necessary.
+ */
+
+#define CHANGE_TYPE(thing, type) do { \
+    if ((thing)->vtable == &Parrot_base_vtables[enum_class_PerlString]) { \
+	(thing)->flags &= ~(UINTVAL)PMC_is_buffer_ptr_FLAG; \
+    } \
+    (thing)->vtable = &Parrot_base_vtables[enum_class_ ## type]; \
+    if (enum_class_ ## type == enum_class_PerlString) { \
+	(thing)->flags = PMC_is_buffer_ptr_FLAG; \
+    } \
+} while (0)
+
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
