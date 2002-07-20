58ea7c054eca5243f1a0e83a27cea7128680208a
Sean O'Rourke <educated_foo@yahoo.com>
2002-07-20 14:58:09 +0000

    Fix bug 15204: Not finding parrot".exe" on cygwin.
    Also:
    	- save program output on test failures.
    	- stop tests when child receives SIGINT.
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@1909 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/perl6/P6C/TestCompiler.pm
+++ languages/perl6/P6C/TestCompiler.pm
@@ -3,8 +3,16 @@ package P6C::TestCompiler;
 my $PARROT = '../..';
 my $PERL = $ENV{PERL} || 'perl';
 my $ERR = 'a.err';
+my $PARROT_BIN;
+if ($^O eq 'cygwin') {
+    $PARROT_BIN = 'parrot.exe';
+else {
+    $PARROT_BIN = 'parrot';
+}
+
 my $testno = 0;
 my $code;
+my $out;
 
 sub dumperr {
     open IN, $ERR;
@@ -13,17 +21,20 @@ sub dumperr {
     open O, ">test-$testno.p6";
     print O $code;
     close O;
+    open O, ">test-$testno.out";
+    print O $out;
+    close O;
     for my $ext (qw(pasm pbc imc err)) {
 	rename "a.$ext", "test-$testno.$ext";
     }
-    print STDERR "See test-$testno.{p6,imc,pasm,pbc} for output,",
+    print STDERR "See test-$testno.{p6,imc,pasm,pbc,out},",
 	"and test-$testno.err for errors\n";
 }
 
 sub import {
     my $pkg = caller;
+    no strict 'refs';
     for (qw(output_is)) {
-	no strict 'refs';
 	*{$pkg.'::'.$_} = \&$_;
     }
     my $class = shift;
@@ -35,6 +46,9 @@ sub mysystem($$) {
     if (system $cmd) {
 	my ($subcmd) = split ' ', $cmd;
 	ok(0, "$desc: $subcmd: ".($? >> 8));
+	if (($? & 255) == 2) {
+	    die "interrupted";
+	}
 	return 0;
     }
     return 1;
@@ -43,7 +57,7 @@ sub mysystem($$) {
 sub output_is {
     $code = shift;
     ++$testno;
-    my ($out, $desc) = @_;
+    ($out, my $desc) = @_;
     unless ($desc) {
 	(undef, my $file, my $line) = caller;
 	$desc = "($file line $line)";
@@ -51,19 +65,27 @@ sub output_is {
     open(O, "| perl prd-perl6.pl --batch --imc > a.imc 2>$ERR") or die $!;
     print O $code;
     unless (close O) {
-	ok(0, "$desc: compile error: $!");
+	ok(0, "$desc: compile error: ".($?>>8));
+	if (($? & 255) == 2) {
+	    die "interrupted";
+	}
 	dumperr;
 	return;
     }
     unless(mysystem("$PARROT/languages/imcc/imcc a.imc a.pasm 2>$ERR", $desc)
 	   && mysystem("$PERL $PARROT/assemble.pl a.pasm > a.pbc 2>$ERR",$desc)
-	   && mysystem("$PARROT/parrot a.pbc > a.output 2>$ERR", $desc)) {
+	   && mysystem("$PARROT/$PARROT_BIN a.pbc > a.output 2>$ERR", $desc)) {
 	dumperr;
 	return;
     }
     open(I, 'a.output');
     my $result = join '', <I>;
-    ok($out eq $result, $desc);
+    if ($out eq $result) {
+	ok(1, $desc);
+    } else {
+	ok(0, $desc);
+	dumperr;
+    }
 }
 
 1;
