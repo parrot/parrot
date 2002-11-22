79231e29a40c9850cc859a1249aa38764ad38eda
Nicholas Clark <nick@ccl4.org>
2002-11-22 20:04:29 +0000

    Correct typo in ARM conditional code
    git-svn-id: https://svn.parrot.org/parrot/trunk@2601 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/jit.c
+++ src/jit.c
@@ -666,7 +666,7 @@ build_asm(struct Parrot_Interp *interpreter, opcode_t *pc,
     }
 
 #ifdef ARM
-    arm_sync_d_i_cache(jit_info->arena.start, jit_inf->native_ptr);
+    arm_sync_d_i_cache(jit_info->arena.start, jit_info->native_ptr);
 #endif
 #ifdef PPC
     ppc_sync_cache(jit_info->arena.start, jit_info->native_ptr);
