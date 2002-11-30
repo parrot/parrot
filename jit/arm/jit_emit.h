777fff640de109aea9cd00700a716c6d2aa60e31
Nicholas Clark <nick@ccl4.org>
2002-11-30 17:35:27 +0000

    Need to define char *floatval_map = 0; to make ARM compile
    git-svn-id: https://svn.parrot.org/parrot/trunk@2639 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- jit/arm/jit_emit.h
+++ jit/arm/jit_emit.h
@@ -989,8 +989,22 @@ Parrot_jit_emit_mov_rm_n(struct Parrot_Interp * interpreter, int reg, char *mem)
 #  define INT_REGISTERS_TO_MAP 10
 #  define FLOAT_REGISTERS_TO_MAP 0
 
+/* XXX NOTE before actually mapping things
+
+   1: currently the code has r4  (INTERP_STRUCT_ADDR_REG) is in use
+   2: r12 is trashed over any function call
+   3: currently the entry code doesn't save r5,r6,r7,r8 (or r9) - if the
+      mapping code uses them then it must arrange to save (and restore them)
+
+   and as we're not generating re-entrant code (I assume) surely we can also
+   map r9?
+
+   NWC
+*/
 char intval_map[INT_REGISTERS_TO_MAP] =
     { r0, r1, r2, r3, r4, r5, r6, r7, r8, r12 };
+
+char *floatval_map = 0;
 
 static void
 arm_sync_d_i_cache (void *start, void *end) {
