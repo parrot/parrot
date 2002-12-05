28c5609d0805b3901b45b69c81430038e31c0f60
Gregor N. Purdy <gregor@focusresearch.com>
2002-12-05 23:34:46 +0000

    * Change the internal documentation to match what the program is actually     doing.
      * A few other small mods related to error conditions, etc.
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@2699 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- ops2pm.pl
+++ ops2pm.pl
@@ -2,7 +2,32 @@
 #
 # ops2pm.pl
 #
-# Generate a Perl module from the operation definitions in an .ops file.
+# Generate a Perl module from the operation definitions in .ops files.
+#
+# The first .ops file on the command line is read in and its name is used to
+# determine the name of the Perl module that will contain the op info.
+#
+# Any remaining .ops files on the command line are read in, and their op
+# info objects are appended, in order, to the op info array obtained from the
+# first .ops file.
+#
+# WARNING: Generating a combined Perl module for a set of .ops files that
+# you do not later turn into a combined opcode table with the same content
+# and order is a recipe for disaster.
+# 
+# XXX: The original design of the .ops processing code was intended to be a
+# read-only representation of what was in a particular .ops file. It was
+# not originally intended that it was a mechanism for building a bigger
+# virtual .ops file from multiple physical .ops files. This code does half of
+# that job (the other half is getting them to compile together instead of
+# separately in a *_ops.c file). You can see evidence of this by the way this
+# code reaches in to the internal OPS hash key to do its concatenation, and
+# the way it twiddles each op's CODE hash key after that. If the op and oplib
+# Perl modules are going to be used for modifying information read from .ops
+# files in addition to reading it, they should be changed to make the above
+# operations explicitly supported. Otherwise, the Parrot build and interpreter
+# start-up logic should be modified so that it doesn't need to concatenate 
+# separate .ops files.
 #
 
 use strict;
@@ -14,6 +39,8 @@ $Data::Dumper::Useqq  = 1;
 #$Data::Dumper::Terse  = 1;
 #$Data::Dumper::Indent = 0;
 
+my $moddir  = "lib/Parrot/OpLib";
+
 sub Usage {
     print STDERR <<_EOF_;
 usage: $0 input.ops [input2.ops ...]
@@ -21,34 +48,31 @@ _EOF_
     exit;
 }
 
-
-#
-# Process command-line argument:
-#
-
 Usage() unless @ARGV;
 
-my $file = 'core.ops';
 
-my $base = $file;
-$base =~ s/\.ops$//;
+#
+# Use the first .ops file to determine the output file name, and read in its
+# ops:
+#
 
+my $file = shift @ARGV;
+
+my ($base)  = ($file =~ m{^(.*)\.ops$});
 my $package = "${base}";
-my $moddir  = "lib/Parrot/OpLib";
 my $module  = "lib/Parrot/OpLib/${package}.pm";
 
-
-#
-# Read the first input file:
-#
-
-use Data::Dumper;
-
-$file = shift @ARGV;
-die "$0: Could not read ops file '$file'!\n" unless -e $file;
+die "$0: Could not find ops file '$file'!\n" unless -e $file;
 my $ops = new Parrot::OpsFile $file;
+die "$0: Could not read ops file '$file'!\n" unless defined $ops;
+
+
+#
+# Copy the ops from the remaining .ops files to the object just created.
+#
 
 my %seen;
+
 for $file (@ARGV) {
     if ($seen{$file}) {
       print STDERR "$0: Ops file '$file' mentioned more than once!\n";
@@ -56,20 +80,24 @@ for $file (@ARGV) {
     }
     $seen{$file} = 1;
 
-    die "$0: Could not read ops file '$file'!\n" unless -e $file;
+    die "$0: Could not find ops file '$file'!\n" unless -e $file;
     my $temp_ops = new Parrot::OpsFile $file;
-    for(@{$temp_ops->{OPS}}) {
-       push @{$ops->{OPS}},$_;
-    }
+    die "$0: Could not read ops file '$file'!\n" unless defined $temp_ops;
+
+    push @{$ops->{OPS}}, @{$temp_ops->{OPS}};
 }
+
+
+#
+# Renumber the ops based on their new absolute positions (they started out
+# being numbered according to their relative position within their respective
+# .ops files):
+#
+
 my $cur_code = 0;
 for(@{$ops->{OPS}}) {
     $_->{CODE}=$cur_code++;
 }
-
-
-my $num_ops     = scalar $ops->ops;
-my $num_entries = $num_ops + 1; # For trailing NULL
 
 
 #
