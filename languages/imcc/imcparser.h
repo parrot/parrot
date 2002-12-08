449e95ecc35b6488c6db1c4337587a465df33750
Gregor N. Purdy <gregor@focusresearch.com>
2002-12-08 18:23:36 +0000

    * Give the examples their own directory.
    git-svn-id: https://svn.parrot.org/parrot/trunk@2713 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/imcc/imcparser.h
+++ languages/imcc/imcparser.h
@@ -1,68 +1,77 @@
+#ifndef BISON_IMCPARSER_H
+# define BISON_IMCPARSER_H
+
+#ifndef YYSTYPE
 typedef union {
     int t;
     char * s;
     SymReg * sr;
     Instruction *i;
-} YYSTYPE;
-#define	CALL	258
-#define	GOTO	259
-#define	ARG	260
-#define	PRINT	261
-#define	IF	262
-#define	UNLESS	263
-#define	NEW	264
-#define	END	265
-#define	SAVEALL	266
-#define	RESTOREALL	267
-#define	SUB	268
-#define	NAMESPACE	269
-#define	ENDNAMESPACE	270
-#define	CLASS	271
-#define	ENDCLASS	272
-#define	SYM	273
-#define	LOCAL	274
-#define	PARAM	275
-#define	INC	276
-#define	DEC	277
-#define	SHIFT_LEFT	278
-#define	SHIFT_RIGHT	279
-#define	INTV	280
-#define	FLOATV	281
-#define	STRINGV	282
-#define	DEFINED	283
-#define	LOG_XOR	284
-#define	RELOP_EQ	285
-#define	RELOP_NE	286
-#define	RELOP_GT	287
-#define	RELOP_GTE	288
-#define	RELOP_LT	289
-#define	RELOP_LTE	290
-#define	GLOBAL	291
-#define	ADDR	292
-#define	CLONE	293
-#define	RESULT	294
-#define	RETURN	295
-#define	POW	296
-#define	SHIFT_RIGHT_U	297
-#define	LOG_AND	298
-#define	LOG_OR	299
-#define	COMMA	300
-#define	ESUB	301
-#define	LABEL	302
-#define	EMIT	303
-#define	EOM	304
-#define	IREG	305
-#define	NREG	306
-#define	SREG	307
-#define	PREG	308
-#define	IDENTIFIER	309
-#define	STRINGC	310
-#define	INTC	311
-#define	FLOATC	312
-#define	REG	313
-#define	MACRO	314
-#define	PARROT_OP	315
-#define	VAR	316
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	CALL	257
+# define	GOTO	258
+# define	ARG	259
+# define	PRINT	260
+# define	IF	261
+# define	UNLESS	262
+# define	NEW	263
+# define	END	264
+# define	SAVEALL	265
+# define	RESTOREALL	266
+# define	SUB	267
+# define	NAMESPACE	268
+# define	ENDNAMESPACE	269
+# define	CLASS	270
+# define	ENDCLASS	271
+# define	SYM	272
+# define	LOCAL	273
+# define	PARAM	274
+# define	INC	275
+# define	DEC	276
+# define	SHIFT_LEFT	277
+# define	SHIFT_RIGHT	278
+# define	INTV	279
+# define	FLOATV	280
+# define	STRINGV	281
+# define	DEFINED	282
+# define	LOG_XOR	283
+# define	RELOP_EQ	284
+# define	RELOP_NE	285
+# define	RELOP_GT	286
+# define	RELOP_GTE	287
+# define	RELOP_LT	288
+# define	RELOP_LTE	289
+# define	GLOBAL	290
+# define	ADDR	291
+# define	CLONE	292
+# define	RESULT	293
+# define	RETURN	294
+# define	POW	295
+# define	SHIFT_RIGHT_U	296
+# define	LOG_AND	297
+# define	LOG_OR	298
+# define	COMMA	299
+# define	ESUB	300
+# define	LABEL	301
+# define	EMIT	302
+# define	EOM	303
+# define	IREG	304
+# define	NREG	305
+# define	SREG	306
+# define	PREG	307
+# define	IDENTIFIER	308
+# define	STRINGC	309
+# define	INTC	310
+# define	FLOATC	311
+# define	REG	312
+# define	MACRO	313
+# define	PARROT_OP	314
+# define	VAR	315
 
 
 extern YYSTYPE yylval;
+
+#endif /* not BISON_IMCPARSER_H */
