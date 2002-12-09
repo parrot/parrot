15946761574f5310ed8b8ad79493c12faf3a0bd5
Steve Fink <steve@fink.com>
2002-12-09 04:02:09 +0000

    Allow a local hints file too (applied after platform hints file)
    git-svn-id: https://svn.parrot.org/parrot/trunk@2718 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints.pl
+++ config/init/hints.pl
@@ -4,17 +4,25 @@ use strict;
 use vars qw($description @args);
 use Parrot::Configure::Step;
 
-$description="Loading platform hints file...";
+$description="Loading platform and local hints files...";
 
 @args=();
 
 sub runstep {
   my $hints = "config/init/hints/" . lc($^O) . ".pl";
+  my $hints_used = 0;
   if(-e $hints) {
     do $hints;
     die $@ if $@;
+    $hints_used++;
   }
-  else {
+  $hints = "config/init/hints/local.pl";
+  if(-e $hints) {
+    do $hints;
+    die $@ if $@;
+    $hints_used++;
+  }
+  if ($hints_used == 0) {
     print "(no hints) ";
   }
 }
