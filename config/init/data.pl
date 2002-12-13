64a29316c79a70917c9a66b30c415f9afcade938
Sean O'Rourke <educated_foo@yahoo.com>
2002-12-13 15:11:27 +0000

    apply #19090
    git-svn-id: https://svn.parrot.org/parrot/trunk@2786 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/data.pl
+++ config/init/data.pl
@@ -16,7 +16,7 @@ sub runstep {
   use Data::Dumper;
 
   # We need a Glossary somewhere!
-  
+
   my(%c)=(
     debugging     => $debugging ? 1 : 0,
 
@@ -27,7 +27,7 @@ sub runstep {
 
     # Linker, used to link object files (plus libraries) into
     # an executable.  It is usually $cc on Unix-ish systems.
-    # VMS and Win32 might use "Link". 
+    # VMS and Win32 might use "Link".
     # Perl5's Configure doesn't distinguish linking from loading, so
     # make a reasonable guess at defaults.
     link          => $Config{cc},
@@ -37,9 +37,9 @@ sub runstep {
     # $cc on Unix-ish systems, but apparently sometimes it's ld.
     ld            => $Config{ld},
     ldflags       => $Config{ldflags},
-    
+
     libs          => $Config{libs},
-    
+
     cc_inc	  => "-I./include",
     cc_debug      => '-g',
     link_debug    => '',
@@ -50,18 +50,18 @@ sub runstep {
     exe           => $Config{_exe},       # executable files extension
     cc_o_out      => '-o ',               # cc object output file
     cc_exe_out    => '-o ',               # cc executable output file (different on Win32)
-    
+
     cc_ldflags    => '',                  # prefix for ldflags (necessary for Win32)
-    
+
     ld_out        => '-o ',               # ld output file
-    ld_debug      => '',                  # include debug info in executable
-    ld_shared     => $Config{lddlflags}, 
+    ld_debug      => '-g ',               # include debug info in executable
+    ld_shared     => $Config{lddlflags},
     ld_shared_flags=> '', # What is this, exactly?  For GNU ld, it was
     # '-Wl,-soname,libparrot$(SO)'
 
     # should we have a dependancy upon arc to generate .a's?
     blib_lib_libparrot_a => 'blib/lib/libparrot$(A)',
-    
+
     perl          => $^X,
     test_prog     => 'parrot',
     rm_f          => 'rm -f',
@@ -70,34 +70,34 @@ sub runstep {
     ranlib        => $Config{ranlib},
     make          => $Config{make},
     make_set_make => $Config{make_set_make},
-    
+
     cp            => 'cp',
     slash         => '/',
-    
+
     VERSION       => $main::parrot_version,
     MAJOR         => $main::parrot_version[0],
     MINOR         => $main::parrot_version[1],
     PATCH         => $main::parrot_version[2],
     DEVEL         => (-e 'DEVELOPING' ? '-devel' : ''),
-    
+
     configdate    => scalar localtime,
     PQ            => "'",
   );
-  
+
   *get=sub {
     shift;
     @c{@_};
   };
-  
+
   *set=sub {
     shift;
     %c=(%c, @_);
   };
-  
+
   *keys=sub {
     return keys %c;
   };
-  
+
   *dump=sub {
     Data::Dumper->Dump([\%c], ['*PConfig']);
   };
