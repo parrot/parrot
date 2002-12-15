fb1ecf5b4f4862ce1c854e308ab3de0e103bc07f
Gregor N. Purdy <gregor@focusresearch.com>
2002-12-15 21:26:25 +0000

    * Fixed an erroneous call that should have been made via $self-> instead     of directly.
    git-svn-id: https://svn.parrot.org/parrot/trunk@2823 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/jako/lib/Jako/Construct/Declaration/Variable.pm
+++ languages/jako/lib/Jako/Construct/Declaration/Variable.pm
@@ -78,7 +78,7 @@ sub new
   #
 
   if (defined $sym and $sym->block eq $block) {
-    SYNTAX_ERROR("Redeclaration of identifier '%s' in the same block. Previous declaration on line %d of file '%s'.",
+    $self->SYNTAX_ERROR("Redeclaration of identifier '%s' in the same block. Previous declaration on line %d of file '%s'.",
       $self->name, $sym->line, $sym->file);
   }
 
