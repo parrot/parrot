5f5da099950234b308438851ee6db32f649f339c
Gregor N. Purdy <gregor@focusresearch.com>
2002-12-15 05:14:56 +0000

    * Fixed an inverted sense bug in 'if' processing.
    git-svn-id: https://svn.parrot.org/parrot/trunk@2814 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/jako/lib/Jako/Construct/Block/Conditional/Unless.pm
+++ languages/jako/lib/Jako/Construct/Block/Conditional/Unless.pm
@@ -13,6 +13,8 @@ use warnings;
 
 package Jako::Construct::Block::Conditional::Unless;
 
+use Carp;
+
 use base qw(Jako::Construct::Block::Conditional);
 
 #
@@ -23,6 +25,16 @@ sub new
 {
   my $class = shift;
   my ($block, $left, $op, $right) = @_;
+
+  confess "Block is not defined!" unless defined $block;
+  confess "Left is not defined!" unless defined $left;
+  confess "Op is not defined!" unless defined $op;
+  confess "Right is not defined!" unless defined $right;
+
+  confess "Block is not!" unless UNIVERSAL::isa($block, 'Jako::Construct::Block');
+  confess "Left is not Value!" unless UNIVERSAL::isa($left, 'Jako::Construct::Expression::Value');
+  confess "Op is not scalar!" if ref $op;
+  confess "Right is not Value!" unless UNIVERSAL::isa($right, 'Jako::Construct::Expression::Value');
 
   my $index = ++$Jako::Compiler::block_count; # TODO: YUCK!
 
