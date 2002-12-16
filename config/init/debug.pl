a944aff474ef4a3fd1683163d3397d048ec98fca
Steve Fink <steve@fink.com>
2002-12-16 06:10:06 +0000

    Ticket #18967 Only add debugging if --debugging requested. -- Mr. Nobody
    git-svn-id: https://svn.parrot.org/parrot/trunk@2830 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/debug.pl
+++ config/init/debug.pl
@@ -9,22 +9,23 @@ $description="Enabling debugging...";
 @args=();
 
 sub runstep {
-  if (! Configure::Data->get('debugging')) {
+  if (Configure::Data->get('debugging')) {
+    my($ccflags, $linkflags, $ldflags) = 
+      Configure::Data->get(qw(ccflags linkflags ldflags));
+    my($cc_debug, $link_debug, $ld_debug) = 
+      Configure::Data->get(qw(cc_debug link_debug ld_debug));
+    $ccflags .= " $cc_debug";
+    $linkflags .= " $link_debug";
+    $ldflags .= " $ld_debug";
+
+    Configure::Data->set(
+                         ccflags => $ccflags,
+                         linkflags => $linkflags,
+                         ldflags => $ldflags,
+                        );
+  } else {
     print "(none requested) ";
   }
-  my($ccflags, $linkflags, $ldflags) = 
-      Configure::Data->get(qw(ccflags linkflags ldflags));
-  my($cc_debug, $link_debug, $ld_debug) = 
-      Configure::Data->get(qw(cc_debug link_debug ld_debug));
-  $ccflags .= " $cc_debug";
-  $linkflags .= " $link_debug";
-  $ldflags .= " $ld_debug";
-
-  Configure::Data->set(
-                       ccflags => $ccflags,
-                       linkflags => $linkflags,
-                       ldflags => $ldflags,
-                      );
 }
 
 1;
