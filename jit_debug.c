fd2cbfdef88b0ca7b9ac6f24aba0a0b6a811fcab
Steve Fink <steve@fink.com>
2003-02-24 06:58:39 +0000

    Improve debugging support and documentation:  - added new document describing various debugging tricks  - added the PMC registers to the stabs type info
    git-svn-id: https://svn.parrot.org/parrot/trunk@3088 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- jit_debug.c
+++ jit_debug.c
@@ -13,8 +13,58 @@
 #include <parrot/parrot.h>
 #include "parrot/jit.h"
 
+#define N_GSYM "32"     /* global variable */
+#define N_FNAME "34"
+#define N_FUN "36"
+#define N_STSYM "38"    /* variable in data section */
+#define N_LCSYM "40"    /* bss section */
+#define N_MAIN "42"
+#define N_ROSYM "44"
+#define N_PC "48"
+#define N_NSYMS "50"
+#define N_NOMAP "52"
+#define N_OBJ "56"
+#define N_OPT "60"
+#define N_RSYM "64"     /* register variable */
+#define N_M2C "66"
+#define N_SLINE "68"
+#define N_DSLINE "70"
+#define N_BSLINE "72"
+#define N_BROWS "72"
+#define N_DEFD "74"
+#define N_FLINE "76"
+#define N_EHDECL "80"
+#define N_MOD2 "80"
+#define N_CATCH "84"
+#define N_SSYM "96"
+#define N_ENDM "98"
+#define N_SO "100"     /* filename */
+#define N_LSYM "128"   /* stack variable */
+#define N_BINCL "130"
+#define N_SOL "132"
+#define N_PSYM "160"   /* parameter */
+#define N_EINCL "162"
+#define N_ENTRY "164"
+#define N_LBRAC "192"
+#define N_EXCL "194"
+#define N_SCOPE "196"
+#define N_RBRAC "224"
+#define N_BCOMM "226"
+#define N_ECOMM "228"
+#define N_ECOML "232"
+#define N_WITH "234"
+#define N_NBTEXT "240"
+#define N_NBDATA "242"
+#define N_NBBSS "244"
+#define N_NBSTS "246"
+#define N_NBLCS "248"
+
 #ifdef __GNUC__
 void Parrot_jit_debug(struct Parrot_Interp* interpreter);
+
+#define BIT_SIZE(t) ((int)(sizeof(t)*8))
+#define BYTE_SIZE(t) ((int)sizeof(t))
+#define BIT_OFFSET(str, field) ((int)(offsetof(str, field) * 8))
 
 typedef struct {
 	const char *name;
@@ -23,7 +73,7 @@ typedef struct {
 static void
 write_types(FILE *stabs)
 {
-    int i;
+    int i, j;
     /* borrowed from mono */
     static BaseTypes base_types[] = {
             {"Void", "(0,1)"},
@@ -61,11 +111,10 @@ write_types(FILE *stabs)
         } else {
             fprintf (stabs, "%s\"", base_types[i].spec);
         }
-        fprintf (stabs, ",128,0,0,0\n");
+        fprintf (stabs, "," N_LSYM ",0,0,0\n");
     }
     fprintf(stabs, ".stabs \"STRING:t(0,%d)=*(0,%d)\""
-                ",128,0,0,0\n", i, i+1);
-    i++;
+                "," N_LSYM ",0,0,0\n", i, i+1);
     fprintf(stabs, ".stabs \"Parrot_String:T(0,%d)=s%d"
                 "bufstart:(0,14),%d,%d;"
                 "buflen:(0,6),%d,%d;"   /* XXX type */
@@ -73,18 +122,60 @@ write_types(FILE *stabs)
                 "bufused:(0,12),%d,%d;"
                 "strstart:(0,15),%d,%d;"        /* fake a char* */
                 ";\""
-                ",128,0,0,0\n", i++, (int) sizeof(STRING),
-                (int)(offsetof(STRING, bufstart) * 8),
-                (int)(sizeof(void *)* 8),
-                (int)(offsetof(STRING, buflen) * 8), 
-                (int)(sizeof(size_t) * 8),
-                (int)(offsetof(STRING, obj.flags) * 8), 
-                (int)(sizeof(UINTVAL) * 8),
-                (int)(offsetof(STRING, bufused) * 8), 
-                (int)(sizeof(UINTVAL) * 8),
-                (int)(offsetof(STRING, strstart) * 8), 
-                (int)(sizeof(void *) * 8)
+                "," N_LSYM ",0,0,0\n", i++, BYTE_SIZE(STRING),
+                BIT_OFFSET(STRING, bufstart), BIT_SIZE(void*),
+                BIT_OFFSET(STRING, buflen), BIT_SIZE(size_t),
+                BIT_OFFSET(STRING, obj.flags), BIT_SIZE(UINTVAL),
+                BIT_OFFSET(STRING, bufused), BIT_SIZE(UINTVAL),
+                BIT_OFFSET(STRING, strstart), BIT_SIZE(void*)
                 );
+
+    fprintf(stabs, ".stabs \"PMCType:T(0,%d)=e", i++);
+    for (j = 0; j < enum_class_max; ++j) {
+        if (Parrot_base_vtables[j].name) {
+            STRING* name = (*Parrot_base_vtables[j].name)(NULL, NULL);
+            fwrite(name->strstart, name->strlen, 1, stabs);
+            fprintf(stabs, ":%d,", j);
+        }
+    }
+    fprintf(stabs, ";\"," N_LSYM ",0,0,0\n");
+
+    /* PMC type */
+    fprintf(stabs, ".stabs \"PMC:T(0,%d)=s%d", i, BYTE_SIZE(PMC));
+    fprintf(stabs, "obj:(0,%d),%d,%d;",
+            i + 1, BIT_OFFSET(PMC, obj), BIT_SIZE(pobj_t));
+    fprintf(stabs, "vtable:*(0,%d),%d,%d;",
+            i + 3, BIT_OFFSET(PMC, vtable), BIT_SIZE(void*));
+    fprintf(stabs, "data:(0,14),%d,%d;",
+            BIT_OFFSET(PMC, data), BIT_SIZE(void*));
+    fprintf(stabs, "metadata:*(0,%d),%d,%d;",
+            i, BIT_OFFSET(PMC, metadata), BIT_SIZE(void*));
+    fprintf(stabs, ";\"");
+    fprintf(stabs, "," N_LSYM ",0,0,0\n");
+
+    fprintf(stabs, ".stabs \"pobj_t:T(0,%d)=s%d"
+                "u:(0,%d),%d,%d;"
+                "flags:(0,12),%d,%d;"
+                ";\""
+                "," N_LSYM ",0,0,0\n", i + 1, (int)(sizeof(pobj_t)),
+                i + 2, BIT_OFFSET(pobj_t, u), BIT_SIZE(UnionVal),
+                BIT_OFFSET(pobj_t, flags), BIT_SIZE(Parrot_UInt)
+                );
+    fprintf(stabs, ".stabs \"UnionVal:T(0,%d)=u%d"
+                "int_val:(0,12),%d,%d;"
+                "pmc_val:*(0,%d),%d,%d;"
+                ";\""
+                "," N_LSYM ",0,0,0\n", i + 2, BYTE_SIZE(UnionVal),
+                BIT_OFFSET(UnionVal, int_val), BIT_SIZE(INTVAL),
+                i, BIT_OFFSET(UnionVal, pmc_val), BIT_SIZE(void*)
+                );
+    fprintf(stabs, ".stabs \"VTABLE:T(0,%d)=s%d"
+                "base_type:(0,%d),%d,%d;"
+                ";\""
+                "," N_LSYM ",0,0,0\n", i + 3, BYTE_SIZE(UnionVal),
+                i - 1, BIT_OFFSET(VTABLE, base_type), BIT_SIZE(INTVAL)
+                );
+    i += 4;
 
 }
 
@@ -94,12 +185,14 @@ write_vars(FILE *stabs, struct Parrot_Interp *interpreter)
     int i;
     /* fake static var stabs */
     for (i = 0; i < NUM_REGISTERS; i++) {
-        fprintf(stabs, ".stabs \"I%d:S(0,12)\",38,0,0,%p\n", i,
+        fprintf(stabs, ".stabs \"I%d:S(0,12)\"," N_STSYM ",0,0,%p\n", i,
                 (char*)&interpreter->ctx.int_reg.registers[i]);
-        fprintf(stabs, ".stabs \"N%d:S(0,13)\",38,0,0,%p\n", i,
+        fprintf(stabs, ".stabs \"N%d:S(0,13)\"," N_STSYM ",0,0,%p\n", i,
                 (char*)&interpreter->ctx.num_reg.registers[i]);
-        fprintf(stabs, ".stabs \"S%d:S(0,16)\",38,0,0,%p\n", i,
+        fprintf(stabs, ".stabs \"S%d:S(0,16)\"," N_STSYM ",0,0,%p\n", i,
                 (char*)&interpreter->ctx.string_reg.registers[i]);
+        fprintf(stabs, ".stabs \"P%d:S*(0,18)\"," N_STSYM ",0,0,%p\n", i,
+                (char*)&interpreter->ctx.pmc_reg.registers[i]);
     }
 }
 
@@ -156,10 +249,10 @@ Parrot_jit_debug_stabs(struct Parrot_Interp *interpreter)
         return;
 
     /* filename info */
-    fprintf(stabs, ".stabs \"%s\",100,0,0,0\n",
+    fprintf(stabs, ".stabs \"%s\"," N_SO ",0,0,0\n",
             string_to_cstring(interpreter, pasmfile));
     /* jit_func start addr */
-    fprintf(stabs, ".stabs \"jit_func:F(0,1)\",36,0,1,%p\n",
+    fprintf(stabs, ".stabs \"jit_func:F(0,1)\"," N_FUN ",0,1,%p\n",
             jit_info->arena.start);
 
     write_types(stabs);
@@ -169,22 +262,24 @@ Parrot_jit_debug_stabs(struct Parrot_Interp *interpreter)
      */
 
     /* jit_begin */
-    fprintf(stabs, ".stabn 68,0,1,0\n");
+    fprintf(stabs, ".stabn " N_SLINE ",0,1,0\n");
     line = 1;
     lc = 0;
     for (i = 0; i < jit_info->arena.map_size; i++) {
         if (jit_info->arena.op_map[i].ptr) {
+            op_info_t* op = &interpreter->op_info_table[interpreter->code->byte_code[i]];
             if (interpreter->code->cur_cs->debug) {
                 line = (int)interpreter->code->cur_cs->debug->base.data[lc++];
             }
-            fprintf(stabs, ".stabn 68,0,%d,%d\n", line,
+            fprintf(stabs, ".stabn " N_SLINE ",0,%d,%d /* %s */\n", line,
                     (int)((char *)jit_info->arena.op_map[i].ptr -
-                          (char *)jit_info->arena.start));
+                    (char *)jit_info->arena.start),
+                    op->full_name);
             line++;
         }
     }
     /* eof */
-    fprintf(stabs, ".stabs \"\",36,0,1,%p\n",
+    fprintf(stabs, ".stabs \"\"," N_FUN ",0,1,%p\n",
             (char *) jit_info->arena.size);
     fclose(stabs);
     /* run the stabs file through C<as> generating file.o */
