3c0a9a6767551deeffd997e3764abaa801e07107
Leopold Toetsch <lt@toetsch.at>
2003-03-18 09:29:28 +0000

    forward slashes; run via imcc
    git-svn-id: https://svn.parrot.org/parrot/trunk@3151 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/BASIC/compiler/testrun.pl
+++ languages/BASIC/compiler/testrun.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 #
 no warnings 'once';
-$parpath='..\..\..';
+$parpath='../../../languages/imcc/imcc';
 if (! -d $parpath) {
-	$parpath='..\..\..';
+	$parpath='../../..';
 }
 
 unlink "test.pbc";
 
-$a=system("perl -I$parpath $parpath/assemble.pl -o _test.pbc TARG_test.pasm");
-system("$parpath\\parrot.exe @ARGV _test.pbc");
+#$a=system("perl -I$parpath $parpath/assemble.pl -o _test.pbc TARG_test.pasm");
+system("$parpath @ARGV TARG_test.pasm");
