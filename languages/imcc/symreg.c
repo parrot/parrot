90b99c1dffb997598a043ba8d648d5ced725ce75
Leopold Toetsch <lt@toetsch.at>
2003-05-16 08:31:01 +0000

    fixed missing register range check
    git-svn-id: https://svn.parrot.org/parrot/trunk@3273 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/imcc/symreg.c
+++ languages/imcc/symreg.c
@@ -78,6 +78,9 @@ SymReg * mk_pasm_reg(char * name) {
     r = mk_symreg(name, *name);
     r->type = VTPASM;
     r->color = atoi(name+1);
+    if (r->color < 0 || r->color > 31)
+        fataly(1, "mk_pasm_reg", line,
+                "register number out of range '%s'\n", name);
     debug(DEBUG_PARSER, "mk_pasm_reg: name %s set %c color %d type 0x%x\n",
             name, r->set, r->color, r->type);
     return r;
