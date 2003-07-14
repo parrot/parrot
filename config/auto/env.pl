2e9088e00092f4f4ea278aefc37127938872bb63
Leopold Toetsch <lt@toetsch.at>
2003-07-14 10:32:47 +0000

    Win32 has unsetenv
    git-svn-id: https://svn.parrot.org/parrot/trunk@3627 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/env.pl
+++ config/auto/env.pl
@@ -9,22 +9,27 @@ $description="Determining if your C library has setenv / unsetenv...";
 sub runstep {
     my ($setenv, $unsetenv) = (0, 0);
 
-    cc_gen('config/auto/env/test_setenv.in');
-    eval { cc_build(); };
-    unless ($@ || cc_run() !~ /ok/) {
+    if ($^O eq 'Win32') {
 	$setenv = 1;
-    }
-    cc_clean();
-
-    cc_gen('config/auto/env/test_unsetenv.in');
-    eval { cc_build(); };
-    unless ($@ || cc_run() !~ /ok/) {
 	$unsetenv = 1;
     }
-    cc_clean();
+    else {
+	cc_gen('config/auto/env/test_setenv.in');
+	eval { cc_build(); };
+	unless ($@ || cc_run() !~ /ok/) {
+	    $setenv = 1;
+	}
+	cc_clean();
+	cc_gen('config/auto/env/test_unsetenv.in');
+	eval { cc_build(); };
+	unless ($@ || cc_run() !~ /ok/) {
+	    $unsetenv = 1;
+	}
+	cc_clean();
+    }
 
     Configure::Data->set(
-	setenv => $setenv, 
+	setenv => $setenv,
 	unsetenv => $unsetenv
     );
 
