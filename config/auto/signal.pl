39f75822a26419a2513e710d36bd8ca0e49e1229
Leopold Toetsch <lt@toetsch.at>
2003-07-14 08:38:24 +0000

    test for setitimer
    git-svn-id: https://svn.parrot.org/parrot/trunk@3621 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/signal.pl
+++ config/auto/signal.pl
@@ -13,7 +13,8 @@ sub runstep {
     my ($miniparrot) = @_;
     Configure::Data->set(
 	has___sighandler_t => undef,
-	has_sigaction    => undef
+	has_sigaction    => undef,
+	has_setitimer    => undef
     );
     if (defined $miniparrot) {
 	return;
@@ -48,6 +49,15 @@ sub runstep {
 
     Configure::Data->set('malloc_header', undef);
 
+    cc_gen('config/auto/signal/test_itimer.in');
+    eval { cc_build(); };
+    unless ($@ || cc_run() !~ /ok/) {
+	Configure::Data->set(
+	    has_setitimer    => 'define'
+	);
+	print " (setitimer) ";
+    }
+    cc_clean();
     # now generate signal constants
     open O, ">runtime/parrot/include/signal.pasm" or die
     "Cant write runtime/parrot/include/signal.pasm";
