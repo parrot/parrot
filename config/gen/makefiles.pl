5d0776da0cec3884173a812e5ddc6767733a4381
Simon Glover <scog@aip.de>
2003-07-23 14:38:28 +0000

    Generate POD from the ops files in portable fashion. Courtesy of Lars Balker Rasmussen.
    git-svn-id: https://svn.parrot.org/parrot/trunk@3698 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/makefiles.pl
+++ config/gen/makefiles.pl
@@ -13,8 +13,6 @@ sub runstep {
           commentType => '#');
   genfile('config/gen/makefiles/classes.in',   'classes/Makefile',
           commentType => '#');
-  genfile('config/gen/makefiles/docs.in',      'docs/Makefile',
-          commentType => '#');
   genfile('config/gen/makefiles/languages.in', 'languages/Makefile',
           commentType => '#');
   genfile('config/gen/makefiles/jako.in',      'languages/jako/Makefile',
@@ -33,6 +31,34 @@ sub runstep {
           commentType => '#');
   genfile('config/gen/makefiles/ook.in',       'languages/ook/Makefile',
           commentType => '#');
+
+  # set up docs/Makefile, partly based on the .ops in the root dir
+
+  opendir ROOT, "." or die "opendir .: $!";
+  my @ops = sort grep { -f $_ and /\.ops$/ } readdir ROOT;
+  closedir ROOT;
+
+  my $pod = join " ", map { my $t = $_; $t =~ s/\.ops$/.pod/; "ops/$t" } @ops;
+
+  Configure::Data->set(pod => $pod);
+
+  genfile('config/gen/makefiles/docs.in',      'docs/Makefile',
+          commentType => '#');
+
+  Configure::Data->set(pod => undef);
+
+  open MAKEFILE, ">> docs/Makefile" or die "open >> docs/Makefile: $!";
+  
+  foreach my $ops (@ops) {
+      my $pod = $ops;
+      $pod =~ s/\.ops$/.pod/;
+      print MAKEFILE <<"EOM";
+ops/$pod: ../$ops
+	perldoc -u ../$ops > ops/$pod
+EOM
+  }
+
+  close MAKEFILE
 }
 
 1;
