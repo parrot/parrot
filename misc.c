12d2a4f93ae7892d30175b3a3cc7de94e345f79a
Simon Glover <scog@aip.de>
2003-07-29 02:32:15 +0000

    Remove unnecessary safety checks
    git-svn-id: https://svn.parrot.org/parrot/trunk@3774 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- misc.c
+++ misc.c
@@ -45,13 +45,13 @@ Parrot_vsprintf_c(struct Parrot_Interp *interpreter, const char *pat,
                   va_list args)
 {
     STRING *realpat, *ret;
-    if (interpreter)
-        Parrot_block_DOD(interpreter);
+        
+    Parrot_block_DOD(interpreter);
     realpat = string_make(interpreter, pat, strlen(pat),
                                   NULL, PObj_external_FLAG, NULL);
     ret = Parrot_vsprintf_s(interpreter, realpat, args);
-    if (interpreter)
-        Parrot_unblock_DOD(interpreter);
+    Parrot_unblock_DOD(interpreter);
+
     return ret;
 }
 
