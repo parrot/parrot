c1d5b503174f1c6a72b1e72326fea1f930dca112
Simon Glover <scog@aip.de>
2003-08-19 03:02:27 +0000

    Add header and footer comments
    git-svn-id: https://svn.parrot.org/parrot/trunk@3957 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/enums.h
+++ include/parrot/enums.h
@@ -1,3 +1,13 @@
+/* enums.h
+ *  Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+ *  Overview:
+ *     enums shared by much of the stack-handling code
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References: 
+ */
+
 #if !defined(PARROT_ENUMS_H_GUARD)
 #define PARROT_ENUMS_H_GUARD
 
@@ -23,3 +33,13 @@ typedef enum {
 
 
 #endif
+
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
