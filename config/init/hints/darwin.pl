d8eb43e6c3d85e8b7033bf51b5fcbd000f13e985
Brent Royal-Gordon <brent@brentdax.com>
2003-09-11 18:43:41 +0000

    From: Arthur Bergman <arthur@fotango.com> Sent: Thursday, September 11, 2003 7:47 AM To: perl6-internals@perl.org Subject: [PATH] Get rid of very annoying header warnings on darwin
    Hi,
    
    This gets rid of the very annoying long double might change warning
    under darwin, the warning comes from my config.h which has
    
    typedef long double HUGEFLOATVAL;
    
    Arthur
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@4075 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/darwin.pl
+++ config/init/hints/darwin.pl
@@ -1,6 +1,6 @@
 my($ccflags, $ldflags)=Configure::Data->get(qw(ccflags ldflags));
 
-$ccflags .= " -pipe -fno-common ";
+$ccflags .= " -pipe -fno-common -Wno-long-double ";
 $ccflags =~ s/-flat_namespace\s*//;
 $ldflags =~ s/-flat_namespace\s*//;
 $ldflags .= " -flat_namespace ";
