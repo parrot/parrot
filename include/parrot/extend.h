8e48dd3ed9ab94565e87f607a1e009a0aa4ebabb
Dan Sugalski <dan@sidhe.org>
2003-09-26 18:14:27 +0000

    start of extension interface
    git-svn-id: https://svn.parrot.org/parrot/trunk@4207 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ include/parrot/extend.h
@@ -0,0 +1,37 @@
+/* extend.h
+ *  Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+ *  CVS Info
+ *     $Id$
+ *  Overview:
+ *     This is the Parrot extension mechanism, the face we present to
+ *     extension modules and whatnot
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References:
+ *      extend.c, docs/extend.pod.
+ */
+
+#if !defined(PARROT_EXTEND_H_GUARD)
+#define PARROT_EXTEND_H_GUARD
+
+#include "parrot/config.h"      /* PARROT_VERSION, PARROT_JIT_CAPABLE... */
+#include "parrot/interpreter.h" /* give us the interpreter flags */
+#include "parrot/warnings.h"    /* give us the warnings flags    */
+
+typedef void * Parrot_STRING;
+typedef void * Parrot_PMC;
+typedef INTVAL Parrot_INTVAL;
+typedef NUMVAL Parrot_NUMVAL;
+
+#endif
+
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+ */
