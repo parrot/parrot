8960a2f23e70aef9822a63c96437b5b1aef61abe
purl <purl@localhost>
2003-10-23 02:15:28 +0000

    Remove old imcc copies
    git-svn-id: https://svn.parrot.org/parrot/trunk@4535 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/imcc/optimizer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __OPTIMIZER_H
-#define __OPTIMIZER_H
-void pre_optimize(struct Parrot_Interp *);
-int cfg_optimize(struct Parrot_Interp *);
-int optimize(struct Parrot_Interp *);
-void post_optimize(struct Parrot_Interp *);
-
-#endif
