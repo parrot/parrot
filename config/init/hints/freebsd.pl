010164c2e1f06e50dae29e5d416c0eed977a235e
Jürgen Bömmels <boemmels@web.de>
2003-11-19 15:37:18 +0000

    Fix pthread Configure on FreeBSD as discussed in issue #24333
    Courtesy of "Lars Balker Rasmussen" <lars@balker.org>
      and "Nick Kostirya" <nick@han.kherson.ua>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@4825 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/freebsd.pl
+++ config/init/hints/freebsd.pl
@@ -1,7 +1,28 @@
 my $libs = Configure::Data->get('libs');
-if ( $libs !~ /-lpthreads/ ) {
-    $libs .= ' -lpthreads';
+
+# get rid of old pthread-stuff, if any
+$libs =~ s/(-lpthreads|-lc_r)\b\s*//g;
+
+# The following test is from FreeBSD's /usr/ports/Mk/bsd.port.mk,
+# which must be assumed to do the right thing.
+
+my $osversion;
+if (-e "/sbin/sysctl") {
+    $osversion = `/sbin/sysctl -n kern.osreldate`;
 }
+else {
+    $osversion = `/usr/sbin/sysctl -n kern.osreldate`;
+}
+chomp $osversion;
+
+if ($osversion < 500016) { 
+    $libs .= ' -pthread';
+}
+else {
+    $libs =~ s/-lc\b\s*//;
+    $libs .= ' -lc_r';
+}
+
 Configure::Data->set(
     libs => $libs,
 );
