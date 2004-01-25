8f00b37f1c5e4ee31131f76b29fba5463fc12932
Michael Scott <michael_scott@mac.com>
2004-01-25 18:14:15 +0000

    Documentation update
    git-svn-id: https://svn.parrot.org/parrot/trunk@5148 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/parrot.c
+++ src/parrot.c
@@ -1,16 +1,32 @@
-/* parrot.c
- *  Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
- *  CVS Info
- *     $Id$
- *  Overview:
- *      main driver file for Parrot
- *  Data Structure and Algorithms:
- *  History:
- *  Notes:
- *  References:
- */
+/*
+Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+$Id$
+
+=head1 NAME
+
+src/parrot.c
+
+=head1 DESCRIPTION
+
+THIS FILE IS NOT USED.
+
+=cut
+
+*/
 
 #include "parrot/parrot.h"
+
+/*
+
+=head1 HISTORY
+
+"It was originally supposed to serve the role of C<test_main.c>, but that
+didn't ever happen for some reason. Ask Simon if you really want to
+know." -- Brent Dax
+
+=cut
+
+*/
 
 /*
  * Local variables:
