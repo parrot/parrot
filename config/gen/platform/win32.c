1bf656c76b768556bd81dc1190cf867f3b5eb35e
Dan Sugalski <dan@sidhe.org>
2004-02-12 19:20:34 +0000

    Correct error message
    git-svn-id: https://svn.parrot.org/parrot/trunk@5254 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/win32.c
+++ config/gen/platform/win32.c
@@ -135,7 +135,7 @@ Parrot_dlclose(void *handle)
  *
  */
 INTVAL Parrot_Run_OS_Command(Parrot_Interp interpreter, STRING *command) {
-    Parrot_warn(NULL, PARROT_WARNINGS_PLATFORM_FLAG, "Parrot_dlclose not implemented");
+    Parrot_warn(NULL, PARROT_WARNINGS_PLATFORM_FLAG, "Parrot_Run_OS_Command not implemented");
     return 0;
 }
 
