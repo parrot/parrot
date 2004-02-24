63d1872e57bb0af0c1574fe2002c48741c01e867
Leopold Toetsch <lt@toetsch.at>
2004-02-24 12:16:06 +0000

    This patch implements Parrot_Run_OS_Command() for Win32.
    Courtesy of Nigelsandever @ Btconnect . Com
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@5385 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/win32/exec.c
+++ config/gen/platform/win32/exec.c
@@ -2,8 +2,42 @@
  * Spawn a subprocess
  *
  */
-INTVAL Parrot_Run_OS_Command(Parrot_Interp interpreter, STRING *command) {
-    Parrot_warn(NULL, PARROT_WARNINGS_PLATFORM_FLAG, "Parrot_Run_OS_Command not implemented");
-    return 0;
+INTVAL
+Parrot_Run_OS_Command(Parrot_Interp interpreter, STRING *command) {
+    int status = 0;
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    int free_it = 0;
+    char* cmd = mem_sys_allocate( command->strlen + 4 );
+    char* shell = Parrot_getenv( "ComSpec", &free_it );
+
+    strcpy( cmd, "/c " );
+    strcat( cmd, string_to_cstring(interpreter, command) );
+
+    memset( &si, 0, sizeof(si) );
+    si.cb = sizeof(si);
+    memset( &pi, 0, sizeof(pi) );
+
+    /* Start the child process. */
+    if(
+        !CreateProcess( shell, cmd,
+        NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi )
+    ) {
+        internal_exception(NOSPAWN, "Can't spawn child process");
+    }
+
+    WaitForSingleObject( pi.hProcess, INFINITE );
+
+    if( !GetExitCodeProcess( pi.hProcess, &status ) ) {
+        /* XXX njs Should call GetLastError for failure message? */
+        Parrot_warn( interpreter, PARROT_WARNINGS_PLATFORM_FLAG,
+            "Process completed: Failed to get exit code.");
+    }
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+    if( free_it ) free( shell );
+    free( cmd );
+
+    return status;
 }
 
