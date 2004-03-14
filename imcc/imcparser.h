c3a03dc90bd85695695eafd0f9a1e994a2b79f9e
Leopold Toetsch <lt@toetsch.at>
2004-03-14 09:46:33 +0000

    more PIR syntax improvements - generated files
    git-svn-id: https://svn.parrot.org/parrot/trunk@5641 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- imcc/imcparser.h
+++ imcc/imcparser.h
@@ -66,20 +66,20 @@
      MINUS_ASSIGN = 289,
      MUL_ASSIGN = 290,
      DIV_ASSIGN = 291,
-     BAND_ASSIGN = 292,
-     BOR_ASSIGN = 293,
-     BXOR_ASSIGN = 294,
-     SHR_ASSIGN = 295,
-     SHL_ASSIGN = 296,
-     SHR_U_ASSIGN = 297,
-     SHIFT_LEFT = 298,
-     SHIFT_RIGHT = 299,
-     INTV = 300,
-     FLOATV = 301,
-     STRINGV = 302,
-     PMCV = 303,
-     OBJECTV = 304,
-     DEFINED = 305,
+     CONCAT_ASSIGN = 292,
+     BAND_ASSIGN = 293,
+     BOR_ASSIGN = 294,
+     BXOR_ASSIGN = 295,
+     SHR_ASSIGN = 296,
+     SHL_ASSIGN = 297,
+     SHR_U_ASSIGN = 298,
+     SHIFT_LEFT = 299,
+     SHIFT_RIGHT = 300,
+     INTV = 301,
+     FLOATV = 302,
+     STRINGV = 303,
+     PMCV = 304,
+     OBJECTV = 305,
      LOG_XOR = 306,
      RELOP_EQ = 307,
      RELOP_NE = 308,
@@ -90,54 +90,53 @@
      GLOBAL = 313,
      GLOBALOP = 314,
      ADDR = 315,
-     CLONE = 316,
-     RESULT = 317,
-     RETURN = 318,
-     POW = 319,
-     SHIFT_RIGHT_U = 320,
-     LOG_AND = 321,
-     LOG_OR = 322,
-     COMMA = 323,
-     ESUB = 324,
-     PCC_BEGIN = 325,
-     PCC_END = 326,
-     PCC_CALL = 327,
-     PCC_SUB = 328,
-     PCC_BEGIN_RETURN = 329,
-     PCC_END_RETURN = 330,
-     PCC_BEGIN_YIELD = 331,
-     PCC_END_YIELD = 332,
-     NCI_CALL = 333,
-     METH_CALL = 334,
-     INVOCANT = 335,
-     PROTOTYPED = 336,
-     NON_PROTOTYPED = 337,
-     MAIN = 338,
-     LOAD = 339,
-     IMMEDIATE = 340,
-     POSTCOMP = 341,
-     METHOD = 342,
-     LABEL = 343,
-     EMIT = 344,
-     EOM = 345,
-     IREG = 346,
-     NREG = 347,
-     SREG = 348,
-     PREG = 349,
-     IDENTIFIER = 350,
-     STRINGC = 351,
-     INTC = 352,
-     FLOATC = 353,
-     REG = 354,
-     MACRO = 355,
-     ENDM = 356,
-     PARROT_OP = 357,
-     VAR = 358,
-     LINECOMMENT = 359,
-     FILECOMMENT = 360,
-     DOT = 361,
-     CONCAT = 362,
-     POINTY = 363
+     RESULT = 316,
+     RETURN = 317,
+     POW = 318,
+     SHIFT_RIGHT_U = 319,
+     LOG_AND = 320,
+     LOG_OR = 321,
+     COMMA = 322,
+     ESUB = 323,
+     PCC_BEGIN = 324,
+     PCC_END = 325,
+     PCC_CALL = 326,
+     PCC_SUB = 327,
+     PCC_BEGIN_RETURN = 328,
+     PCC_END_RETURN = 329,
+     PCC_BEGIN_YIELD = 330,
+     PCC_END_YIELD = 331,
+     NCI_CALL = 332,
+     METH_CALL = 333,
+     INVOCANT = 334,
+     PROTOTYPED = 335,
+     NON_PROTOTYPED = 336,
+     MAIN = 337,
+     LOAD = 338,
+     IMMEDIATE = 339,
+     POSTCOMP = 340,
+     METHOD = 341,
+     LABEL = 342,
+     EMIT = 343,
+     EOM = 344,
+     IREG = 345,
+     NREG = 346,
+     SREG = 347,
+     PREG = 348,
+     IDENTIFIER = 349,
+     STRINGC = 350,
+     INTC = 351,
+     FLOATC = 352,
+     REG = 353,
+     MACRO = 354,
+     ENDM = 355,
+     PARROT_OP = 356,
+     VAR = 357,
+     LINECOMMENT = 358,
+     FILECOMMENT = 359,
+     DOT = 360,
+     CONCAT = 361,
+     POINTY = 362
    };
 #endif
 #define LOW_PREC 258
@@ -174,20 +173,20 @@
 #define MINUS_ASSIGN 289
 #define MUL_ASSIGN 290
 #define DIV_ASSIGN 291
-#define BAND_ASSIGN 292
-#define BOR_ASSIGN 293
-#define BXOR_ASSIGN 294
-#define SHR_ASSIGN 295
-#define SHL_ASSIGN 296
-#define SHR_U_ASSIGN 297
-#define SHIFT_LEFT 298
-#define SHIFT_RIGHT 299
-#define INTV 300
-#define FLOATV 301
-#define STRINGV 302
-#define PMCV 303
-#define OBJECTV 304
-#define DEFINED 305
+#define CONCAT_ASSIGN 292
+#define BAND_ASSIGN 293
+#define BOR_ASSIGN 294
+#define BXOR_ASSIGN 295
+#define SHR_ASSIGN 296
+#define SHL_ASSIGN 297
+#define SHR_U_ASSIGN 298
+#define SHIFT_LEFT 299
+#define SHIFT_RIGHT 300
+#define INTV 301
+#define FLOATV 302
+#define STRINGV 303
+#define PMCV 304
+#define OBJECTV 305
 #define LOG_XOR 306
 #define RELOP_EQ 307
 #define RELOP_NE 308
@@ -198,60 +197,59 @@
 #define GLOBAL 313
 #define GLOBALOP 314
 #define ADDR 315
-#define CLONE 316
-#define RESULT 317
-#define RETURN 318
-#define POW 319
-#define SHIFT_RIGHT_U 320
-#define LOG_AND 321
-#define LOG_OR 322
-#define COMMA 323
-#define ESUB 324
-#define PCC_BEGIN 325
-#define PCC_END 326
-#define PCC_CALL 327
-#define PCC_SUB 328
-#define PCC_BEGIN_RETURN 329
-#define PCC_END_RETURN 330
-#define PCC_BEGIN_YIELD 331
-#define PCC_END_YIELD 332
-#define NCI_CALL 333
-#define METH_CALL 334
-#define INVOCANT 335
-#define PROTOTYPED 336
-#define NON_PROTOTYPED 337
-#define MAIN 338
-#define LOAD 339
-#define IMMEDIATE 340
-#define POSTCOMP 341
-#define METHOD 342
-#define LABEL 343
-#define EMIT 344
-#define EOM 345
-#define IREG 346
-#define NREG 347
-#define SREG 348
-#define PREG 349
-#define IDENTIFIER 350
-#define STRINGC 351
-#define INTC 352
-#define FLOATC 353
-#define REG 354
-#define MACRO 355
-#define ENDM 356
-#define PARROT_OP 357
-#define VAR 358
-#define LINECOMMENT 359
-#define FILECOMMENT 360
-#define DOT 361
-#define CONCAT 362
-#define POINTY 363
+#define RESULT 316
+#define RETURN 317
+#define POW 318
+#define SHIFT_RIGHT_U 319
+#define LOG_AND 320
+#define LOG_OR 321
+#define COMMA 322
+#define ESUB 323
+#define PCC_BEGIN 324
+#define PCC_END 325
+#define PCC_CALL 326
+#define PCC_SUB 327
+#define PCC_BEGIN_RETURN 328
+#define PCC_END_RETURN 329
+#define PCC_BEGIN_YIELD 330
+#define PCC_END_YIELD 331
+#define NCI_CALL 332
+#define METH_CALL 333
+#define INVOCANT 334
+#define PROTOTYPED 335
+#define NON_PROTOTYPED 336
+#define MAIN 337
+#define LOAD 338
+#define IMMEDIATE 339
+#define POSTCOMP 340
+#define METHOD 341
+#define LABEL 342
+#define EMIT 343
+#define EOM 344
+#define IREG 345
+#define NREG 346
+#define SREG 347
+#define PREG 348
+#define IDENTIFIER 349
+#define STRINGC 350
+#define INTC 351
+#define FLOATC 352
+#define REG 353
+#define MACRO 354
+#define ENDM 355
+#define PARROT_OP 356
+#define VAR 357
+#define LINECOMMENT 358
+#define FILECOMMENT 359
+#define DOT 360
+#define CONCAT 361
+#define POINTY 362
 
 
 
 
 #ifndef YYSTYPE
-#line 231 "imcc/imcc.y"
+#line 244 "imcc/imcc.y"
 typedef union {
     int t;
     char * s;
@@ -262,7 +260,7 @@ typedef union {
     SymbolTable * symtab;
 } yystype;
 /* Line 1281 of /usr/share/bison/yacc.c.  */
-#line 266 "imcc/imcparser.h"
+#line 264 "imcc/imcparser.h"
 # define YYSTYPE yystype
 #endif
 
