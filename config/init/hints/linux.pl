09273aaaa23e87a9897d116ee22f7ee9dbde38cc
Leopold Toetsch <lt@toetsch.at>
2004-05-18 09:22:01 +0000

    use -fPIC for libnci
    Courtesy of Adam Thomason <thomason@xencor.com>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@6076 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/linux.pl
+++ config/init/hints/linux.pl
@@ -3,10 +3,15 @@ my $cflags = Configure::Data->get('ccflags');
 if ($libs !~ /-lpthread/) {
     $libs .= ' -lpthread';
 }
+my $ld_shared = Configure::Data->get('ld_shared');
+if ( $ld_shared !~ /-fPIC/ ) {
+    $ld_shared .= ' -fPIC';
+}
 
 Configure::Data->set(
     ccflags => $cflags,
     libs => $libs,
+    ld_shared     => $ld_shared,
     i_lib_pthread => 1,		# XXX fake a header entry
     linkflags     => '-Wl,-E',  # --export-dynamic, s. info gcc, ld
     link          => 'c++',
