11e0404a4bc4894ccc49e0131a4b1e5dcb32ed23
Leopold Toetsch <lt@toetsch.at>
2004-07-08 06:00:50 +0000

    [perl #30630] [PATCH] classes/bigint.pmc breaks non GMP builds
    The BigInt PMC breaks parrot when building without GMP support because
    a few GMP symbols are in code not protected in PARROT_HAS_GMP.
    
    This patch
    also removes a NULL return from the non GMP bigint_set_self() which
    stops a returning from void function warning from gcc. I tested the
    patched version against t/pmc/bigint.t on OS X with gcc 3.1 and linux
    with gcc 3.2.2.
    
    Courtesy of Matt Kennedy <arturus@gmail.com>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@6335 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/gmp.pl
+++ config/auto/gmp.pl
@@ -25,7 +25,25 @@ sub runstep {
     my $test;
     my ($verbose) = @_;
     my $libs = Configure::Data->get('libs');
+    my $linkflags = Configure::Data->get('linkflags');
+    my $ccflags = Configure::Data->get('ccflags');
     Configure::Data->add(' ', 'libs', '-lgmp');
+    
+    my $archname = $Config{archname};
+    my ($cpuarch, $osname) = split('-', $archname);
+    if(!defined $osname) {
+        ($osname, $cpuarch) = ($cpuarch, "");
+    }
+
+    # On OS X check the presence of the gmp header in the standard
+    # Fink location. TODO: Need a more generalized way for finding 
+    # where Fink lives.
+    if($osname =~ /darwin/) {
+        if( -f "/sw/include/gmp.h") {
+            Configure::Data->add(' ', 'linkflags', '-L/sw/lib');
+            Configure::Data->add(' ', 'ccflags', '-I/sw/include');
+        }
+    }
 
     cc_gen('config/auto/gmp/gmp.in');
     eval { cc_build(); };
@@ -42,8 +60,10 @@ sub runstep {
 
     }
     else {
-	Configure::Data->set('libs', $libs);
-	print " (no) " if $verbose;
+        Configure::Data->set('libs', $libs);
+        Configure::Data->set('ccflags', $ccflags);
+        Configure::Data->set('linkflags', $linkflags);
+        print " (no) " if $verbose;
     }
 }
 
