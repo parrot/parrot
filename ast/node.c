2c4fb59b750de909363ca7e210426e87c52c0eb8
Leopold Toetsch <lt@toetsch.at>
2004-08-01 11:13:22 +0000

    simpler ast dump
    git-svn-id: https://svn.parrot.org/parrot/trunk@6454 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- ast/node.c
+++ ast/node.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 #include "ast.h"
+#ifndef AST_TEST
 #include "../imcc/imc.h"
+#endif
 
 static void
 pr(nodeType *p)
 {
     if (!p)
 	return;
-    printf("Const(%s)", p->u.r->name);
+    printf("%s", p->u.r->name);
 }
 
 static void
@@ -28,53 +30,34 @@ static void
 set_const(nodeType *p)
 {
     p->expand = const_expand;
-    // p->context = const_context;
+    /* p->context = const_context; */
     p->dump = const_dump;
     p->opt = NULL;
-    p->d = "const";
+    p->d = "Const";
 }
 
 static void
-dump_st(nodeType *p, int l)
+dump(nodeType *p, int l)
 {
-    if (p) {
-	printf("\n%*s", l*2, "");
-        printf("%s(", p->d);
+    nodeType *child;
+    printf("\n%*s", l*2, "");
+    printf("%s(", p->d);
+    if (p->dump)
+	p->dump(p, l);
+    else {
+	child = NODE0(p);
+	if (child)
+	    dump(child, l + 1);
     }
-}
-
-static void
-dump_e(nodeType *p, int l)
-{
-    if (p) {
-	printf(")");
-    }
-}
-
-static void
-dump_0(nodeType *p, int l)
-{
-    dump_st(p, l);
-    dump_e(p, l);
-    if ( p->next )
-	p->next->dump(p->next, l);
-}
-
-static void
-dump_1(nodeType *p, int l)
-{
-    dump_st(p, l);
-    if ( NODE0(p) )
-	NODE0(p)->dump(NODE0(p), l + 1);
-    dump_e(p, l);
-    if ( p->next )
-	p->next->dump(p->next, l);
+    printf(")");
+    if (!p->dump && p->next )
+	dump(p->next, l);
 }
 
 void
 dump_nodes(nodeType *p)
 {
-    p->dump(p, 0);
+    dump(p, 0);
     printf("\n");
 }
 
@@ -120,12 +103,12 @@ static node_names ast_list[] = {
     { "--no-node", 	NULL, NULL, NULL, NULL },
     { "Const", 		NULL, NULL, NULL, const_dump },
 #define CONST_NODE 1
-    { "Py_Module", 	create_1, NULL, NULL, dump_1 },
-    { "Py_Print" , 	create_1, NULL, NULL, dump_1 },
-    { "Py_Print_nl",	create_0, NULL, NULL, dump_0 },
-    { "Src_File",    	create_1, NULL, NULL, dump_1 },
-    { "Src_Line",    	create_1, NULL, NULL, dump_1 },
-    { "Stmts",          create_1, NULL, NULL, dump_1 }
+    { "Py_Module", 	create_1, NULL, NULL, NULL },
+    { "Py_Print" , 	create_1, NULL, NULL, NULL },
+    { "Py_Print_nl",	create_0, NULL, NULL, NULL },
+    { "Src_File",    	create_1, NULL, NULL, NULL },
+    { "Src_Line",    	create_1, NULL, NULL, NULL },
+    { "Stmts",          create_1, NULL, NULL, NULL }
 };
 
 static int
@@ -212,3 +195,14 @@ IMCC_append_node(nodeType *a, nodeType *b, YYLTYPE *loc)
     last->next = b;
     return a;
 }
+
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
+
