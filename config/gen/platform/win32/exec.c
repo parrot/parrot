f9d942839ab987ec5a5d9569c629138a3ba7fc0d
Dan Sugalski <dan@sidhe.org>
2004-08-07 14:09:39 +0000

    Add proper exec code for Win32
    Courtesy of "Jonathan Worthington" <jonathan@jwcs.net>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@6500 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/win32/exec.c
+++ config/gen/platform/win32/exec.c
@@ -44,13 +44,80 @@ Parrot_Run_OS_Command(Parrot_Interp interpreter, STRING *command) {
 }
 
 void 
-Parrot_Exec_OS_Command(Parrot_Interp interpreter, STRING *command) {
-  /* Be horribly profligate with memory, since we're
-     about to be something else */
-  int status;
-  status = _execlp(string_to_cstring(interpreter, command), NULL);
-  /* if we get here, something's horribly wrong... */
-  if (status) {
-    internal_exception(NOSPAWN, "Exec failed, code %i", status);
-  }
+Parrot_Exec_OS_Command(Parrot_Interp interpreter, STRING *command)
+{
+	int status;
+	char *in = string_to_cstring(interpreter, command);
+	char *cmd = NULL;
+	char **argv = mem_sys_allocate_zeroed(2 * sizeof(int));
+
+	/* Grab string, extract command and parameters. */
+	char *curPos = in;
+	char *lastCommandStart = in;
+	char seekChar = 0; 
+	int argc = 1;
+	while (*curPos)
+	{
+		/* If we don't have a seek character and this is a quote... */
+		if (seekChar == 0 && (*curPos == '\'' || *curPos == '"'))
+		{
+			seekChar = *curPos;
+			lastCommandStart = curPos;
+		}
+
+		/* If we don't have a seek character and this is not a space... */
+		else if (seekChar == 0 && *curPos != ' ')
+		{
+			if (!seekChar)
+				seekChar = ' ';
+			lastCommandStart = curPos;
+		}
+
+		/* If we seek the seek character... */
+		else if (*curPos == seekChar || (*(curPos + 1) == 0 && seekChar == ' '))
+		{
+			/* Copy what we found to a temporary string. */
+			char *tmp;
+			int lenFound = curPos - lastCommandStart;
+			if (*(curPos + 1) == 0)
+				lenFound++;
+			tmp = mem_sys_allocate(1 + lenFound);
+			memcpy(tmp, lastCommandStart, lenFound);
+			*(tmp + lenFound) = 0;
+			
+			/* Is it command or argument? */
+			if (cmd == NULL)
+			{
+				cmd = tmp;
+				*argv = tmp;
+			}
+			else
+			{
+				/* Allocate space for another pointer in **argv. */
+				argc++;
+				argv = mem_sys_realloc(argv, (argc + 1) * sizeof(int));
+				*(argv + (argc - 1)) = tmp;
+				*(argv + argc) = NULL;
+			}
+
+			/* Clear seek character. */
+			seekChar = 0;
+		}
+
+		/* Move to next character. */
+		curPos ++;
+	}
+
+	/* If we still have a seek char, then the input was improper. */
+	if (seekChar)
+	{
+		internal_exception(NOSPAWN, "Exec failed, invalid command string");
+	}
+
+	/* Now do the exec. */
+	status = _execvp(cmd, argv);
+	if (status) {
+		internal_exception(NOSPAWN, "Exec failed, code %i", status);
+	}
 }
+
