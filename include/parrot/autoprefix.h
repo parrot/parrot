5b88f2a50acf637dac1b9afc41c4829f397302f7
Dan Sugalski <dan@sidhe.org>
2004-08-09 21:53:50 +0000

    Framework to start prefixing un-prefixed symbols
    git-svn-id: https://svn.parrot.org/parrot/trunk@6520 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ include/parrot/autoprefix.h
@@ -0,0 +1,31 @@
+/* autoprefix.h
+ *  Copyright: 2004 The Perl Foundation.  All Rights Reserved.
+ *  CVS Info
+ *     $Id$
+ *  Overview:
+ *     provide Parrot_ prefixes for functions that don't have them
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *  References:
+ */
+
+#if !defined(PARROT_AUTOPREFIX_H_GUARD)
+#define PARROT_AUTOPREFIX_H_GUARD
+
+#if !defined HAS_LINKER_EXPORT_RESTRICTION
+/* The prefixed symbols go here */
+
+#endif
+
+#endif   /* PARROT_AUTOPREFIX_H_GUARD */
+
+/*
+ * Local variables:
+ * c-indentation-style: bsd
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+*/
