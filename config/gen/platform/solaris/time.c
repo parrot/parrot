16ffc42c83781f2f08d715f3e75f637cd5966177
Dan Sugalski <dan@sidhe.org>
2004-09-02 00:47:28 +0000

    On Solaris 8, asctime_r takes 3 parameters instead of 2.  The prototype looks like this:
    char *asctime_r(const struct tm *tm, char *buf, int buflen);
    
    This patch adds a time.c file to the solaris platform directory.  The
    only change in this time.c file and the stock generic/time.c is that
    the call to asctime_r passes 26 as the buflen.
    
    The Solaris man page for asctime_r says that the result will always be
    26 characters exactly.  I'm guessing the POSIX asctime assumes that
    the buffer is at least 26  characters, so assuming the buffer that
    Parrot_asctime_r gets is at least 26 characters isn't a new risk.
    
    Without this patch, the latest version of parrot from CVS will not
    compile on Solaris 8.
    
    Courtesy of coneill@oneill.net
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@6589 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ config/gen/platform/solaris/time.c
@@ -0,0 +1,69 @@
+/*
+** Time stuff
+*/
+
+#include <time.h>
+#include <sys/time.h>
+
+/*
+** Parrot_intval_time()
+*/
+
+INTVAL
+Parrot_intval_time(void)
+{
+    return time(NULL);
+}
+
+
+/*
+** Parrot_floatval_time()
+*/
+
+FLOATVAL
+Parrot_floatval_time(void)
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (FLOATVAL)t.tv_sec + ((FLOATVAL)t.tv_usec / 1000000.0);
+}
+
+/*
+** Parrot_sleep()
+*/
+
+void
+Parrot_sleep(unsigned int seconds)
+{
+    sleep(seconds);
+}
+
+/*
+ * Parrot_gmtime_r()
+ */
+
+struct tm *
+Parrot_gmtime_r(const time_t *t, struct tm *tm)
+{
+    return gmtime_r(t, tm);
+}
+
+/*
+ * Parrot_localtime_r()
+ */
+
+struct tm *
+Parrot_localtime_r(const time_t *t, struct tm *tm)
+{
+    return localtime_r(t, tm);
+}
+
+/*
+ * Parrot_asctime_r()
+ */
+
+char*
+Parrot_asctime_r(const struct tm *tm, char *buffer)
+{
+    return asctime_r(tm, buffer, 26);
+}
