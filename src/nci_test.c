b22dc25621ff7b7b059c93515a7504cf8b75d4e6
Leopold Toetsch <lt@toetsch.at>
2004-09-06 12:13:21 +0000

    [perl #31447] [PATCH] POD and coding standard in src/nci_test.c
    this patch adds a POD section on top of 'src/nci_test.c'. I also tried to
    make 'nci_test.c' more conformant to the coding standards from 'pdd07'.
    
    Courtesy of Bernhard Schmalhofer <Bernhard.Schmalhofer@biomax.de>
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@6602 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/nci_test.c
+++ src/nci_test.c
@@ -1,48 +1,87 @@
-#include <stdio.h>
 /*
- * cc -shared -fpic nci_test.c -o libnci.so -g
- * export LD_LIBRARY_PATH=.
- */
+Copyright: 2001-2004 The Perl Foundation.  All Rights Reserved.
+$Id$
+
+=head1 NAME
+
+src/nci_test.c - tests of the NCI use this as an example shared library 
+
+=head1 DESCRIPTION
+
+From this code a shared library can be compiled and linked with a command like:
+
+   cc -shared -fpic nci_test.c -o libnci.so -g
+
+For non-Unix platforms the above command has to be modified appropriately.
+
+The resulting shared library should be copied to a location like:
+
+   parrot/runtime/parrot/dynext/libnci.so
+
+At that location the shared library should be loadable with the opcode 'loadlib'. 
+The functions should be loadable with the opcode 'dlfunc'.
+
+=head1 Functions
+
+The name of a test function is usually 'nci_<signature>'. E.g. the function
+'nci_ip' takes a 'pointer' and returns a 'int'.
+
+=head1 SEE ALSO:
+
+  F<docs/pdds/pdd16_native_call.pod>
+  F<config/gen/makefiles/root.in>
+  F<t/pmc/nci.t>
+ 
+=cut
+
+*/
+
+#include <stdio.h>
+
+/*
+
+Declarations
+
+*/
+
 double nci_dd(double d);
-short nci_ssc(short l1, char l2);
-int nci_csc(short l1, char l2);
-int nci_isc(short l1, char l2);
-float nci_fff(float l1, float l2);
-int nci_ip(void *p);
-int nci_it(void *p);
-char *nci_tt(void *p);
-char *nci_tb(void *p);
-char *nci_tB(void **p);
+short  nci_ssc(short l1, char l2);
+int    nci_csc(short l1, char l2);
+int    nci_isc(short l1, char l2);
+float  nci_fff(float l1, float l2);
+int    nci_ip(void *p);
+int    nci_it(void *p);
+char * nci_tt(void *p);
+char * nci_tb(void *p);
+char * nci_tB(void **p);
 void * nci_pp(void *p);
-int nci_iiii(int i1, int i2, int i3);
-int nci_i4i(long * l, int i);
-int nci_ii3(int a, int *b);
+int    nci_iiii(int i1, int i2, int i3);
+int    nci_i4i(long * l, int i);
+int    nci_ii3(int a, int *b);
 void * nci_pi(int test);
-void  nci_vP(void *pmc);
+void   nci_vP(void *pmc);
 
 typedef void (*cb_C1_func)(const char*, void*);
 void nci_cb_C1(cb_C1_func, void*);
+
 typedef void (*cb_C2_func)(int, void*);
 void nci_cb_C2(cb_C2_func, void*);
 
 typedef void (*cb_D1_func)(void*, const char*);
 void nci_cb_D1(cb_D1_func, void*);
 
-typedef struct
-{
-	int y;
+typedef struct {
+    int y;
 } Nested;
 
-typedef struct
-{
-	int x;
-	Nested *nested;
+typedef struct {
+    int x;
+    Nested *nested;
 } Outer;
 
-typedef struct
-{
-	int x, y;
-	int w, h;
+typedef struct {
+    int x, y;
+    int w, h;
 } Rect_Like;
 
 void nci_pip (int count, Rect_Like *rects);
@@ -50,27 +89,39 @@ int nci_i_33 (int *double_me, int *triple_me);
 void nci_v_pii (Outer *my_data, int my_x, int my_y);
 void * nci_p_iiii (int alpha, int beta, int gamma, int delta);
 
-double nci_dd(double d) {
+/*
+
+Definitions
+
+*/
+
+double 
+nci_dd(double d) {
     return d * 2.0;
 }
 
-short nci_ssc(short l1, char l2) {
+short 
+nci_ssc(short l1, char l2) {
     return l1 * l2;
 }
 
-int nci_csc(short l1, char l2) {
+int 
+nci_csc(short l1, char l2) {
     return l1 * l2;
 }
 
-int nci_isc(short l1, char l2) {
+int 
+nci_isc(short l1, char l2) {
     return l1 * l2;
 }
 
-float nci_fff(float l1, float l2) {
+float 
+nci_fff(float l1, float l2) {
     return l1 / l2;
 }
 
-int nci_ip(void *p) {
+int 
+nci_ip(void *p) {
     typedef struct _dfi {
         double d;
         float f;
@@ -80,13 +131,16 @@ int nci_ip(void *p) {
     dfi *sp = (dfi*) p;
     puts(sp->s);
     fflush(stdout);
+
     return (int) (sp->d + sp->f + sp->i);
 }
 
 /* test calls this with a string */
-int nci_it(void *p) {
+int 
+nci_it(void *p) {
     fprintf(stderr, "%c%c\n", ((char*) p)[1], ((char *) p)[0]);
     fflush(stderr);
+
     return 2;
 }
 
@@ -94,50 +148,65 @@ static char s[] = "xx worked\n";
 char *nci_tt(void *p) {
     s[0] = ((char*) p)[1];
     s[1] = ((char*) p)[0];
+
     return s;
 }
 
 static char b[] = "xx worked\n";
-char *nci_tb(void *p) {
+char *
+nci_tb(void *p) {
     b[0] = ((char*) p)[1];
     b[1] = ((char*) p)[0];
+
     return b;
 }
 
 static char B[] = "xx done\n";
-char *nci_tB(void **p) {
+char *
+nci_tB(void **p) {
     B[0] = (*(char**) p)[1];
     B[1] = (*(char**) p)[0];
+
     return B;
 }
 
-void * nci_pp(void *p) {
+void * 
+nci_pp(void *p) {
     return p;
 }
 
-int nci_iiii(int i1, int i2, int i3) {
+int 
+nci_iiii(int i1, int i2, int i3) {
     fprintf(stderr, "%d %d %d\n", i1, i2, i3);
     fflush(stderr);
+
     return 2;
 }
 
-int nci_i4i(long * l, int i) {
+int 
+nci_i4i(long * l, int i) {
+
     return (int) (*l * i);
 }
 
-int nci_ii3(int a, int *bp) {
+int 
+nci_ii3(int a, int *bp) {
     int r = a * *bp;
     *bp = 4711;
+
     return r;
 }
 
-static int call_back(char *str) {
+static int 
+call_back(char *str) {
     puts(str);
     fflush(stdout);
+
     return 4711;
 }
 
-void * nci_pi(int test) {
+void * 
+nci_pi(int test) {
     switch (test) {
         case 0:
             {
@@ -240,7 +309,7 @@ void * nci_pi(int test) {
                 };
                 return &_x;
             }
-		case 8:
+        case 8:
             {
                 static struct _z {
                     int i;
@@ -251,7 +320,7 @@ void * nci_pi(int test) {
                     struct yt {
                         int i;
                         int j;
-                    	struct _z *z;
+                        struct _z *z;
                     } _y;
                 } _x = {
                     32,
@@ -262,99 +331,105 @@ void * nci_pi(int test) {
         default:
             fprintf(stderr, "unknown test number\n");
     }
+
     return NULL;
 }
 
-void nci_vP(void *pmc)
-{
+void 
+nci_vP(void *pmc) {
     if (pmc)
         puts("ok");
     else
         puts("got null");
 }
 
+
 /*
- * pdd16 tests
- */
+
+Functions used for pdd16 tests
+
+*/
+
 void
-nci_cb_C1(cb_C1_func cb, void* user_data)
-{
+nci_cb_C1(cb_C1_func cb, void* user_data) {
     const char *result = "succeeded";
     /* call the cb synchronously */
     (cb)(result, user_data);
 }
 
 void
-nci_cb_C2(cb_C2_func cb, void* user_data)
-{
+nci_cb_C2(cb_C2_func cb, void* user_data) {
     /* call the cb synchronously */
     (cb)(77, user_data);
 }
 
 
 void
-nci_cb_D1(cb_D1_func cb, void* user_data)
-{
+nci_cb_D1(cb_D1_func cb, void* user_data) {
     const char *result = "succeeded";
     /* call the cb synchronously */
     (cb)(user_data, result);
 }
 
-void nci_pip (int count, Rect_Like *rects)
-{
+void 
+nci_pip (int count, Rect_Like *rects) {
     int i;
     printf( "Count: %d\n", count);
     for (i = 0; i < 4; ++i)
         printf("X: %d\nY: %d\nW: %d\nH: %d\n",
-		rects[i].x, rects[i].y, rects[i].w, rects[i].h );
+        rects[i].x, rects[i].y, rects[i].w, rects[i].h );
 }
 
-int nci_i_33 (int *double_me, int *triple_me)
-{
-	*double_me *= 2;
-	*triple_me *= 3;
+int 
+nci_i_33 (int *double_me, int *triple_me) {
+    *double_me *= 2;
+    *triple_me *= 3;
 
-	return( *double_me + *triple_me );
+    return( *double_me + *triple_me );
 }
 
-void nci_v_pii (Outer *my_data, int my_x, int my_y)
-{
-	my_data->x            = my_x;
-	my_data->nested->y    = my_y;
+void 
+nci_v_pii (Outer *my_data, int my_x, int my_y) {
+    my_data->x            = my_x;
+    my_data->nested->y    = my_y;
 }
 
 static int my_array[4];
-void * nci_p_iiii (int alpha, int beta, int gamma, int delta)
-{
+void * 
+nci_p_iiii (int alpha, int beta, int gamma, int delta) {
+    static struct array_container
+    {
+        int   x;
+        int *array;
+    } container;
 
-	static struct array_container
-	{
-		int   x;
-		int *array;
-	} container;
+    my_array[0] = alpha;
+    my_array[1] = beta;
+    my_array[2] = gamma;
+    my_array[3] = delta;
 
-	my_array[0] = alpha;
-	my_array[1] = beta;
-	my_array[2] = gamma;
-	my_array[3] = delta;
+    container.x = 4;
+    container.array = my_array;
 
-        container.x = 4;
-        container.array = my_array;
-	return &container;
+    return &container;
 }
 
 #ifdef TEST
+
 char l2 = 4;
 float f2 = 4.0;
-int main() {
+int 
+main() {
     short l1 = 3;
     float f, f1 = 3.0;
     int l = nci_ssc(l1, l2);
     printf("%d\n", l);
     f = nci_fff(f1, f2);
     printf("%f\n", f);
+
     return 0;
 }
+
 #endif
 
 /*
