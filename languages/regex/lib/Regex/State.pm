f58e978a58ec7e6108c795c0aabb1e02ca923e36
Steve Fink <steve@fink.com>
2004-11-08 03:15:14 +0000

    split out literal_label so I can call it directly if needed
    git-svn-id: https://svn.parrot.org/parrot/trunk@6969 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/regex/lib/Regex/State.pm
+++ languages/regex/lib/Regex/State.pm
@@ -8,14 +8,19 @@ sub new {
     return $self;
 }
 
+sub literal_label {
+    my ($self, $label, $desc) = @_;
+    $desc ||= '';
+    return bless { name => 'LABEL',
+                   label => $label,
+                 }, 'Regex::Ops::Label';
+}
+
 sub genlabel {
     my ($self, $desc) = @_;
-    $desc ||= '';
     my $number = ++$self->{_markers}->{$desc};
     $number = '' if ($number == 1) && ($desc ne '');
-    return bless { name => 'LABEL',
-                   label => "\@$desc$number",
-                 }, 'Regex::Ops::Label';
+    return $self->literal_label("\@$desc$number", $desc);
 }
 
 1;
