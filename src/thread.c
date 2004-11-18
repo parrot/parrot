5113077c009052ed7ac6074a42b23f45ce5dc545
Leopold Toetsch <lt@toetsch.at>
2004-11-18 11:14:25 +0000

    add returncc opcode * returncc replaces return P1 or similar * adapt bits of streams code
    git-svn-id: https://svn.parrot.org/parrot/trunk@7049 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/thread.c
+++ src/thread.c
@@ -118,7 +118,8 @@ pt_thread_prepare_for_run(Parrot_Interp d, Parrot_Interp s)
      * are working - create it in the new interpreters mem space
      */
     ret_c = pmc_new(d, enum_class_RetContinuation);
-    INTERP_REG_PMC(d, 1) = ret_c;
+    INTERP_REG_PMC(d, 1) =      /* XXX remove when done pdd03 */
+        d->ctx.current_cont = ret_c;
 }
 
 /*
