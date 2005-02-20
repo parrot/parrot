0e78b0497a9d1cbe710060e2696d686642071372
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2005-02-20 12:39:30 +0000

    Report the minimum pointer alignment when doing a 'perl Configure.pl'.
    git-svn-id: https://svn.parrot.org/parrot/trunk@7545 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/alignptrs.pl
+++ config/auto/alignptrs.pl
@@ -1,57 +1,65 @@
-# Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+# Copyright: 2001-2005 The Perl Foundation.  All Rights Reserved.
 # $Id$
 
 =head1 NAME
 
-config/auto/alignptrs.pl - Pointer Alignment
+config/auto/alignptrs.pl - pointer alignment
 
 =head1 DESCRIPTION
 
-Determines the minimum pointer alignment.
+Determine the minimum pointer alignment.
 
 =cut
 
 package Configure::Step;
 
 use strict;
-use vars qw($description @args);
+use vars qw($description $result @args);
 use Parrot::Configure::Step ':auto';
 use Config;
 
-$description="Determining your minimum pointer alignment...";
+$description = "Determining your minimum pointer alignment...";
 
-@args=qw(miniparrot verbose);
+@args = qw(miniparrot verbose);
 
 sub runstep {
-  my ($miniparrot) = @_;
+    my ($miniparrot) = @_;
 
-  return if $miniparrot;
-  # HP-UX 10.20/32 hangs in this test.
-  if ($^O eq 'hpux' && $Config{ccflags} !~ /DD64/) {
-      Configure::Data->set(ptr_alignment => 4);
-      return;
-  }
+    return if $miniparrot;
 
-  return if (defined(Configure::Data->get('ptr_alignment')));
-  cc_gen('config/auto/alignptrs/test_c.in');
-  cc_build();
+    $result = '';
+    my $align;
+    if (defined(Configure::Data->get('ptr_alignment')))
+    {
+        $align = Configure::Data->get('ptr_alignment');
+        $result = "configured: ";
+    }
+    elsif ($^O eq 'hpux' && $Config{ccflags} !~ /DD64/) {
+        # HP-UX 10.20/32 hangs in this test.
+        my $align = 4;
+        Configure::Data->set(ptr_alignment => $align);
+        $result = "for hpux: ";
+    }
+    else {
+        # Now really test by compiling some code
+        cc_gen('config/auto/alignptrs/test_c.in');
+        cc_build();
+        for my $try_align (64, 32, 16, 8, 4, 2, 1) {
+            my $results=cc_run_capture($try_align);
+            if ($results =~ /OK/ && $results !~ /align/i) {
+                $align = $try_align;
+            }
+        }
+        cc_clean();
 
-  my $align = 999999;
+        die "Can't determine alignment!\n" unless defined $align;
+        Configure::Data->set(ptr_alignment => $align);
+    }
 
-  for my $try_align (64, 32, 16, 8, 4, 2, 1) {
-      my $results=cc_run_capture($try_align);
-      if ($results =~ /OK/ && $results !~ /align/i) {
-        $align = $try_align;
-      }
-  }
+    $result .= " $align byte";
+    $result .= 's' unless $align == 1;
 
-  cc_clean();
-
-  if ($align == 999999) {
-    die "Can't determine alignment!\n";
-  }
-
-  Configure::Data->set(ptr_alignment => $align);
+    return;
 }
 
 1;
