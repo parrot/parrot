02dec262f76d8fc2b9ee6ded8a41893963bdbb83
Leopold Toetsch <lt@toetsch.at>
2005-02-24 13:42:50 +0000

    GMS generational MS 7 - remove warning
    git-svn-id: https://svn.parrot.org/parrot/trunk@7559 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/dod.h
+++ include/parrot/dod.h
@@ -124,7 +124,7 @@ void Parrot_dod_ims_wb(Interp*, PMC *, PMC *);
 #if PARROT_GC_GMS
 #  define DOD_WRITE_BARRIER(interp, agg, old, new) do { \
     UINTVAL gen_agg, gen_new; \
-    if (PMC_IS_NULL(new)) \
+    if (!(new) || (new) == (void*)PMCNULL) \
         break; \
     gen_agg = PObj_to_GMSH(agg)->gen->gen_no; \
     gen_new = PObj_to_GMSH(new)->gen->gen_no; \
@@ -134,7 +134,7 @@ void Parrot_dod_ims_wb(Interp*, PMC *, PMC *);
 
 #  define DOD_WRITE_BARRIER_KEY(interp, agg, old, old_key, new, new_key) do { \
     UINTVAL gen_agg, gen_new, gen_key; \
-    if (PMC_IS_NULL(new)) \
+    if (!(new) || (new) == (void*)PMCNULL) \
         break; \
     gen_agg = PObj_to_GMSH(agg)->gen->gen_no; \
     gen_new = PObj_to_GMSH(new)->gen->gen_no; \
