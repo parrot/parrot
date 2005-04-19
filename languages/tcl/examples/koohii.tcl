ae3edc100a543b535972c42958e33a73b52ccf20
Will "Coke" Coleda <will@coleda.com>
2005-04-19 02:52:05 +0000

    Convert mime-type to text/plain...
    git-svn-id: https://svn.parrot.org/parrot/trunk@7876 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/tcl/examples/koohii.tcl
+++ languages/tcl/examples/koohii.tcl
@@ -1,6 +1,4 @@
 # Works with tclsh on OS X if you set your display encoding to UTF-8 
-# This definitely does NOT work with tcl.pbc yet. Consider it a TODO.
 
 # print out the kana for coffee.
-fconfigure stdout -encoding utf-8
-puts "\u30b3\u30fc\u30d2\u30fc"
+puts \u30b3\u30fc\u30d2\u30fc
