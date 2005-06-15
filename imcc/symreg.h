127c2264ace3a35964fca7040d45ce136eaf61a4
Jerry Gay <Jerry.Gay@gmail.com>
2005-06-15 20:40:54 +0000

    fix some cpp-style comments
    git-svn-id: https://svn.parrot.org/parrot/trunk@8384 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- imcc/symreg.h
+++ imcc/symreg.h
@@ -132,9 +132,9 @@ void add_pcc_multi(SymReg *r, SymReg * arg);
 void add_namespace(Parrot_Interp interpreter, struct _IMC_Unit *);
 
 typedef enum {
-	//P_NON_PROTOTYPED = 0x00,	/* must be 0 */
-	//P_PROTOTYPED     = 0x01,	/* must be 1 */
-	//P_NONE           = 0x02,
+	/* P_NON_PROTOTYPED = 0x00,	/* must be 0 */
+	/* P_PROTOTYPED     = 0x01,	/* must be 1 */
+	/* P_NONE           = 0x02, */
 	P_METHOD         = 0x04,
 
 	P_ANON           = SUB_FLAG_PF_ANON,  /* 0x8 - private3 */
