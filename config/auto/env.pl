c82bd0d9dbff7eb855f710b6b5444b8469ad1498
François Perrad <francois.perrad@gadz.org>
2005-10-25 10:45:54 +0000

    bad comparison (Win32 instead of MSWin32). so remove code never reached.
    git-svn-id: https://svn.parrot.org/parrot/trunk@9556 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/env.pl
+++ config/auto/env.pl
@@ -23,24 +23,18 @@ $description="Determining if your C library has setenv / unsetenv...";
 sub runstep {
     my ($setenv, $unsetenv) = (0, 0);
 
-    if ($^O eq 'Win32') {
-	$setenv = 1;
-	$unsetenv = 1;
+    cc_gen('config/auto/env/test_setenv.in');
+    eval { cc_build(); };
+    unless ($@ || cc_run() !~ /ok/) {
+        $setenv = 1;
     }
-    else {
-	cc_gen('config/auto/env/test_setenv.in');
-	eval { cc_build(); };
-	unless ($@ || cc_run() !~ /ok/) {
-	    $setenv = 1;
-	}
-	cc_clean();
-	cc_gen('config/auto/env/test_unsetenv.in');
-	eval { cc_build(); };
-	unless ($@ || cc_run() !~ /ok/) {
-	    $unsetenv = 1;
-	}
-	cc_clean();
+    cc_clean();
+    cc_gen('config/auto/env/test_unsetenv.in');
+    eval { cc_build(); };
+    unless ($@ || cc_run() !~ /ok/) {
+        $unsetenv = 1;
     }
+    cc_clean();
 
     Configure::Data->set(
 	setenv => $setenv,
