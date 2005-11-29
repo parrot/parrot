028e16f6c5bb5db973a37efd2ce550b5950c8e9c
Chip Salzenberg <chip@pobox.com>
2005-11-29 00:51:25 +0000

    Update i386 public interface header, completing r10231.
    git-svn-id: https://svn.parrot.org/parrot/trunk@10233 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/platform_interface.h
+++ config/gen/platform/platform_interface.h
@@ -46,14 +46,14 @@ void* Parrot_memcpy_aligned(void*, void*, size_t);
 
 #if defined(PARROT_HAS_I386_SSE)
 
-typedef void* (*Parrot_memcpy_func_t)(void *dest, void *src, size_t);
+typedef void* (*Parrot_memcpy_func_t)(void *dest, const void *src, size_t);
 extern Parrot_memcpy_func_t Parrot_memcpy_aligned_sse;
 
 #  define Parrot_memcpy_aligned(d,s,l) Parrot_memcpy_aligned_sse(d,s,l)
 
 #elif defined(PARROT_HAS_I386_MMX)
 
-typedef void* (*Parrot_memcpy_func_t)(void *dest, void *src, size_t);
+typedef void* (*Parrot_memcpy_func_t)(void *dest, const void *src, size_t);
 extern Parrot_memcpy_func_t Parrot_memcpy_aligned_mmx;
 
 #  define Parrot_memcpy_aligned(d,s,l) Parrot_memcpy_aligned_mmx(d,s,l)
