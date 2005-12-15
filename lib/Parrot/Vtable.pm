986babd2ae21d0f317ed244cbbbeaff961784211
Jerry Gay <Jerry.Gay@gmail.com>
2005-12-15 21:33:32 +0000

    #37905: [TODO] move *.pl out of src/ * move src/classes/genclass.pl to tools/dev/gen_class.pl * cleaned up code, modified documentation
    git-svn-id: https://svn.parrot.org/parrot/trunk@10541 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Vtable.pm
+++ lib/Parrot/Vtable.pm
@@ -13,7 +13,7 @@ Parrot::Vtable - Functions for manipulating vtables
 
 C<Parrot::Vtable> provides a collection of functions for manipulating
 PMC vtables. It is used by F<tools/build/jit2h.pl>, F<tools/build/pmc2c.pl>,
-F<tools/build/vtable_h.pl>, F<src/classes/genclass.pl>, and
+F<tools/build/vtable_h.pl>, F<tools/dev/gen_class.pl>, and
 F<src/classes/null.pl>.
 
 =head2 Functions
@@ -401,7 +401,7 @@ sub parse_params
 
 =item F<tools/build/vtable_h.pl>
 
-=item F<src/classes/genclass.pl>
+=item F<tools/dev/gen_class.pl>
 
 =item F<src/classes/null.pl>
 
---
+++ tools/dev/gen_class.pl
@@ -0,0 +1,82 @@
+#! perl
+# Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+# $Id: genclass.pl 10472 2005-12-12 22:12:28Z particle $
+
+=head1 NAME
+
+tools/dev/gen_class.pl - Create a template PMC file
+
+=head1 SYNOPSIS
+
+	% perl tools/dev/gen_class.pl Foo > src/classes/Foo.pmc
+
+=head1 DESCRIPTION
+
+Use this script to generate a template PMC file with stubs for all the
+methods you need to fill in. See F<docs/vtables.pod> for more
+information on adding a new PMC to Parrot.
+
+To see what a minimal PMC looks like, create a PMC template and compile
+it to C.
+
+	% perl tools/dev/gen_class.pl Foo > src/classes/foo.pmc
+	% perl tools/build/pmc2c.pl -c src/classes/foo.pmc
+
+=head1 SEE ALSO
+
+F<tools/build/pmc2c.pl>, F<docs/vtables.pod>.
+
+=cut
+
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
+use Parrot::Vtable;
+
+
+my $vtbl = parse_vtable("$FindBin::Bin/../../vtable.tbl");
+
+my $classname = shift
+	or die "No classname given!\n";
+
+
+## emit file header
+print <<EOF;
+/* ${classname}.pmc
+ *  Copyright: 2001-2003 The Perl Foundation.  All Rights Reserved.
+ *  CVS Info
+ *     \$Id\$
+ *  Overview:
+ *     These are the vtable functions for the $classname base class
+ *  Data Structure and Algorithms:
+ *  History:
+ *  Notes:
+ *     Please remove unneeded entries.
+ *  References:
+ */
+
+#include "parrot/parrot.h"
+
+pmclass $classname {
+
+EOF
+
+## emit method bodies
+for (@$vtbl) {
+	my ($retval, $methname, $args) = @$_;
+	if ($methname eq 'type' || $methname eq 'name' || $methname =~ /prop/) {
+		# default.pmc handles these
+		next;
+	}
+
+	print "    $retval $methname ($args) {\n";
+	if($retval ne 'void') {
+		print "        return ($retval)0;\n";
+	}
+	print "    }\n\n";
+}
+
+## emit file footer
+print "}\n";
