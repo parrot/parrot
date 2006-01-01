dc2c536ee12a19884f0327bf9b4c7790b4615a7a
Joshua Hoblitt <jhoblitt@cpan.org>
2006-01-01 23:50:21 +0000

    update to r10839: experimental patch to force atan2() to use IEEE behavior on openbsd, solaris, & cygwin
    git-svn-id: https://svn.parrot.org/parrot/trunk@10843 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/openbsd/math.c
+++ config/gen/platform/openbsd/math.c
@@ -9,3 +9,36 @@
 #include <math.h>
 
 _LIB_VERSION_TYPE _LIB_VERSION = _IEEE_;
+
+/*
+ * return true if the Numval has a negative sign
+ */
+#if DOUBLE_SIZE == 2 * INT_SIZE
+int
+Parrot_signbit(double x)
+{
+   union {
+       double d;
+       int i[2];
+   } u;
+   u.d = x;
+#if PARROT_BIGENDIAN
+   return u.i[0] < 0;
+#else
+   return u.i[1] < 0;
+#endif
+}
+#endif
+
+#if NUMVAL_SIZE == 12 && DOUBLE_SIZE == 3 * INT_SIZE && PARROT_LITTLE_ENDIAN
+int
+Parrot_signbit_l(long double x)
+{
+   union {
+       long double d;
+       int i[3];
+   } u;
+   u.d = x;
+   return u.i[2] < 0;
+}
+#endif
