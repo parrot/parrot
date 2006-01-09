402ca5f499556be7bab5fb79793f25a1f63f8a9a
Joshua Hoblitt <jhoblitt@cpan.org>
2006-01-09 00:07:57 +0000

    complete Pod overhaul
    git-svn-id: https://svn.parrot.org/parrot/trunk@11005 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Configure/Step/Base.pm
+++ lib/Parrot/Configure/Step/Base.pm
@@ -1,20 +1,46 @@
 # Copyright: 2001-2005 The Perl Foundation.  All Rights Reserved.
 # $Id$
 
+=pod
+
 =head1 NAME
 
 Parrot::Configure::Step::Base - Configuration Step Base Class
+
+=head1 SYNOPSIS
+    
+    use base qw(Parrot::Configure::Step::Base);
 
 =head1 DESCRIPTION
 
 The C<Parrot::Configure::Step::Base> module contains utility methods that
 should be inherited by all configuration steps.
 
+=head1 USAGE
+
+=head2 Import Parameters
+
+This module accepts no arguments to it's C<import> method and exports no
+I<symbols>.
+
 =cut
 
 package Parrot::Configure::Step::Base;
 
 use strict;
+
+=head2 Methods
+
+=head3 Class Methods
+
+=over 4
+
+=item * C<description()>
+
+Accepts no arguments and returns the value of C<$description> from the
+inheriting classes namespace.
+
+=cut
 
 sub description
 {
@@ -25,6 +51,13 @@ sub description
     }
 }
 
+=item * C<result()>
+
+Accepts no arguments and returns the value of C<$result> from the inheriting
+classes namespace.
+
+=cut
+
 sub result
 {
     my $class = shift;
@@ -34,6 +67,13 @@ sub result
     }
 }
 
+=item * C<args()>
+
+Accepts no arguments and returns the value of C<@args> from the inheriting
+classes namespace.
+
+=cut
+
 sub args
 {
     my $class = shift;
@@ -42,5 +82,18 @@ sub args
         @{$class . "::args"}
     }
 }
+
+=back
+
+=head1 AUTHOR
+
+Joshua Hoblitt C<jhoblitt@cpan.org>
+
+=head1 SEE ALSO
+
+F<docs/configuration.pod>, L<Parrot::Configure>, L<Parrot::Configure::Data>,
+L<Parrot::Configure::Step>
+
+=cut
 
 1;
