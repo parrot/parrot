80a56ae8c7ad0294969c2fe2e863b324e4062b99
Joshua Isom <loneowl@ritalin.shout.net>
2006-01-15 18:04:01 +0000

    Undo the last commit for freebsd (wrong problem).
    git-svn-id: https://svn.parrot.org/parrot/trunk@11197 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/freebsd.pm
+++ config/init/hints/freebsd.pm
@@ -33,7 +33,7 @@ sub runstep
         rpath                   => '-Wl,-R',
 
         has_dynamic_linking     => 1,
-        parrot_is_shared        => (`uname -r` !~ /5\.4/ ? 1 : 0),
+        parrot_is_shared        => 1,
         libparrot_shared        => 'libparrot$(SHARE_EXT).$(SOVERSION)',
         libparrot_shared_alias  => 'libparrot$(SHARE_EXT)',
         libparrot_soname        => '-Wl,-soname=libparrot$(SHARE_EXT).$(SOVERSION)',
