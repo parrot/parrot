2f41b741eb194e0faae1ddbed4b62bf37796cacc
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2006-01-16 19:13:20 +0000

    LANGUAGES.STATUS is now LANGUAGES.STATUS.pod
    git-svn-id: https://svn.parrot.org/parrot/trunk@11218 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Docs/Section/Languages.pm
+++ lib/Parrot/Docs/Section/Languages.pm
@@ -7,7 +7,7 @@ Parrot::Docs::Section::Languages - Languages documentation section
 
 =head1 SYNOPSIS
 
-	use Parrot::Docs::Section::Languages;
+        use Parrot::Docs::Section::Languages;
 
 =head1 DESCRIPTION
 
@@ -34,37 +34,37 @@ Returns a new section.
 
 sub new
 {
-	my $self = shift;
-	
-	return $self->SUPER::new(
-		'Language Implementations', 'languages.html', '',
-		$self->new_item('', 'languages/LANGUAGES.STATUS'),
-		$self->new_section('Perl6', 'perl6.html', '', 'languages/perl6'),
-		$self->new_section('Jako', 'jako.html', '', 'languages/jako'),
-		$self->new_section('M4', 'm4.html', '', 'languages/m4'),
-		$self->new_section('BASIC', 'basic.html', '',
-			$self->new_section('BASIC Interpreter', 'basic_interpreter.html', 
-				'', 'languages/BASIC/interpreter'),
-			$self->new_section('BASIC Compiler', 'basic_compiler.html', 
-				'', 'languages/BASIC/compiler'),
-		),
-		$self->new_section('Regex', 'regex.html', '', 'languages/regex'),
-		$self->new_section('Cola', 'cola.html', '', 'languages/cola'),
-		$self->new_section('TCL', 'tcl.html', '', 'languages/tcl'),
-		$self->new_section('Scheme', 'scheme.html', '', 'languages/scheme'),
-		$self->new_section('URM', 'urm.html', '', 'languages/urm'),
-		$self->new_section('Ruby', 'ruby.html', '', 'languages/ruby'),
-		$self->new_section('Language Conversion', 'conversion.html', '', 
-			'languages/converter.pl', 'languages/conversion'),
-		$self->new_section('miniperl', 'miniperl.html', '', 'languages/miniperl'),
-		$self->new_section('Befunge', 'befunge.html', '', 'languages/befunge'),
-		$self->new_section('Forth', 'forth.html', '', 'languages/forth'),
-		$self->new_section('BF', 'bf.html', '', 'languages/bf'),
-		$self->new_section('Ook!', 'ook.html', '', 'languages/ook'),
-		$self->new_section('Python', 'python.html', '', 'languages/python'),
-		$self->new_section('PASM', 'pasm.html', '', 'languages/parrot_compiler'),
-		$self->new_section('Lua', 'lua.html', '', 'languages/lua'),
-	);
+   my $self = shift;
+        
+   return $self->SUPER::new(
+              'Language Implementations', 'languages.html', '',
+              $self->new_item('', 'languages/LANGUAGES.STATUS.pod'),
+              $self->new_section('Perl6', 'perl6.html', '', 'languages/perl6'),
+              $self->new_section('Jako', 'jako.html', '', 'languages/jako'),
+              $self->new_section('M4', 'm4.html', '', 'languages/m4'),
+              $self->new_section('BASIC', 'basic.html', '',
+                  $self->new_section('BASIC Interpreter', 'basic_interpreter.html', 
+                                     '', 'languages/BASIC/interpreter'),
+                  $self->new_section('BASIC Compiler', 'basic_compiler.html', 
+                                     '', 'languages/BASIC/compiler'),
+                                ),
+              $self->new_section('Regex', 'regex.html', '', 'languages/regex'),
+              $self->new_section('Cola', 'cola.html', '', 'languages/cola'),
+              $self->new_section('TCL', 'tcl.html', '', 'languages/tcl'),
+              $self->new_section('Scheme', 'scheme.html', '', 'languages/scheme'),
+              $self->new_section('URM', 'urm.html', '', 'languages/urm'),
+              $self->new_section('Ruby', 'ruby.html', '', 'languages/ruby'),
+              $self->new_section('Language Conversion', 'conversion.html', '', 
+                                 'languages/converter.pl', 'languages/conversion'),
+              $self->new_section('miniperl', 'miniperl.html', '', 'languages/miniperl'),
+              $self->new_section('Befunge', 'befunge.html', '', 'languages/befunge'),
+              $self->new_section('Forth', 'forth.html', '', 'languages/forth'),
+              $self->new_section('BF', 'bf.html', '', 'languages/bf'),
+              $self->new_section('Ook!', 'ook.html', '', 'languages/ook'),
+              $self->new_section('Python', 'python.html', '', 'languages/python'),
+              $self->new_section('PASM', 'pasm.html', '', 'languages/parrot_compiler'),
+              $self->new_section('Lua', 'lua.html', '', 'languages/lua'),
+        );
 }
 
 =back
