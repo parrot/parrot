829933cd474ccd3531e1b8964951ffcd421a4735
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2006-03-04 10:06:22 +0000

    #38670: [NEW] Support for NetBSD Added support for NetBSD. Thanks to Steve Peters
    git-svn-id: https://svn.parrot.org/parrot/trunk@11785 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ config/init/hints/netbsd.pm
@@ -0,0 +1,25 @@
+# Copyright: 2006 The Perl Foundation.  All Rights Reserved.
+# $Id$
+
+package init::hints::netbsd;
+
+use strict;
+
+sub runstep
+{
+    my ($self, $conf) = @_;
+
+    my $ccflags = $conf->data->get('ccflags');
+    if ($ccflags !~ /-pthread/) {
+        $ccflags .= ' -pthread';
+    }
+    $conf->data->set(ccflags => $ccflags);
+
+    my $libs = $conf->data->get('libs');
+    if ($libs !~ /-lpthread/) {
+        $libs .= ' -lpthread';
+    }
+    $conf->data->set(libs => $libs);
+}
+
+1;
