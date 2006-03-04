275244321a2c90f7a876cf82da443b2689efa29e
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2006-03-04 16:41:43 +0000

    docs: Tools.pm - detabify - make docs for mk_manifest_and_skip.pl - jit2h.pl is now jit2c.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@11789 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Docs/Section/Tools.pm
+++ lib/Parrot/Docs/Section/Tools.pm
@@ -1,4 +1,4 @@
-# Copyright: 2004 The Perl Foundation.  All Rights Reserved.
+# Copyright: 2006 The Perl Foundation.  All Rights Reserved.
 # $Id$
 
 =head1 NAME
@@ -7,7 +7,7 @@ Parrot::Docs::Section::Tools - Tools documentation section
 
 =head1 SYNOPSIS
 
-	use Parrot::Docs::Section::Tools;
+    use Parrot::Docs::Section::Tools;
 
 =head1 DESCRIPTION
 
@@ -22,9 +22,10 @@ A documentation section describing Parrot tools.
 package Parrot::Docs::Section::Tools;
 
 use strict;
+use warnings;
 
 use Parrot::Docs::Section;
-@Parrot::Docs::Section::Tools::ISA = qw(Parrot::Docs::Section);
+our @ISA = qw(Parrot::Docs::Section);
 
 use Parrot::Docs::Item;
 
@@ -36,70 +37,71 @@ Returns a new section.
 
 sub new
 {
-	my $self = shift;
-	
-	return $self->SUPER::new(
-		'Tools', 'tools.html', '',
-		$self->new_group('Configuration', '',
-			$self->new_item('', 'tools/dev/as2c.pl'),
-			$self->new_item('', 'tools/dev/cc_flags.pl'),
-			$self->new_item('', 'tools/build/nativecall.pl'),
-			$self->new_item('', 'tools/build/jit2h.pl'),
-			$self->new_item('', 'tools/build/vtable_h.pl'),
-			$self->new_item('', 'tools/build/vtable_extend.pl'),
-		),
-		$self->new_group('Bytecode', '',
-			$self->new_item('', 'tools/build/pbc2c.pl'),
-			$self->new_item('', 'tools/dev/pbc_header.pl'),
-		),
-		$self->new_group('QA', '',
-			$self->new_item('', 'parrotbug'),
-			$self->new_item('', 'tools/dev/check_source_standards.pl'),
-			$self->new_item('', 'tools/docs/pod_errors.pl'),
-			$self->new_item('', 'tools/dev/manicheck.pl'),
-			$self->new_item('', 'tools/dev/run_indent.pl'),
-		),
-		$self->new_group('Documentation', '',
-			$self->new_item('', 'tools/dev/extract_file_descriptions.pl'),
-			$self->new_item('', 'tools/dev/lib_deps.pl'),
-			$self->new_item('', 'tools/dev/parrot_coverage.pl'),
-			$self->new_item('', 'tools/docs/write_docs.pl'),
-		),
-		$self->new_group('Building', '',
-			$self->new_item('', 'tools/build/c2str.pl'),
-			$self->new_item('', 'tools/build/dynoplibs.pl'),
-			$self->new_item('', 'tools/build/dynpmc.pl'),
-			$self->new_item('', 'tools/build/fingerprint_c.pl'),
-			$self->new_item('', 'tools/build/ops2c.pl'),
-			$self->new_item('', 'tools/build/ops2pm.pl'),
-			$self->new_item('', 'tools/build/parrot_config_c.pl'),
-			$self->new_item('', 'tools/build/pmc2c.pl'),
-			$self->new_item('', 'tools/build/revision_c.pl'),
-			$self->new_item('', 'tools/dev/gen_manifest_skip.pl'),
-			$self->new_item('', 'tools/dev/install_files.pl'),
-			$self->new_item('', 'tools/dev/mk_manifests.pl'),
-			$self->new_item('', 'tools/dev/rebuild_miniparrot.pl'),
-			$self->new_item('', 'tools/dev/symlink.pl'),
-		),
-		$self->new_group('Testing', '',
-			$self->new_item('', 'tools/dev/mk_native_pbc'),
-		),
-		$self->new_group('Benchmarking', '',
-			$self->new_item('', 'tools/dev/bench_op.pir'),
-			$self->new_item('', 'tools/dev/parrotbench.pl'),
-		),
-		$self->new_group('Utilities', '',
-			$self->new_item('', 'tools/build/list_unjitted.pl'),
-			$self->new_item('', 'tools/dev/gen_class.pl'),
-			$self->new_item('', 'tools/dev/nm.pl'),
-			$self->new_item('', 'tools/util/ncidef2pasm.pl'),
-			$self->new_item('', 'tools/util/smokeserv-README.pod'),
-		),
-		$self->new_group('Possibly obsolete', '',
-			$self->new_item('', 'tools/dev/gen_charset_tables.pl'),
-			$self->new_item('', 'tools/dev/rebuild_miniparrot.pl'),
-		),
-	);
+    my $self = shift;
+    
+    return $self->SUPER::new(
+        'Tools', 'tools.html', '',
+        $self->new_group('Configuration', '',
+            $self->new_item('', 'tools/dev/as2c.pl'),
+            $self->new_item('', 'tools/dev/cc_flags.pl'),
+            $self->new_item('', 'tools/build/nativecall.pl'),
+            $self->new_item('', 'tools/build/jit2c.pl'),
+            $self->new_item('', 'tools/build/vtable_h.pl'),
+            $self->new_item('', 'tools/build/vtable_extend.pl'),
+        ),
+        $self->new_group('Bytecode', '',
+            $self->new_item('', 'tools/build/pbc2c.pl'),
+            $self->new_item('', 'tools/dev/pbc_header.pl'),
+        ),
+        $self->new_group('QA', '',
+            $self->new_item('', 'parrotbug'),
+            $self->new_item('', 'tools/dev/check_source_standards.pl'),
+            $self->new_item('', 'tools/docs/pod_errors.pl'),
+            $self->new_item('', 'tools/dev/manicheck.pl'),
+            $self->new_item('', 'tools/dev/run_indent.pl'),
+        ),
+        $self->new_group('Documentation', '',
+            $self->new_item('', 'tools/dev/extract_file_descriptions.pl'),
+            $self->new_item('', 'tools/dev/lib_deps.pl'),
+            $self->new_item('', 'tools/dev/parrot_coverage.pl'),
+            $self->new_item('', 'tools/docs/write_docs.pl'),
+        ),
+        $self->new_group('Building', '',
+            $self->new_item('', 'tools/build/c2str.pl'),
+            $self->new_item('', 'tools/build/dynoplibs.pl'),
+            $self->new_item('', 'tools/build/dynpmc.pl'),
+            $self->new_item('', 'tools/build/fingerprint_c.pl'),
+            $self->new_item('', 'tools/build/ops2c.pl'),
+            $self->new_item('', 'tools/build/ops2pm.pl'),
+            $self->new_item('', 'tools/build/parrot_config_c.pl'),
+            $self->new_item('', 'tools/build/pmc2c.pl'),
+            $self->new_item('', 'tools/build/revision_c.pl'),
+            $self->new_item('', 'tools/dev/gen_manifest_skip.pl'),
+            $self->new_item('', 'tools/dev/install_files.pl'),
+            $self->new_item('', 'tools/dev/mk_manifests.pl'),
+            $self->new_item('', 'tools/dev/mk_manifest_and_skip.pl'),
+            $self->new_item('', 'tools/dev/rebuild_miniparrot.pl'),
+            $self->new_item('', 'tools/dev/symlink.pl'),
+        ),
+        $self->new_group('Testing', '',
+            $self->new_item('', 'tools/dev/mk_native_pbc'),
+        ),
+        $self->new_group('Benchmarking', '',
+            $self->new_item('', 'tools/dev/bench_op.pir'),
+            $self->new_item('', 'tools/dev/parrotbench.pl'),
+        ),
+        $self->new_group('Utilities', '',
+            $self->new_item('', 'tools/build/list_unjitted.pl'),
+            $self->new_item('', 'tools/dev/gen_class.pl'),
+            $self->new_item('', 'tools/dev/nm.pl'),
+            $self->new_item('', 'tools/util/ncidef2pasm.pl'),
+            $self->new_item('', 'tools/util/smokeserv-README.pod'),
+        ),
+        $self->new_group('Possibly obsolete', '',
+            $self->new_item('', 'tools/dev/gen_charset_tables.pl'),
+            $self->new_item('', 'tools/dev/rebuild_miniparrot.pl'),
+        ),
+    );
 }
 
 =back
