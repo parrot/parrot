07de917645abfd880be14c7053c4819535399841
Joshua Isom <loneowl@ritalin.shout.net>
2006-03-05 03:35:47 +0000

    Quick attempt to get openbsd building on ppc
    git-svn-id: https://svn.parrot.org/parrot/trunk@11794 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/openbsd.pm
+++ config/init/hints/openbsd.pm
@@ -18,8 +18,13 @@ sub runstep
     my $libs = $conf->data->get('libs');
     if ($libs !~ /-lpthread/) {
         $libs .= ' -lpthread';
-    }
-    $conf->data->set(libs => $libs);
+	}
+	$conf->data->set(libs => $libs);
+
+	if ((split('-', $Config{archname}))[0] eq 'powerpc') {
+		$conf->data->set(as => 'as -mregnames');
+	}
+
 }
 
 1;
