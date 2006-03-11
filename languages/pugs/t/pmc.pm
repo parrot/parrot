daf99cb29fa2160666e02ae67ae0a3a6b0282c7d
Audrey Tang <audreyt@audreyt.org>
2006-03-11 22:01:15 +0000

    * Pugs: scaffolding for basic types involved in bootstrapping:     Any, Bit, Bool, int, Num, Str, Code, Mapping, Module, Tuple, Undef   only the "Mapping" is non-canonical -- it's referred variously as   ArgList, Sigs, List, NamedPairs, etc.
    git-svn-id: https://svn.parrot.org/parrot/trunk@11869 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ languages/pugs/t/pmc.pm
@@ -0,0 +1,45 @@
+# Pugs-specific PMC testing
+# Copyright: 2005-2006 The Perl Foundation.  All Rights Reserved.
+# $Id: /mirror/trunk/languages/lua/t/pmc/boolean.t 11586 2006-02-16T17:44:54.559622Z fperrad  $
+
+package t::pmc;
+use Parrot::Test 'no_plan';
+use Test::More;
+
+sub import {
+    my $class = shift;
+
+    my (undef, $file, undef) = caller();
+    $file =~ /(\w+)\.t$/ or die "malformed file: $file";
+    my $type = "Pugs\u$1";
+
+    main::pir_output_is(<< 'CODE', << 'OUTPUT', "check sanity for pugs_group");
+.sub _main
+    loadlib $P1, "pugs_group"
+    $I0 = defined $P1 
+    if $I0 goto ok
+    print "not "
+ok:
+    print "ok\n"
+.end
+CODE
+ok
+OUTPUT
+
+main::pir_output_is(<< "CODE", << "OUTPUT", "check sanity for creation");
+.HLL "Perl6", "pugs_group"
+.sub _main
+    .local pmc pmc1
+    .local string typ
+    pmc1 = new .$type
+    typ = typeof pmc1
+    print typ
+    print "\\n"
+.end
+CODE
+$type
+OUTPUT
+
+}
+
+1;
