fb0081276412d81c739c499104e404226686841c
Leopold Toetsch <lt@toetsch.at>
2006-03-24 14:13:06 +0000

    remove -mcpu=xx when --optimize
    git-svn-id: https://svn.parrot.org/parrot/trunk@12004 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/optimize.pm
+++ config/init/optimize.pm
@@ -40,9 +40,13 @@ sub runstep
         $conf->data->add(' ', ccflags => "-DDISABLE_GC_DEBUG=1 -DNDEBUG");
         if ($optimize eq "1") {
             # use perl5's value
-            $conf->data->add(' ', ccflags => $Config{optimize});
+	    # gcc 4.1 doesn't like -mcpu=xx, i.e. it's deprecated
+	    # XXX do we know compiler (version) already?
+	    my $opts = $Config{optimize};
+	    $opts =~ s/-mcpu=\S+//;
+            $conf->data->add(' ', ccflags => $opts);
             # record what optimization was enabled
-            $conf->data->set(optimize => $Config{optimize});
+            $conf->data->set(optimize => $opts);
         } else {
             # use what was passed to --optimize on the CLI
             $conf->data->add(' ', ccflags => $optimize);
