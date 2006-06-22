6b05047221da5bed4973973edfbb9411b9c4ba5e
Jonathan Worthington <jnthn@jnthn.net>
2006-06-22 16:18:58 +0000

    Work around versioning oddness that causes 64-bit integer type detection to fail on Win32.
    git-svn-id: https://svn.parrot.org/parrot/trunk@13003 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/sizes.pm
+++ config/auto/sizes.pm
@@ -129,6 +129,7 @@ END
             cc_gen('config/auto/sizes/test2_c.in');
             cc_build();
             %hugeintval = eval cc_run();
+            cc_clean();
         };
 
         # clear int8_t on error
@@ -138,7 +139,6 @@ END
         }
 
         if ($hugeintval{hugeintvalsize} > $intvalsize) {
-
             # We found something bigger than intval.
             $conf->data->set(%hugeintval);
             last;
