a9a67e6bb8d16c661d4be05373c0c28e7f1caa80
Audrey Tang <audreyt@audreyt.org>
2006-07-16 16:51:05 +0000

    * Parrot::Revision - support for svk-based numbering scheme.
    git-svn-id: https://svn.parrot.org/parrot/trunk@13320 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Revision.pm
+++ lib/Parrot/Revision.pm
@@ -27,18 +27,33 @@ use warnings;
 our $svn_entries = undef;
 
 sub __get_revision {
-    return 0 unless (-e ".svn/entries");
+    return 0 unless (-e 'DEVELOPING');
     $svn_entries = ".svn/entries";
 
+    my $revision;
     # code taken from pugs/util/version_h.pl rev 859
     if (-r $svn_entries) {
         open my $FH, '<', $svn_entries or die $!;
         while (<$FH>) {
             /^ *committed-rev=.(\d+)./ or next;
-            return $1;
+            $revision = $1;
         }
     }
-    return 0;
+    elsif (my @info = qx/svk info/ and $? == 0) {
+        if (my ($line) = grep /(?:file|svn|https?)\b/, @info) {
+            ($revision) = $line =~ / (\d+)$/;
+        } elsif (my ($source_line) = grep /^(Copied|Merged) From/, @info) {
+            if (my ($source_depot) = $source_line =~ /From: (.*?), Rev\. \d+/) {
+                $source_depot = '/'.$source_depot; # convert /svk/trunk to //svk/trunk
+                if (my @info = qx/svk info $source_depot/ and $? == 0) {
+                    if (my ($line) = grep /(?:file|svn|https?)\b/, @info) {
+                        ($revision) = $line =~ / (\d+)$/;
+                    }
+                }
+            }
+        }
+    }
+    return($revision || 0);
 }
 
 our $current = __get_revision();
