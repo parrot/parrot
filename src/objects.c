526a1ccd4bcf6d8fbb1c2503170e692f8529a794
Chip Salzenberg <chip@pobox.com>
2006-07-25 00:40:21 +0000

    fix comment, no functional change
    git-svn-id: https://svn.parrot.org/parrot/trunk@13528 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/objects.c
+++ src/objects.c
@@ -208,7 +208,7 @@ create_deleg_pmc_vtable(Interp *interpreter, PMC *class,
     union {
         const void * __c_ptr;
         void * __ptr;
-    } __ptr_u;                  /* FIXME - THIS SHOULD NOT BE HERE */
+    } __ptr_u; /* for const_cast */
 
     PMC * const vtable_pmc = get_attrib_num((SLOTTYPE*)PMC_data(class), PCD_OBJECT_VTABLE);
     VTABLE * const vtable           = PMC_struct_val(vtable_pmc);
