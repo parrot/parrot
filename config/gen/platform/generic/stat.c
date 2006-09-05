7d4267f03b51a998777832bdfa8ded727f8075ae
Leopold Toetsch <lt@toetsch.at>
2006-09-05 11:02:13 +0000

    stat.c - remove c&p programming style
    git-svn-id: https://svn.parrot.org/parrot/trunk@14412 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/generic/stat.c
+++ config/gen/platform/generic/stat.c
@@ -14,12 +14,82 @@ Parrot_stat_info_pmc(Interp* interpreter, STRING *filename, INTVAL thing)
     return NULL;
 }
 
+static INTVAL
+stat_common(Interp *interpreter, struct stat *statbuf, 
+	INTVAL thing, int status)
+{
+    INTVAL result = -1;
+
+    if (thing == STAT_EXISTS)
+	return status == 0;
+    if (status == -1) {
+	const char *err = strerror(errno);
+	real_exception(interpreter, NULL, E_IOError,
+		"stat failed: %s", err);
+    }
+
+    switch (thing) {
+	case STAT_FILESIZE:
+	    result = statbuf->st_size;
+	    break;
+	case STAT_ISDIR:
+	    result = S_ISDIR(statbuf->st_mode);
+	    break;
+	case STAT_ISDEV:
+	    result = S_ISCHR(statbuf->st_mode) || S_ISBLK(statbuf->st_mode);
+	    break;
+	case STAT_CREATETIME:
+	    result = -1;
+	    break;
+	case STAT_ACCESSTIME:
+	    result = statbuf->st_atime;
+	    break;
+	case STAT_MODIFYTIME:
+	    result = statbuf->st_mtime;
+	    break;
+	case STAT_CHANGETIME:
+	    result = statbuf->st_ctime;
+	    break;
+	case STAT_BACKUPTIME:
+	    result = -1;
+	    break;
+	case STAT_UID:
+	    result = statbuf->st_uid;
+	    break;
+	case STAT_GID:
+	    result = statbuf->st_gid;
+	    break;
+	case STAT_PLATFORM_DEV:
+	    result = statbuf->st_dev;
+	    break;
+	case STAT_PLATFORM_INODE:
+	    result = statbuf->st_ino;
+	    break;
+	case STAT_PLATFORM_MODE:
+	    result = statbuf->st_mode;
+	    break;
+	case STAT_PLATFORM_NLINKS:
+	    result = statbuf->st_nlink;
+	    break;
+	case STAT_PLATFORM_DEVTYPE:
+	    result = statbuf->st_rdev;
+	    break;
+	case STAT_PLATFORM_BLOCKSIZE:
+	    result = statbuf->st_blksize;
+	    break;
+	case STAT_PLATFORM_BLOCKS:
+	    result = statbuf->st_blocks;
+	    break;
+    }
+
+    return result;
+}
+
 INTVAL
 Parrot_stat_info_intval(Interp* interpreter, STRING *file, INTVAL thing)
 {
     struct stat statbuf;
     char *filename;
-    INTVAL result = -1;
     int status;
 
     /* Get the name of the file as something we can use */
@@ -27,144 +97,19 @@ Parrot_stat_info_intval(Interp* interpreter, STRING *file, INTVAL thing)
 
     /* Everything needs the result of stat, so just go do it */
     status = stat(filename, &statbuf);
-    if (thing == STAT_EXISTS)
-	return status == 0;
-    if (status == -1) {
-	const char *err = strerror(errno);
-	real_exception(interpreter, NULL, E_IOError,
-		"stat failed: %s", err);
-    }
-
-    switch (thing) {
-	case STAT_FILESIZE:
-	    result = statbuf.st_size;
-	    break;
-	case STAT_ISDIR:
-	    result = S_ISDIR(statbuf.st_mode);
-	    break;
-	case STAT_ISDEV:
-	    result = S_ISCHR(statbuf.st_mode) || S_ISBLK(statbuf.st_mode);
-	    break;
-	case STAT_CREATETIME:
-	    result = -1;
-	    break;
-	case STAT_ACCESSTIME:
-	    result = statbuf.st_atime;
-	    break;
-	case STAT_MODIFYTIME:
-	    result = statbuf.st_mtime;
-	    break;
-	case STAT_CHANGETIME:
-	    result = statbuf.st_ctime;
-	    break;
-	case STAT_BACKUPTIME:
-	    result = -1;
-	    break;
-	case STAT_UID:
-	    result = statbuf.st_uid;
-	    break;
-	case STAT_GID:
-	    result = statbuf.st_gid;
-	    break;
-	case STAT_PLATFORM_DEV:
-	    result = statbuf.st_dev;
-	    break;
-	case STAT_PLATFORM_INODE:
-	    result = statbuf.st_ino;
-	    break;
-	case STAT_PLATFORM_MODE:
-	    result = statbuf.st_mode;
-	    break;
-	case STAT_PLATFORM_NLINKS:
-	    result = statbuf.st_nlink;
-	    break;
-	case STAT_PLATFORM_DEVTYPE:
-	    result = statbuf.st_rdev;
-	    break;
-	case STAT_PLATFORM_BLOCKSIZE:
-	    result = statbuf.st_blksize;
-	    break;
-	case STAT_PLATFORM_BLOCKS:
-	    result = statbuf.st_blocks;
-	    break;
-    }
-
     string_cstring_free(filename);
-    return result;
+    return stat_common(interpreter, &statbuf, thing, status);
 }
 
 INTVAL
 Parrot_fstat_info_intval(Interp* interpreter, INTVAL file, INTVAL thing)
 {
     struct stat statbuf;
-    INTVAL result = -1;
     int status;
 
     /* Everything needs the result of stat, so just go do it */
     status = fstat(file, &statbuf);
-    if (thing == STAT_EXISTS)
-	return status == 0;
-    if (status == -1) {
-	const char *err = strerror(errno);
-	real_exception(interpreter, NULL, E_IOError,
-		"stat failed: %s", err);
-    }
-
-    switch (thing) {
-	case STAT_FILESIZE:
-	    result = statbuf.st_size;
-	    break;
-	case STAT_ISDIR:
-	    result = S_ISDIR(statbuf.st_mode);
-	    break;
-	case STAT_ISDEV:
-	    result = S_ISCHR(statbuf.st_mode) || S_ISBLK(statbuf.st_mode);
-	    break;
-	case STAT_CREATETIME:
-	    result = -1;
-	    break;
-	case STAT_ACCESSTIME:
-	    result = statbuf.st_atime;
-	    break;
-	case STAT_MODIFYTIME:
-	    result = statbuf.st_mtime;
-	    break;
-	case STAT_CHANGETIME:
-	    result = statbuf.st_ctime;
-	    break;
-	case STAT_BACKUPTIME:
-	    result = -1;
-	    break;
-	case STAT_UID:
-	    result = statbuf.st_uid;
-	    break;
-	case STAT_GID:
-	    result = statbuf.st_gid;
-	    break;
-	case STAT_PLATFORM_DEV:
-	    result = statbuf.st_dev;
-	    break;
-	case STAT_PLATFORM_INODE:
-	    result = statbuf.st_ino;
-	    break;
-	case STAT_PLATFORM_MODE:
-	    result = statbuf.st_mode;
-	    break;
-	case STAT_PLATFORM_NLINKS:
-	    result = statbuf.st_nlink;
-	    break;
-	case STAT_PLATFORM_DEVTYPE:
-	    result = statbuf.st_rdev;
-	    break;
-	case STAT_PLATFORM_BLOCKSIZE:
-	    result = statbuf.st_blksize;
-	    break;
-	case STAT_PLATFORM_BLOCKS:
-	    result = statbuf.st_blocks;
-	    break;
-    }
-
-    return result;
+    return stat_common(interpreter, &statbuf, thing, status);
 }
 
 FLOATVAL
