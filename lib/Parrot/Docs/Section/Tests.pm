0989346a13d3f727458d7e6c7711e42ab78e7874
François Perrad <francois.perrad@gadz.org>
2006-09-11 14:02:34 +0000

    add some items
    git-svn-id: https://svn.parrot.org/parrot/trunk@14552 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Docs/Section/Tests.pm
+++ lib/Parrot/Docs/Section/Tests.pm
@@ -39,10 +39,19 @@ sub new
 	return $self->SUPER::new(
 		'Tests', 'tests.html', '',
 		$self->new_group('Tools', '', 't/harness'),
+		$self->new_group('Benchmarks Tests', '', 't/benchmark'),
+		$self->new_group('Coding Standards Tests', '', 't/codingstd'),
+		$self->new_group('Compilers Tests', '', 't/compilers'),
+		$self->new_group('Configure Tests', '', 't/configure'),
+		$self->new_group('Distribution Tests', '', 't/distro'),
+		$self->new_group('Documentation Tests', '', 't/doc'),
+		$self->new_group('Examples Tests', '', 't/examples'),
+		$self->new_group('Libraries Tests', '', 't/library'),
 		$self->new_group('Native Bytecode Tests', '', 't/native_pbc'),
-		$self->new_group('Ops Tests', '', 't/op'),
-		$self->new_group('PMC Tests', '', 't/pmc'),
+		$self->new_group('Ops Tests', '', 't/op', 't/dynoplibs'),
+		$self->new_group('PMC Tests', '', 't/pmc', 't/dynpmc'),
 		$self->new_group('C Source Code Tests', '', 't/src'),
+		$self->new_group('Software Transactional Memory Tests', '', 't/stm'),
 		$self->new_group('Stress Tests', '', 't/stress'),
 	);
 }
