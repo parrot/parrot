d1d12fa07e579e2c8350bb77af5516e862401021
Will "Coke" Coleda <will@coleda.com>
2006-10-01 05:14:58 +0000

    perlcritic: whitespace updates
    git-svn-id: https://svn.parrot.org/parrot/trunk@14815 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/tcl/lib/Parrot/Test/Tcl.pm
+++ languages/tcl/lib/Parrot/Test/Tcl.pm
@@ -14,92 +14,101 @@ Perhaps someone can do a better job of delegation here.
 =cut
 
 sub new {
-  return bless {};
+    return bless {};
 }
 
-
 my %language_test_map = (
-    output_is         => 'is_eq',
-    output_like       => 'like',
-    output_isnt       => 'isnt_eq'
-                        );
+    output_is   => 'is_eq',
+    output_like => 'like',
+    output_isnt => 'isnt_eq'
+);
 
 foreach my $func ( keys %language_test_map ) {
 
-  *{"Parrot::Test::Tcl::$func"} = sub ($$;$) {
+    *{"Parrot::Test::Tcl::$func"} = sub ($$;$) {
 
-    my ($self, $code, $output, $desc) = @_;
+        my ( $self, $code, $output, $desc ) = @_;
 
-    $count = $self->{builder}->current_test + 1;
+        $count = $self->{builder}->current_test + 1;
 
-    $desc = $language unless $desc;
+        $desc = $language unless $desc;
 
-    # Figure out how many levels we have to go back to get to parrot.
-    # And, conversely, how many levels we have to go down to get to
-    # the tcl binary.
+        # Figure out how many levels we have to go back to get to parrot.
+        # And, conversely, how many levels we have to go down to get to
+        # the tcl binary.
 
-    # There are basically 3 choices: run in one of:
-    #  languages
-    #  languages/tcl
-    #  languages/tcl/t
+        # There are basically 3 choices: run in one of:
+        #  languages
+        #  languages/tcl
+        #  languages/tcl/t
 
-    my $path_to_parrot = $INC{"Parrot/Config.pm"};
-    $path_to_parrot =~ s:/lib/Parrot/Config.pm$::;
-    my $dir_count = scalar(File::Spec->splitdir($path_to_parrot));
-    my $path_to_tcl;
-    if ($dir_count == 0) {
-      $path_to_tcl = File::Spec->join('languages','tcl');
-    } elsif ($dir_count == 1) {
-      $path_to_tcl = 'tcl';
-    } elsif ($dir_count == 2) {
-      $path_to_tcl = '.';
-    } elsif ($dir_count >2) {
-      $path_to_tcl = File::Spec->join(File::Spec->updir() x ($dir_count - 2));
-    }
+        my $path_to_parrot = $INC{"Parrot/Config.pm"};
+        $path_to_parrot =~ s:/lib/Parrot/Config.pm$::;
+        my $dir_count = scalar( File::Spec->splitdir($path_to_parrot) );
+        my $path_to_tcl;
+        if ( $dir_count == 0 ) {
+            $path_to_tcl = File::Spec->join( 'languages', 'tcl' );
+        }
+        elsif ( $dir_count == 1 ) {
+            $path_to_tcl = 'tcl';
+        }
+        elsif ( $dir_count == 2 ) {
+            $path_to_tcl = '.';
+        }
+        elsif ( $dir_count > 2 ) {
+            $path_to_tcl = File::Spec->join( File::Spec->updir() x ( $dir_count - 2 ) );
+        }
 
-    my $lang_f = Parrot::Test::per_test('.tcl',$count);
-    my $out_f = Parrot::Test::per_test('.out',$count);
+        my $lang_f = Parrot::Test::per_test( '.tcl', $count );
+        my $out_f  = Parrot::Test::per_test( '.out', $count );
 
-    $TEST_PROG_ARGS = $ENV{TEST_PROG_ARGS} || '';
-    my $args = $TEST_PROG_ARGS;
+        $TEST_PROG_ARGS = $ENV{TEST_PROG_ARGS} || '';
+        my $args = $TEST_PROG_ARGS;
 
-    Parrot::Test::write_code_to_file( $code, $lang_f );
+        Parrot::Test::write_code_to_file( $code, $lang_f );
 
-    my $cmd;
-    my $exit_code = 0;
-    my $pass = 0;
+        my $cmd;
+        my $exit_code = 0;
+        my $pass      = 0;
 
-    my $executable = File::Spec->join($path_to_parrot,$self->{parrot}) .
-    " $args " . File::Spec->join($path_to_tcl, 'tcl.pbc');
-    if (defined($ENV{PARROT_TCLSH})) {
-      $executable = $ENV{PARROT_TCLSH};
-    }
-    $cmd = "$executable $lang_f";
+        my $executable =
+              File::Spec->join( $path_to_parrot, $self->{parrot} )
+            . " $args "
+            . File::Spec->join( $path_to_tcl, 'tcl.pbc' );
+        if ( defined( $ENV{PARROT_TCLSH} ) ) {
+            $executable = $ENV{PARROT_TCLSH};
+        }
+        $cmd = "$executable $lang_f";
 
-    $exit_code = Parrot::Test::run_command(
-        $cmd, STDOUT => $out_f, STDERR => $out_f,
-        #CD => $self->{relpath},
-    );
+        $exit_code = Parrot::Test::run_command(
+            $cmd,
+            STDOUT => $out_f,
+            STDERR => $out_f,
 
-    unless ($pass) {
-      my $file = Parrot::Test::slurp_file($out_f);
-      my $builder_func = $language_test_map{$func};
+            #CD => $self->{relpath},
+        );
 
-      {
-        no strict 'refs';
+        unless ($pass) {
+            my $file         = Parrot::Test::slurp_file($out_f);
+            my $builder_func = $language_test_map{$func};
 
-        $pass = $self->{builder}->$builder_func( Parrot::Test::slurp_file($out_f), $output, $desc );
-      }
-      $self->{builder}->diag("'$cmd' failed with exit code $exit_code")
-      if $exit_code and not $pass;
-    }
+            {
+                no strict 'refs';
 
-    unless($ENV{POSTMORTEM}) {
-      unlink $out_f;
-    }
+                $pass =
+                    $self->{builder}
+                    ->$builder_func( Parrot::Test::slurp_file($out_f), $output, $desc );
+            }
+            $self->{builder}->diag("'$cmd' failed with exit code $exit_code")
+                if $exit_code and not $pass;
+        }
 
-    return $pass;
-  }
+        unless ( $ENV{POSTMORTEM} ) {
+            unlink $out_f;
+        }
+
+        return $pass;
+        }
 }
 
 1;
