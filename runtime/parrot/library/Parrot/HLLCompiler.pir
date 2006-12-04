parrot/parrot

commit 3dd6ab54dc228b2fe4e98a8d10a6688b14e78bf3

[HLLCompiler]: * Fix use of --target=pir for files compiled on command line.
git-svn-id: https://svn.parrot.org/parrot/trunk@15967 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

runtime/parrot/library/Parrot/HLLCompiler.pir
@@ -290,7 +290,13 @@ options are passed to the evaluator.
     close ifh
     goto iter_loop
   iter_end:
-    .return self.'eval'(code, adverbs :flat :named)
+    $P0 = self.'eval'(code, adverbs :flat :named)
+    if null $P0 goto end
+    $I0 = isa $P0, 'String'
+    if $I0 == 0 goto end
+    say $P0
+  end:
+    .return ($P0)
 
   err_infile:
     $P0 = new .Exception

commit 0415a8c61b1260512237e976b057e56aed237750

[HLLCompiler]: * Fix meaning of --output option to default to standard output if not   specified. * (Corrects pgc problem introduced in r15967.)
git-svn-id: https://svn.parrot.org/parrot/trunk@15968 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

runtime/parrot/library/Parrot/HLLCompiler.pir
@@ -290,13 +290,7 @@ options are passed to the evaluator.
     close ifh
     goto iter_loop
   iter_end:
-    $P0 = self.'eval'(code, adverbs :flat :named)
-    if null $P0 goto end
-    $I0 = isa $P0, 'String'
-    if $I0 == 0 goto end
-    say $P0
-  end:
-    .return ($P0)
+    .return self.'eval'(code, adverbs :flat :named)
 
   err_infile:
     $P0 = new .Exception
@@ -364,10 +358,10 @@ Generic method for compilers invoked from a shell command line.
 
   save_output:
     .local string output
-    output = adverbs['output']
-    unless output goto end
     .local pmc ofh
     ofh = getstdout
+    output = adverbs['output']
+    if output == '' goto save_output_1
     if output == '-' goto save_output_1
     ofh = open output, '>'
     unless ofh goto err_output