9d81923c9afe94c29e8d30519b7e4822f15a5ab8
Paul Cochrane <paultcochrane@gmail.com>
2006-12-17 18:57:56 +0000

    [urm] Tidied Perl source with perltidy.  (part 27)
    git-svn-id: https://svn.parrot.org/parrot/trunk@16169 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/urm/lib/URM/Test.pm
+++ languages/urm/lib/URM/Test.pm
@@ -15,7 +15,7 @@ require Parrot::Test;
 @ISA = qw(Exporter Test::More);
 
 sub import {
-    my( $class, $plan, @args ) = @_;
+    my ( $class, $plan, @args ) = @_;
 
     Test::More->import( $plan, @args );
 
@@ -24,67 +24,73 @@ sub import {
 
 my $count;
 
-foreach my $meth ( qw(is isnt like) ) {
+foreach my $meth (qw(is isnt like)) {
     no strict 'refs';
 
     *{"URM::Test::output_$meth"} = sub {
-        my( $lang_code, $output, $desc, @other ) = @_;
+        my ( $lang_code, $output, $desc, @other ) = @_;
 
         ++$count;
-        my( $lang_f, $pasm_f, $by_f, $out_f ) = map { # JMG
-            my $t = $0; $t =~ s/\.t$/_$count\.$_/; $t
-        } ( qw(urm pasm pbc out) ); # JMG
+        my ( $lang_f, $pasm_f, $by_f, $out_f ) = map {    # JMG
+            my $t = $0;
+            $t =~ s/\.t$/_$count\.$_/;
+            $t
+        } (qw(urm pasm pbc out));    # JMG
 
         # STDERR is written into same output file
-        open LANG, ">", "$lang_f" or die "Unable to open '$lang_f'"; # JMG
-        binmode LANG; # JMG
-        print LANG $lang_code; # JMG
-        close LANG; # JMG
+        open LANG, ">", "$lang_f" or die "Unable to open '$lang_f'";    # JMG
+        binmode LANG;                                                   # JMG
+        print LANG $lang_code;                                          # JMG
+        close LANG;                                                     # JMG
 
-        Parrot::Test::run_command( 
+        Parrot::Test::run_command(
             "$PConfig{perl} languages/urm/urmc -s languages/$lang_f",
-            CD => '..', # $self->{relpath}, 
-            STDOUT => $pasm_f, STDERR => $pasm_f,
+            CD     => '..',                                             # $self->{relpath},
+            STDOUT => $pasm_f,
+            STDERR => $pasm_f,
         );
-        Parrot::Test::run_command( 
+        Parrot::Test::run_command(
             "./parrot languages/$pasm_f @other",
-            CD => '..', # $self->{relpath}, 
-            STDOUT => $out_f, STDERR => $out_f, 
+            CD     => '..',                                             # $self->{relpath},
+            STDOUT => $out_f,
+            STDERR => $out_f,
         );
-        my $prog_output = Parrot::Test::slurp_file( "$out_f" );
+        my $prog_output = Parrot::Test::slurp_file("$out_f");
 
         @_ = ( $prog_output, $output, $desc );
+
         #goto &{"Test::More::$meth"};
-        my $ok = &{"Test::More::$meth"}( @_ );
+        my $ok = &{"Test::More::$meth"}(@_);
+
         # if( $ok ) { foreach my $meth ( $lang_f, $pasm_f, $by_f, $out_f ) { unlink $meth } } # JMG
-    }
+        }
 }
 
 1;
 
-my $urmc = "$PConfig{perl} $FindBin::RealBin$PConfig{slash}..$PConfig{slash}urmc";
+my $urmc    = "$PConfig{perl} $FindBin::RealBin$PConfig{slash}..$PConfig{slash}urmc";
 my $compile = "-c -s";
-my $run = "-s";
+my $run     = "-s";
 
 sub compile_test {
     my $file = shift;
 
-    my $ret = system ("$urmc $compile $FindBin::RealBin$PConfig{slash}$file");
+    my $ret = system("$urmc $compile $FindBin::RealBin$PConfig{slash}$file");
     if ($ret) {
         print STDERR "TEST FAILED: $file ($ret)\n";
         return;
-        }
+    }
     print "OK: $file\n";
 }
 
 sub run_test {
-    my ($file, $expect) = @_;
+    my ( $file, $expect ) = @_;
     my $ret = `$urmc $run $FindBin::RealBin$PConfig{slash}$file`;
-    if (!$ret) {
+    if ( !$ret ) {
         print STDERR "TEST FAILED: $file didn't return a value, Parrot crashed?\n";
         return;
     }
-    if ($ret != $expect) {
+    if ( $ret != $expect ) {
         print STDERR "TEST FAILED: $file (got $ret expected $expect)\n";
         return;
     }
--- languages/urm/urm-old.pl
+++ languages/urm/urm-old.pl
@@ -1,5 +1,5 @@
 #! perl
-# urm.pl 
+# urm.pl
 # 2003 (c) by Marcus Thiesen
 # Maybe you can use it for debugging
 
@@ -13,27 +13,28 @@ my $outreg;
 open PROGRAM, '<', $ARGV[0] or die "Couldn't get file";
 foreach my $line (<PROGRAM>) {
     chomp $line;
-    next if ($line =~ /^ *\#/);
-    if ($line =~ /in *\(([r\d,]+)\)/) {
+    next if ( $line =~ /^ *\#/ );
+    if ( $line =~ /in *\(([r\d,]+)\)/ ) {
         print "Werte für $1: ";
         my $input = readline(STDIN);
-        my @register = split (",", $1);
+        my @register = split( ",", $1 );
         chomp $input;
-        my @input = split(",", $input);
+        my @input = split( ",", $input );
         foreach my $reg (@register) {
             $register{$reg} = shift @input;
         }
     }
-    if ($line =~ /(r\d+)/) { $register{$1} = 0 unless defined $register{$1}}
-    if ($line =~ /out\((r\d+)/) { $outreg = $1 }
-    if ($line =~ /^\d+:(.+)/) { 
+    if ( $line =~ /(r\d+)/ ) { $register{$1} = 0 unless defined $register{$1} }
+    if ( $line =~ /out\((r\d+)/ ) { $outreg = $1 }
+    if ( $line =~ /^\d+:(.+)/ ) {
         my $code = $1;
+
         # Some beautification!
         $code =~ s/ +/ /g;
         $code =~ s/\#.*//g;
         next unless $code;
-        push @program, $code ;
-        }
+        push @program, $code;
+    }
 }
 close PROGRAM;
 
@@ -41,36 +42,39 @@ close PROGRAM;
 
 my $pos = 1;
 while (1) {
-    my $line = $program[$pos -1];
-    if ($pos > @program) {last;}
+    my $line = $program[ $pos - 1 ];
+    if ( $pos > @program ) { last; }
     print "($pos";
-    foreach my $reg (sort keys %register) {
-        print ", " . $register{$reg} ;
+    foreach my $reg ( sort keys %register ) {
+        print ", " . $register{$reg};
     }
     print ") \t$line\n";
-    if ($line =~ /if *(r\d) *= *(\d) *goto *(\d+)/) {
+    if ( $line =~ /if *(r\d) *= *(\d) *goto *(\d+)/ ) {
         $register{$1} = 0 unless defined $register{$1};
-        if ($register{$1} == $2) {$pos = $3;} else {$pos++}
+        if ( $register{$1} == $2 ) { $pos = $3; }
+        else { $pos++ }
     }
-    elsif ($line =~ /(r\d) *<- *(\d+)/) {
+    elsif ( $line =~ /(r\d) *<- *(\d+)/ ) {
         $register{$1} = $2;
         $pos++;
     }
-    elsif ($line =~ /(r\d) *<- *(r\d+) *([+-]) *1/) {
-        if ($1 ne $2) {print "Syntax error: Registers don't match!\n"; exit 1;}
-        $register{$1} = eval( "$register{$2} $3 1;");
-        if ($register{$1} < 0) { $register{$1} = 0 }
+    elsif ( $line =~ /(r\d) *<- *(r\d+) *([+-]) *1/ ) {
+        if ( $1 ne $2 ) { print "Syntax error: Registers don't match!\n"; exit 1; }
+        $register{$1} = eval("$register{$2} $3 1;");
+        if ( $register{$1} < 0 ) { $register{$1} = 0 }
         $pos++;
     }
-    elsif ($line =~ / *goto *(\d+)/) {
-        $pos=$1 ;
+    elsif ( $line =~ / *goto *(\d+)/ ) {
+        $pos = $1;
     }
     else {
-        print "Syntax error\n"; exit;
+        print "Syntax error\n";
+        exit;
     }
-}  
+}
 
 print "Output: $register{$outreg}\n";
+
 #print Dumper %register;
 
 # Local Variables:
