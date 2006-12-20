19fef8e9a9ee5c18973b4a65f765887b8f823f92
Matt Diephouse <matt@diephouse.com>
2006-12-20 05:55:52 +0000

    [tcl]: Add a module that lets use run tcl tests from anywhere in the parrot tree (and use it for [append])
    git-svn-id: https://svn.parrot.org/parrot/trunk@16198 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ languages/tcl/lib/Tcl/Test.pm
@@ -0,0 +1,17 @@
+package Tcl::Test;
+use warnings;
+use strict;
+
+use lib qw(../lib ../../lib ../../../lib ../../../../lib);
+use Parrot::Config;
+use File::Spec;
+
+sub import {
+    my $parrot = File::Spec->catfile($PConfig{build_dir}, 'parrot');
+    my $tcl    = File::Spec->catdir($PConfig{build_dir}, qw(languages tcl));
+    my $test   = File::Spec->abs2rel( File::Spec->rel2abs($0), $tcl );
+
+    chdir $tcl;
+    exec $parrot, "tcl.pbc", $test;
+}
+1;
