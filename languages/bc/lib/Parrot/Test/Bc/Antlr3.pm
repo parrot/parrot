033a6150383a24e68cc645f888fcbab901aed8b5
Paul Cochrane <paultcochrane@gmail.com>
2006-12-25 22:23:30 +0000

    [bc] Added/corrected Parrot coda (part 18)
    git-svn-id: https://svn.parrot.org/parrot/trunk@16260 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/bc/lib/Parrot/Test/Bc/Antlr3.pm
+++ languages/bc/lib/Parrot/Test/Bc/Antlr3.pm
@@ -45,3 +45,10 @@ sub skip_why {
 }
 
 1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
--- languages/bc/lib/Parrot/Test/Bc/Posix.pm
+++ languages/bc/lib/Parrot/Test/Bc/Posix.pm
@@ -38,3 +38,10 @@ sub skip_why {
 }
 
 1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
