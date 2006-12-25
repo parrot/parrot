f4a2d6f4e259062dce73a615d6b05aac6f36d851
Paul Cochrane <paultcochrane@gmail.com>
2006-12-25 22:18:11 +0000

    [plumhead] Added/corrected Parrot coda (part 9)
    git-svn-id: https://svn.parrot.org/parrot/trunk@16251 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/plumhead/plumhead.pl
+++ languages/plumhead/plumhead.pl
@@ -5,3 +5,10 @@
 # A workaround for run-tests.php
 
 exec './parrot', 'languages/plumhead/plumhead.pbc', @ARGV;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
