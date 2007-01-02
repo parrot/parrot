81da5b6a3b56741e3eb7f0e5e07b6f9b3f7eaaa3
Will "Coke" Coleda <will@coleda.com>
2007-01-02 03:00:50 +0000

    [tcl] - fix typo
    git-svn-id: https://svn.parrot.org/parrot/trunk@16368 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/tcl/lib/skipped_tests.tcl
+++ languages/tcl/lib/skipped_tests.tcl
@@ -45,7 +45,7 @@ set skipped_tests [dict create \
   } {nested dictionaries} {
     dict-3.5 dict-3.6 dict-3.7 dict-3.8 dict-3.9 dict-3.10 dict-9.3 dict-9.4
     dict-9.5 dict-15.3 dict-15.5 dict-16.4
-  } {support for the variable named ""}
+  } {support for the variable named ""} {
     var-6.3 var-7.12 var-12.1
   } {Cannot get character past end of string} {
     format-8.17 format-8.18
