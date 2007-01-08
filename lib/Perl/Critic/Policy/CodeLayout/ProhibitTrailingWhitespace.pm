a6514bdf739e63c92f758ca435e41bed9694cae0
Paul Cochrane <paultcochrane@gmail.com>
2007-01-08 17:54:10 +0000

    [lib][codingstd] Added a Perl::Critic policy to search for trailing whitespace (spaces or tabs) in perl source files.
    git-svn-id: https://svn.parrot.org/parrot/trunk@16491 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ lib/Perl/Critic/Policy/CodeLayout/ProhibitTrailingWhitespace.pm
@@ -0,0 +1,55 @@
+# $Id: /parrot/local/lib/Perl/Critic/Policy/CodeLayout/UseParrotCoda.pm 24928 2007-01-08T15:18:49.395523Z cochrane  $
+package Perl::Critic::Policy::CodeLayout::ProhibitTrailingWhitespace;
+
+use strict;
+use warnings;
+use Perl::Critic::Utils;
+use Perl::Critic::Violation;
+use base 'Perl::Critic::Policy';
+
+=head1 NAME
+
+Perl::Critic::Policy::CodeLayout::ProhibitTrailingWhitespace
+
+=head1 DESCRIPTION
+
+Parrot source code should be free from trailing tabs and spaces.
+
+=cut
+
+our $VERSION = '0.1';
+$VERSION = eval $VERSION;    ## no critic
+
+my $desc = q{Found trailing whitespace};
+my $expl = q{All perl source in parrot should not have trailing whitespace};
+
+#----------------------------------------------------------------------------
+
+sub default_severity { return $SEVERITY_LOW }
+sub applies_to       { return 'PPI::Document' }
+
+#----------------------------------------------------------------------------
+
+sub violates {
+    my ( $self, $elem, $doc ) = @_;
+
+    my $content = $doc->content();
+    my @lines = split /\n/, $content;
+
+    foreach my $line ( @lines ) {
+        if ( $line =~ m{.?[ \t]+$}m ) {
+            return $self->violation( $desc, $expl, $doc );
+        }
+    }
+
+    return;
+}
+
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
