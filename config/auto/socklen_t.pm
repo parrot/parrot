9b2b4457f3ecb839e3999b3696904d504f70492f
Kevin Tew <tewk@tewk.com>
2007-01-18 16:03:50 +0000

    [config] fix socklen_t detection for MSYS
    git-svn-id: https://svn.parrot.org/parrot/trunk@16691 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/socklen_t.pm
+++ config/auto/socklen_t.pm
@@ -22,7 +22,7 @@ use Config;
 
 use Parrot::Configure::Step ':auto';
 
-our $description = 'Asking Perl5 whether there is socklen_t';
+our $description = 'Determining whether there is socklen_t';
 our @args        = qw();
 
 sub runstep {
@@ -30,7 +30,8 @@ sub runstep {
 
     my $verbose = $conf->options->get('verbose');
 
-    my $d_socklen_t = $Config{d_socklen_t} || $conf->data->get('has_socklen_t');
+    my $d_socklen_t = $conf->data->get('has_socklen_t');
+    $d_socklen_t = $Config{d_socklen_t} unless defined $d_socklen_t;
     my $has_socklen_t = ( $d_socklen_t && $d_socklen_t ne 'undef' ) ? 1 : 0;
     $self->set_result( $has_socklen_t ? 'yes' : 'no' );
     $conf->data->set( has_socklen_t => $has_socklen_t, );
