6076e7035af1cfe5537d06322e3797b6eb09700d
Allison Randal <allison@parrot.org>
2007-01-19 06:47:26 +0000

    Updating copyright date displayed during configure process.
    git-svn-id: https://svn.parrot.org/parrot/trunk@16702 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- Configure.pl
+++ Configure.pl
@@ -447,7 +447,7 @@ $args{maintainer} = 1 if defined $args{lex} or defined $args{yacc};
 
 print <<"END";
 Parrot Version $parrot_version Configure 2.0
-Copyright (C) 2001-2006, The Perl Foundation.
+Copyright (C) 2001-2007, The Perl Foundation.
 
 Hello, I'm Configure. My job is to poke and prod your system to figure out
 how to build Parrot. The process is completely automated, unless you passed in
