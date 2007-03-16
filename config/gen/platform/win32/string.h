4ecb8b13811c22a6be6cf59aea0787c206cfae41
Will "Coke" Coleda <will@coleda.com>
2007-03-16 12:53:00 +0000

    RT # 41602 - add file that wasn't missed from ticket.
    git-svn-id: https://svn.parrot.org/parrot/trunk@17520 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ config/gen/platform/win32/string.h
@@ -0,0 +1,19 @@
+#ifndef PLATFORM_WIN32_STRING_H
+#define PLATFORM_WIN32_STRING_H
+
+#include <string.h>
+
+#ifdef _MSC_VER
+# if _MSC_VER >= 1400
+# define strdup _strdup
+# endif
+#endif
+
+#endif
+
+/*
+ * Local variables:
+ * c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
