b35fb24106ac9c7128db0b50a6b989bf82282e19
Klaas-Jan Stol <parrotcode@gmail.com>
2007-03-26 12:19:29 +0000

    compilers/pirc: * added missing files: pirout.{c,h}
    git-svn-id: https://svn.parrot.org/parrot/trunk@17768 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/pirc/src/pirout.c
@@ -0,0 +1,39 @@
+#include "pirout.h"
+#include "pirlexer.h"
+#include "pirparser.h"
+#include <stdio.h>
+
+/* pirout()
+ *
+ * Prints the current token from the specified parser.
+ * NOT COMPLETE YET, and no fancy output as well...
+ */
+void
+pirout(struct parser_state *p) {
+    struct lexer_state *l = get_lexer(p);
+    token t = get_token(p);
+    
+    switch (t) {
+        case T_RPAREN:            /* HACK */
+            fprintf(stderr, ")"); /* and print newline as well, nec. for macros */
+        case T_NEWLINE:
+            fprintf(stderr, "\n");
+            break;              
+        case T_IDENTIFIER:
+        case T_PASM_PREG:
+        case T_PASM_NREG:                                
+        case T_PASM_IREG:
+        case T_PASM_SREG:
+        case T_PREG:
+        case T_NREG:
+        case T_SREG:    
+        case T_IREG:    
+            fprintf(stderr, " %s ", get_current_token(l));	                              
+            break;
+        default:            
+            fprintf(stderr, "%s ", find_keyword(t));
+            break;
+    }
+	
+}
+
---
+++ compilers/pirc/src/pirout.h
@@ -0,0 +1,9 @@
+#ifndef __PIROUT_H
+#define __PIROUT_H
+
+
+#include "pirparser.h"
+
+extern void pirout(struct parser_state *parser);
+
+#endif
