8f76da614d4a7150d90bece8c2cc17755d226d4b
Jonathan Worthington <jnthn@jnthn.net>
2007-04-14 12:43:22 +0000

    Make io.c C89 compliant, to fix the build.
    git-svn-id: https://svn.parrot.org/parrot/trunk@18187 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/io/io.c
+++ src/io/io.c
@@ -1325,11 +1325,15 @@ microseconds.
 INTVAL
 PIO_poll(Interp *interp, PMC *pmc, INTVAL which, INTVAL sec, INTVAL usec)
 {
+    ParrotIOLayer *l;
+    ParrotIO *io;
+
     if (pmc == PMCNULL) {
         real_exception(interp, NULL, E_ValueError, "Can't poll NULL pmc");
     }
-    ParrotIOLayer * const l = PMC_struct_val(pmc);
-    ParrotIO * const io = PMC_data0(pmc);
+
+    l = PMC_struct_val(pmc);
+    io = PMC_data0(pmc);
     return PIO_poll_down(interp, l, io, which, sec, usec);
 }
 
