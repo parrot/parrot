4cf06bdcc7e6cf1d340170853e7dbafa127e4471
Jonathan Worthington <jnthn@jnthn.net>
2007-04-16 23:26:34 +0000

    [PDD15]: Add classobject.h, and move Class and Object PMC's structures, typedefs and macros into it. Include it from Class and Object. Now these two can know about each other's guts (but nothing outside of them will).
    git-svn-id: https://svn.parrot.org/parrot/trunk@18244 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ src/pmc/classobject.h
@@ -0,0 +1,51 @@
+/*
+** classobject.h
+**
+** structs, typedefs and macros for the Class and Object PMCs. This header
+** file is only included by the two PMCs, since they are allowed to know
+** about each other's internals, but all other usage should be through the
+** documented interface.
+*/
+
+#if !defined(PARROT_CLASSOBJECT_H_GUARD)
+#  define PARROT_CLASSOBJECT_GUARD
+
+/* Class PMC's underlying struct. */
+typedef struct Parrot_Class {
+    STRING *name;         /* The name of the class. */
+    PMC *namespace;       /* The namespace it's linked to, if any. */
+    int instantiated;     /* Any instantiations since last modification? */
+    PMC *parents;         /* Immediate parent classes. */
+    PMC *all_parents;     /* Cached list of ourself and all parents, in MRO order. */
+    PMC *roles;           /* An array of roles. */
+    PMC *methods;         /* Hash of method names to methods in this class. */
+    PMC *vtable_methods;  /* Hash of Parrot v-table methods we override. */
+    PMC *attrib_metadata; /* Hash of attributes in this class to hashes of metadata. */
+    PMC *attrib_index;    /* Lookup table for attributes in this and parents. */
+    PMC *attrib_cache;    /* Cache of visible attrib names to indexes. */
+    PMC *resolve_method;  /* List of method names the class provides to resolve
+                           * conflicts with methods from roles. */
+} Parrot_Class;
+
+/* Macro to access underlying structure of a Class PMC. */
+#define PARROT_CLASS(o) ((Parrot_Class *) PMC_data(o))
+
+/* Object PMC's underlying struct. */
+typedef struct Parrot_Object {
+    PMC *class;          /* The class this is an instance of. */
+    PMC *attrib_store;   /* The attributes store - a resizable PMC array. */
+} Parrot_Object;
+
+/* Macro to access underlying structure of an Object PMC. */
+#define PARROT_OBJECT(o) ((Parrot_Object *) PMC_data(o))
+
+
+#endif /* PARROT_CLASSOBJECT_GUARD */
+
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
