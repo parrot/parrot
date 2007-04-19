27658360a21aea73eac5c4d27dddee252d7abffc
chromatic <chromatic@wgz.org>
2007-04-19 07:59:32 +0000

    Fixed a typo, then fixed some indentation problems the typo had masked.
    git-svn-id: https://svn.parrot.org/parrot/trunk@18286 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/atomic.h
+++ include/parrot/atomic.h
@@ -11,38 +11,38 @@
  */
 
 
-#if !defined(PARROT_ATOMIC_H_GAURD)
-#  define PARROT_ATOMIC_H_GAURD
+#if !defined(PARROT_ATOMIC_H_GUARD)
+#  define PARROT_ATOMIC_H_GUARD
 
 #  include "parrot/has_header.h"
 #  include "parrot/thread.h"
 
 #  define PARROT_HAS_NATIVE_ATOMIC 1      /* we'll redefine later if need be */
 
-#  if (PARROT_HAS_THREADS)
-#    if defined(PARROT_HAS_I386_GCC_CMPXCHG)
-#      include "parrot/atomic/gcc_x86.h"
-#    elif defined(PARROT_HAS_PPC_GCC_CMPSET)
-#      include "parrot/atomic/gcc_pcc.h"
-#    elif defined(PARROT_HAS_SPARC_ATOMIC)
-#      include "parrot/atomic/sparc.h"
-#    else
-#      undef PARROT_HAS_NATIVE_ATOMIC
-#      define PARROT_HAS_NATIVE_ATOMIC 0
-#      include "parrot/atomic/fallback.h"
-#    endif
+#if (PARROT_HAS_THREADS)
+#  if defined(PARROT_HAS_I386_GCC_CMPXCHG)
+#    include "parrot/atomic/gcc_x86.h"
+#  elif defined(PARROT_HAS_PPC_GCC_CMPSET)
+#    include "parrot/atomic/gcc_pcc.h"
+#  elif defined(PARROT_HAS_SPARC_ATOMIC)
+#    include "parrot/atomic/sparc.h"
 #  else
+#    undef PARROT_HAS_NATIVE_ATOMIC
+#    define PARROT_HAS_NATIVE_ATOMIC 0
+#    include "parrot/atomic/fallback.h"
+#  endif
+#else
 typedef struct {
     void *val;
 } Parrot_atomic_pointer;
 
-typedef struct {
+typedef Parrot_atomic_integer {
     INTVAL val;
 } Parrot_atomic_integer;
 
-#    define PARROT_ATOMIC_PTR_INIT(a)
-#    define PARROT_ATOMIC_PTR_GET(result,a) result = (a).val
-#    define PARROT_ATOMIC_PTR_SET(a,b) (a).val = b
+#  define PARROT_ATOMIC_PTR_INIT(a)
+#  define PARROT_ATOMIC_PTR_GET(result,a) result = (a).val
+#  define PARROT_ATOMIC_PTR_SET(a,b) (a).val = b
 
 /* a is the Parrot_atomic.
  * expect is the value we expect the atomic to be holding,
@@ -50,42 +50,42 @@ typedef struct {
  * result will be 1 if the value was as expected (and thus
  * the update was performed) and 0 otherwise
  */
-#    define PARROT_ATOMIC_PTR_CAS(result, a, expect, update) \
-        do { \
-            void * orig; \
-            PARROT_ATOMIC_PTR_GET(a, orig); \
-            if (expect == orig) { \
-                ATOMIC_SET(a, update); \
-                result = 1; \
-            } \
-            else { \
-                result = 0; \
-            } \
-        } while (0)
-#    define PARROT_ATOMIC_PTR_DESTROY(a)
-#    define PARROT_ATOMIC_INT_INIT(a)
-#    define PARROT_ATOMIC_INT_DESTROY(a)
-#    define PARROT_ATOMIC_INT_GET(result, a) result = (a).val
-#    define PARROT_ATOMIC_INT_SET(a, value)  (a).val = value
-#    define PARROT_ATOMIC_INT_CAS(result, a, expect, update) \
-        do { \
-            INTVAL orig; \
-            PARROT_ATOMIC_PTR_GET(a, orig); \
-            if (expect == orig) { \
-                ATOMIC_SET(a, update); \
-                result = 1; \
-            } \
-            else { \
-                result = 0; \
-            } \
-        } while (0)
-#    define PARROT_ATOMIC_INT_INC(result, a) (result = ++(a).val)
-#    define PARROT_ATOMIC_INT_DEC(result, a) (result = --(a).val)
+#  define PARROT_ATOMIC_PTR_CAS(result, a, expect, update) \
+      do { \
+          void * orig; \
+          PARROT_ATOMIC_PTR_GET(a, orig); \
+          if (expect == orig) { \
+              ATOMIC_SET(a, update); \
+              result = 1; \
+          } \
+          else { \
+              result = 0; \
+          } \
+      } while (0)
+#  define PARROT_ATOMIC_PTR_DESTROY(a)
+#  define PARROT_ATOMIC_INT_INIT(a)
+#  define PARROT_ATOMIC_INT_DESTROY(a)
+#  define PARROT_ATOMIC_INT_GET(result, a) result = (a).val
+#  define PARROT_ATOMIC_INT_SET(a, value)  (a).val = value
+#  define PARROT_ATOMIC_INT_CAS(result, a, expect, update) \
+      do { \
+          INTVAL orig; \
+          PARROT_ATOMIC_PTR_GET(a, orig); \
+          if (expect == orig) { \
+              ATOMIC_SET(a, update); \
+              result = 1; \
+          } \
+          else { \
+              result = 0; \
+          } \
+      } while (0)
+#  define PARROT_ATOMIC_INT_INC(result, a) (result = ++(a).val)
+#  define PARROT_ATOMIC_INT_DEC(result, a) (result = --(a).val)
 
 
-#  endif /* PARROT_HAS_THREADS */
+#endif /* PARROT_HAS_THREADS */
 
-#endif /* PARROT_ATOMIC_H_GAURD */
+#endif /* PARROT_ATOMIC_H_GUARD */
 
 /*
  * Local variables:
