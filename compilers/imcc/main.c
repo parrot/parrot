parrot/parrot

commit 446b42a2253b1d5912ec956bcd50197edbcacc55

[IMCC] Moved main() from compilers/imcc/main.c to src/main.c.
This is the first step in a laborious process that likely culminates in separating the concerns of IMCC from the rest of Parrot in a nicely decoupled fashion.

git-svn-id: https://svn.parrot.org/parrot/trunk@18277 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

compilers/imcc/main.c
@@ -24,6 +24,11 @@
 
 static int load_pbc, run_pbc, write_pbc, pre_process, pasm_file;
 
+char * parseflags(Interp *interp, int *argc, char **argv[]);
+int    imcc_initialize(Interp *interp);
+int    imcc_run(Interp *interp, const char *sourcefile, int argc,
+                char * argv[]);
+
 static void
 usage(FILE* fp)
 {
@@ -194,7 +199,7 @@ is_all_hex_digits(const char *s)
 }
 
 /* most stolen from test_main.c */
-static char *
+char *
 parseflags(Parrot_Interp interp, int *argc, char **argv[])
 {
     struct longopt_opt_info opt = LONGOPT_OPT_INFO_INIT;
@@ -511,7 +516,7 @@ imcc_get_optimization_description(Interp *interp, int opt_level, char *opt_desc)
     return;
 }
 
-static int
+int
 imcc_initialize(Interp *interp)
 {
     yyscan_t yyscanner = IMCC_INFO(interp)->yyscanner;
@@ -592,7 +597,7 @@ imcc_write_pbc(Interp *interp, const char *output_file)
     free(packed);
 }
 
-static int
+int
 imcc_run(Interp *interp, const char *sourcefile, int argc, char * argv[])
 {
     PackFile *pf;
@@ -759,41 +764,6 @@ imcc_run(Interp *interp, const char *sourcefile, int argc, char * argv[])
     IMCC_INFO(interp)->yyscanner = NULL;
     return 0;
 }
-
-int
-main(int argc, char * argv[])
-{
-    char    *sourcefile;
-    Interp  *interp;
-    STRING  *executable_name;
-    PMC     *executable_name_pmc;
-    int      status;
-
-    Parrot_set_config_hash();
-
-    interp     = Parrot_new(NULL);
-    if (!imcc_initialize(interp))
-        internal_exception(1, "Could not initialize IMCC\n");
-
-    /* We parse the arguments, but first store away the name of the Parrot
-       executable, since parsing destroys that and we want to make it
-       available. */
-    executable_name     = string_from_cstring(interp, argv[0], 0);
-    executable_name_pmc = pmc_new(interp, enum_class_String);
-    VTABLE_set_string_native(interp, executable_name_pmc, executable_name);
-    VTABLE_set_pmc_keyed_int(interp, interp->iglobals, IGLOBALS_EXECUTABLE,
-        executable_name_pmc);
-
-    sourcefile = parseflags(interp, &argc, &argv);
-    status     = imcc_run(interp, sourcefile, argc, argv);
-
-    /* Clean-up after ourselves */
-    Parrot_destroy(interp);
-    Parrot_exit(interp, 0);
-
-    return status;
-}
-
 
 /*
  * Local variables:

config/gen/makefiles/root.in
@@ -371,6 +371,7 @@ IMCC_O_FILES = \
     $(IMCC_DIR)/imcparser$(O) \
     $(IMCC_DIR)/imclexer$(O) \
     $(IMCC_DIR)/imc$(O) \
+    $(IMCC_DIR)/main$(O) \
     $(IMCC_DIR)/symreg$(O) \
     $(IMCC_DIR)/instructions$(O) \
     $(IMCC_DIR)/cfg$(O) \
@@ -425,6 +426,7 @@ INTERP_O_FILES = \
     $(SRC_DIR)/inter_run$(O)  \
     $(SRC_DIR)/gc/register$(O) \
     $(SRC_DIR)/gc/memory$(O) \
+    $(SRC_DIR)/main$(O) \
     $(SRC_DIR)/objects$(O) \
     $(SRC_DIR)/packfile$(O) \
     $(SRC_DIR)/stacks$(O) \
@@ -1081,6 +1083,8 @@ $(IO_DIR)/io_string$(O) : $(GENERAL_H_FILES) $(IO_DIR)/io_private.h
 $(IO_DIR)/io_passdown$(O) : $(GENERAL_H_FILES) $(IO_DIR)/io_private.h
 
 $(SRC_DIR)/gc/memory$(O) : $(GENERAL_H_FILES)
+
+$(SRC_DIR)/main$(O) : $(SRC_DIR)/main.c $(GENERAL_H_FILES)
 
 $(SRC_DIR)/pic$(O) : $(GENERAL_H_FILES)
 

include/parrot/imcc.h
@@ -1,0 +1,20 @@
+/*
+ * Copyright (C) 2007, The Perl Foundation
+ * $Id$
+ */
+
+#if !defined(PARROT_IMCC_H_GUARD)
+#define PARROT_IMCC_H_GUARD
+
+int imcc_initialize(Interp *interp);
+char * parseflags(Interp *interp, int *argc, char **argv[]);
+int imcc_run(Interp *interp, const char *sourcefile, int argc, char * argv[]);
+
+#endif
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */

src/main.c
@@ -1,0 +1,87 @@
+/*
+Copyright (C) 2007, The Perl Foundation
+$Id$
+
+=head1 NAME
+
+src/main.c - the Entry Point to Parrot Programs
+
+=head1 DESCRIPTION
+
+Start Parrot
+
+=head2 Functions
+
+=over 4
+
+=cut
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "parrot/parrot.h"
+#include "parrot/embed.h"
+#include "parrot/imcc.h"
+
+/*
+
+=item C<int main(int argc, char * argv[])>
+
+The entry point from the command line into Parrot.
+
+*/
+
+int
+main(int argc, char * argv[])
+{
+    char    *sourcefile;
+    Interp  *interp;
+    STRING  *executable_name;
+    PMC     *executable_name_pmc;
+    int      status;
+
+    Parrot_set_config_hash();
+
+    interp     = Parrot_new(NULL);
+    if (!imcc_initialize(interp))
+        internal_exception(1, "Could not initialize IMCC\n");
+
+    /* We parse the arguments, but first store away the name of the Parrot
+       executable, since parsing destroys that and we want to make it
+       available. */
+    executable_name     = string_from_cstring(interp, argv[0], 0);
+    executable_name_pmc = pmc_new(interp, enum_class_String);
+    VTABLE_set_string_native(interp, executable_name_pmc, executable_name);
+    VTABLE_set_pmc_keyed_int(interp, interp->iglobals, IGLOBALS_EXECUTABLE,
+        executable_name_pmc);
+
+    sourcefile = parseflags(interp, &argc, &argv);
+    status     = imcc_run(interp, sourcefile, argc, argv);
+
+    /* Clean-up after ourselves */
+    Parrot_destroy(interp);
+    Parrot_exit(interp, 0);
+
+    return status;
+}
+
+/*
+
+=back
+
+=head1 SEE ALSO
+
+F<compilers/imcc/main.c>, unfortunately.
+
+=cut
+
+*/
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */

MANIFEST
@@ -771,6 +771,7 @@ include/parrot/global_setup.h                               [main]include
 include/parrot/hash.h                                       [main]include
 include/parrot/headers.h                                    [main]include
 include/parrot/hll.h                                        [main]include
+include/parrot/imcc.h                                       [main]include
 include/parrot/inter_call.h                                 [main]include
 include/parrot/interpreter.h                                [main]include
 include/parrot/intlist.h                                    [main]include
@@ -2544,6 +2545,7 @@ src/list.c                                                  []
 src/longopt.c                                               []
 src/malloc-trace.c                                          []
 src/malloc.c                                                []
+src/main.c                                                  []
 src/misc.c                                                  []
 src/mmd.c                                                   []
 src/nci_test.c                                              []

commit 6e33d923056a438ee8699f2df19be8015ecb9d8f

[build] fix build on mingw32 (broken since r18277)
git-svn-id: https://svn.parrot.org/parrot/trunk@18290 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

compilers/imcc/main.c
@@ -9,8 +9,6 @@
  *
  */
 
-#define PARROT_IN_EXTENSION
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,15 +17,11 @@
 #include "imc.h"
 #include "parrot/embed.h"
 #include "parrot/longopt.h"
+#include "parrot/imcc.h"
 #include "pbc.h"
 #include "parser.h"
 
 static int load_pbc, run_pbc, write_pbc, pre_process, pasm_file;
-
-char * parseflags(Interp *interp, int *argc, char **argv[]);
-int    imcc_initialize(Interp *interp);
-int    imcc_run(Interp *interp, const char *sourcefile, int argc,
-                char * argv[]);
 
 static void
 usage(FILE* fp)

config/gen/makefiles/root.in
@@ -426,7 +426,6 @@ INTERP_O_FILES = \
     $(SRC_DIR)/inter_run$(O)  \
     $(SRC_DIR)/gc/register$(O) \
     $(SRC_DIR)/gc/memory$(O) \
-    $(SRC_DIR)/main$(O) \
     $(SRC_DIR)/objects$(O) \
     $(SRC_DIR)/packfile$(O) \
     $(SRC_DIR)/stacks$(O) \
@@ -762,25 +761,25 @@ runtime/parrot/include/config.fpmc : myconfig config_lib.pasm $(MINIPARROT)
 	@echo Invoking Parrot to generate runtime/parrot/include/config.fpmc --cross your fingers
 	$(MINIPARROT) config_lib.pasm > $@
 
-$(PARROT) : $(IMCC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
+$(PARROT) : $(SRC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
     lib/Parrot/OpLib/core.pm $(SRC_DIR)/parrot_config$(O) \
     $(MINIPARROT)
 	$(LINK) @ld_out@$@ \
-    $(IMCC_DIR)/main$(O) @rpath_blib@ $(ALL_PARROT_LIBS) $(LINKFLAGS) $(LINK_DYNAMIC) \
+    $(SRC_DIR)/main$(O) @rpath_blib@ $(ALL_PARROT_LIBS) $(LINKFLAGS) $(LINK_DYNAMIC) \
     $(SRC_DIR)/parrot_config$(O)
 #
 # TODO build the real miniparrot
 #
-$(MINIPARROT) : $(IMCC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
+$(MINIPARROT) : $(SRC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
     lib/Parrot/OpLib/core.pm $(SRC_DIR)/null_config$(O)
-	$(LINK) @ld_out@$@ $(IMCC_DIR)/main$(O) \
+	$(LINK) @ld_out@$@ $(SRC_DIR)/main$(O) \
     @rpath_blib@ $(ALL_PARROT_LIBS) $(LINKFLAGS) $(SRC_DIR)/null_config$(O)
 
-$(INSTALLABLEPARROT) : $(IMCC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
+$(INSTALLABLEPARROT) : $(SRC_DIR)/main$(O) $(GEN_HEADERS) $(LIBPARROT) \
     lib/Parrot/OpLib/core.pm $(SRC_DIR)/install_config$(O) \
     $(PARROT)
 	$(LINK) @ld_out@$@ \
-    $(IMCC_DIR)/main$(O) \
+    $(SRC_DIR)/main$(O) \
     $(ALL_PARROT_LIBS) $(LINKFLAGS) $(SRC_DIR)/install_config$(O)
 
 $(SRC_DIR)/parrot_config.c :  runtime/parrot/include/config.fpmc \

include/parrot/imcc.h
@@ -6,9 +6,9 @@
 #if !defined(PARROT_IMCC_H_GUARD)
 #define PARROT_IMCC_H_GUARD
 
-int imcc_initialize(Interp *interp);
-char * parseflags(Interp *interp, int *argc, char **argv[]);
-int imcc_run(Interp *interp, const char *sourcefile, int argc, char * argv[]);
+PARROT_API int imcc_initialize(Interp *interp);
+PARROT_API char * parseflags(Interp *interp, int *argc, char **argv[]);
+PARROT_API int imcc_run(Interp *interp, const char *sourcefile, int argc, char * argv[]);
 
 #endif
 