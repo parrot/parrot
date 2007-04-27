ff80b6a6b46ab6c20949515e505130267b93819f
Matt Diephouse <matt@diephouse.com>
2007-04-27 04:30:50 +0000

    #42594: [PATCH] Probable buffer overflow in compilers/imcc/parser_util.c
    Courtesy Steve Peters <steve@fisharerojo.org>.
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@18342 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/imcc/parser_util.c
+++ compilers/imcc/parser_util.c
@@ -494,8 +494,10 @@ INS(Interp *interp, IMC_Unit * unit, char *name,
     if (len >= 2)
         len -= 2;
     format[len] = '\0';
-    if (fmt && *fmt)
-        strcpy(format, fmt);
+    if (fmt && *fmt) {
+        strncpy(format, fmt, sizeof(format) - 1);
+        format[sizeof(format) - 1] = '\0';
+    }
 #if 1
     IMCC_debug(interp, DEBUG_PARSER,"%s %s\t%s\n", name, format, fullname);
 #endif
