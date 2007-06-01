669486b44ee2fb9a0db5fa53f48d246a0b475c50
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2007-06-01 20:11:57 +0000

    [languages/lisp] Beautify Parrot::Test::Lisp. Add 'lisp' to unified languages testing.
    git-svn-id: https://svn.parrot.org/parrot/trunk@18725 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/lisp/lib/Parrot/Test/Lisp.pm
+++ languages/lisp/lib/Parrot/Test/Lisp.pm
@@ -22,29 +22,25 @@ sub new {
 sub output_is {
     my ( $self, $code, $output, $desc ) = @_;
 
-    my $count = $self->{builder}->current_test + 1;
+    my $count = $self->{builder}->current_test() + 1;
 
     my $lang_fn = File::Spec->rel2abs(Parrot::Test::per_test( '.l',  $count ));
     my $out_fn  = File::Spec->rel2abs(Parrot::Test::per_test( '.out', $count ));
     Parrot::Test::write_code_to_file( $code, $lang_fn );
 
-    my $exit_code = 0;
-    my $pass      = 0;
-    my $args      = $ENV{TEST_PROG_ARGS} || '';
-    my $cmd       = "../../$self->{parrot} $args lisp.pbc $lang_fn";
+    my $args     = $ENV{TEST_PROG_ARGS} || '';
+    my $cmd      = "../../$self->{parrot} $args lisp.pbc $lang_fn";
+    my $lang_dir = File::Spec->catdir( $self->{relpath}, qw( languages lisp ) );
 
-    $exit_code = Parrot::Test::run_command(
+    my $exit_code = Parrot::Test::run_command(
         $cmd,
-        CD     => "$self->{relpath}/languages/lisp!",
+        CD     => $lang_dir,
         STDOUT => $out_fn,
         STDERR => $out_fn
     );
-    if ( ! $pass ) {
-        my $file = Parrot::Test::slurp_file($out_fn);
-        $pass = $self->{builder}->is_eq( Parrot::Test::slurp_file($out_fn), $output, $desc );
-        $self->{builder}->diag("'$cmd' failed with exit code $exit_code")
-            if $exit_code and not $pass;
-    }
+    my $pass = $self->{builder}->is_eq( Parrot::Test::slurp_file($out_fn), $output, $desc );
+    $self->{builder}->diag("'$cmd' failed with exit code $exit_code")
+        if $exit_code and not $pass;
 
     return $pass;
 }
