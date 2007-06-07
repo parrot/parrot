3c007b5d968f415b1a41875968a6ae1ae94ea38c
Paul Cochrane <paultcochrane@gmail.com>
2007-06-07 09:08:18 +0000

    [config] Converted TODO items to RT tickets in config/init/hints/darwin.pm
    git-svn-id: https://svn.parrot.org/parrot/trunk@18860 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/darwin.pm
+++ config/init/hints/darwin.pm
@@ -42,14 +42,14 @@ sub runstep {
         memalign            => 'some_memalign',
         has_dynamic_linking => 1,
 
-        # XXX when built against a dynamic libparrot installable_parrot records
+        # RT#43147 when built against a dynamic libparrot installable_parrot records
         # the path to the blib version of the library
         parrot_is_shared       => 0,
         libparrot_shared       => 'libparrot.$(SOVERSION)$(SHARE_EXT)',
         libparrot_shared_alias => 'libparrot$(SHARE_EXT)',
 
         # This variable needs renaming to be more general
-        # XXX ugly hack for rpath_lib in config/inter/libparrot.pm
+        # RT#43148 ugly hack for rpath_lib in config/inter/libparrot.pm
         rpath            => "-L",
         libparrot_soname => "-install_name "
             . $conf->data->get('lib_dir')
