e64f0d22565310526f7b221737b032f4870a4641
Andy Lester <andy@petdance.com>
2007-06-20 17:04:18 +0000

    Fixed a cast that threw away constness
    git-svn-id: https://svn.parrot.org/parrot/trunk@19177 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/encodings/utf8.c
+++ src/encodings/utf8.c
@@ -349,12 +349,12 @@ to_encoding(Interp *interp, STRING *src, STRING *dest)
 static UINTVAL
 get_codepoint(Interp *interp, const STRING *src, UINTVAL offset)
 {
-    const utf8_t * const start = (utf8_t *)utf8_skip_forward(src->strstart, offset);
+    const utf8_t * const start = (const utf8_t *)utf8_skip_forward(src->strstart, offset);
     return utf8_decode(start);
 }
 
 static void
-set_codepoint(Interp *interp, STRING *src,
+set_codepoint(Interp *interp, STRING *src /*NN*/,
         UINTVAL offset, UINTVAL codepoint)
 {
     const void *start;
