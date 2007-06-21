ceb9fffbe4d45ceb17c2956e83cfcc6e931e1d70
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2007-06-21 17:59:39 +0000

    [languages/lisp] Forgot to remove hidden sub in Parrot::Test::Lisp.
    git-svn-id: https://svn.parrot.org/parrot/trunk@19227 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/lisp/lib/Parrot/Test/Lisp.pm
+++ languages/lisp/lib/Parrot/Test/Lisp.pm
@@ -72,39 +72,6 @@ sub get_test_prog {
               $lang_fn );
 }
 
-sub output_is_aaaaaaaaaa {
-    my ( $self, $code, $output, $desc, %extra ) = @_;
-
-    my $count = $self->{builder}->current_test() + 1;
-
-    # set a TODO for Test::Builder to find
-    my $call_pkg = $self->{builder}->exported_to() || '';
-
-    no strict 'refs';
-    local *{ $call_pkg . '::TODO' } = \$extra{todo}
-        if defined $extra{todo};
-
-    my $lang_fn = File::Spec->rel2abs(Parrot::Test::per_test( '.l',  $count ));
-    my $out_fn  = File::Spec->rel2abs(Parrot::Test::per_test( '.out', $count ));
-    Parrot::Test::write_code_to_file( $code, $lang_fn );
-
-    my $args     = $ENV{TEST_PROG_ARGS} || '';
-    my $cmd      = "../../$self->{parrot} $args lisp.pbc $lang_fn";
-    my $lang_dir = File::Spec->catdir( $self->{relpath}, qw( languages lisp ) );
-
-    my $exit_code = Parrot::Test::run_command(
-        $cmd,
-        CD     => $lang_dir,
-        STDOUT => $out_fn,
-        STDERR => $out_fn
-    );
-    my $pass = $self->{builder}->is_eq( Parrot::Test::slurp_file($out_fn), $output, $desc );
-    $self->{builder}->diag("'$cmd' failed with exit code $exit_code")
-        if $exit_code and not $pass;
-
-    return $pass;
-}
-
 1;
 
 # Local Variables:
