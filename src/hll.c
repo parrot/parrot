e6f35a0a6b0404fab1091c3bcacf9cbf5a1dff01
Andy Lester <andy@petdance.com>
2007-06-26 15:54:10 +0000

    Turned string_as_const_string into an assertion macro, which is what it really was.
    git-svn-id: https://svn.parrot.org/parrot/trunk@19341 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/hll.c
+++ src/hll.c
@@ -65,7 +65,6 @@ the context.  If no type is registered, returns C<core_type>.
 static PMC* new_hll_entry( Interp *interp /*NN*/ )
         __attribute__nonnull__(1);
 
-static STRING* string_as_const_string( Interp* interp, STRING *src );
 /* HEADERIZER END: static */
 
 
@@ -90,15 +89,8 @@ enum {
     } while (0)
 #define END_WRITE_HLL_INFO(interp, hll_info)
 
+#define ASSERT_CONST_STRING(src) assert(PObj_constant_TEST(src))
 
-static STRING*
-string_as_const_string(Interp* interp, STRING *src)
-{
-    if (PObj_constant_TEST(src))
-        return src;
-    assert(0);
-    return NULL;
-}
 
 static PMC*
 new_hll_entry(Interp *interp /*NN*/)
@@ -137,7 +129,7 @@ Parrot_register_HLL(Interp *interp /*NN*/, STRING *hll_name /*NULLOK*/, STRING *
 
         /* register dynlib */
         name    = constant_pmc_new_noinit(interp, enum_class_String);
-        hll_lib = string_as_const_string(interp, hll_lib);
+        ASSERT_CONST_STRING(hll_lib);
 
         VTABLE_set_string_native(interp, name, hll_lib);
         VTABLE_set_pmc_keyed_int(interp, entry, e_HLL_lib, name);
@@ -161,7 +153,7 @@ Parrot_register_HLL(Interp *interp /*NN*/, STRING *hll_name /*NULLOK*/, STRING *
 
     /* register HLL name */
     name     = constant_pmc_new_noinit(interp, enum_class_String);
-    hll_name = string_as_const_string(interp, hll_name);
+    ASSERT_CONST_STRING(hll_name);
 
     VTABLE_set_string_native(interp, name, hll_name);
     VTABLE_set_pmc_keyed_int(interp, entry, e_HLL_name, name);
@@ -187,7 +179,7 @@ Parrot_register_HLL(Interp *interp /*NN*/, STRING *hll_name /*NULLOK*/, STRING *
     if (!hll_lib)
         hll_lib = const_string(interp, "");
 
-    hll_lib = string_as_const_string(interp, hll_lib);
+    ASSERT_CONST_STRING(hll_lib);
 
     VTABLE_set_string_native(interp, name, hll_lib);
     VTABLE_set_pmc_keyed_int(interp, entry, e_HLL_lib, name);
