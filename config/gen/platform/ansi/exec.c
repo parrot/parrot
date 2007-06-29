4b8389da61ed9efcc8cf411b822229606ff196d8
Paul Cochrane <paultcochrane@gmail.com>
2007-06-29 18:26:12 +0000

    [config/gen/platform/ansi] Instances of 'interpreter' changed to 'interp' as per convention
    git-svn-id: https://svn.parrot.org/parrot/trunk@19483 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/ansi/exec.c
+++ config/gen/platform/ansi/exec.c
@@ -2,22 +2,22 @@
  * Spawn a subprocess
  *
  */
-INTVAL Parrot_Run_OS_Command(Parrot_Interp interpreter, STRING *command) {
+INTVAL Parrot_Run_OS_Command(Parrot_Interp interp, STRING *command) {
     Parrot_warn(NULL, PARROT_WARNINGS_PLATFORM_FLAG,
             "Parrot_Run_OS_Command not implemented");
     return 0;
 }
 
 INTVAL
-Parrot_Run_OS_Command_Argv(Parrot_Interp interpreter, PMC *cmdargs)
+Parrot_Run_OS_Command_Argv(Parrot_Interp interp, PMC *cmdargs)
 {
     Parrot_warn(NULL, PARROT_WARNINGS_PLATFORM_FLAG,
             "Parrot_Run_OS_Command_Argv not implemented");
     return 0;
 }
 
-void Parrot_Exec_OS_Comman(Parrot_Interp interpreter, STRING *command) {
-  real_exception(interpreter, NULL, NOSPAWN, "Exec not implemented");
+void Parrot_Exec_OS_Comman(Parrot_Interp interp, STRING *command) {
+  real_exception(interp, NULL, NOSPAWN, "Exec not implemented");
 }
 
 /*
