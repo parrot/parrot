8cc61a13d59fc9859391a60d2a78be12e4b3f2cc
Andy Lester <andy@petdance.com>
2007-07-09 18:12:05 +0000

    added a special typedef for Splint
    git-svn-id: https://svn.parrot.org/parrot/trunk@19725 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/generic/signal.h
+++ config/gen/platform/generic/signal.h
@@ -13,13 +13,16 @@
 #ifdef PARROT_HAS_HEADER_SIGNAL
 #  undef Parrot_set_sighandler
 #  ifdef PARROT_HAS___SIGHANDLER_T
+#    ifdef S_SPLINT_S
+    typedef void (*__sighandler_t)(int);
+#    endif
     typedef __sighandler_t Parrot_sighandler_t;
 #  else
     typedef void (*Parrot_sighandler_t) (int);
-#  endif
+#  endif /* PARROT_HAS___SIGHANDLER_T */
 
     Parrot_sighandler_t Parrot_set_sighandler(int s, Parrot_sighandler_t f);
-#endif
+#endif /* PARROT_HAS_HEADER_SIGNAL */
 
 #endif /* PARROT_PLATFORM_GENERIC_SIGNAL_H_GUARD */
 
