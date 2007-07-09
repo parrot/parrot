b08b18139bc87fa11883de94d9bb3dfd3445d55c
Paul Cochrane <paultcochrane@gmail.com>
2007-07-09 15:55:31 +0000

    Merging splint branch back to trunk (19718:19585).
    git-svn-id: https://svn.parrot.org/parrot/trunk@19719 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/generic/signal.c
+++ config/gen/platform/generic/signal.c
@@ -8,11 +8,13 @@
  * for now use signal based functions
  */
 
+#  ifndef S_SPLINT_S
 Parrot_sighandler_t
 Parrot_set_sighandler(int signum, Parrot_sighandler_t handler)
 {
     return signal(signum, handler);
 }
+#  endif
 #endif
 
 /*
