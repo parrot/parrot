a6c406edc3e5a4bb43d709c76174326399c1ce39
Will "Coke" Coleda <will@coleda.com>
2007-07-11 02:46:09 +0000

    [tcl] Actually be able to run the native tcl test suite for the first time in a while. (Run the tests the same way we do our own tests.)
    git-svn-id: https://svn.parrot.org/parrot/trunk@19779 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/tcl/tools/tcl_test.pl
+++ languages/tcl/tools/tcl_test.pl
@@ -177,8 +177,16 @@ END_TCL
     }
 
     $source = <<'END_SHEBANG' . $source . <<'END_CLEANUP';
-#!../../parrot tcl.pbc
+#!perl
+
+# the following lines re-execute this as a tcl script
+# the \ at the end of these lines makes them a comment in tcl \
+use lib qw(languages/tcl/lib tcl/lib lib ../lib ../../lib); # \
+use Tcl::Test; #\
+__DATA__
+
 source lib/test_more.tcl
+
 END_SHEBANG
 plan no_plan
 END_CLEANUP
