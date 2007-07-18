2148930860e638b96317716f849d75030309966a
Andy Lester <andy@petdance.com>
2007-07-18 16:55:30 +0000

    Removed string_fill_from_buffer.  Its marked as PARROT_API but is neither used nor tested.
    git-svn-id: https://svn.parrot.org/parrot/trunk@19967 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/string_primitives.h
+++ include/parrot/string_primitives.h
@@ -21,16 +21,6 @@ PARROT_API
 UINTVAL Parrot_char_digit_value( SHIM_INTERP, UINTVAL character );
 
 PARROT_API
-void string_fill_from_buffer( PARROT_INTERP,
-    NOTNULL(const void *buffer),
-    UINTVAL len,
-    NOTNULL(const char *encoding_name),
-    NULLOK(STRING *s) )
-        __attribute__nonnull__(1)
-        __attribute__nonnull__(2)
-        __attribute__nonnull__(4);
-
-PARROT_API
 void string_set_data_directory( PARROT_INTERP, const char *dir )
         __attribute__nonnull__(1);
 
