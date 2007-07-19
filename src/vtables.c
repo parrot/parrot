e428bc4a2f8509742597345c798a93a0146cce9a
Jerry Gay <Jerry.Gay@gmail.com>
2007-07-19 16:18:40 +0000

    Parrot_clone_vtable can't return null
    git-svn-id: https://svn.parrot.org/parrot/trunk@20016 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/vtables.c
+++ src/vtables.c
@@ -44,7 +44,7 @@ Clones C<*base_vtable> and returns a pointer to the new C<VTABLE>.
 
 PARROT_API
 PARROT_MALLOC
-PARROT_CAN_RETURN_NULL
+PARROT_CANNOT_RETURN_NULL
 VTABLE *
 Parrot_clone_vtable(SHIM_INTERP, NOTNULL(const VTABLE *base_vtable))
 {
