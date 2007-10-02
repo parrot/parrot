parrot/parrot

commit 7483c5bbc9382b0645c70dec6a1f248bd67515aa

[build]
Update c2str.pl so that multiple copies running simultaneously no longer
step on each other's toes drunkenly with a jackhammer.

With this fix (Courtesy of jhorwitz++), 'make -j 2' now works, tested
on a variety of platforms.

jhorwitz++
jhorwitz++
jhorwitz++
jhorwitz++



git-svn-id: https://svn.parrot.org/parrot/trunk@21613 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

tools/build/c2str.pl
@@ -19,6 +19,21 @@ use Getopt::Long;
 
 my $outfile          = 'all_cstring.str';
 my $string_private_h = 'src/string_private_cstring.h';
+my $lockfile         = "$outfile.lck";
+my $max_lock_wait    = 120;
+
+my $start_time = time;
+while ( -e $lockfile ) {
+    sleep 1;
+    if ( time - $start_time > $max_lock_wait ) {
+        die "Lock still held after $max_lock_wait seconds -- something is wrong!";
+    }
+}
+open my $lock, '>', $lockfile or die "Can't write '$lockfile': $!";
+print $lock "$$\n";
+close $lock;
+
+$SIG{'__DIE__'} = sub { unlink $lockfile };
 
 my ( $result, $do_all, $do_init, $file );
 $result = GetOptions(
@@ -29,10 +44,12 @@ $result = GetOptions(
 $do_all and do {
     &read_all;
     &create_c_include;
+    unlink $lockfile;
     exit;
 };
 $do_init and do {
     unlink $outfile;
+    unlink $lockfile;
     exit;
 };
 
@@ -48,6 +65,8 @@ my @all_strings;
 open my $ALL, '>>', $outfile or die "Can't write '$outfile': $!";
 process_cfile();
 close $ALL;
+
+unlink $lockfile;
 
 sub hash_val {
     my $h = Math::BigInt->new('+0');

lib/Parrot/Configure/Messages.pm
@@ -35,7 +35,6 @@ sub print_conclusion {
 Okay, we're done!
 
 You can now use `$make' to build your Parrot.
-(NOTE: do not use `$make -j <n>'!)
 After that, you can use `$make test' to run the test suite.
 
 Happy Hacking,

commit 30a239de2bcd1565470add32df9c79114cf075da

[tools] Fix a race condition in parallel builds introduced in r21613.
git-svn-id: https://svn.parrot.org/parrot/trunk@21757 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

tools/build/c2str.pl
@@ -13,6 +13,7 @@ use warnings;
 use strict;
 use lib 'lib';
 
+use Fcntl qw( :DEFAULT :flock );
 use Text::Balanced qw(extract_delimited);
 use Math::BigInt;
 use Getopt::Long;
@@ -20,20 +21,12 @@ use Getopt::Long;
 my $outfile          = 'all_cstring.str';
 my $string_private_h = 'src/string_private_cstring.h';
 my $lockfile         = "$outfile.lck";
-my $max_lock_wait    = 120;
 
-my $start_time = time;
-while ( -e $lockfile ) {
-    sleep 1;
-    if ( time - $start_time > $max_lock_wait ) {
-        die "Lock still held after $max_lock_wait seconds -- something is wrong!";
-    }
-}
-open my $lock, '>', $lockfile or die "Can't write '$lockfile': $!";
-print $lock "$$\n";
-close $lock;
+sysopen( my $lock, $lockfile, O_CREAT ) or die "Can't write '$lockfile': $!\n";
 
-$SIG{'__DIE__'} = sub { unlink $lockfile };
+flock($lock, LOCK_EX) or die "Can't lock '$lockfile': $!\n";
+
+END { close $lock; unlink $lockfile; }
 
 my ( $result, $do_all, $do_init, $file );
 $result = GetOptions(
@@ -42,14 +35,12 @@ $result = GetOptions(
 );
 
 $do_all and do {
-    &read_all;
-    &create_c_include;
-    unlink $lockfile;
+    read_all();
+    create_c_include();
     exit;
 };
 $do_init and do {
     unlink $outfile;
-    unlink $lockfile;
     exit;
 };
 
@@ -61,12 +52,10 @@ die "$0: $infile: $!" unless -e $infile;
 my %known_strings = ();
 my @all_strings;
 
-&read_all;
+read_all();
 open my $ALL, '>>', $outfile or die "Can't write '$outfile': $!";
 process_cfile();
 close $ALL;
-
-unlink $lockfile;
 
 sub hash_val {
     my $h = Math::BigInt->new('+0');