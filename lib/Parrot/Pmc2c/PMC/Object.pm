858333f71862d630f3d320d77e86938da0c2f085
Paul Cochrane <paultcochrane@gmail.com>
2007-10-18 16:29:03 +0000

    [pmc] Getting Parrot to build again with icc.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22208 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PMC/Object.pm
+++ lib/Parrot/Pmc2c/PMC/Object.pm
@@ -61,8 +61,8 @@ sub pre_method_gen {
         if (cur_class->vtable->base_type == enum_class_PMCProxy) {
             
             /* Get the PMC instance and call the vtable method on that. */
-            PMC * const del_object = VTABLE_get_attr_keyed(interp, SELF,
-                cur_class, CONST_STRING(interp, "proxy"));
+            PMC * const del_object = 
+                VTABLE_get_attr_keyed(interp, SELF, cur_class, CONST_STRING(interp, "proxy"));
 
             if (!PMC_IS_NULL(del_object)) {
                 ${return}VTABLE_$vt_method_name(interp, del_object$args); 
