4252afc03f7655be1ff96f6ef73b3af42f37e463
Paul Cochrane <paultcochrane@gmail.com>
2007-10-18 18:47:06 +0000

    [plumhead] Shortened some lines to be within the 100 char line limit
    git-svn-id: https://svn.parrot.org/parrot/trunk@22232 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/plumhead/lex_yy.c
+++ languages/plumhead/lex_yy.c
@@ -1377,14 +1377,12 @@ static void yyensure_buffer_stack (void)
 
         if (!(yy_buffer_stack)) {
 
-                /* First allocation is just for 2 elements, since we don't know if this
-                 * scanner will even need a stack. We use 2 instead of 1 to avoid an
-                 * immediate realloc on the next call.
-         */
+            /* First allocation is just for 2 elements, since we don't
+             * know if this scanner will even need a stack. We use 2
+             * instead of 1 to avoid an immediate realloc on the next call. */
                 num_to_alloc = 1;
-                (yy_buffer_stack) = (struct yy_buffer_state**)yyalloc
-                                                                (num_to_alloc * sizeof(struct yy_buffer_state*)
-                                                                );
+                (yy_buffer_stack) = (struct yy_buffer_state**)
+                    yyalloc(num_to_alloc * sizeof(struct yy_buffer_state*));
 
                 memset((yy_buffer_stack), 0, num_to_alloc * sizeof(struct yy_buffer_state*));
 
@@ -1399,13 +1397,13 @@ static void yyensure_buffer_stack (void)
                 int grow_size = 8 /* arbitrary grow size */;
 
                 num_to_alloc = (yy_buffer_stack_max) + grow_size;
-                (yy_buffer_stack) = (struct yy_buffer_state**)yyrealloc
-                                                                ((yy_buffer_stack),
-                                                                num_to_alloc * sizeof(struct yy_buffer_state*)
-                                                                );
+                (yy_buffer_stack) = (struct yy_buffer_state**)
+                    yyrealloc((yy_buffer_stack), 
+                            num_to_alloc * sizeof(struct yy_buffer_state*));
 
                 /* zero only the new slots.*/
-                memset((yy_buffer_stack) + (yy_buffer_stack_max), 0, grow_size * sizeof(struct yy_buffer_state*));
+                memset((yy_buffer_stack) + (yy_buffer_stack_max), 0,
+                        grow_size * sizeof(struct yy_buffer_state*));
                 (yy_buffer_stack_max) = num_to_alloc;
         }
 }
