04086c21f31ffc6f893e98ce2ec571a7764061ae
Klaas-Jan Stol <parrotcode@gmail.com>
2007-10-19 20:04:53 +0000

    make darwin/memalign.c pass codingstd.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22278 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/darwin/memalign.c
+++ config/gen/platform/darwin/memalign.c
@@ -39,7 +39,7 @@ Parrot_memalign(size_t align, size_t size)
     size_t padding = 0;
     size_t amountToAllocate = 0;
 
-    if ( effectiveAlign < sizeof (void *) )
+    if (effectiveAlign < sizeof (void *))
     {
         effectiveAlign = roundUpPowerOf2(sizeof (void *));
     }
@@ -48,7 +48,7 @@ Parrot_memalign(size_t align, size_t size)
         effectiveAlign = roundUpPowerOf2(effectiveAlign);
     }
 
-    if ( effectiveAlign < sizeof (memalign_marker_t) )
+    if (effectiveAlign < sizeof (memalign_marker_t))
     {
         padding = sizeof (memalign_marker_t);
     }
@@ -64,7 +64,7 @@ Parrot_memalign(size_t align, size_t size)
         kern_return_t status = vm_allocate(mach_task_self(), &p,
                                                 amountToAllocate, 1);
 
-        if ( status != KERN_SUCCESS )
+        if (status != KERN_SUCCESS)
         {
             return NULL;
         }
@@ -90,11 +90,11 @@ Parrot_memalign(size_t align, size_t size)
             marker->start = usedPageBase;
             marker->size = returnAddress + size - usedPageBase;
 
-            if ( usedPageBase > p )
+            if (usedPageBase > p)
             {
                 status = vm_deallocate(mach_task_self(), p, usedPageBase - p);
 
-                if ( status != KERN_SUCCESS )
+                if (status != KERN_SUCCESS)
                 {
                     fprintf(stderr,
                             "Parrot_memalign(%zx, %zx) failed to deallocate extra header space.\n",
@@ -104,12 +104,12 @@ Parrot_memalign(size_t align, size_t size)
 
             firstUnneededPage = roundUpToPageBoundary(returnAddress + size);
 
-            if ( firstUnneededPage < p + amountToAllocate )
+            if (firstUnneededPage < p + amountToAllocate)
             {
                 status = vm_deallocate(mach_task_self(), firstUnneededPage,
                                     p + amountToAllocate - firstUnneededPage);
 
-                if ( status != KERN_SUCCESS )
+                if (status != KERN_SUCCESS)
                 {
                     fprintf(stderr,
                             "Parrot_memalign(%zx, %zx) failed to deallocate extra footer space.\n",
@@ -130,7 +130,7 @@ Parrot_free_memalign(void *p)
     kern_return_t status = vm_deallocate(mach_task_self(),
                                             marker->start, marker->size);
 
-    if ( status != KERN_SUCCESS )
+    if (status != KERN_SUCCESS)
     {
         fprintf(stderr, "Parrot_free_memalign(%p) failed!\n", p);
     }
