539409d36dde19d488ebdf30374e1cd3d7a26ce7
Klaas-Jan Stol <parrotcode@gmail.com>
2007-10-21 19:46:06 +0000

    compilers/pirc/new: *add pircompiler.h needed to build the new pir compiler.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22370 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/pirc/new/pircompiler.h
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ * Copyright (C) 2007, The Perl Foundation.
+ */
+
+#ifndef PARROT_PIR_PIRCOMPILER_H_GUARD
+#define PARROT_PIR_PIRCOMPILER_H_GUARD
+
+/* forward declaration, lexer_state definition must stay in pir.l */
+struct lexer_state;
+
+/* constructor for a lexer_state object */
+struct lexer_state *new_lexer(char *filename);
+
+/* accessor functions for parse_errors in the specified lexer. */
+extern int  get_parse_errors(struct lexer_state *lexer);
+extern void parse_error(struct lexer_state *lexer);
+
+extern int get_line_nr(struct lexer_state *lexer);
+extern char const *get_current_file(struct lexer_state *lexer);
+
+#endif
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
+
