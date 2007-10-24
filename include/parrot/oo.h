914529ddc3dfce6316a34aaf15430b6ea17553ed
Ronald Blaschke <ron@rblasch.org>
2007-10-24 18:16:52 +0000

    [core] Headerize src/oo.c to fix build on Windows.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22452 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/oo.h
+++ include/parrot/oo.h
@@ -17,6 +17,13 @@
 
 /* HEADERIZER BEGIN: src/oo.c */
 
+PARROT_API
+PARROT_CAN_RETURN_NULL
+PARROT_WARN_UNUSED_RESULT
+PMC * Parrot_oo_get_class_str(PARROT_INTERP, NOTNULL(STRING *name))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(2);
+
 void Parrot_oo_extract_methods_from_namespace(PARROT_INTERP,
     NOTNULL(PMC *self))
         __attribute__nonnull__(1)
@@ -43,12 +50,6 @@ PMC * Parrot_oo_find_vtable_override_for_class(PARROT_INTERP,
 PARROT_CAN_RETURN_NULL
 PARROT_WARN_UNUSED_RESULT
 PMC * Parrot_oo_get_class(PARROT_INTERP, NOTNULL(PMC *key))
-        __attribute__nonnull__(1)
-        __attribute__nonnull__(2);
-
-PARROT_CAN_RETURN_NULL
-PARROT_WARN_UNUSED_RESULT
-PMC * Parrot_oo_get_class_str(PARROT_INTERP, NOTNULL(STRING *name))
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
 
