4eae23610b893b3f8480f6eec05859539e79aecb
Klaas-Jan Stol <parrotcode@gmail.com>
2007-10-30 12:50:34 +0000

    pirc/macro: * remove tabs.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22618 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/pirc/macro/macroparser.h
+++ compilers/pirc/macro/macroparser.h
@@ -86,7 +86,7 @@ typedef union YYSTYPE
 }
 /* Line 1489 of yacc.c.  */
 #line 89 "macroparser.h"
-	YYSTYPE;
+    YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
