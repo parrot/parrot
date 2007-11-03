6e2627670466e1da90cb723db2bfdd7491a5f5f3
Klaas-Jan Stol <parrotcode@gmail.com>
2007-11-03 12:12:27 +0000

    [lib] new heredoc preprocessor is an exempted file.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22671 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Distribution.pm
+++ lib/Parrot/Distribution.pm
@@ -360,6 +360,7 @@ This is to exclude automatically generated C-language files Parrot might have.
             compilers/pirc/macro/macrolexer.h
             compilers/pirc/macro/macroparser.c
             compilers/pirc/macro/macroparser.h
+            compilers/pirc/heredoc/hdocprep.l
             languages/cola/lexer.c
             languages/cola/parser.c
             languages/cola/parser.h
