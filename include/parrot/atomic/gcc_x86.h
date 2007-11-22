52e678d27dc516041227a5da718606931594d3c3
Paul Cochrane <paultcochrane@gmail.com>
2007-11-22 12:45:09 +0000

    [core,config] Reverting r22938 as it broke the build on Windows.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22959 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/atomic/gcc_x86.h
+++ include/parrot/atomic/gcc_x86.h
@@ -28,8 +28,22 @@ typedef struct Parrot_atomic_integer {
  * reuse existing code
  */
 PARROT_INLINE
-void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
-                                        void *update);
+static void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
+                                        void *update)
+{
+#if defined(PARROT_HAS_X86_64_GCC_CMPXCHG)
+    __asm__ __volatile__("lock\n"
+                         "cmpxchgq %1,%2":"=a"(expect):"q"(update), "m"(*ptr),
+                         "0"(expect)
+                         :"memory");
+#else
+    __asm__ __volatile__("lock\n"
+                         "cmpxchgl %1,%2":"=a"(expect):"q"(update), "m"(*ptr),
+                         "0"(expect)
+                         :"memory");
+#endif
+    return expect;
+}
 
 #define PARROT_ATOMIC_PTR_GET(result, a) (result = (a).val)
 
@@ -70,7 +84,18 @@ void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
     } while (0)
 
 PARROT_INLINE
-long parrot_i386_xadd(volatile long *l, long amount);
+static long parrot_i386_xadd(volatile long *l, long amount)
+{
+    long result = amount;
+#if defined(PARROT_HAS_X86_64_GCC_CMPXCHG)
+    __asm__ __volatile__("lock\n" "xaddq %0, %1" : "=r"(result), "=m"(*l) :
+            "0"(result), "m"(*l));
+#else
+    __asm__ __volatile__("lock\n" "xaddl %0, %1" : "=r"(result), "=m"(*l) :
+            "0"(result), "m"(*l));
+#endif
+    return result + amount;
+}
 
 #define PARROT_ATOMIC_INT_INC(result, a) \
     do { \
