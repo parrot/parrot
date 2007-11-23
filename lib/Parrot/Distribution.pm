f2dc8bff69f0f4aa1f3067cf75d31a11687425e2
Klaas-Jan Stol <parrotcode@gmail.com>
2007-11-23 19:22:19 +0000

    [t,lib] exempt new pirc/new files from codingstd tests.
    git-svn-id: https://svn.parrot.org/parrot/trunk@22996 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Distribution.pm
+++ lib/Parrot/Distribution.pm
@@ -358,6 +358,8 @@ This is to exclude automatically generated C-language files Parrot might have.
             compilers/pirc/new/pirlexer.h
             compilers/pirc/new/pirparser.c
             compilers/pirc/new/pirparser.h
+            compilers/pirc/new/pircompunit.c
+            compilers/pirc/new/pircompunit.h
             compilers/pirc/macro/lexer.h
             compilers/pirc/macro/macro.h
             compilers/pirc/macro/macro.l
--- t/codingstd/linelength.t
+++ t/codingstd/linelength.t
@@ -137,6 +137,8 @@ compilers/pirc/new/main.c
 compilers/pirc/new/pirlexer.c
 compilers/pirc/new/pirlexer.h
 compilers/pirc/new/pirparser.c
+compilers/pirc/new/pircompunit.h
+compilers/pirc/new/pircompunit.c
 compilers/pirc/new/pasm.l
 compilers/pirc/new/pasm.y
 compilers/pirc/macro/macro.h
