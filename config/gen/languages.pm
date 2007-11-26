fc9cff2bcf802257bbdac338ec531500a20478e2
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2007-11-26 20:36:03 +0000

    #47792: [BUG]: languages/dotnet/Configure.pl causes configuration error Add support for dotnet again, after having cleaned up languages/dotnet/Configure.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@23094 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/languages.pm
+++ config/gen/languages.pm
@@ -9,11 +9,17 @@ config/gen/languages.pm - Build files for language implementations
 
 Config step for languages.
 Builds C<languages/Makefile> and loops over list of languages.
-Special support for C<languages/tcl>.
+Special support for C<languages/tcl> and C<languages/dotnet>.
 
 A space seperated list of languages can be passed in with
 the option 'languages'.
-An empty list of languages is OK.
+An empty list of languages is OK. This means that only C<languages/Makefile>
+will be regenerated.
+
+=head1 TODO
+
+RT#31633. Languages should know how to configure themselves
+and not rely on gen::languages.
 
 =cut
 
@@ -43,14 +49,31 @@ sub runstep {
 
     my $languages = $conf->options->get('languages');
     $languages = qq{
-        APL amber abc befunge bf cardinal c99 cola eclectus
-        ecmascript forth HQ9plus jako lisp lua m4 nqp ook
-        parrot_compiler perl5 perl6 pheme PIR plumhead pugs punie
-        pynie regex scheme tap tcl urm WMLScript Zcode
+        APL amber abc
+        befunge bf
+        cardinal c99 cola
+        dotnet
+        eclectus ecmascript
+        forth
+        HQ9plus jako
+        lisp lua
+        m4
+        nqp
+        ook
+        parrot_compiler perl5 perl6 pheme PIR plumhead pugs punie pynie
+        regex
+        scheme
+        tap tcl
+        urm
+        WMLScript
+        Zcode
     } unless defined $languages;
 
     foreach my $language ( split ' ', $languages ) {        # split ' ' splits on all whitespace
-        if ( $language eq 'tcl' ) {
+        if ( $language eq 'dotnet' ) {
+            system( 'cd languages/dotnet; perl Configure.pl' );
+        }
+        elsif ( $language eq 'tcl' ) {
             # tcl has more than one Makefile
             # currently this is handled as a special case
             genfile(
