cbbf8eb930cf37b60e4ea8a1df8b128af6f35335
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2007-11-26 20:50:27 +0000

    #31633: [TODO] Allow Languages to be self contained Add just some comments in languages/plumhead/Configure.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@23095 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/plumhead/Configure.pl
+++ languages/plumhead/Configure.pl
@@ -1,17 +1,30 @@
 # $Id$
 
-# Copyright (C) 2006, The Perl Foundation.
+# Copyright (C) 2006-2007, The Perl Foundation.
+
+# Configuration script for Plumhead.
+
+# RT#31633: need to steal from gen::languages and tools/dev/reconfigure.pl
+#           in order to make this work
+
+package main;
 
 # pragmata
 use strict;
 use warnings;
-use 5.006_001;
+use 5.008;
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
 
-package main;
+# Parrot specific Perl modules
+use Parrot::Configure::Step  ();
+
+# RT#31633: This is too simplistic
+# Parrot::Configure::Step::genfile( 'config/makefiles/root.in' => 'Makefile' );
 
 print <<'END_TEXT';
 Hi, 
-I'm Configure.pl. Currently I'm doing nothing.
+I'm Configure.pl. Eventually I'll generate Makefile.
 END_TEXT
 
 # Local Variables:
