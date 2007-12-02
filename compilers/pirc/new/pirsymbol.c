84469c9cd76c849d77c0faf7eb72e4b75ef9b2c7
Paul Cochrane <paultcochrane@gmail.com>
2007-12-02 14:03:29 +0000

    [pirc] Added function documentation.
    git-svn-id: https://svn.parrot.org/parrot/trunk@23363 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/pirc/new/pirsymbol.c
+++ compilers/pirc/new/pirsymbol.c
@@ -7,21 +7,63 @@
 #include "pircompunit.h"
 #include <stdlib.h>
 
+/*
+
+=head1 NAME
+
+compilers/pirc/new/pirsymbol.c
+
+=head1 DESCRIPTION
+
+TODO: Not yet documented!!!
+
+=head2 Functions
+
+=over 4
+
+=cut
+
+*/
+
+/*
+
+=item C<void
+declare_local(struct lexer_state *lexer, pir_type type, target *list)>
+
+TODO: Not yet documented!!!
+
+=cut
+
+*/
 void
-declare_local(struct lexer_state *lexer, pir_type type, target *list) {
+declare_local(struct lexer_state *lexer, pir_type type, target *list)
+{
 
 }
 
 /*
+=item C<target *
+find_symbol(struct lexer_state *lexer, char * const name)>
 
 Return the target node for the symbol or NULL if the symbol
 is not defined.
 
+=cut
+
 */
 target *
-find_symbol(struct lexer_state *lexer, char * const name) {
+find_symbol(struct lexer_state *lexer, char * const name)
+{
     return NULL;
 }
+
+/*
+
+=back
+
+=cut
+
+*/
 
 /*
  * Local variables:
