657b715faf071ee734cf5a56b3edfa9d09ac4931
Paul Cochrane <paultcochrane@gmail.com>
2007-12-03 21:36:43 +0000

    [core] Added function documentation.
    git-svn-id: https://svn.parrot.org/parrot/trunk@23420 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/atomic/gcc_x86.c
+++ src/atomic/gcc_x86.c
@@ -2,23 +2,47 @@
  *  Copyright (C) 2006, The Perl Foundation.
  *  SVN Info
  *     $Id$
- *  Overview:
- *     An implementation of atomic
- *     operations on x86 platforms with GCC-style
- *     inline assembly suppport.
- *  Data Structure and Algorithms:
- *  History:
- *  Notes:
- *  References:
  */
 
 #include "parrot/parrot.h"
 #include "parrot/atomic/gcc_x86.h"
 
 /*
+
+=head1 NAME
+
+src/atomic/gcc_x86.c
+
+=head1 DESCRIPTION
+
+An implementation of atomic operations on x86 platforms with GCC-style inline
+assembly suppport.
+
+=head2 Functions
+
+=over 4
+
+=cut
+
+*/
+
+/*
  * if both I386 and X86_64 cmpxchg are defined, we are on x86_64 -
  * reuse existing code
  */
+/*
+
+=item C<PARROT_INLINE
+void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
+                                        void *update)>
+
+TODO: Not yet documented!!!
+
+=cut
+
+*/
+
+
 PARROT_INLINE
 void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
                                         void *update)
@@ -37,6 +61,18 @@ void *parrot_i386_cmpxchg(void *volatile *ptr, void *expect,
     return expect;
 }
 
+/*
+
+=item C<PARROT_INLINE
+long parrot_i386_xadd(volatile long *l, long amount)>
+
+TODO: Not yet documented!!!
+
+=cut
+
+*/
+
+
 PARROT_INLINE
 long parrot_i386_xadd(volatile long *l, long amount)
 {
@@ -50,6 +86,14 @@ long parrot_i386_xadd(volatile long *l, long amount)
 #endif
     return result + amount;
 }
+
+/*
+
+=back
+
+=cut
+
+*/
 
 /*
  * Local variables:
