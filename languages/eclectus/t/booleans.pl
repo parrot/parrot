dbfcfe826cd8456e14d9297e335049348daed5b8
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2007-12-11 20:56:25 +0000

    [Eclectus] Start on checking, whether 'petite' is available.
    git-svn-id: https://svn.parrot.org/parrot/trunk@23761 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/eclectus/t/booleans.pl
+++ languages/eclectus/t/booleans.pl
@@ -9,11 +9,26 @@ use strict;
 use warnings;
 
 use FindBin;
+use lib "$FindBin::Bin/../../../lib";
+
+use Test::More;
+use Data::Dumper;
 
 ( my $t_fn = $FindBin::Script ) =~ s/\.pl$/\.t/;
 
-chdir 'eclectus';
-exec 'petite', '--script', "t/$t_fn";
+# 'petite' is Petite Chez Scheme
+# 7.3 is the current version
+my $petite_version = `petite --version 2>&1`;
+my $has_petite = $petite_version =~ m/7/xms;
+diag( Dumper( $petite_version, $has_petite ) );
+
+if ( ! $has_petite ) {
+    plan skip_all => 'petite is needed for running this test';
+}
+else {
+    chdir 'eclectus';
+    exec 'petite', '--script', "t/$t_fn";
+}
 
 # Local Variables:
 #   mode: cperl
