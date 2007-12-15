b8ef98e0b8d08504b9da1afeee3f960e5321269e
Klaas-Jan Stol <parrotcode@gmail.com>
2007-12-15 13:41:03 +0000

    [PIR] more updates to actions.pm
    git-svn-id: https://svn.parrot.org/parrot/trunk@23933 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/PIR/src/actions.pm
+++ languages/PIR/src/actions.pm
@@ -23,20 +23,112 @@ method compilation_unit($/, $key) {
 }
 
 method sub_def($/) {
-    my $sub := PAST::Block.new( :blocktype('declaration'),
-                                :node($/) );
-
+    my $sub := PAST::Block.new( :blocktype('declaration'), :node($/) );
     my $subname := $($<sub_id>);
     $sub.name($subname);
+
+    if $<param_decl> {
+        my @params := $<param_decl>;
+        for @params {
+            $sub.push( $($_) );
+        }
+    }
+
+    if $<labeled_pir_instr> {
+        my @instructions := $<labeled_pir_instr>;
+        my $statements := PAST::Stmts.new( :node($/) );
+        for @instructions {
+            $statements.push( $($_) );
+        }
+        $sub.push($statements);
+    }
     make $sub;
+}
+
+method labeled_pir_instr($/) {
+    my $instr;
+    if $<instr> {
+        $instr := $($<instr>);
+    }
+
+    if $<label> {
+
+    }
+    make $instr;
+}
+
+method instr($/, $key) {
+    make $( $/{$key} );
+}
+
+method pir_instr($/, $key) {
+    make $( $/{$key} );
+}
+
+method local_decl($/) {
+    my $declarations := PAST::Stmts.new( :node($/) );
+    my $type := $( $<pir_type> );
+    my @ids := $<local_id>;
+
+    for @ids {
+        $($_);
+    }
+    make $declarations;
 }
 
 method sub_id($/, $key) {
     make $( $/{$key} );
 }
 
+method param_decl($/) {
+    my $param := $( $<parameter> );
+    make $param;
+}
+
+method parameter($/) {
+    my $parameter := $($<IDENT>);
+    my $type := $($<pir_type>);
+    $parameter.type($type);
+    $parameter.scope('parameter');
+    make $parameter;
+}
+
+method pir_type($/) {
+    make PAST::Val.new( :type('string'), :value(~$/), :node($/) );
+}
+
+method expression($/, $key) {
+    make $( $/{$key} );
+}
+
+method simple_expr($/, $key) {
+    make $( $/{key} );
+}
+
+method unary_expr($/) {
+
+}
+
+method binary_expr($/) {
+
+}
+
+method constant($/) {
+
+}
+
+method target($/) {
+
+}
+
+method key($/) {
+    make $( $<simple_expr> );
+}
+
 method IDENT($/) {
-    make PAST::Val.new( :value(~$/), :node($/) );
+    print("IDENT");
+    print(~$/);
+    make PAST::Var.new( :name($<name>), :node($/) );
 }
 
 # Local Variables:
