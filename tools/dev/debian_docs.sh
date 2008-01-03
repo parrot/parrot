549bb9c3d56f15f5bb62900c4820784f592b434b
Allison Randal <allison@parrot.org>
2008-01-03 11:05:04 +0000

    [tools] Add an initial script for generating debian package document file list, regenerate manifest and manifest skip.
    git-svn-id: https://svn.parrot.org/parrot/trunk@24479 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ tools/dev/debian_docs.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+# 		debian_docs.sh 	Specified in RT #49274
+
+
+# A script to generate the file ports/debian/parrot-doc.docs 
+# Creates a complete list of all files (but not directories) in the docs/ 
+# directory, with repository-relative paths to the files, e.g. docs/ops/math.pod
+
+run_from=($PWD) # Save starting directory for error message
+# Assuming run from top-level parrot directory as:
+#    sh tools/dev/debian_docs.sh
+
+if [ $(basename $PWD) != parrot ] 
+then 
+    echo "Should be run from parrot/, not $run_from"
+    exit
+fi
+
+find docs -name "\.svn" -prune -type f -o -type f  > ports/debian/parrot-doc.docs
+
+#		End debian_docs.sh
