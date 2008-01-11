9fbe8754613a5c7b80e4aa8d243a3fd11bff5ee8
James E Keenan (Jim) <jkeenan@cpan.org>
2008-01-11 21:55:02 +0000

    Eliminating superseded comment in order to resolve http://rt.perl.org/rt3/Ticket/Display.html?id=43170.
    git-svn-id: https://svn.parrot.org/parrot/trunk@24764 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/inter/lex.pm
+++ config/inter/lex.pm
@@ -76,8 +76,6 @@ sub runstep {
         }
         else {
 
-            # RT#43170 should --ask be handled like the other user defines or
-            # checked for version requirements?
             if ( $conf->options->get('ask') ) {
                 $prog = prompt( $prompt, $prog ? $prog : $conf->data->get($util) );
             }
--- config/inter/yacc.pm
+++ config/inter/yacc.pm
@@ -77,8 +77,6 @@ sub runstep {
         }
         else {
 
-            # RT#43170 should --ask be handled like the other user defines or
-            # checked for version requirements?
             if ( $conf->options->get('ask') ) {
                 $prog = prompt( $prompt, $prog ? $prog : $conf->data->get($util) );
             }
