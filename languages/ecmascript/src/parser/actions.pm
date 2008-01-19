26236048df3de7f1d8b5a406ca993a66bd8f0740
Klaas-Jan Stol <parrotcode@gmail.com>
2008-01-19 11:44:30 +0000

    [ecmascript] * add rule for comment (which was missing, and is just to group multi and single line comments as alternatives) * add [ .. ]>> to keyword and futurekeywords, to make sure they have a word boundary at the end. * add action for statement list and block.
    git-svn-id: https://svn.parrot.org/parrot/trunk@24997 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/ecmascript/src/parser/actions.pm
+++ languages/ecmascript/src/parser/actions.pm
@@ -12,3 +12,27 @@ method statement($/) {
     make PAST::Block.new( PAST::Stmts.new(), :node($/) );
 }
 
+method statement($/,$key) {
+    make $( $/{$key} );
+}
+
+
+method block($/) {
+    my $past := PAST::Block.new( :node($/) );
+    if $<statement_list> {
+        $past.push( $<statement_list>[0] );
+    }
+    else {
+        $past.push( PAST::Stmts.new( :node($/) ) );
+    }
+    make $past;
+}
+
+method statement_list($/) {
+    my $past := PAST::Stmts.new( :node($/) );
+    for $<statement> {
+        $past.push( $( $_ ) );
+    }
+    make $past;
+}
+
