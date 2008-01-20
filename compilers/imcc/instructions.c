bf341e5eee6f6cb3d370c7720095a87842773b31
Andy Lester <andy@petdance.com>
2008-01-20 19:44:09 +0000

    consted the Emitter table
    git-svn-id: https://svn.parrot.org/parrot/trunk@25063 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/imcc/instructions.c
+++ compilers/imcc/instructions.c
@@ -64,7 +64,7 @@ static int e_file_open(SHIM_INTERP, ARGIN(void *param))
 
 static const char types[] = "INPS";
 
-Emitter emitters[2] = {
+const Emitter emitters[] = {
     {e_file_open,
      e_file_emit,
      NULL,
