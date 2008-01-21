6daa997ffb93164f7903854d9700fbb05a5b0b71
Klaas-Jan Stol <parrotcode@gmail.com>
2008-01-21 16:49:23 +0000

    [ecmascript] * put whole grammar in comments * start reconstructing grammar based on this, but piece by piece, **keeping it working** by adding actions too. It needs a lot of work. This task involves more complex parts, but also a lot of beginners tasks (for instance adding a new statement type, like "while". Perfect for learning PCT!).
    git-svn-id: https://svn.parrot.org/parrot/trunk@25100 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/ecmascript/src/parser/actions.pm
+++ languages/ecmascript/src/parser/actions.pm
@@ -3,36 +3,71 @@
 class JS::Grammar::Actions;
 
 method TOP($/) {
-    my $past := $( $<statement> );
-    $past.blocktype('declaration');
+    my $past := PAST::Stmts.new( :node($/) );
+    for $<source_element> {
+        $past.push( $( $_ ) );
+    }
+    #my $past := $( $<source_element> );
+    #$past.blocktype('declaration');
     make $past;
 }
 
-method statement($/) {
-    make PAST::Block.new( PAST::Stmts.new(), :node($/) );
-}
-
-method statement($/,$key) {
+method source_element($/, $key) {
     make $( $/{$key} );
 }
 
+method function_declaration($/) {
+    my $past := $( $<block> );
+    $past.blocktype('declaration');
+    $past.name( $( $<identifier> ).name() );
+    make $past;
+}
+
+method statement($/, $key) {
+   make $( $/{$key} );
+}
 
 method block($/) {
     my $past := PAST::Block.new( :node($/) );
-    if $<statement_list> {
-        $past.push( $<statement_list>[0] );
+    my $stmt := PAST::Stmts.new( :node($/) );
+    for $<statement> {
+        $stmt.push( $( $_ ) );
     }
-    else {
-        $past.push( PAST::Stmts.new( :node($/) ) );
+    $past.push($stmt);
+    make $past;
+}
+
+method if_statement($/) {
+    my $past := PAST::Op.new( :pasttype('if'), :node($/) );
+    $past.push( $( $<expression> ) );
+    $past.push( $( $<statement> ) );
+    if $<else> {
+        $past.push( $( $<else> ) );
     }
     make $past;
 }
 
-method statement_list($/) {
-    my $past := PAST::Stmts.new( :node($/) );
-    for $<statement> {
-        $past.push( $( $_ ) );
-    }
-    make $past;
+method expression($/, $key) {
+   make $( $/{$key} );
+}
+
+method identifier($/) {
+    make PAST::Var.new( :name(~$/), :scope('package'), :node($/) );
+}
+
+method literal($/, $key) {
+    make $( $/{$key} );
+}
+
+method string_literal($/) {
+    make PAST::Val.new( :value( ~$<string_literal> ), :node($/) );
+}
+
+method numeric_literal($/) {
+    make $( $<decimal_literal> );
+}
+
+method decimal_literal($/) {
+    make PAST::Val.new( :value( ~$/ ), :returns('Integer'), :node( $/ ) );
 }
 
