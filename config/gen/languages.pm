b87b1c0bfae58c0a14b71ec75e76b6cb23859cf2
Jerry Gay <Jerry.Gay@gmail.com>
2008-01-30 14:44:52 +0000

    [config] reduce repetition in language makefile generation step
    git-svn-id: https://svn.parrot.org/parrot/trunk@25361 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/languages.pm
+++ config/gen/languages.pm
@@ -66,6 +66,7 @@ sub runstep {
     } unless defined $languages;
 
     foreach my $language ( split ' ', $languages ) {        # split ' ' splits on all whitespace
+        my $langdir = "languages/$language";
         if ( $language eq 'dotnet' ) {
             # RT#47792
             # languages/dotnet/Configure.pl works only after the root 'perl Configure.pl'.
@@ -74,14 +75,14 @@ sub runstep {
         elsif ( $language eq 'tcl' ) {
             # tcl has more than one Makefile
             # currently this is handled as a special case
-            $conf->genfile("languages/$language/config/makefiles/examples.in" => "languages/$language/examples/Makefile"
+            $conf->genfile("$langdir/config/makefiles/examples.in" => "$langdir/examples/Makefile"
             );
-            $conf->genfile("languages/$language/config/makefiles/root.in"     => "languages/$language/Makefile",
+            $conf->genfile("$langdir/config/makefiles/root.in"     => "$langdir/Makefile",
                 expand_gmake_syntax                                => 1,
             );
         }
         else {
-            $conf->genfile("languages/$language/config/makefiles/root.in"     => "languages/$language/Makefile"
+            $conf->genfile("$langdir/config/makefiles/root.in"     => "$langdir/Makefile"
             );
         }
     }
