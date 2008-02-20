d8cb51c5916adeb8dc033ba3ba531aa74e01dc35
chromatic <chromatic@wgz.org>
2008-02-20 05:44:32 +0000

    [config] Added hints for QNX (RT #49818, Matt Kraai).
    git-svn-id: https://svn.parrot.org/parrot/trunk@25893 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ config/init/hints/nto.pm
@@ -0,0 +1,22 @@
+# Copyright (C) 2008, The Perl Foundation.
+# $Id$
+
+package init::hints::nto;
+
+use strict;
+use warnings;
+
+sub runstep {
+    my ( $self, $conf ) = @_;
+
+    $conf->data->set( memalign => 'posix_memalign' );
+}
+
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
