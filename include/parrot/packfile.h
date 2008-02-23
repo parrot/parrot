b16784111a3da851445a5103e8c724c1b6d45527
James E Keenan (Jim) <jkeenan@cpan.org>
2008-02-23 14:24:40 +0000

    In two locations, eliminate excess wordspace before parens, thereby enabling this file to get past t/codingstd/c_parens.t.
    git-svn-id: https://svn.parrot.org/parrot/trunk@26023 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/packfile.h
+++ include/parrot/packfile.h
@@ -227,8 +227,8 @@ typedef struct PackFile_Directory {
 
 
 typedef opcode_t (*packfile_fetch_op_t)(ARGIN(const unsigned char *));
-typedef INTVAL   (*packfile_fetch_iv_t)(ARGIN(const unsigned char *));
-typedef void     (*packfile_fetch_nv_t)(ARGOUT(unsigned char *), ARGIN(const unsigned char *));
+typedef INTVAL (*packfile_fetch_iv_t)(ARGIN(const unsigned char *));
+typedef void (*packfile_fetch_nv_t)(ARGOUT(unsigned char *), ARGIN(const unsigned char *));
 
 typedef struct PackFile {
     /* the packfile is its own directory */
