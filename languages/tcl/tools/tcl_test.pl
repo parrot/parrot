91410c664ff163338ade92d66054010287dd4e65
Will "Coke" Coleda <will@coleda.com>
2008-03-12 15:23:53 +0000

    [tcl]
    Now that 8.5.0 is released, when running the tcl tests, specifically
    use the 8.5.0 tests, not HEAD.
    
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@26330 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/tcl/tools/tcl_test.pl
+++ languages/tcl/tools/tcl_test.pl
@@ -92,12 +92,14 @@ sub convert_tests {
 sub checkout_tests {
     print "Checking out tests from CVS\n";
 
+    my $tag = "core-8-5-0"; # For the version we're targeting.
+
     my $command =
           "cvs -z3 -d :pserver:anonymous:\@tcl.cvs.sourceforge.net:"
-        . "/cvsroot/tcl co -d $DIR tcl/tests";
+        . "/cvsroot/tcl co -d $DIR -r $tag tcl/tests";
     my $rc = system $command;
 
-    return ( $rc == 0 );    # just care if it failed, not howm
+    return ( $rc == 0 );    # just care if it failed, not how
 }
 
 ##
