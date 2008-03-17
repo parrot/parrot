53c67f56f47e7c8a596c6f1ab3752d0a452951b1
François Perrad <francois.perrad@gadz.org>
2008-03-17 09:14:51 +0000

    [build] - fix the following error src\packdump.c:39: error: nonnull argument references non-pointer operand (arg 1, operand 2)
    git-svn-id: https://svn.parrot.org/parrot/trunk@26446 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/packdump.c
+++ src/packdump.c
@@ -35,8 +35,7 @@ static void PackFile_Constant_dump(PARROT_INTERP,
         __attribute__nonnull__(3);
 
 static void pobj_flag_dump(PARROT_INTERP, ARGIN(long flags))
-        __attribute__nonnull__(1)
-        __attribute__nonnull__(2);
+        __attribute__nonnull__(1);
 
 /* HEADERIZER END: static */
 
