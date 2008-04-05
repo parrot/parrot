9d5f606eeec183da354d7db9fa04c4e06b9e3e17
James E Keenan (Jim) <jkeenan@cpan.org>
2008-04-05 23:53:22 +0000

    Revised per Coke's specifications in http://rt.perl.org/rt3/Ticket/Display.html?id=52472.
    git-svn-id: https://svn.parrot.org/parrot/trunk@26792 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/dev/list_unjitted.pl
+++ tools/dev/list_unjitted.pl
@@ -1,60 +1,90 @@
-#! perl -l
-
-# Copyright (C) 2004-2006, The Perl Foundation.
+#! perl
+# Copyright (C) 2001-2008, The Perl Foundation.
 # $Id$
-
 use warnings;
 use strict;
+use Carp;
 
-unless ( 1 == scalar @ARGV ) {
-    print "Must give a directory as an argument.\nPossible options are:";
-    opendir my $DIR, 'jit' or die $!;
-    while ( my $dir = readdir $DIR ) {
-        next if $dir =~ /^\./ or $dir eq '\.svn';
-        print $dir;
-    }
-    exit 1;
-}
+my $srcjit = q{src/jit};
+opendir my $DIRH, $srcjit
+    or croak "Unable to open $srcjit for reading: $!";
+my %jitted_platforms =
+    map { $_ => 1 }
+    grep { -d qq[$srcjit/$_] and ! m|^\.| and -f qq[$srcjit/$_/core.jit]  }
+    readdir $DIRH;
+closedir $DIRH or croak "Unable to close $srcjit after reading: $!";
 
-my @ops;
-my @jitted;
-
-open my $OPS, '<', 'ops/ops.num' or die "Damn can not open 'ops/ops.num': $!";
-
+my %ops;
+my $opsnum = 'src/ops/ops.num';
+open my $OPS, '<',  $opsnum or croak "Cannot open $opsnum for reading: $!";
 while (<$OPS>) {
+    chomp;
     next if /^\s*#/ or /^\s*$/;
     if ( my ($op) = /(\S+)\s+\d+/ ) {
-        push @ops, $op;
+        $ops{$op}++;
     }
-    else { die "Misformated line: $."; }
+    else {
+        croak "Malformed line in $opsnum: $.";
+    }
 }
+close $OPS or croak "Unable to close $opsnum after reading: $!";
 
-close $OPS or die "Noo.. $!";
+croak "Must supply single jitted platform as command-line argument"
+    unless ( @ARGV == 1 and $jitted_platforms{$ARGV[0]} );
 
-open my $JIT, '<', "jit/$ARGV[0]/core.jit"
-    or die "Could not open jit/$ARGV[0]/core.jit: $!";
-
+my $jit = shift(@ARGV);
+my $jitfile = qq{$srcjit/$jit/core.jit};
+open my $JIT, '<', $jitfile
+    or croak "Unable to open $jitfile for reading: $!";
+my %jitted;
 while (<$JIT>) {
-    if ( my ($jit) = /^Parrot_(\S+)\s*\{\s*$/ ) {
-        push @jitted, $jit;
+    chomp;
+    if ( my ($j) = /^Parrot_(\S+)\s*\{\s*$/ ) {
+        $jitted{$j}++;
     }
 }
-close $JIT or die "Noo.. $!";
+close $JIT or croak "Unable to close $jitfile after reading: $!";
 
 my @not_jitted;
-
-OP: foreach my $op (@ops) {
-    foreach my $jit (@jitted) {
-        next OP if $jit eq $op;
-    }
-    push @not_jitted, $op;
+foreach my $op (keys %ops) {
+    push @not_jitted, $op unless $jitted{$op};
 }
 
-print foreach @not_jitted;
+printf "%-9s%9s\n",  ( 'Platform:',   $jit );
+printf "%-13s%5d\n", ( 'Not jitted:', scalar @not_jitted );
+printf "%-13s%5d\n", ( 'Jitted:'    , scalar keys %jitted );
+printf "%-13s%5d\n", ( 'Total ops:' , scalar keys %ops );
 
-print 'Not jitted: ', scalar @not_jitted;
-print 'Jitted: ',     scalar @jitted;
-print 'Total ops: ',  scalar @ops;
+=head1 NAME
+
+tools/dev/list_unjitted.pl
+
+=head1 ABSTRACT
+
+For a given platform, report the number of ops JITted or not JITted.
+
+=head1 USAGE
+
+Called from top-level directory of Parrot distribution:
+
+    perl tools/dev/list_unjitted.pl [platform]
+
+... where I<platform> is a directory in F<src/jit> under which there exists a
+F<core.jit> file.
+
+Sample output on C<STDOUT>:
+
+    Platform:      ppc
+    Not jitted:   1085
+    Jitted:        141
+    Total ops:    1223
+
+=head1 AUTHOR
+
+Stefan Lidman (April 2004).  Revised and renamed by James E Keenan (April
+2008).
+
+=cut
 
 # Local Variables:
 #   mode: cperl
