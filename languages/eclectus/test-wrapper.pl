47497bbbd15793a0057d8a69e5b16713b08f9598
François Perrad <francois.perrad@gadz.org>
2008-04-14 20:50:03 +0000

    [eclectus] - use petite on win32
    git-svn-id: https://svn.parrot.org/parrot/trunk@26973 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/eclectus/test-wrapper.pl
+++ languages/eclectus/test-wrapper.pl
@@ -16,19 +16,37 @@ use Data::Dumper;
 
 ( my $t_fn = $FindBin::Script ) =~ s/\.pl$/\.t/;
 
-# 'gosh' is Gauche
-# 0.8 is the current version
-my $gauche_version = `gosh -V 2>&1` || q{};
-my $has_gauche = $gauche_version =~ m/  0\.8 # inexact version
-                                     /xms;
-#diag( Dumper( $gauche_version, $has_gauche ) );
 
-if ( ! $has_gauche ) {
-    plan skip_all => 'gauche 0.8 is needed for running this test';
+if ($^O eq 'MSWin32') {
+    # 'petite' is Petite Chez Scheme
+    # 7.4 is the current version
+    my $petite_version = `petite --version 2>&1` || q{};
+    my $has_petite = $petite_version =~ /^7.4/;
+    #diag( Dumper( $petite_version, $has_petite ) );
+
+    if ( ! $has_petite ) {
+        plan skip_all => 'petite 7.4 is needed for running this test';
+    }
+    else {
+        chdir 'eclectus';
+        exec 'petite', '--script', "t/$t_fn";
+    }
 }
 else {
-    chdir 'eclectus';
-    exec 'gosh', '-fcase-fold', '-I', '.',  '-l', 'gauche/prelude.scm', "t/$t_fn";
+    # 'gosh' is Gauche
+    # 0.8 is the current version
+    my $gauche_version = `gosh -V 2>&1` || q{};
+    my $has_gauche = $gauche_version =~ m/  0\.8 # inexact version
+                                         /xms;
+    #diag( Dumper( $gauche_version, $has_gauche ) );
+
+    if ( ! $has_gauche ) {
+        plan skip_all => 'gauche 0.8 is needed for running this test';
+    }
+    else {
+        chdir 'eclectus';
+        exec 'gosh', '-fcase-fold', '-I', '.',  '-l', 'gauche/prelude.scm', "t/$t_fn";
+    }
 }
 
 # Local Variables:
