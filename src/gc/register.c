parrot/parrot

commit 41f58eac8ac38cd12103e1aa8170ea199e97d934

[GC] Fixed another type-punning compiler warning.
git-svn-id: https://svn.parrot.org/parrot/trunk@27074 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/gc/register.c
@@ -726,21 +726,25 @@ void
 mark_register_stack(PARROT_INTERP, ARGMOD(Stack_Chunk_t* chunk))
 {
     for (; ; chunk = chunk->prev) {
-        int i;
         save_regs_t   *save_r;
         Interp_Context ctx;
+        int            i;
 
-        pobject_lives(interp, (PObj*)chunk);
+        pobject_lives(interp, (PObj *)chunk);
+
         if (chunk == chunk->prev)
             break;
-        save_r = (save_regs_t *)STACK_DATAP(chunk);
+
+        save_r           = (save_regs_t *)chunk->u.data;
         ctx.bp.regs_i    = NULL;
         ctx.bp_ps.regs_p = save_r->old_bp_ps.regs_p;
+
         for (i = 0; i < save_r->n_regs_pmc; ++i) {
             PObj * const obj = (PObj *)CTX_REG_PMC(&ctx, i);
             if (obj)
                 pobject_lives(interp, obj);
         }
+
         for (i = 0; i < save_r->n_regs_str; ++i) {
             PObj * const obj = (PObj *)CTX_REG_STR(&ctx, i);
             if (obj)

commit 2d3aacf9b07f3fff4f18cc94b6dd8881295d4bdd

[GC] Fixed a transient GC segfault introduced by r27074 and worked around the type punning error another way.
git-svn-id: https://svn.parrot.org/parrot/trunk@27078 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/gc/register.c
@@ -723,9 +723,10 @@ Marks the register stack and its registers as live.
 */
 
 void
-mark_register_stack(PARROT_INTERP, ARGMOD(Stack_Chunk_t* chunk))
+mark_register_stack(PARROT_INTERP, ARGMOD(Stack_Chunk_t *chunk))
 {
     for (; ; chunk = chunk->prev) {
+        void          **chunk_data;
         save_regs_t   *save_r;
         Interp_Context ctx;
         int            i;
@@ -735,7 +736,8 @@ mark_register_stack(PARROT_INTERP, ARGMOD(Stack_Chunk_t* chunk))
         if (chunk == chunk->prev)
             break;
 
-        save_r           = (save_regs_t *)chunk->u.data;
+        chunk_data       = STACK_DATAP(chunk);
+        save_r           = (save_regs_t *)chunk_data;
         ctx.bp.regs_i    = NULL;
         ctx.bp_ps.regs_p = save_r->old_bp_ps.regs_p;
 