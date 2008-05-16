f1566e38bd07a1fd5ed7616b9e9c2893cfbc2f6d
chromatic <chromatic@wgz.org>
2008-05-16 00:19:17 +0000

    [src] Plugged a memory leak in Parrot_cx_handle_tasks.
    git-svn-id: https://svn.parrot.org/parrot/trunk@27528 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/scheduler.c
+++ src/scheduler.c
@@ -139,8 +139,9 @@ Parrot_cx_handle_tasks(PARROT_INTERP, ARGMOD(PMC *scheduler))
         }
         else {
             real_exception(interp, NULL, INVALID_OPERATION,
-                    "Unknown task type '%s'.\n", string_to_cstring(interp, type));
+                "Unknown task type '%Ss'.\n", type);
         }
+
         Parrot_cx_delete_task(interp, task);
         }
 
@@ -150,7 +151,6 @@ Parrot_cx_handle_tasks(PARROT_INTERP, ARGMOD(PMC *scheduler))
             Parrot_cx_refresh_task_list(interp, scheduler);
 
     } /* end of pending tasks */
-
 }
 
 /*
