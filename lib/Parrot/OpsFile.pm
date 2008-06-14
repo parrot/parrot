7ef8880a017fdfc8deeec9aff501052e075461a2
chromatic <chromatic@wgz.org>
2008-06-14 18:49:32 +0000

    [ops] Updated label syntax in ops: they're not INT anymore; they're LABELs (Alek Storm, RT #41620).
    git-svn-id: https://svn.parrot.org/parrot/trunk@28359 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/OpsFile.pm
+++ lib/Parrot/OpsFile.pm
@@ -53,9 +53,6 @@ Argument direction is one of:
     inout         the argument passes a value into and out of the op
     inconst       the argument passes a constant value into the op
     invar         the argument passes a variable value into the op
-    label         an in argument containing a branch offset or address
-    labelconst    an invar argument containing a branch offset or address
-    labelvar      an inconst argument containing a branch offset or address
 
 Argument direction is used to determine the life times of symbols and
 their related register allocations. When an argument is passed into an
@@ -70,6 +67,7 @@ Argument type is one of:
     PMC       the argument is an PMC
     KEY       the argument is an aggregate PMC key
     INTKEY    the argument is an aggregate PMC integer key
+    LABEL     the argument is an integer branch offset or address
 
 The size of the return offset is determined from the op function's
 signature.
@@ -355,33 +353,31 @@ sub read_ops {
 
             $flags = { map { $_ => undef } (split(/[ :]+/, $flags)) };
 
-            my @temp = ();
+            my @temp;
 
-            foreach my $arg (@args) {
+            for my $arg (@args) {
                 my ( $use, $type ) =
-                    $arg =~ m/^(in|out|inout|inconst|invar|label|labelconst|labelvar)
+                    $arg =~ m/^(in|out|inout|inconst|invar)
                     \s+
-                    (INT|NUM|STR|PMC|KEY|INTKEY)$/ix;
+                    (INT|NUM|STR|PMC|KEY|INTKEY|LABEL)$/ix;
 
                 die "Unrecognized arg format '$arg' in '$_'!"
-                    unless defined($use)
-                        and defined($type);
+                    unless defined($use) and defined($type);
 
-                if ( $type =~ /^INTKEY$/i ) {
-                    $type = "ki";
-                }
-                else {
-                    $type = lc substr( $type, 0, 1 );
-                }
-
-                # convert e.g. "labelvar" to "invar" and remember labels
-
-                if ( $use =~ /label(\w*)/ ) {
+                # remember it's a label, then turn it to an int
+                if ( $type =~ /^LABEL$/i ) {
+                    $type = 'i';
                     push @labels, 1;
-                    $use = "in$1";
                 }
                 else {
                     push @labels, 0;
+                }
+
+                if ( $type =~ /^INTKEY$/i ) {
+                    $type = 'ki';
+                }
+                else {
+                    $type = lc substr( $type, 0, 1 );
                 }
 
                 if ( $use eq 'in' ) {
