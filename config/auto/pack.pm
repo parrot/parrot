06bffbaab917351e8648ffecb09b7de6301c8f2e
James E Keenan (Jim) <jkeenan@cpan.org>
2008-07-08 01:50:16 +0000

    Make program flow inside runstep() more coherent.
    git-svn-id: https://svn.parrot.org/parrot/trunk@29141 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/pack.pm
+++ config/auto/pack.pm
@@ -42,40 +42,47 @@ sub runstep {
         my $which = $type eq 'intvalsize' ? 'packtype_i' : 'packtype_op';
         my $size = $conf->data->get($type);
         my $format;
-        if (    $size == $longsize
-             && $size == $conf->data->get_p5('longsize')
-           ) {
+        if (    
+            $size == $longsize
+                &&
+            $size == $conf->data->get_p5('longsize')
+        ) {
             $format = 'l!';
         }
         elsif ( $size == 4 ) {
             $format = 'l';
         }
-        elsif (    $size == 8
-                || $conf->data->get_p5('use64bitint') eq 'define'
-              ) {
+        elsif (
+            $size == 8
+                ||
+            $conf->data->get_p5('use64bitint') eq 'define'
+        ) {
             # pp_pack is annoying, and this won't work unless sizeof(UV) >= 8
             $format = 'q';
         }
-        warn "Configure.pl:  Unable to find a suitable packtype for $type.\n"
-            unless $format;
+        else {
+            warn "Configure.pl:  Unable to find a suitable packtype for $type.\n"
+        }
 
         my $test = eval { pack $format, 0 };
-        unless ( defined $test ) {
+        if ( ! defined $test ) {
             warn <<"AARGH"
 Configure.pl:  Unable to find a functional packtype for $type.
                '$format' failed: $@
 AARGH
         }
-        if ($test) {
-            unless ( length $test == $size ) {
-                warn sprintf <<"AARGH", $size, length $test;
+        else {
+            if ($test) {
+                unless ( length $test == $size ) {
+                    warn sprintf <<"AARGH", $size, length $test;
 Configure.pl:  Unable to find a functional packtype for $type.
                Need a format for %d bytes, but '$format' gave %d bytes.
 AARGH
+                }
             }
-        }
-        else {
-            $format = '?';
+            else {
+                $format = '?';
+            }
         }
 
         $conf->data->set( $which => $format );
