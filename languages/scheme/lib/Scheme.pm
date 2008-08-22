062acbbd37171121d8fa7681d96ec4db7f6a936a
Klaas-Jan Stol <parrotcode@gmail.com>
2008-08-22 11:36:59 +0000

    [scheme] change .HLL_map "x", "y" into .HLL_map "x" = "y".
    git-svn-id: https://svn.parrot.org/parrot/trunk@30438 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/scheme/lib/Scheme.pm
+++ languages/scheme/lib/Scheme.pm
@@ -103,7 +103,7 @@ sub link_functions {
 # is parsed, therefore the .DynLexPad constant is already available
 .loadlib "dynlexpad"
 .HLL "SomethingWithScheme", "dynlexpad"
-.HLL_map 'LexPad', 'DynLexPad'
+.HLL_map 'LexPad' = 'DynLexPad'
 
 .sub init__scheme_types :init
 
