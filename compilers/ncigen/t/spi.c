54152ec186a30168582711b942123b0c62258efb
James E Keenan (Jim) <jkeenan@cpan.org>
2008-08-23 14:34:56 +0000

    Change all hard tabs to 4 spaces.
    git-svn-id: https://svn.parrot.org/parrot/trunk@30483 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/ncigen/t/spi.c
+++ compilers/ncigen/t/spi.c
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------------
  *
  * spi.h
- *				Server Programming Interface public declarations
+ *                Server Programming Interface public declarations
  *
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
@@ -21,8 +21,8 @@
 #include "postgres.h"
 
 /*
- *	Most of these are not needed by this file, but may be used by
- *	user-written code that uses SPI
+ *    Most of these are not needed by this file, but may be used by
+ *    user-written code that uses SPI
  */
 #include "access/heapam.h"
 #include "access/xact.h"
@@ -49,38 +49,38 @@
 
 typedef struct SPITupleTable
 {
-	MemoryContext tuptabcxt;	/* memory context of result table */
-	uint32		alloced;		/* # of alloced vals */
-	uint32		free;			/* # of free vals */
-	TupleDesc	tupdesc;		/* tuple descriptor */
-	HeapTuple  *vals;			/* tuples */
+    MemoryContext tuptabcxt;    /* memory context of result table */
+    uint32        alloced;        /* # of alloced vals */
+    uint32        free;            /* # of free vals */
+    TupleDesc    tupdesc;        /* tuple descriptor */
+    HeapTuple  *vals;            /* tuples */
 } SPITupleTable;
 
 /* Plans are opaque structs for standard users of SPI */
 typedef struct _SPI_plan *SPIPlanPtr;
 
-#define SPI_ERROR_CONNECT		(-1)
-#define SPI_ERROR_COPY			(-2)
-#define SPI_ERROR_OPUNKNOWN		(-3)
-#define SPI_ERROR_UNCONNECTED	(-4)
-#define SPI_ERROR_CURSOR		(-5)	/* not used anymore */
-#define SPI_ERROR_ARGUMENT		(-6)
-#define SPI_ERROR_PARAM			(-7)
-#define SPI_ERROR_TRANSACTION	(-8)
-#define SPI_ERROR_NOATTRIBUTE	(-9)
-#define SPI_ERROR_NOOUTFUNC		(-10)
-#define SPI_ERROR_TYPUNKNOWN	(-11)
+#define SPI_ERROR_CONNECT        (-1)
+#define SPI_ERROR_COPY            (-2)
+#define SPI_ERROR_OPUNKNOWN        (-3)
+#define SPI_ERROR_UNCONNECTED    (-4)
+#define SPI_ERROR_CURSOR        (-5)    /* not used anymore */
+#define SPI_ERROR_ARGUMENT        (-6)
+#define SPI_ERROR_PARAM            (-7)
+#define SPI_ERROR_TRANSACTION    (-8)
+#define SPI_ERROR_NOATTRIBUTE    (-9)
+#define SPI_ERROR_NOOUTFUNC        (-10)
+#define SPI_ERROR_TYPUNKNOWN    (-11)
 
-#define SPI_OK_CONNECT			1
-#define SPI_OK_FINISH			2
-#define SPI_OK_FETCH			3
-#define SPI_OK_UTILITY			4
-#define SPI_OK_SELECT			5
-#define SPI_OK_SELINTO			6
-#define SPI_OK_INSERT			7
-#define SPI_OK_DELETE			8
-#define SPI_OK_UPDATE			9
-#define SPI_OK_CURSOR			10
+#define SPI_OK_CONNECT            1
+#define SPI_OK_FINISH            2
+#define SPI_OK_FETCH            3
+#define SPI_OK_UTILITY            4
+#define SPI_OK_SELECT            5
+#define SPI_OK_SELINTO            6
+#define SPI_OK_INSERT            7
+#define SPI_OK_DELETE            8
+#define SPI_OK_UPDATE            9
+#define SPI_OK_CURSOR            10
 #define SPI_OK_INSERT_RETURNING 11
 #define SPI_OK_DELETE_RETURNING 12
 #define SPI_OK_UPDATE_RETURNING 13
@@ -90,47 +90,47 @@ extern PGDLLIMPORT Oid SPI_lastoid;
 extern PGDLLIMPORT SPITupleTable *SPI_tuptable;
 extern PGDLLIMPORT int SPI_result;
 
-extern int	SPI_connect(void);
-extern int	SPI_finish(void);
+extern int    SPI_connect(void);
+extern int    SPI_finish(void);
 extern void SPI_push(void);
 extern void SPI_pop(void);
 extern void SPI_restore_connection(void);
-extern int	SPI_execute(const char *src, bool read_only, long tcount);
+extern int    SPI_execute(const char *src, bool read_only, long tcount);
 extern int SPI_execute_plan(SPIPlanPtr plan, Datum *Values, const char *Nulls,
-				 bool read_only, long tcount);
-extern int	SPI_exec(const char *src, long tcount);
+                 bool read_only, long tcount);
+extern int    SPI_exec(const char *src, long tcount);
 extern int SPI_execp(SPIPlanPtr plan, Datum *Values, const char *Nulls,
-		  long tcount);
+          long tcount);
 extern int SPI_execute_snapshot(SPIPlanPtr plan,
-					 Datum *Values, const char *Nulls,
-					 Snapshot snapshot,
-					 Snapshot crosscheck_snapshot,
-					 bool read_only, bool fire_triggers, long tcount);
-extern int	SPI_execute_with_args(const char *src,
-								  int nargs, Oid *argtypes,
-								  Datum *Values, const char *Nulls,
-								  bool read_only, long tcount);
+                     Datum *Values, const char *Nulls,
+                     Snapshot snapshot,
+                     Snapshot crosscheck_snapshot,
+                     bool read_only, bool fire_triggers, long tcount);
+extern int    SPI_execute_with_args(const char *src,
+                                  int nargs, Oid *argtypes,
+                                  Datum *Values, const char *Nulls,
+                                  bool read_only, long tcount);
 extern SPIPlanPtr SPI_prepare(const char *src, int nargs, Oid *argtypes);
 extern SPIPlanPtr SPI_prepare_cursor(const char *src, int nargs, Oid *argtypes,
-				   int cursorOptions);
+                   int cursorOptions);
 extern SPIPlanPtr SPI_saveplan(SPIPlanPtr plan);
-extern int	SPI_freeplan(SPIPlanPtr plan);
+extern int    SPI_freeplan(SPIPlanPtr plan);
 
-extern Oid	SPI_getargtypeid(SPIPlanPtr plan, int argIndex);
-extern int	SPI_getargcount(SPIPlanPtr plan);
+extern Oid    SPI_getargtypeid(SPIPlanPtr plan, int argIndex);
+extern int    SPI_getargcount(SPIPlanPtr plan);
 extern bool SPI_is_cursor_plan(SPIPlanPtr plan);
 extern const char *SPI_result_code_string(int code);
 
 extern HeapTuple SPI_copytuple(HeapTuple tuple);
 extern HeapTupleHeader SPI_returntuple(HeapTuple tuple, TupleDesc tupdesc);
 extern HeapTuple SPI_modifytuple(Relation rel, HeapTuple tuple, int natts,
-				int *attnum, Datum *Values, const char *Nulls);
-extern int	SPI_fnumber(TupleDesc tupdesc, const char *fname);
+                int *attnum, Datum *Values, const char *Nulls);
+extern int    SPI_fnumber(TupleDesc tupdesc, const char *fname);
 extern char *SPI_fname(TupleDesc tupdesc, int fnumber);
 extern char *SPI_getvalue(HeapTuple tuple, TupleDesc tupdesc, int fnumber);
 extern Datum SPI_getbinval(HeapTuple tuple, TupleDesc tupdesc, int fnumber, bool *isnull);
 extern char *SPI_gettype(TupleDesc tupdesc, int fnumber);
-extern Oid	SPI_gettypeid(TupleDesc tupdesc, int fnumber);
+extern Oid    SPI_gettypeid(TupleDesc tupdesc, int fnumber);
 extern char *SPI_getrelname(Relation rel);
 extern char *SPI_getnspname(Relation rel);
 extern void *SPI_palloc(Size size);
@@ -140,12 +140,12 @@ extern void SPI_freetuple(HeapTuple pointer);
 extern void SPI_freetuptable(SPITupleTable *tuptable);
 
 extern Portal SPI_cursor_open(const char *name, SPIPlanPtr plan,
-				Datum *Values, const char *Nulls, bool read_only);
+                Datum *Values, const char *Nulls, bool read_only);
 extern Portal SPI_cursor_open_with_args(const char *name,
-										const char *src,
-										int nargs, Oid *argtypes,
-										Datum *Values, const char *Nulls,
-										bool read_only, int cursorOptions);
+                                        const char *src,
+                                        int nargs, Oid *argtypes,
+                                        Datum *Values, const char *Nulls,
+                                        bool read_only, int cursorOptions);
 extern Portal SPI_cursor_find(const char *name);
 extern void SPI_cursor_fetch(Portal portal, bool forward, long count);
 extern void SPI_cursor_move(Portal portal, bool forward, long count);
