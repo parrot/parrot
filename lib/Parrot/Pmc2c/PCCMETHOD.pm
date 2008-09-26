79f0146081c71b28f0667da09e69d7eecc7aaedd
Jerry Gay <Jerry.Gay@gmail.com>
2008-09-26 17:37:04 +0000

    [perl #59374] [BUG] pmc2c produces incorrect code for RETURN(INTVAL -1) -- fixed bug in Parrot::Pmc2c::PCCMETHOD
    git-svn-id: https://svn.parrot.org/parrot/trunk@31441 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PCCMETHOD.pm
+++ lib/Parrot/Pmc2c/PCCMETHOD.pm
@@ -282,9 +282,11 @@ sub parse_p_args_string {
         die "invalid PCC arg '$x': did you forget to specify a type?\n"
              unless defined $name;
 
-        if ($name =~ /[\**]?(\"?\w+\"?)/) {
+print "<$name>";
+        if ($name =~ /\**([a-zA-Z_]\w*)/) {
             $name = $1;
         }
+print "<$name>$/";
 
         my $arg = {
             type  => convert_type_string_to_reg_type($type),
