76a0abe8813916646dbd0b0c4b46aca4b87c2567
Reini Urban <rurban@cpan.org>
2008-10-08 09:55:39 +0000

    [cage] RT#48260 add op docs. Fix typo.
    git-svn-id: https://svn.parrot.org/parrot/trunk@31786 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/bcg/src/bcg.c
+++ compilers/bcg/src/bcg.c
@@ -16,7 +16,7 @@ for BCG.
 
 =head2 Methods
 
-List of methods imlemented in this file.
+List of methods implemented in this file.
 
 =over 4
 
@@ -371,7 +371,8 @@ BCG_label(BCG_info * bcg_info, char *label)
 =item C<void
 BCG_print_pasm(BCG_info * bcg_info)>
 
-RT#48260: Not yet documented!!!
+This is the implementation for the op printPASM(),
+which decompiles the bytecode to PASM.
 
 =cut
 
