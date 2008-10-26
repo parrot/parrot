3ccbd6321bc611e68332f39289454d557238d4c0
Klaas-Jan Stol <parrotcode@gmail.com>
2008-10-26 21:55:23 +0000

    [pirc/new] add a header file for the heredoc preprocessor + update MANIFEST.
    git-svn-id: https://svn.parrot.org/parrot/trunk@32186 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/pirc/new/pirheredoc.h
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ * Copyright (C) 2008, The Perl Foundation.
+ */
+
+
+#ifndef PARROT_PIR_PIRHEREDOC_H_GUARD
+#define PARROT_PIR_PIRHEREDOC_H_GUARD
+
+#include <stdio.h> /* for FILE */
+
+void process_heredocs(char * const filename, FILE *outputfile);
+
+#endif /* PARROT_PIR_PIRHEREDOC_H_GUARD */
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
+
