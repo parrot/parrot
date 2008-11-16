cf433f2075d0c50a61416225ed023eddc7516f7e
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2008-11-16 20:38:10 +0000

    [Pipp] Remove trailing whitspace
    git-svn-id: https://svn.parrot.org/parrot/trunk@32731 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/pipp/lib/Parrot/Test/Pipp/Antlr3.pm
+++ languages/pipp/lib/Parrot/Test/Pipp/Antlr3.pm
@@ -26,7 +26,7 @@ sub get_test_prog {
     my ( $count, $options ) = @_;
 
     my $lang_fn = Parrot::Test::per_test( '.php', $count );
-    $lang_fn =~ s!^pipp/!!;  
+    $lang_fn =~ s!^pipp/!!; # fix for unified languages testing
 
     return "$self->{relpath}/parrot $self->{relpath}/languages/pipp/pipp.pbc --variant=antlr3 ${lang_fn}";
 }
