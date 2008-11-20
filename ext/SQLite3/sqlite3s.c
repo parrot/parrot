c02d9f83eca138c01ed9c55cd53af42d4b0e90cc
Allison Randal <allison@parrot.org>
2008-11-20 00:51:28 +0000

    [cage] Add boilerplate to SQLite C file, to quiet the coding standards tests.
    git-svn-id: https://svn.parrot.org/parrot/trunk@32913 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- ext/SQLite3/sqlite3s.c
+++ ext/SQLite3/sqlite3s.c
@@ -1,5 +1,18 @@
 /* $Id$ */
 /* Wrappers around the SQLite interface to avoid the use of out-arguments */
+/*
+Copyright (C) 2008, The Perl Foundation.
+$Id$
+
+=head1 NAME
+
+ext/SQLite3/sqlite3s.c - SQLite wrappers
+
+=head1 SYNOPSIS
+
+Wrappers around the SQLite interface to avoid the use of out-arguments
+
+*/
 
 #include <stddef.h>
 #include <stdio.h>
