5529ee3a6c0fa3259b92dd085cfc12bb2c6a8382
Kevin Tew <tewk@tewk.com>
2008-12-02 22:38:46 +0000

    [ncigen] perl6 start to autogenerating interfaces
    git-svn-id: https://svn.parrot.org/parrot/trunk@33449 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/ncigen/sqlite_interface_generator.pl
@@ -0,0 +1,24 @@
+#!/home/tewk/srcs/parrot/perl6 
+use v6;
+evalfile('./ncigen.pbc', lang => 'Parrot');
+evalfile('./compreg.pir', lang => 'Parrot');
+
+my $fn = @*ARGS[0];
+my $pp_fn = mktempfile('ptemp');
+
+run("gcc -x c -E $fn > $pp_fn");
+my $compiler = compreg('NCIGEN');
+my $ast = $compiler.parse(slurp($pp_fn));
+say +$ast;
+for $ast.item() -> $x {
+  say "====================================================";
+  say $x;
+}
+
+
+
+sub mktempfile($prefix) {
+  sub nonce() { ".{$*PID}." ~ int 1000.rand }
+  $prefix ~ nonce;
+}
+
