3327341ef9390900d30e8603368a150cba45b49d
chromatic <chromatic@wgz.org>
2008-12-05 07:54:23 +0000

    [IO] Made one more attempt (this time more verbose) to avoid signedness assignment problems in Parrot_io_peek_buffer(), reported by Coverity Scan as CID #148.
    git-svn-id: https://svn.parrot.org/parrot/trunk@33501 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/io/buffer.c
+++ src/io/buffer.c
@@ -423,9 +423,14 @@ ret_string:
 
         /* Parrot_io_fill_readbuf() can return -1, but len should be positive */
         got = Parrot_io_fill_readbuf(interp, filehandle);
-        len = (len < got)
-            ? len
-            : (got > 0) ? got : 0;
+
+        /* avoid signedness problems between size_t got and UINTVAL len */
+        if (len > got) {
+            if (got > 0)
+                len = (UINTVAL)got;
+            else
+                len = 0;
+        }
     }
 
     /* if we got any data, then copy out the next byte */
