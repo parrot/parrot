b64661d6fe65a7c9a5d5166eee276928ad05eef2
Mark Glines <mark@glines.org>
2008-12-06 06:53:42 +0000

    [CAGE] * Fix a couple of t/codingstd/c_header_guards.t failures. * Fix several t/codingstd/c_operator.t failures.
    git-svn-id: https://svn.parrot.org/parrot/trunk@33545 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/ncigen/k.h
+++ compilers/ncigen/k.h
@@ -1,3 +1,6 @@
+#ifndef PARROT_COMPILERS_NCIGEN_K_GUARD
+#define PARROT_COMPILERS_NCIGEN_K_GUARD
+
 extern const char sqlite3_version[];
 const char *sqlite3_libversion(void);
 int sqlite3_libversion_number(void);
@@ -8,8 +11,8 @@ typedef unsigned long long int sqlite_uint64;
 typedef sqlite_int64 sqlite3_int64;
 typedef sqlite_uint64 sqlite3_uint64;
 int sqlite3_close(sqlite3 *);
-typedef int (*sqlite3_callback)(void*,int,char**, char**);
-int sqlite3_exec(sqlite3*, const char *sql, int (*callback)(void*,int,char**,char**), void *, char **errmsg);
+typedef int (*sqlite3_callback)(void*, int, char**, char**);
+int sqlite3_exec(sqlite3*, const char *sql, int (*callback)(void*, int, char**, char**), void *, char **errmsg);
 typedef struct sqlite3_mutex sqlite3_mutex;
 int sqlite3_extended_result_codes(sqlite3*, int onoff);
 sqlite3_int64 sqlite3_last_insert_rowid(sqlite3*);
@@ -18,7 +21,7 @@ int sqlite3_total_changes(sqlite3*);
 void sqlite3_interrupt(sqlite3*);
 int sqlite3_complete(const char *sql);
 int sqlite3_complete16(const void *sql);
-int sqlite3_busy_handler(sqlite3*, int (*)(void*,int), void*);
+int sqlite3_busy_handler(sqlite3*, int (*)(void*, int), void*);
 int sqlite3_busy_timeout(sqlite3*, int ms);
 int sqlite3_get_table(sqlite3*, const char *sql, char ***pResult, int *nrow, int *ncolumn, char **errmsg);
 void sqlite3_free_table(char **result);
@@ -29,9 +32,9 @@ void sqlite3_free(void*);
 sqlite3_int64 sqlite3_memory_used(void);
 sqlite3_int64 sqlite3_memory_highwater(int resetFlag);
 void sqlite3_randomness(int N, void *P);
-int sqlite3_set_authorizer(sqlite3*, int (*xAuth)(void*,int,const char*,const char*,const char*,const char*), void *pUserData);
-void *sqlite3_trace(sqlite3*, void (*xTrace)(void*,const char*), void*);
-void *sqlite3_profile(sqlite3*, void (*xProfile)(void*,const char*,sqlite3_uint64), void*);
+int sqlite3_set_authorizer(sqlite3*, int (*xAuth)(void*, int, const char*, const char*, const char*, const char*), void *pUserData);
+void *sqlite3_trace(sqlite3*, void (*xTrace)(void*, const char*), void*);
+void *sqlite3_profile(sqlite3*, void (*xProfile)(void*, const char*, sqlite3_uint64), void*);
 void sqlite3_progress_handler(sqlite3*, int, int (*)(void*), void*);
 int sqlite3_open(const char *filename, sqlite3 **ppDb);
 int sqlite3_open16(const void *filename, sqlite3 **ppDb);
@@ -45,6 +48,8 @@ int sqlite3_prepare(sqlite3 *db, const char *zSql, int nByte, sqlite3_stmt **ppS
 int sqlite3_prepare_v2(sqlite3 *db, const char *zSql, int nByte, sqlite3_stmt **ppStmt, const char **pzTail);
 int sqlite3_prepare16(sqlite3 *db, const void *zSql, int nByte, sqlite3_stmt **ppStmt, const void **pzTail);
 int sqlite3_prepare16_v2(sqlite3 *db, const void *zSql, int nByte, sqlite3_stmt **ppStmt, const void **pzTail);
+
+#endif /* PARROT_COMPILERS_NCIGEN_K_GUARD */
 
 /*
  * Local variables:
--- compilers/ncigen/kk.h
+++ compilers/ncigen/kk.h
@@ -1,5 +1,10 @@
+#ifndef PARROT_COMPILERS_NCIGEN_KK_GUARD
+#define PARROT_COMPILERS_NCIGEN_KK_GUARD
+
 int a(int a, int b);
 int b(int a, int b);
+
+#endif /* PARROT_COMPILERS_NCIGEN_KK_GUARD */
 
 /*
  * Local variables:
