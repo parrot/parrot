2ab4234263edf911be26a1452c7c9ddd5df18feb
chromatic <chromatic@wgz.org>
2008-12-18 21:43:04 +0000

    [PIC] Removed the only reference to deprecated old MMD functions in src/pic.c. There are no consumers of this PIC function anyway.  Let the chainsaw continue.
    git-svn-id: https://svn.parrot.org/parrot/trunk@34082 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/pic.c
+++ src/pic.c
@@ -907,56 +907,7 @@ void
 parrot_pic_find_infix_v_pp(PARROT_INTERP, ARGIN(PMC *left), ARGIN(PMC *right),
                 ARGOUT(Parrot_MIC *mic), ARGOUT(opcode_t *cur_opcode))
 {
-    funcptr_t func;
-    int is_pmc;
-    INTVAL left_type, right_type;
-    PARROT_ASSERT(0);
-    /*
-     * if 2 threads are entering here, there is a chance
-     * that one moves the lru structure under the other thread
-     * and vv - just lock in case
-     *
-     * TODO
-     *
-     * if (TRY_LOCK_INTERPRETER(i) == EBUSY)
-     *      return;  - reexec
-     */
-    LOCK_INTERPRETER(interp);
-
-    /* move entries back and set topmost entry */
-    parrot_pic_move(interp, mic);
-
-    /* get real dispatch function */
-    left_type  = VTABLE_type(interp, left);
-    right_type = VTABLE_type(interp, right);
-    func       = get_mmd_dispatch_type(interp,
-                    mic->m.func_nr, left_type, right_type, &is_pmc);
-
-    if (is_pmc) {
-        const size_t offs = cur_opcode - (opcode_t *)interp->code->prederef.code;
-        opcode_t* const real_op = interp->code->base.data + offs + 1;
-
-        /* set prederef code address to orig slot for now */
-        ((void**)cur_opcode)[0] =
-            parrot_pic_opcode(interp, PARROT_OP_infix_ic_p_p);
-
-        /* restore 1st operand i.e. .MMD_func_nr */
-        ((void**)cur_opcode)[1] = (void*)*real_op;
-        mic->lru.f.sub = (PMC*)F2DPTR(func);
-    }
-    else {
-        INTVAL op = PARROT_OP_pic_infix___ic_p_p;
-
-#if ENABLE_INLINING
-        if (func == (funcptr_t)Parrot_Integer_i_subtract_Integer && !mic->pic)
-            op = PARROT_OP_pic_inline_sub___ic_p_p;
-#endif
-        ((void**)cur_opcode)[0] =
-            parrot_pic_opcode(interp, op);
-        mic->lru.f.real_function = func;
-    }
-    mic->lru.u.type = (left_type << 16) | right_type;
-    UNLOCK_INTERPRETER(interp);
+    /* unused; deprecated */
 }
 
 /*
