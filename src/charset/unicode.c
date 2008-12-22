ed4a9f1ef58f1b0379a1dce1e7437f34d8b868e1
Patrick R. Michaud <pmichaud@pobox.com>
2008-12-22 08:27:26 +0000

    [core]:  Recognize LINE/PARAGRAPH SEPARATOR unicode chars as whitespace.
    git-svn-id: https://svn.parrot.org/parrot/trunk@34238 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/charset/unicode.c
+++ src/charset/unicode.c
@@ -758,7 +758,8 @@ u_iscclass(PARROT_INTERP, UINTVAL codepoint, INTVAL flags)
             case 0x1680: case 0x180e: case 0x2000: case 0x2001:
             case 0x2002: case 0x2003: case 0x2004: case 0x2005:
             case 0x2006: case 0x2007: case 0x2008: case 0x2009:
-            case 0x200a: case 0x202f: case 0x205f: case 0x3000:
+            case 0x200a: case 0x2028: case 0x2029: case 0x202f: 
+            case 0x205f: case 0x3000:
                 return 1;
             default:
                 break;
