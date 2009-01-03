26434cfddf02ec5c35748719bb7bc7519fcb4275
James E Keenan (Jim) <jkeenan@cpan.org>
2009-01-03 18:24:32 +0000

    Run 'make headerizer' on Darwin/PPC to capture recent changes, particularly ASSERT_ARGS.
    git-svn-id: https://svn.parrot.org/parrot/trunk@34895 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/jit/ppc/exec_dep.h
+++ src/jit/ppc/exec_dep.h
@@ -20,19 +20,31 @@
 #define PARROT_PPC_EXEC_DEP_H_GUARD
 
 /* HEADERIZER BEGIN: src/exec_dep.c */
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
 
-void
-Parrot_exec_normal_op(Parrot_jit_info_t *jit_info, PARROT_INTERP);
+void offset_fixup(Parrot_exec_objfile_t *obj);
+void Parrot_exec_cpcf_op(Parrot_jit_info_t *jit_info, PARROT_INTERP)
+        __attribute__nonnull__(2);
 
-void
-Parrot_exec_cpcf_op(Parrot_jit_info_t *jit_info, PARROT_INTERP);
+void Parrot_exec_normal_op(Parrot_jit_info_t *jit_info, PARROT_INTERP)
+        __attribute__nonnull__(2);
 
-void
-Parrot_exec_restart_op(Parrot_jit_info_t *jit_info, PARROT_INTERP);
+void Parrot_exec_normal_op(Parrot_jit_info_t *jit_info, PARROT_INTERP)
+        __attribute__nonnull__(2);
 
-void
-offset_fixup(Parrot_exec_objfile_t *obj);
+void Parrot_exec_restart_op(Parrot_jit_info_t *jit_info, PARROT_INTERP)
+        __attribute__nonnull__(2);
 
+#define ASSERT_ARGS_offset_fixup __attribute__unused__ int _ASSERT_ARGS_CHECK = 0
+#define ASSERT_ARGS_Parrot_exec_cpcf_op __attribute__unused__ int _ASSERT_ARGS_CHECK = \
+       PARROT_ASSERT_ARG(interp)
+#define ASSERT_ARGS_Parrot_exec_normal_op __attribute__unused__ int _ASSERT_ARGS_CHECK = \
+       PARROT_ASSERT_ARG(interp)
+#define ASSERT_ARGS_Parrot_exec_normal_op __attribute__unused__ int _ASSERT_ARGS_CHECK = \
+       PARROT_ASSERT_ARG(interp)
+#define ASSERT_ARGS_Parrot_exec_restart_op __attribute__unused__ int _ASSERT_ARGS_CHECK = \
+       PARROT_ASSERT_ARG(interp)
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
 /* HEADERIZER END: src/exec_dep.c */
 
 #endif /* PARROT_PPC_EXEC_DEP_H_GUARD */
