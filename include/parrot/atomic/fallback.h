718ad90c0dd4587dce3f29d0539216e87dd550e4
Mark Glines <mark@glines.org>
2009-01-04 16:00:23 +0000

    Apply patch from jimmy++ in TT124.
    git-svn-id: https://svn.parrot.org/parrot/trunk@34923 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/atomic/fallback.h
+++ include/parrot/atomic/fallback.h
@@ -71,9 +71,7 @@ typedef struct Parrot_atomic_integer {
     do { \
         LOCK((a).lock); \
         if ((a).val == (expect)) { \
-            void * orig = (a).val; \
             (a).val = (update); \
-            UNUSED(orig) \
             (result) = 1; \
         } \
         else { \
@@ -86,9 +84,7 @@ typedef struct Parrot_atomic_integer {
     do { \
         LOCK((a).lock); \
         if ((a).val == (expect)) { \
-            INTVAL orig = (a).val; \
             (a).val = (update); \
-            UNUSED(orig) \
             (result) = 1; \
         } \
         else { \
