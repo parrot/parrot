f036491043b032086f49c787d5632de5636cb14d
James E Keenan (Jim) <jkeenan@cpan.org>
2009-01-05 01:01:14 +0000

    Remove dwim.pir example.  Cf.:  https://trac.parrot.org/parrot/ticket/120.
    git-svn-id: https://svn.parrot.org/parrot/trunk@34951 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Manifest.pm
+++ lib/Parrot/Manifest.pm
@@ -170,7 +170,6 @@ sub _get_special {
         examples/sdl/raw_pixels.pir                     [devel]
         languages/t/harness                             []
         runtime/parrot/dynext/README                    [devel]doc
-        runtime/parrot/include/DWIM.pir                 [devel]doc
         runtime/parrot/include/README                   [devel]doc
         src/call_list.txt                               [devel]doc
         src/ops/ops.num                                 [devel]
