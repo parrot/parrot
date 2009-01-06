2f2b846a7d8845bc7c4aedc5ed5a11a9f2dc0bfc
Klaas-Jan Stol <parrotcode@gmail.com>
2009-01-06 13:26:07 +0000

    [pirc] _tempnam is not available on linux; change into tmpnam.
    git-svn-id: https://svn.parrot.org/parrot/trunk@35030 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/pirc/src/main.c
+++ compilers/pirc/src/main.c
@@ -310,7 +310,7 @@ main(int argc, char *argv[]) {
         return 0;
     }
     else {
-        hdocoutfile = _tempnam(NULL, "hdoc");
+        hdocoutfile = tmpnam("hdoc");
         file = open_file(hdocoutfile, "w");
         process_heredocs(interp, argv[0], file);
         fclose(file);
