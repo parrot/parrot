054cd1a6cfc7c3dc96d588be960fa02d1bb386e3
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2009-01-11 14:43:34 +0000

    [befunge] Add a Configure.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@35416 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ languages/befunge/Configure.pl
@@ -0,0 +1,21 @@
+# $Id$
+# Copyright (C) 2009, The Perl Foundation.
+
+use strict;
+use warnings;
+use 5.008;
+
+my $build_dir = '../..';
+my $cmd = qq{$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=befunge};
+
+print "Running '$cmd' in $build_dir\n";
+
+chdir $build_dir;
+`$cmd`
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
