230e45487540b2f5556b2d22c275c6a0c68543de
Klaas-Jan Stol <parrotcode@gmail.com>
2009-01-27 19:49:16 +0000

    [pod] implement some bits for languages/pod. Can't test, as no Makefile is generated on my box (windows; should it?) Needs more work, but it's a start.
    git-svn-id: https://svn.parrot.org/parrot/trunk@36060 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/pod/src/parser/actions.pm
+++ languages/pod/src/parser/actions.pm
@@ -18,6 +18,110 @@ class Pod::Grammar::Actions;
 
 method TOP($/) {
     make $( $/ );
+    # for $<pod_section> {
+    #     $( $_  );
+    # }
+}
+
+method skipped($/) {
+
+}
+
+method pod_section($/) {
+    for $<pod_sequence> {
+        ## XXX store it where? A block?
+        $( $_ );
+    }
+}
+
+method pod_sequence($/, $key) {
+    make $( $/{$key} );
+}
+
+method pod_directive($/) {
+
+}
+
+method cut_directive($/) {
+
+}
+
+## XXX refactor the block_title stuff for heading and begin_directive.
+
+method heading($/) {
+    my $heading := Pod::DocTree::Heading.new();
+    ## set the level of the heading
+    $heading.level($<digit>);
+
+    if $<block_title> {
+        my $title := $( $<block_title>[0] );
+        $heading.title( $title.name() );
+    }
+    make $heading;
+}
+
+
+method begin_directive($/) {
+    my $block := Pod::DocTree::Block.new();
+    my $name  := $( $<block_name> );
+    $block.name( $name.name() );
+
+    if $<block_title> {
+        my $title := $( $<block_title>[0] );
+        $heading.title( $title.name() );
+    }
+    make $block;
+}
+
+method end_directive($/) {
+
+}
+method for_directive($/) {
+    # use same code as in begin-directive.
+}
+
+method over_directive($/) {
+
+}
+
+method back_directive($/) {
+
+}
+
+method item_directive($/) {
+    my $item := Pod::DocTree::Item.new();
+
+    make $item;
+}
+
+method encoding_directive($/) {
+
+}
+
+method paragraph($/) {
+
+}
+
+method literal_paragraph($/) {
+
+}
+
+method block_name($/) {
+    ## XXX fix: only match the non-whitespace text
+    make Pod::DocTree::Text.new( :name( $/ ) );
+}
+
+method block_title($/) {
+    make Pod::DocTree::Text.new( :name( $<formatted_text> ) );
+}
+
+method format_code($/) {
+
+}
+
+## XXX not sure if this needs action method is needed.
+method pod_ws($/) {
+
 }
 
 # Local Variables:
