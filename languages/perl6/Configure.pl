00b0999e86c8b0318c3047bab35afd7a659a2644
Moritz Lenz <moritz@faui2k3.org>
2009-02-08 18:46:46 +0000

    [rakudo] Rakudo left the nest - so remove most of languages/perl6/ and change the README accordingly.
    git-svn-id: https://svn.parrot.org/parrot/trunk@36451 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/perl6/Configure.pl
+++
@@ -1,9 +0,0 @@
-#! perl
-# $Id$
-# Copyright (C) 2008 The Perl Foundation
-
-use strict;
-use warnings;
-
-chdir '../..';
-`$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=perl6`;
