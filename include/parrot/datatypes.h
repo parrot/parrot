c25f820a388233a56b5d3ec4c59e3c3f207f7c6f
Andrew Whitworth <wknight8111@gmail.com>
2009-02-08 19:21:49 +0000

    [Core] rip out src/bignum.*. It's unused and when we do have a BigNum it will likely be a GMP-based PMC. Rip out associated tests for this too.AAA
    git-svn-id: https://svn.parrot.org/parrot/trunk@36461 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/datatypes.h
+++ include/parrot/datatypes.h
@@ -25,7 +25,6 @@ typedef enum {
     enum_type_STRING,
     enum_type_PMC,              /* actual PMCs have positive class numbers */
     enum_type_BIGINT,
-    enum_type_BIGNUM,           /* might be different */
     enum_type_DPOINTER,
 
     enum_type_char,             /* native integer types */
@@ -82,7 +81,6 @@ const struct _data_types data_types[] = {
     { "STRING", sizeof (void *) },
     { "PMC",    sizeof (void *) },           /* actual PMCs have positive class numbers */
     { "BIGINT", sizeof (void *) },
-    { "BIGNUM", sizeof (void *) },          /* might be different */
     { "DPOINTER", sizeof (void *) },
 
     { "char",   sizeof (char) },          /* native integer types */
