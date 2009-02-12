parrot/parrot

commit 06515f6a76704ad8ef90aba022ca4ac56b5c08e0

[configure] rename some C templates & fix coding standard
git-svn-id: https://svn.parrot.org/parrot/trunk@36176 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

config/auto/jit.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2008, The Perl Foundation.
+# Copyright (C) 2001-2009, The Perl Foundation.
 # $Id$
 
 =head1 NAME
@@ -115,7 +115,7 @@ sub runstep {
     # test for executable malloced memory
     if ( -e "config/auto/jit/test_exec_$osname.in" ) {
         print " (has_exec_protect " if $verbose;
-        $conf->cc_gen("config/auto/jit/test_exec_$osname.in");
+        $conf->cc_gen("config/auto/jit/test_exec_${osname}_c.in");
         eval { $conf->cc_build(); };
         if ($@) {
             print " $@) " if $verbose;

MANIFEST
@@ -1,7 +1,7 @@
 # ex: set ro:
 # $Id$
 #
-# generated by tools/dev/mk_manifest_and_skip.pl Thu Jan 29 01:00:00 2009 UT
+# generated by tools/dev/mk_manifest_and_skip.pl Fri Jan 30 14:36:00 2009 UT
 #
 # See tools/dev/install_files.pl for documentation on the
 # format of this file.
@@ -234,8 +234,8 @@ config/auto/crypto.pm                                       []
 config/auto/crypto/crypto.in                                []
 config/auto/ctags.pm                                        []
 config/auto/env.pm                                          []
-config/auto/env/test_setenv.in                              []
-config/auto/env/test_unsetenv.in                            []
+config/auto/env/test_setenv_c.in                            []
+config/auto/env/test_unsetenv_c.in                          []
 config/auto/fink.pm                                         []
 config/auto/format.pm                                       []
 config/auto/format/floatval_maxmin.in                       []
@@ -258,19 +258,19 @@ config/auto/headers.pm                                      []
 config/auto/headers/test_c.in                               []
 config/auto/icu.pm                                          []
 config/auto/inline.pm                                       []
-config/auto/inline/test_1.in                                []
-config/auto/inline/test_2.in                                []
+config/auto/inline/test1_c.in                               []
+config/auto/inline/test2_c.in                               []
 config/auto/isreg.pm                                        []
 config/auto/isreg/test_c.in                                 []
 config/auto/jit.pm                                          []
 config/auto/jit/test_c.in                                   []
-config/auto/jit/test_exec_cygwin.in                         []
-config/auto/jit/test_exec_linux.in                          []
-config/auto/jit/test_exec_openbsd.in                        []
+config/auto/jit/test_exec_cygwin_c.in                       []
+config/auto/jit/test_exec_linux_c.in                        []
+config/auto/jit/test_exec_openbsd_c.in                      []
 config/auto/macports.pm                                     []
 config/auto/memalign.pm                                     []
+config/auto/memalign/test2_c.in                             []
 config/auto/memalign/test_c.in                              []
-config/auto/memalign/test_c2.in                             []
 config/auto/msvc.pm                                         []
 config/auto/msvc/test_c.in                                  []
 config/auto/opengl.pm                                       []
@@ -285,15 +285,15 @@ config/auto/readline.pm                                     []
 config/auto/readline/readline.in                            []
 config/auto/revision.pm                                     []
 config/auto/signal.pm                                       []
-config/auto/signal/test_1.in                                []
-config/auto/signal/test_2.in                                []
-config/auto/signal/test_itimer.in                           []
+config/auto/signal/test1_c.in                               []
+config/auto/signal/test2_c.in                               []
+config/auto/signal/test_itimer_c.in                         []
 config/auto/sizes.pm                                        []
 config/auto/sizes/test2_c.in                                []
 config/auto/sizes/test3_c.in                                []
 config/auto/sizes/test_c.in                                 []
 config/auto/snprintf.pm                                     []
-config/auto/snprintf/test.in                                []
+config/auto/snprintf/test_c.in                              []
 config/auto/socklen_t.pm                                    []
 config/auto/va_ptr.pm                                       []
 config/auto/va_ptr/test_c.in                                []
@@ -2971,6 +2971,7 @@ t/codingstd/filenames.t                                     []
 t/codingstd/fixme.t                                         []
 t/codingstd/gmt_utc.t                                       []
 t/codingstd/linelength.t                                    []
+t/codingstd/make_code_coda.t                                []
 t/codingstd/pccmethod_deps.t                                []
 t/codingstd/pdd_format.t                                    []
 t/codingstd/perlcritic.t                                    []

MANIFEST.SKIP
@@ -1,6 +1,6 @@
 # ex: set ro:
-# $Id: MANIFEST.SKIP 36083 2009-01-28 08:13:34Z fperrad $
-# generated by tools/dev/mk_manifest_and_skip.pl Wed Jan 28 08:02:55 2009 UT
+# $Id$
+# generated by tools/dev/mk_manifest_and_skip.pl Fri Jan 30 14:36:00 2009 UT
 #
 # This file should contain a transcript of the svn:ignore properties
 # of the directories in the Parrot subversion repository. (Needed for
@@ -403,6 +403,8 @@
 ^languages/gil/
 ^languages/hq9plus$
 ^languages/hq9plus/
+^languages/kea-cl$
+^languages/kea-cl/
 ^languages/languages_smoke\.html$
 ^languages/languages_smoke\.html/
 ^languages/lazy-k$
@@ -411,6 +413,8 @@
 ^languages/m4/
 ^languages/markdown$
 ^languages/markdown/
+^languages/perk$
+^languages/perk/
 ^languages/pjs$
 ^languages/pjs/
 ^languages/primitivearc$

config/auto/env.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2003, The Perl Foundation.
+# Copyright (C) 2001-2009, The Perl Foundation.
 # $Id$
 
 =head1 NAME
@@ -37,13 +37,13 @@ sub runstep {
 
     my ( $setenv, $unsetenv ) = ( 0, 0 );
 
-    $conf->cc_gen('config/auto/env/test_setenv.in');
+    $conf->cc_gen('config/auto/env/test_setenv_c.in');
     eval { $conf->cc_build(); };
     unless ( $@ || $conf->cc_run() !~ /ok/ ) {
         $setenv = 1;
     }
     $conf->cc_clean();
-    $conf->cc_gen('config/auto/env/test_unsetenv.in');
+    $conf->cc_gen('config/auto/env/test_unsetenv_c.in');
     eval { $conf->cc_build(); };
     unless ( $@ || $conf->cc_run() !~ /ok/ ) {
         $unsetenv = 1;

config/auto/env/test_setenv.in
@@ -1,17 +1,0 @@
-/*
- * test for setenv
- *
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-    if (setenv("PARROT_TEST", "flaf", 1)) {
-        puts("borken");
-    } else {
-        puts("ok");
-    }
-
-    return 0;
-}

config/auto/env/test_setenv_c.in
@@ -1,0 +1,28 @@
+/*
+Copyright (C) 2003-2009, The Perl Foundation.
+$Id$
+
+test for setenv
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (setenv("PARROT_TEST", "flaf", 1)) {
+        puts("borken");
+    }
+    else {
+        puts("ok");
+    }
+
+    return 0;
+}
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */

config/auto/env/test_unsetenv.in
@@ -1,16 +1,0 @@
-/*
- * test for unsetenv
- *
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-/* we have an hand-rolled unsetenv */
-#ifndef WIN32
-    unsetenv("PARROT_TEST");
-#endif
-    puts("ok");
-    return 0;
-}

config/auto/env/test_unsetenv_c.in
@@ -1,0 +1,27 @@
+/*
+Copyright (C) 2003-2009, The Perl Foundation.
+$Id$
+
+test for unsetenv
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+/* we have an hand-rolled unsetenv */
+#ifndef WIN32
+    unsetenv("PARROT_TEST");
+#endif
+    puts("ok");
+    return 0;
+}
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */

commit 244fd6db0e84125e79dc788cb85137734122fe4b

Apply exec_test.patch from doughera++ in TT #261.  Fixes some fallout from r36176, and it might fix TT #261.
git-svn-id: https://svn.parrot.org/parrot/trunk@36636 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

config/auto/jit.pm
@@ -113,7 +113,7 @@ sub runstep {
     _handle_execcapable($conf, $execcapable);
 
     # test for executable malloced memory
-    if ( -e "config/auto/jit/test_exec_$osname.in" ) {
+    if ( -e "config/auto/jit/test_exec_${osname}_c.in" ) {
         print " (has_exec_protect " if $verbose;
         $conf->cc_gen("config/auto/jit/test_exec_${osname}_c.in");
         eval { $conf->cc_build(); };