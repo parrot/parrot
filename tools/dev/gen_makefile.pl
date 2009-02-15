1707cbdec2ac5a57a8a33371dac0adedf2e416b3
Allison Randal <allison@parrot.org>
2009-02-15 19:07:28 +0000

    [install] Adding a simple tool that generates a single makefile from the configuration data.
    git-svn-id: https://svn.parrot.org/parrot/trunk@36769 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ tools/dev/gen_makefile.pl
@@ -0,0 +1,42 @@
+#! perl
+
+# Copyright (C) 2009, Parrot Foundation.
+# $Id: $
+
+use 5.008;
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";    # install location
+use lib "$Bin/../../lib"; # build location
+use Parrot::Configure;
+use Parrot::Configure::Options qw( process_options );
+
+$| = 1;    # $OUTPUT_AUTOFLUSH = 1;
+
+my $args = process_options(
+    {
+        step => 'gen::makefiles',
+        mode => 'reconfigure',
+        conditioned_lines => 1,
+        replace_slashes => 1,
+    }
+);
+exit(1) unless ( defined $args );
+
+my $template = $ARGV[0];
+my $makefile = $ARGV[1];
+
+my $conf = Parrot::Configure->new;
+$conf->options->set( %{$args} );
+$conf->data()->get_PConfig(); #load configuration data
+$conf->genfile( $template => $makefile);
+
+exit(0);
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
