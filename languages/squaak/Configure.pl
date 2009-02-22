68a5d48711ce2c15740f4d7b1aa9cd5fd44246fc
François Perrad <francois.perrad@gadz.org>
2009-02-22 21:09:27 +0000

    [squaak] re-run tools\dev\mk_language_shell.pl (r36933)
    git-svn-id: https://svn.parrot.org/parrot/trunk@36934 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/squaak/Configure.pl
+++ languages/squaak/Configure.pl
@@ -4,13 +4,57 @@ use strict;
 use warnings;
 use 5.008;
 
-my $build_dir = '../..';
-my $hll       = 'squaak';
-my $cmd       = qq{$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=$hll};
+#  Get a list of parrot-configs to invoke.
+my @parrot_config_exe = (
+    'parrot/parrot_config',
+    '../../parrot_config',
+    'parrot_config',
+);
 
-print "Running '$cmd' in $build_dir\n";
-chdir $build_dir;
-`$cmd`
+#  Get configuration information from parrot_config
+my %config = read_parrot_config(@parrot_config_exe);
+unless (%config) {
+    die "Unable to locate parrot_config.";
+}
+
+#  Create the Makefile using the information we just got
+create_makefiles(%config);
+
+sub read_parrot_config {
+    my @parrot_config_exe = @_;
+    my %config = ();
+    for my $exe (@parrot_config_exe) {
+        no warnings;
+        if (open my $PARROT_CONFIG, '-|', "$exe --dump") {
+            print "Reading configuration information from $exe\n";
+            while (<$PARROT_CONFIG>) {
+                $config{$1} = $2 if (/(\w+) => '(.*)'/);
+            }
+            close $PARROT_CONFIG;
+            last if %config;
+        }
+    }
+    %config;
+}
+
+
+#  Generate Makefiles from a configuration
+sub create_makefiles {
+    my %config = @_;
+    my %makefiles = (
+        'config/makefiles/root.in' => 'Makefile',
+#        'config/makefiles/pmc.in'  => 'src/pmc/Makefile',
+#        'config/makefiles/ops.in'  => 'src/ops/Makefile',
+    );
+    my $build_tool = $config{libdir} . $config{versiondir}
+                   . '/tools/dev/gen_makefile.pl';
+
+    foreach my $template (keys %makefiles) {
+        my $makefile = $makefiles{$template};
+        print "Creating $makefile\n";
+        system($config{perl}, $build_tool, $template, $makefile);
+    }
+}
 
 # Local Variables:
 #   mode: cperl
