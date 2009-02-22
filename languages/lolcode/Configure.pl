5e6282560c39c0e2e5a7ca73dcadd3fc3f49c458
Bernhard Schmalhofer <Bernhard.Schmalhofer@gmx.de>
2009-02-22 10:04:23 +0000

    [lolcode] add a Configure.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@36929 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ languages/lolcode/Configure.pl
@@ -0,0 +1,21 @@
+# $Id$
+
+use strict;
+use warnings;
+use 5.008;
+
+my $build_dir = '../..';
+my $hll       = 'lolcode';
+my $cmd       = qq{$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=$hll};
+
+print "Running '$cmd' in $build_dir\n";
+chdir $build_dir;
+`$cmd`
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
+
