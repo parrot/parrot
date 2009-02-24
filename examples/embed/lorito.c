4d4314f0ae60ce3e6a609d0deaa59d082c8676c5
Julian Albo <julian.notfound@gmail.com>
2009-02-24 11:58:44 +0000

    [examples] add embed example
    git-svn-id: https://svn.parrot.org/parrot/trunk@36962 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ examples/embed/lorito.c
@@ -0,0 +1,72 @@
+/*
+Copyright (C) 2009, Parrot Foundation.
+$Id$
+
+A parrot embedding test
+'lorito' is 'little parrot' in spanish
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parrot/embed.h"
+#include "parrot/extend.h"
+
+/**********************************************************************/
+
+void fail(const char *msg);
+int lorito_main(Parrot_Interp interp, int argc, char **argv);
+
+/**********************************************************************/
+
+void fail(const char *msg)
+{
+    fprintf(stderr, "lorito failed: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+/**********************************************************************/
+
+int lorito_main(Parrot_Interp interp, int argc, char **argv)
+{
+    char *source;
+    Parrot_PackFile pf;
+    if (argc < 2)
+        fail("no args");
+    source = argv[1];
+
+    pf = Parrot_pbc_read(interp, source, 0);
+    if (! pf)
+        fail("Cannot load file");
+    Parrot_pbc_load(interp, pf);
+
+    Parrot_runcode(interp, argc - 1, argv + 1);
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    Parrot_Interp interp;
+    int r;
+
+    interp = Parrot_new(NULL);
+    if (! interp)
+        fail("Cannot create parrot interpreter");
+
+    Parrot_setwarnings(interp, PARROT_WARNINGS_ALL_FLAG);
+
+    r = lorito_main(interp, argc, argv);
+
+    Parrot_destroy(interp);
+    return r;
+}
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
