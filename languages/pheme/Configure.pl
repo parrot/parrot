67b6af0ef12d17a784ee83e28b6cad3fbee087e7
Allison Randal <allison@parrot.org>
2009-03-10 20:21:00 +0000

    [pheme] Relocating to https://svn.parrot.org/languages/pheme/trunk.
    git-svn-id: https://svn.parrot.org/parrot/trunk@37276 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- languages/pheme/Configure.pl
+++
@@ -1,9 +0,0 @@
-#! perl
-# $Id$
-# Copyright (C) 2009, Parrot Foundation.
-
-use strict;
-use warnings;
-
-chdir '../..';
-`$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=pheme`;
