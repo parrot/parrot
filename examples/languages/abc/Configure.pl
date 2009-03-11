cde6b194a4abdb8330513f807e7708eb3ca0291e
Allison Randal <allison@parrot.org>
2009-03-11 05:32:45 +0000

    [abc] A configuration script for ABC, to generate its makefile based on Parrot config attributes.
    git-svn-id: https://svn.parrot.org/parrot/trunk@37300 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ examples/languages/abc/Configure.pl
@@ -0,0 +1,40 @@
+#! perl
+
+use strict;
+use warnings;
+
+my $perlbin = `../../../parrot_config perl`;
+my $builddir = `../../../parrot_config build_dir`;
+my $slash = `../../../parrot_config slash`;
+my $make = `../../../parrot_config make`;
+
+chomp($perlbin);
+chomp($builddir);
+chomp($slash);
+chomp($make);
+
+my $build_tool = $perlbin . " "
+               . $builddir
+               . $slash
+               . "tools"
+               . $slash
+               . "dev"
+               . $slash
+               . "gen_makefile.pl";
+
+my %makefiles = (
+    "config/makefiles/root.in" => "Makefile",
+);
+
+foreach my $template (keys %makefiles) {
+    my $makefile = $makefiles{$template};
+    print "Creating $makefile\n";
+    system("$build_tool $template $makefile");
+}
+
+print <<"END";
+
+You can now use '$make' to build ABC.
+END
+
+exit;
