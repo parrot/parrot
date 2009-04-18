b3835a1512ae46df3206f0cd6029e244f0ba3c5f
Mark Glines <mark@glines.org>
2009-04-18 03:39:12 +0000

    [jit] More TT #512 followup: Darwin's assembler can't handle registers with the % prefix.  Make it conditional based on the platform.
    git-svn-id: https://svn.parrot.org/parrot/trunk@38190 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/jit/ppc/asm.s
+++ src/jit/ppc/asm.s
@@ -8,6 +8,7 @@ _Parrot_ppc_jit_restore_nonvolatile_registers:
 Parrot_ppc_jit_restore_nonvolatile_registers:
 #endif
 
+#if defined(__NetBSD__) || defined(__OpenBSD__)
     lfd     %f14,-84(%r1)
     lfd     %f15,-92(%r1)
     lfd     %f16,-100(%r1)
@@ -26,5 +27,25 @@ Parrot_ppc_jit_restore_nonvolatile_registers:
     lfd     %f29,-204(%r1)
     lfd     %f30,-212(%r1)
     lfd     %f31,-220(%r1)
+#else /* !__NetBSD__ && !__OpenBSD__ */
+    lfd     f14,-84(r1)
+    lfd     f15,-92(r1)
+    lfd     f16,-100(r1)
+    lfd     f17,-108(r1)
+    lfd     f18,-116(r1)
+    lfd     f19,-124(r1)
+    lfd     f20,-132(r1)
+    lfd     f21,-140(r1)
+    lfd     f22,-148(r1)
+    lfd     f23,-156(r1)
+    lfd     f24,-164(r1)
+    lfd     f25,-172(r1)
+    lfd     f26,-180(r1)
+    lfd     f27,-188(r1)
+    lfd     f28,-196(r1)
+    lfd     f29,-204(r1)
+    lfd     f30,-212(r1)
+    lfd     f31,-220(r1)
+#endif /* __NetBSD__ || __OpenBSD__ */
 
     blr
