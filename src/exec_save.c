132cc32629fd1cd49703f5a9f942c55c22044a1a
Mark Glines <mark@glines.org>
2009-04-19 14:24:49 +0000

    [ppc] Move the TT #512 fix down a bit, it needs to be below the inclusion of <elf.h>. Patch courtesy of heidnes++.
    git-svn-id: https://svn.parrot.org/parrot/trunk@38209 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/exec_save.c
+++ src/exec_save.c
@@ -30,26 +30,6 @@ static void save_int(FILE *fp, int i);
 static void save_short(FILE *fp, short s);
 static void save_struct(FILE *fp, void *sp, size_t size);
 
-#if defined(PARROT_PPC)
-#  if !defined(R_PPC_ADDR16_HI) && !defined(R_PPC_ADDR16_LO) && \
-       defined(R_PPC_16_HI) && defined(R_PPC_16_LO)
-#    define	R_PPC_ADDR16_HI	R_PPC_16_HI
-#    define	R_PPC_ADDR16_LO	R_PPC_16_LO
-#  endif
-   /*
-    * NetBSD/powerpc 3.x and OpenBSD/powerpc doesn't define these constants,
-    * but instead has them as enums, so add some workarounds for those.
-    */
-#  if !defined(R_PPC_ADDR16_HI) && !defined(R_PPC_ADDR16_LO) && \
-       (defined(__NetBSD__) || defined(__OpenBSD__))
-#    define	R_PPC_ADDR16_HI RELOC_16_HI
-#    define	R_PPC_ADDR16_LO RELOC_16_LO
-#  endif
-#  if !defined(R_PPC_REL24) && (defined(__NetBSD__) || defined(__OpenBSD__))
-#    define	R_PPC_REL24	RELOC_REL24
-#  endif
-#endif /* PARROT_PPC */
-
 #ifdef EXEC_A_OUT
 
 #  include <a.out.h>
@@ -169,6 +149,26 @@ Parrot_exec_save(PARROT_INTERP, Parrot_exec_objfile_t *obj, const char *file)
 #  else
 #    include <elf.h>
 #  endif
+
+#  if defined(PARROT_PPC)
+#    if !defined(R_PPC_ADDR16_HI) && !defined(R_PPC_ADDR16_LO) && \
+         defined(R_PPC_16_HI) && defined(R_PPC_16_LO)
+#      define	R_PPC_ADDR16_HI	R_PPC_16_HI
+#      define	R_PPC_ADDR16_LO	R_PPC_16_LO
+#    endif
+     /*
+      * NetBSD/powerpc 3.x and OpenBSD/powerpc doesn't define these constants,
+      * but instead has them as enums, so add some workarounds for those.
+      */
+#    if !defined(R_PPC_ADDR16_HI) && !defined(R_PPC_ADDR16_LO) && \
+         (defined(__NetBSD__) || defined(__OpenBSD__))
+#      define	R_PPC_ADDR16_HI RELOC_16_HI
+#      define	R_PPC_ADDR16_LO RELOC_16_LO
+#    endif
+#    if !defined(R_PPC_REL24) && (defined(__NetBSD__) || defined(__OpenBSD__))
+#      define	R_PPC_REL24	RELOC_REL24
+#    endif
+#  endif /* PARROT_PPC */
 
 /* Add a section to the file
  *
