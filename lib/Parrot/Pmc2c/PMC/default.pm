8b3c1db62aff802629d7738d27a52e2fe6053456
Vasily Chekalkin <bacek@bacek.com>
2009-05-08 12:43:47 +0000

    Merge tt631_part3 branch into trunk.
    git-svn-id: https://svn.parrot.org/parrot/trunk@38603 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PMC/default.pm
+++ lib/Parrot/Pmc2c/PMC/default.pm
@@ -26,27 +26,65 @@ sub pre_method_gen {
     foreach my $method ( @{ $self->vtable->methods } ) {
         my $vt_method_name = $method->name;
         next unless $self->unimplemented_vtable($vt_method_name);
-        my $new_default_method = $method->clone(
-            {
-                parent_name => $self->name,
-                type        => Parrot::Pmc2c::Method::VTABLE,
-            }
-        );
-        my $ret = return_statement($method);
-
-        # take care to mark the parameters as unused
-        # to avoid compiler warnings
-        my $body;
-        foreach my $param (split /,\s*/, $method->parameters) {
-            $param =~ s/.*\b(\w+)/$1/;
-            $body .= "    UNUSED($param)\n";
-        }
-        $body .= qq{    cant_do_method(interp, pmc, "$vt_method_name");\n};
-
-        $new_default_method->body( Parrot::Pmc2c::Emitter->text($body));
-        $self->add_method($new_default_method);
+        $self->add_method($self->_generate_default_method($self, $method, 'cant_do_method'));
     }
     return 1;
+}
+
+sub gen_methods {
+    my ($self) = @_;
+
+    $self->SUPER::gen_methods;
+
+    # Generate RO variants.
+    my $ro = Parrot::Pmc2c::PMC::RO->new($self);
+    $ro->{emitter} = $self->{emitter};
+    foreach my $method ( @{ $self->vtable->methods } ) {
+        my $vt_method_name = $method->name;
+        if ($vt_method_name eq 'find_method') {
+            # Generate default_ro_find_method.
+            $self->{emitter}->emit(<<'EOC');
+static  PMC *
+Parrot_default_ro_find_method(PARROT_INTERP, PMC *pmc, STRING *method_name) {
+    /* Use non-readonly find_method. Current vtable is ro variant. So ro_variant contains non-ro variant */
+    PMC *const method = pmc->vtable->ro_variant_vtable->find_method(interp, pmc, method_name);
+    if (!PMC_IS_NULL(VTABLE_getprop(interp, method, CONST_STRING_GEN(interp, "write"))))
+        return PMCNULL;
+    else
+        return method;
+}
+EOC
+        }
+        if ( $self->vtable_method_does_write($vt_method_name) ) {
+            my $m = $self->_generate_default_method($ro, $method, 'cant_do_write_method');
+            $m->generate_body($ro);
+        }
+    }
+}
+
+sub _generate_default_method {
+    my ($self, $pmc, $method, $stub_func) = @_;
+        
+    my $clone = $method->clone(
+         {
+             parent_name => $self->name,
+             type        => Parrot::Pmc2c::Method::VTABLE,
+         }
+     );
+
+    # take care to mark the parameters as unused
+    # to avoid compiler warnings
+    my $body;
+    foreach my $param (split /,\s*/, $method->parameters) {
+        $param =~ s/.*\b(\w+)/$1/;
+        $body .= "    UNUSED($param)\n";
+    }
+    my $vt_method_name = $method->name;
+    $body .= qq{    $stub_func(interp, pmc, "$vt_method_name");\n};
+
+    $clone->body( Parrot::Pmc2c::Emitter->text($body));
+
+    $clone;
 }
 
 sub update_vtable_func {
@@ -70,6 +108,27 @@ PARROT_EXPORT VTABLE* Parrot_default_get_vtable(PARROT_INTERP) {
 $vtable_decl
 
     return Parrot_clone_vtable(interp, &temp_vtable);
+}
+
+EOC
+
+    # Generate RO version of default VTABLE.
+    my $ro_vtable_decl = '';
+    foreach my $name ( @{ $self->vtable->names } ) {
+        if ($self->vtable_method_does_write($name) || ($name eq 'find_method')) {
+            $ro_vtable_decl .= "    vt->$name = Parrot_default_ro_${name};\n";
+        }
+    }
+
+    $cout .= <<"EOC";
+
+PARROT_EXPORT VTABLE* Parrot_default_ro_get_vtable(PARROT_INTERP) {
+
+    VTABLE * vt = Parrot_default_get_vtable(interp);
+
+$ro_vtable_decl
+
+    return vt;
 }
 
 EOC
--- lib/Parrot/Pmc2c/PMCEmitter.pm
+++ lib/Parrot/Pmc2c/PMCEmitter.pm
@@ -827,28 +827,29 @@ $vtable_updates
 
 EOC
 
-    my %extra_vt;
-    $extra_vt{ro} = $self->{ro} if $self->{ro};
-
-    for my $k (keys %extra_vt) {
-
-        my $vtable_updates = '';
-        foreach my $vt_method ( @{ $self->$k->vtable->names} ) {
-
-            next unless ($self->$k->implements_vtable($vt_method));
-
-            $vtable_updates .= "    vt->$vt_method = Parrot_${classname}_${k}_${vt_method};\n";
+    # Generate RO vtable for implemented non-updating methods
+    $vtable_updates = '';
+    foreach my $name ( @{ $self->vtable->names} ) {
+        next unless exists $self->{has_method}{$name};
+        if ($self->vtable_method_does_write($name)) {
+            # If we override constantness status of vtable
+            if (!$self->vtable->attrs($name)->{write}) {
+                $vtable_updates .= "    vt->$name = Parrot_${classname}_ro_${name};\n";
+            }
         }
+        else {
+            $vtable_updates .= "    vt->$name = Parrot_${classname}_${name};\n";
+        }
+    }
 
-        $cout .= <<"EOC";
+    $cout .= <<"EOC";
 
-PARROT_EXPORT VTABLE *Parrot_${classname}_${k}_update_vtable(VTABLE *vt) {
+PARROT_EXPORT VTABLE *Parrot_${classname}_ro_update_vtable(VTABLE *vt) {
 $vtable_updates
     return vt;
 }
 
 EOC
-    }
 
     $cout;
 }
@@ -885,31 +886,25 @@ $get_vtable
 
 EOC
 
-    my %extra_vt;
-    $extra_vt{ro} = $self->{ro} if $self->{ro};
-
-    for my $k (keys %extra_vt) {
-        my $get_extra_vtable = '';
-        foreach my $parent_name ( reverse ($self->name, @{ $self->parents }) ) {
-            if ($parent_name eq 'default') {
-                $get_extra_vtable .= "    vt = Parrot_default_get_vtable(interp);\n";
-            }
-            else {
-                $get_extra_vtable .= "    Parrot_${parent_name}_update_vtable(vt);\n";
-                $get_extra_vtable .= "    Parrot_${parent_name}_${k}_update_vtable(vt);\n";
-            }
+    my $get_extra_vtable = '';
+    foreach my $parent_name ( reverse ($self->name, @{ $self->parents }) ) {
+        if ($parent_name eq 'default') {
+            $get_extra_vtable .= "    vt = Parrot_default_ro_get_vtable(interp);\n";
         }
+        else {
+            $get_extra_vtable .= "    Parrot_${parent_name}_ro_update_vtable(vt);\n";
+        }
+    }
 
-        $cout .= <<"EOC";
+    $cout .= <<"EOC";
 PARROT_EXPORT
-VTABLE* Parrot_${classname}_${k}_get_vtable(PARROT_INTERP) {
+VTABLE* Parrot_${classname}_ro_get_vtable(PARROT_INTERP) {
     VTABLE *vt;
 $get_extra_vtable
     return vt;
 }
 
 EOC
-    }
 
     $cout;
 }
