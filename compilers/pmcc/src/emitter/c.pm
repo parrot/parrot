a68b71daf80f94b817f819cd1d8a06d03a670182
Vasily Chekalkin <bacek@bacek.com>
2009-05-10 05:34:35 +0000

    Ooops. I forgot to add emitter/c.pm
    git-svn-id: https://svn.parrot.org/parrot/branches/pmc_pct@38669 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/pmcc/src/emitter/c.pm
@@ -0,0 +1,46 @@
+
+# Copyright (C) 2009, Parrot Foundation.
+# $Id$
+
+class PMC::Emitter::C;
+
+method rewrite_op($pmclass, $past) {
+    my $pasttype := $past<pasttype>;
+    #say("OH HI " ~ $pasttype);
+    my $res;
+    if $pasttype eq 'inline' {
+        $res := $past<inline>;
+    }
+    elsif $pasttype eq 'call' {
+        # Technicaly we can have different "calls". But for now only macros supported.
+        $res := self.rewrite_macro($pmclass, $past);
+    }
+
+    $res;
+}
+
+method rewrite_macro($pmclass, $past) {
+    my $res;
+    if $past<is_self> {
+        # FIXME. We should able to handle multiply arguments.
+        $res := "Parrot_" ~ $pmclass.name ~ "_" ~ $past.name
+                ~ '(' ~ full_arguments($past) ~')';
+    }
+
+    $res;
+}
+
+sub full_arguments($past) {
+    my @tmp;
+    @tmp.push('interp, pmc');
+    for @($past) {
+        @tmp.push($_.value);
+    }
+    join(', ', @tmp);
+}
+
+# Local Variables:
+#   mode: pir
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
