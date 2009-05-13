2fee9abbb12a251c95ffbf2c0de50e1c47944aad
Vasily Chekalkin <bacek@bacek.com>
2009-05-13 12:30:18 +0000

    Add pre_method_gen and stub for get_vtable_func into Emitter::PMC::default
    git-svn-id: https://svn.parrot.org/parrot/branches/pmc_pct@38746 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- compilers/pmcc/src/emitter/pmc/default.pm
+++ compilers/pmcc/src/emitter/pmc/default.pm
@@ -11,6 +11,47 @@ Specialised emitter for default PMC.
 
 class PMC::Emitter::PMC::default;
 
+=item C<pre_method_gen>
+
+Generate bunch of "not_implemented_yet" VTABLE functions.
+
+=cut
+method pre_method_gen() {
+    my @vtables := PMC::VTableInfo::vtable_list();
+    my %vtables := self.vtables;
+
+    for @vtables {
+        if !exists(%vtables, $_.name) {
+            my $entry := PAST::Block.new(
+                :name($_.name),
+            );
+            # FIXME Ugly hack
+            $entry<parameters> := PAST::Op.new(
+                PAST::Val.new(
+                    :returns(''),
+                    :name($_<parameters>)
+                )
+            );
+
+            $entry.push(
+                PAST::Op.new(
+                    :pasttype('inline'),
+                    :inline(
+' {
+    cant_do_method(interp, pmc, "' ~ $_.name ~ '");
+}
+'),
+                )
+            );
+            %vtables{$entry.name} := $entry;
+            #say("GENERATING " ~ $_.name);
+        }
+        else {
+            #say("USING " ~ $_.name);
+        }
+    }
+}
+
 =item C<get_vtable_func>
 
 Generate C-code for C<Parrot_default_get_vtable> and
@@ -19,6 +60,28 @@ C<Parrot_default_ro_get_vtable> functions.
 =cut
 
 method get_vtable_func() {
+    my %vtables := self.vtables;
+    my $past := self.past;
+
+    my @res;
+    @res.push('
+PARROT_EXPORT VTABLE* Parrot_default_get_vtable(PARROT_INTERP) {
+
+    static const char attr_defs [] = "";
+'
+    );
+
+#    for @vtables {
+#        say("vtable " ~ $_.name ~ ' ' ~ $past<vtables>{$_.name});
+#    }
+
+    @res.push('
+    return Parrot_clone_vtable(interp, &temp_vtable);
+}
+'
+    );
+
+    join('', @res);
 }
 
 =item C<update_vtable_func>
