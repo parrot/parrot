5e30895044ce90c1c81f0c862de4c348cf959cca
Vasily Chekalkin <bacek@bacek.com>
2009-05-30 01:44:18 +0000

    [pmc2c] Fallback to MMD for non-core PMCs.
    git-svn-id: https://svn.parrot.org/parrot/trunk@39256 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PMCEmitter.pm
+++ lib/Parrot/Pmc2c/PMCEmitter.pm
@@ -983,6 +983,8 @@ sub gen_switch_vtable {
         
         my $body = <<"BODY";
     INTVAL type = VTABLE_type(INTERP, value);
+    if (SELF.type() > enum_class_core_max) /* For dynpmc fallback to MMD */
+        type = enum_class_core_max;
     switch(type) {
 $cases
     }
