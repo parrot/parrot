df2f4da4fc03d193929fe83435d9cc73b2dd8f9b
Christoph Otto <christoph@mksig.org>
2009-06-26 03:00:41 +0000

    [pmcc] add a file I forgot to add earlier
    git-svn-id: https://svn.parrot.org/parrot/branches/pmc_pct@39792 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ compilers/pmcc/src/nodes.pm
@@ -0,0 +1,23 @@
+# Copyright (C) 2009, Parrot Foundation.
+# $Id$
+
+class PMC::Class;
+
+method create_variant_vtable() {
+
+    my %vtdump;
+
+    %vtdump := self.past.vtdump;
+
+    for (self.vtables{'default'}) {
+       if (!%vtdump{$_}{'attributes'}{'write'}) {
+           self.vtables{'ro'}{$_} := self.vtables{'default'}{$_};
+           #copy the function into self.past.vtables{'ro'}
+           #XXX: what needs to be copied?
+           #  - I'll need the vt function name to add to the VTABLE struct
+           #  - if it's a vt function that overrides :write-ness, I'll need to note that and emit code for that function
+           #  -
+       }
+    }
+}
+
