cc052b9f6ee7f40ea51bb915b7527c0988caa0d3
Will "Coke" Coleda <will@coleda.com>
2009-07-09 23:59:15 +0000

    When running c-like tests, don't re-invent configuration.
    Patch courtesy doughera++ (TT #822)
    
    
    
    git-svn-id: https://svn.parrot.org/parrot/trunk@39966 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Test.pm
+++ lib/Parrot/Test.pm
@@ -959,22 +959,13 @@ sub _generate_test_functions {
             {
                 my $cfg = File::Spec->join( 'src', "parrot_config$PConfig{o}" );
                 my $iculibs = $PConfig{has_icu} ? $PConfig{icu_shared} : q{};
-                my $libparrot =
-                    $PConfig{parrot_is_shared}
-                      ? ("$PConfig{rpath_blib} "
-                        . (($^O =~ m/MSWin32/ and $PConfig{cc} =~ /\bcl\b/)
-                          ? ""
-                          : "-L$PConfig{blib_dir} "))
-                        . ($^O =~ m/MSWin32/
-                          ? $PConfig{libparrot_linkflags}
-                          : "-lparrot")
-                      : File::Spec->join(
-                          @PConfig{qw/build_dir blib_dir libparrot_static/},
-                      );
                 my $cmd =
-                      "$PConfig{link} $PConfig{linkflags} $PConfig{ld_debug} "
-                    . "$obj_f $cfg $PConfig{ld_out}$exe_f "
-                    . "$libparrot $iculibs $PConfig{libs}";
+                      "$PConfig{link} "
+                    . "$PConfig{ld_out}$exe_f "
+                    . "$obj_f $cfg "
+		    . "$PConfig{libparrot_linkflags} "
+		    . "$PConfig{linkflags} $PConfig{ld_debug} "
+                    . "$iculibs $PConfig{libs}";
                 my $exit_code = run_command(
                     $cmd,
                     'STDOUT' => $build_f,
