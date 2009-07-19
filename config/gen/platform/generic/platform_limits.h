0b34e9bbcc591a177ee6fc0ae8be7c9bdba09b76
Andrew Whitworth <wknight8111@gmail.com>
2009-07-19 17:44:47 +0000

    [win64] commit a small fix, probably in the worst possible way, to account for the fact that LLONG_MAX and LLONG_MIN don't appear to be defined in 64-bit cl.exe
    git-svn-id: https://svn.parrot.org/parrot/trunk@40157 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/platform/generic/platform_limits.h
+++ config/gen/platform/generic/platform_limits.h
@@ -9,9 +9,23 @@
 /*
  * Define the values for PARROT_INTVAL_MAX and PARROT_INTVAL_MIN
  * in platforms that does no have limits.h or gives wrong values.
- * The generic versions assumes limits.h availability,
- * and does nothing.
+ * The generic versions assumes limits.h availability.
  */
+
+/* LLONG_MAX doesn't appear to be provided for some compilers of Win64, so
+   try to define them here. */
+#ifdef _MSC_VER
+#  ifndef LLONG_MAX
+#    ifdef _I64_MAX
+#      define LLONG_MAX _I64_MAX
+#    endif
+#  endif
+#  ifndef LLONG_MIN
+#    ifdef _I64_MIN
+#      define LLONG_MIN _I64_MIN
+#    endif
+#  endif
+#endif
 
 #endif /* PARROT_PLATFORM_GENERIC_PLATFORM_LIMITS_GUARD */
 
