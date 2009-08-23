9877c67004b7f403028dacfe6a61055cc0960660
Andrew Whitworth <wknight8111@gmail.com>
2009-08-23 11:01:24 +0000

    [gc] fix a problem with mixed declarations and code for TT #942. MoC++
    git-svn-id: https://svn.parrot.org/parrot/trunk@40730 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/gc/mark_sweep.c
+++ src/gc/mark_sweep.c
@@ -298,8 +298,8 @@ Parrot_gc_sweep_pool(PARROT_INTERP, ARGMOD(Small_Object_Pool *pool))
     /* Run through all the PObj header pools and mark */
     for (cur_arena = pool->last_Arena; cur_arena; cur_arena = cur_arena->prev) {
         const size_t objects_end = cur_arena->used;
-        b = (PObj *)cur_arena->start_objects;
         UINTVAL i;
+        b = (PObj *)cur_arena->start_objects;
 
         /* loop only while there are objects in the arena */
         for (i = objects_end; i; i--) {
