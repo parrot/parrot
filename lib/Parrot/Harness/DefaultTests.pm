94fafee851b90276ba872a27c1cadf48163f969a
James E Keenan (Jim) <jkeenan@cpan.org>
2009-10-14 02:12:37 +0000

    Merge library_files branch into trunk.  Replace EXTRA_TEST_FILES defined in Makefile with @library_files defined in lib/Parrot/Harness/DefaultTests.pm.  Cf.:  https://trac.parrot.org/parrot/ticket/1061.
    git-svn-id: https://svn.parrot.org/parrot/trunk@41850 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Harness/DefaultTests.pm
+++ lib/Parrot/Harness/DefaultTests.pm
@@ -7,20 +7,32 @@ Parrot::Harness::DefaultTests - Tests run by default by F<t/harness>
 
 =head1 DESCRIPTION
 
-This file exports by default a single subroutine, C<get_default_tests()>,
-which is the list of tests run by F<t/harness> by default.
-
-Upon request, this package exports six arrays holding various sets of tests:
+Upon request, this package exports six arrays holding various sets of paths to
+directories holding test files:
 
     @runcore_tests
     @core_tests
     @library_tests
     @configure_tests
-    @standard_tests
     @developing_tests
 
-In list context, C<get_default_tests()> returns the list of default tests.  In
-scalar context it returns a reference to that list.
+Each of these arrays holds a list of paths containing wildcards which are
+expanded by the shell when provided to programs such as F<t/harness>.  The
+paths describe directories holding test files.
+
+Example:
+
+    @core_tests = qw(
+        t/run/*.t
+        t/src/*.t
+        t/perl/*.t
+    );
+
+
+In addition, Parrot::Harness::Default Tests exports B<by default> one
+subroutine:  C<get_default_tests()>.  In list context, C<get_default_tests()>
+returns a list of shell-expandable paths to the most common tests.  In scalar
+context it returns a reference to that list.
 
 =cut
 
@@ -34,17 +46,15 @@ our (
     @core_tests,
     @library_tests,
     @configure_tests,
-    @standard_tests,
     @developing_tests
 );
 use base qw( Exporter );
-our @EXPORT = qw( get_default_tests );
+our @EXPORT = qw( get_common_tests );
 our @EXPORT_OK = qw(
     @runcore_tests
     @core_tests
     @library_tests
     @configure_tests
-    @standard_tests
     @developing_tests
 );
 
@@ -79,47 +89,27 @@ our @EXPORT_OK = qw(
 );
 
 # configure tests are tests to be run at the beginning of 'make test';
-# standard tests are other tests run by default with no core options
-# present
 @configure_tests = qw( t/configure/*.t t/steps/*.t t/postconfigure/*.t );
-@standard_tests = qw(
-    t/compilers/json/*.t
-    t/examples/*.t
-    t/doc/*.t
-    t/distro/manifest.t
+
+@developing_tests = (
+    't/distro/file_metadata.t',
+    ( glob 't/codingstd/*.t' ),
 );
 
-@developing_tests = ( 't/distro/file_metadata.t' );
-# Add in all t/codingstd except for a few skips.
-push @developing_tests, glob 't/codingstd/*.t';
+sub get_common_tests {
+    my ($longopts) = @_;
 
-sub get_default_tests {
-    my ($core_tests_only, $runcore_tests_only) = @_;
-
-    # Add metadata.t and coding standards tests only if we're DEVELOPING
-
-    # Note:  As of 2008-10-21, we're no longer including @standard_tests in
-    # @default_tests -- which means they're not included in 'make test'.
-    # But, for the time being, at least, we are still making
-    # it an exportable variable.  So we'll test for it in
-    # t/pharness/01-default_tests.t.
-
-    if ( -e "DEVELOPING" ) {
-        push @standard_tests, @developing_tests;
-    }
-
-    # build the list of default tests
-    my @default_tests = @runcore_tests;
-    unless ($runcore_tests_only) {
-       push @default_tests, @core_tests;
-       unless ($core_tests_only) {
-           push @default_tests, @library_tests;
-           unshift @default_tests, @configure_tests;
+    my @common_tests = @runcore_tests;
+    unless ($longopts->{runcore_tests_only}) {
+       push @common_tests, @core_tests;
+       unless ($longopts->{core_tests_only}) {
+           push @common_tests, @library_tests;
+           unshift @common_tests, @configure_tests;
        }
     }
     wantarray
-        ? return @default_tests
-        : return [ @default_tests ];
+        ? return @common_tests
+        : return [ @common_tests ];
 }
 
 1;
