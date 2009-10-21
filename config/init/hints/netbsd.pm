13175051d3e95b7c309c15ad9fe032372a91eecc
Daniel Arbelo Arrocha <arbelo@gmail.com>
2009-10-21 16:54:07 +0000

    Update the NetBSD hints file, shared libraries will now work there. Patch by he++
    git-svn-id: https://svn.parrot.org/parrot/trunk@41971 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/hints/netbsd.pm
+++ config/init/hints/netbsd.pm
@@ -9,6 +9,9 @@ use warnings;
 sub runstep {
     my ( $self, $conf ) = @_;
 
+    my $share_ext = $conf->option_or_data('share_ext');
+    my $version   = $conf->option_or_data('VERSION');
+
     my $ccflags = $conf->data->get('ccflags');
     if ( $ccflags !~ /-pthread\b/ ) {
         $ccflags .= ' -pthread';
@@ -19,7 +22,16 @@ sub runstep {
     if ( $libs !~ /-lpthread\b/ ) {
         $libs .= ' -lpthread';
     }
-    $conf->data->set( libs => $libs );
+    $conf->data->set(
+	libs => $libs,
+	rpath => '-Wl,-R',
+
+	has_dynamic_linking    => 1,
+	parrot_is_shared       => 1,
+	libparrot_shared       => "libparrot$share_ext.$version",
+	libparrot_shared_alias => "libparrot$share_ext",
+	libparrot_soname       => "-Wl,-soname=libparrot$share_ext.$version",
+    );
 }
 
 1;
