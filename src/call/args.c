65d7f2f086857823cd6232a2d775a4ccaee31895
Vasily Chekalkin <bacek@bacek.com>
2009-10-25 21:18:26 +0000

    Return early in fill_params error check if there is no named arguments. Improve fib.pir by another 1.2%.
    git-svn-id: https://svn.parrot.org/parrot/trunk@42094 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/call/args.c
+++ src/call/args.c
@@ -1288,7 +1288,14 @@ fill_params(PARROT_INTERP, ARGMOD_NULLOK(PMC *call_object),
 
     /* Double check that all named arguments were assigned to parameters. */
     if (err_check) {
-        PMC *named_arg_list = VTABLE_get_attr_str(interp, call_object, CONST_STRING(interp, "named"));
+        PMC  *named_arg_list;
+        Hash *h;
+        /* Early exit to avoid vtable call */
+        GETATTR_CallSignature_hash(interp, call_object, h);
+        if (!h || !h->entries)
+            return;
+
+        named_arg_list = VTABLE_get_attr_str(interp, call_object, CONST_STRING(interp, "named"));
         if (!PMC_IS_NULL(named_arg_list)) {
             INTVAL named_arg_count = VTABLE_elements(interp, named_arg_list);
             if (PMC_IS_NULL(named_used_list))
