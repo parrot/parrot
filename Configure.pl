fb5eae846ca9ee1b2d711f23d634bc3feed75dc3
James E Keenan (Jim) <jkeenan@cpan.org>
2009-11-08 01:30:41 +0000

    POD formatting corrections only.
    git-svn-id: https://svn.parrot.org/parrot/trunk@42348 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- Configure.pl
+++ Configure.pl
@@ -232,8 +232,8 @@ Link parrot dynamically.
 =item C<--m=32>
 
 Create a 32-bit executable on 64-architectures like x86_64. This
-option appends -m32 to compiler and linker programs and does
-s/lib64/lib/g on link flags.
+option appends C<-m32> to compiler and linker programs and does
+C<s/lib64/lib/g> on link flags.
 
 This option is experimental. See F<config/init/defaults.pm> for more.
 
@@ -247,7 +247,7 @@ Turn on profiled compile (gcc only for now)
 
 =item C<--optimize>
 
-Add perl5's $Config{optimize} to the compiler flags.
+Add Perl 5's C<$Config{optimize}> to the compiler flags.
 
 =item C<--optimize=flags>
 
