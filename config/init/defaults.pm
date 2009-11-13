10565823cb93c9e5fc4e41830934007a6d825e9c
James E Keenan (Jim) <jkeenan@cpan.org>
2009-11-13 00:50:48 +0000

    Convert P::C attribute 'sig_name' from get_p5 to _provisional.
    git-svn-id: https://svn.parrot.org/parrot/branches/configtests@42470 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/init/defaults.pm
+++ config/init/defaults.pm
@@ -56,7 +56,6 @@ sub runstep {
         d_socklen_t
         longsize
         optimize
-        sig_name
         use64bitint
     | ) {
         $conf->data->set_p5( $orig => $Config{$orig} );
@@ -75,6 +74,7 @@ sub runstep {
     # configtests branch:  We start to handle these things differently.
     foreach my $orig ( qw|
         scriptdirexp
+        sig_name
         sPRIgldbl
     | ) {
         $conf->data->set( qq|${orig}_provisional| => $Config{$orig} );
