d78bd7f0a6df2cbfe378bf5ae0e0be4a0225d280
James E Keenan (Jim) <jkeenan@cpan.org>
2009-11-20 00:15:55 +0000

    Why do a sort 3 times when you only need to do it once?
    git-svn-id: https://svn.parrot.org/parrot/trunk@42586 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/config_h.pm
+++ config/gen/config_h.pm
@@ -50,24 +50,26 @@ sub runstep {
         feature_file   => 1
     );
 
+    my @sorted_keys = sort $conf->data->keys();
+
     $conf->data->set( TEMP_header =>
         join "\n", map { $conf->data->get($_)
                        ? "#define PARROT_HAS_HEADER_" . uc(substr $_, 2) . " 1"
                        : "#undef  PARROT_HAS_HEADER_" . uc(substr $_, 2) }
                    grep { /^i_\w+/ }
-                   sort $conf->data->keys()
+                   @sorted_keys
     );
 
     $conf->data->set( TEMP_has_config =>
         join "\n", map { "#define PARROT_" . uc($_) . " 1" }
                    grep { /^HAS_\w+/ && $conf->data->get($_) }
-                   sort $conf->data->keys()
+                   @sorted_keys
     );
 
     $conf->data->set( TEMP_d_config =>
         join "\n", map { "#define PARROT_" . uc(substr $_, 2) . " " . $conf->data->get($_) }
                    grep { /^D_\w+/ }
-                   sort $conf->data->keys()
+                   @sorted_keys
     );
 
     $conf->data->set( TEMP_cli_define =>
