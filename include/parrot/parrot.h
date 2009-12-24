54875b38e8080f75cdc9405e84bb050818cd2490
Peter Lobsinger <plobsing@gmail.com>
2009-12-24 21:01:15 +0000

    remove OPCODE_TYPE_(PERL|PYTHON|JAVA|MSNET) which have been unused for 8 years
    git-svn-id: https://svn.parrot.org/parrot/trunk@43241 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/parrot.h
+++ include/parrot/parrot.h
@@ -109,11 +109,6 @@ typedef jmp_buf Parrot_jump_buff;
 #define NUM_REGISTERS 32
 #define PARROT_MAGIC 0x13155a1
 
-#define OPCODE_TYPE_PERL 0x5045524c
-#define OPCODE_TYPE_PYTHON 0x5045524b
-#define OPCODE_TYPE_JAVA 4871757
-#define OPCODE_TYPE_MSNET 0x2e4e4554
-
 typedef void STRING_FUNCS;
 typedef struct parrot_interp_t Interp;
 
