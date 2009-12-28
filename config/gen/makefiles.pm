62d3bdcfa3b4fec633b3d6ae8bd74ec011311446
Will "Coke" Coleda <will@coleda.com>
2009-12-28 15:26:10 +0000

    Eliminate nqp-rx's Makefile, avoid unnecessary copying of PBCs.
    NQP-rx depends on PCT, however, so parrot-nqp still depends on a dummy target.
    
    
    
    git-svn-id: https://svn.parrot.org/parrot/branches/one_make@43278 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/gen/makefiles.pm
+++ config/gen/makefiles.pm
@@ -36,8 +36,6 @@ sub _init {
             conditioned_lines => 1,
         },
 
-        'ext/nqp-rx/Makefile' => { SOURCE => 'config/gen/makefiles/nqp-rx.in' },
-
         'compilers/ncigen/Makefile'  =>
             { SOURCE => 'compilers/ncigen/config/makefiles/ncigen.in' },
         'compilers/nqp/Makefile'     =>
