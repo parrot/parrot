ca37779680bfa2ecb45e92420b774cb6aa9e7fdf
Will "Coke" Coleda <will@coleda.com>
2010-01-06 01:56:43 +0000

    Don't use := in makefiles, remove := > = translation.
    git-svn-id: https://svn.parrot.org/parrot/branches/one_make@43392 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Configure/Compiler.pm
+++ lib/Parrot/Configure/Compiler.pm
@@ -380,7 +380,6 @@ sub genfile {
         if ( $options{file_type} eq 'makefile' ) {
             $options{replace_slashes}   = 1;
             $options{conditioned_lines} = 1;
-            $options{no_simply_expanded_var} = ! $conf->data->get('gmake_version');
         }
     }
 
@@ -562,11 +561,6 @@ sub genfile {
 
             # replace \* with \\*, so make will not eat the \
             $line =~ s{(\\\*)}{\\$1}g;
-        }
-
-        if ( $options{no_simply_expanded_var} ) {
-            # replace := with =
-            $line =~ s{:=}{=};
         }
 
         print $out $line;
