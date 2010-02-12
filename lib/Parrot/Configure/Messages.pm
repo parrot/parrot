0030341f0cf2013134cee63cd98fb1d59f691e20
Gerd Pokorra <pokorra@uni-siegen.de>
2010-02-12 12:25:14 +0000

    only update the configure message also to the new year
    git-svn-id: https://svn.parrot.org/parrot/trunk@43918 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Configure/Messages.pm
+++ lib/Parrot/Configure/Messages.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2009, Parrot Foundation.
+# Copyright (C) 2001-2010, Parrot Foundation.
 # $Id$
 
 package Parrot::Configure::Messages;
@@ -17,7 +17,7 @@ sub print_introduction {
     my $parrot_version = shift;
     print <<"END";
 Parrot Version $parrot_version Configure 2.0
-Copyright (C) 2001-2009, Parrot Foundation.
+Copyright (C) 2001-2010, Parrot Foundation.
 
 Hello, I'm Configure. My job is to poke and prod your system to figure out
 how to build Parrot. The process is completely automated, unless you passed in
