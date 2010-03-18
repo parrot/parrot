3563f751d33d800f43938e3f30577427f6fb0db1
Andy Lester <andy@petdance.com>
2010-03-18 23:14:11 +0000

    disabling more annoying warnings, plus notes about them
    git-svn-id: https://svn.parrot.org/parrot/trunk@45046 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/warnings.pm
+++ config/auto/warnings.pm
@@ -246,7 +246,9 @@ sub _init {
         -Wunused-variable
         -Wwrite-strings
         ),
-        '-diag-disable 981', # Ignore values evaluated in unknown order
+        # Disable some warnings and notifications that are overly noisy
+        '-diag-disable 981',  # Operands are evaluated in unspecified order
+        '-diag-disable 2259', # Non-pointer conversion from "typeA" to "typeB" may lose significant bits
     ];
 
     $data->{'warnings'}{'gcc'} = $gcc;
