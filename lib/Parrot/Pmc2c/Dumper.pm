52f776fd2c0effde47b5b19ff7fa1b9eb9023208
Peter Lobsinger <plobsing@gmail.com>
2010-03-23 23:36:45 +0000

    merge no_pmc_install branch
    git-svn-id: https://svn.parrot.org/parrot/trunk@45138 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/Dumper.pm
+++ lib/Parrot/Pmc2c/Dumper.pm
@@ -51,8 +51,7 @@ sub dump_pmc {
     @files = glob $files[0] if $files[0] eq 'src/pmc/*.pmc';
 
     # make sure that a default.dump will always be created if it doesn't
-    # already exist; do so by adding default.pmc to list of files for dumping
-    unshift @files, 'default.pmc' unless -e './src/pmc/default.dump';
+    $pmc2cMain->find_file('default.dump') or unshift @files, 'default.pmc';
 
     # load and parse all pmc files in @files
     for my $filename (@files) {
@@ -72,7 +71,7 @@ sub dump_pmc {
     }
 
     for my $pmc ( values %$pmcs ) {
-        next if $pmc->name =~ /default$/ && $pmc->dump_is_current;
+        next if $pmc->name =~ /default$/ && $pmc->dump_is_current($pmc2cMain->find_file('default.dump'));
 
         gen_parent_lookup_info( $pmc, $pmc2cMain, $pmcs );
         gen_parent_reverse_lookup_info( $pmc, $pmcs, $vtable_dump );
