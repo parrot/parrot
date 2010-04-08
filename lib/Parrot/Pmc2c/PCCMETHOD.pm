847e8052583ba1d63c4bbc25bde55ae629f7ef9b
chromatic <chromatic@wgz.org>
2010-04-08 20:22:40 +0000

    [lib] Removed a warning message about ignoring the return value of Parrot_pcc_build_call_from_c_args() from METHODs in PMCs.
    git-svn-id: https://svn.parrot.org/parrot/trunk@45455 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PCCMETHOD.pm
+++ lib/Parrot/Pmc2c/PCCMETHOD.pm
@@ -258,8 +258,8 @@ END
     /*BEGIN RETURN $returns */
 END
         $e->emit( <<"END", __FILE__, __LINE__ + 1 );
-    Parrot_pcc_build_call_from_c_args(interp, _call_object, "$returns_signature",
-            $returns_varargs);
+    _ret_object = Parrot_pcc_build_call_from_c_args(interp, _call_object,
+        "$returns_signature", $returns_varargs);
     return;
     /*END RETURN $returns */
     }
@@ -414,7 +414,8 @@ sub rewrite_pccmethod {
     $e->emit( <<"END", __FILE__, __LINE__ + 1 );
     PMC * const _ctx         = CURRENT_CONTEXT(interp);
     PMC * const _ccont       = Parrot_pcc_get_continuation(interp, _ctx);
-    PMC * const _call_object = Parrot_pcc_get_signature(interp, _ctx);
+    PMC * _call_object       = Parrot_pcc_get_signature(interp, _ctx);
+    PMC * _ret_object        = PMCNULL;
 
     { /* BEGIN PARMS SCOPE */
 END
