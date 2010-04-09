f16fdfb7f7ca6591d2264da224f634374c5bdac0
Andy Lester <andy@petdance.com>
2010-04-09 04:16:32 +0000

    removed unused arg from Parrot_oo_clone_object.  Un-reused a loop variable.
    git-svn-id: https://svn.parrot.org/parrot/trunk@45476 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/oo.h
+++ include/parrot/oo.h
@@ -148,11 +148,9 @@ void mark_object_cache(PARROT_INTERP)
 PARROT_CANNOT_RETURN_NULL
 PMC * Parrot_oo_clone_object(PARROT_INTERP,
     ARGIN(PMC *pmc),
-    ARGMOD_NULLOK(PMC *class_),
     ARGMOD_NULLOK(PMC *dest))
         __attribute__nonnull__(1)
         __attribute__nonnull__(2)
-        FUNC_MODIFIES(*class_)
         FUNC_MODIFIES(*dest);
 
 void Parrot_oo_extract_methods_from_namespace(PARROT_INTERP,
