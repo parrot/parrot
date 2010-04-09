35a0e4cbfcaa3fe5d1f11e84276e9a346083c663
Gerd Pokorra <pokorra@uni-siegen.de>
2010-04-09 23:53:57 +0000

    Initial work to generate a PDF file as Parrot book from POD files. The first version has only two chapters from two POD files. It works not on Windows because it use "sed".
    git-svn-id: https://svn.parrot.org/parrot/trunk@45489 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ tools/docs/book-to-latex.pl
@@ -0,0 +1,22 @@
+#!perl -w
+use strict;
+use Pod::PseudoPod::LaTeX;
+
+print <<'HEADER';
+\documentclass[11pt,a4paper,oneside]{report}
+\usepackage{graphics,graphicx}
+\usepackage{colortbl}
+ 
+\begin{document}
+\tableofcontents
+HEADER
+
+for (@ARGV) {
+    my $parser = Pod::PseudoPod::LaTeX->new();
+    $parser->output_fh( *STDOUT );
+    $parser->parse_file( $_ );
+}
+
+print <<'FOOTER';
+\end{document}
+FOOTER
---
+++ tools/docs/filename_and_chapter.pl
@@ -0,0 +1,37 @@
+#!perl -w
+# perl script that deliver all filenames and the associated chapters
+#
+
+use Getopt::Long ();
+
+my $item_list_ref = [ ['intro.pod', 'Introduction to Parrot'],
+                      ['overview.pod', 'Overview'],
+                    ];
+
+my $lang = @$item_list_ref;
+
+my ( $result, $give_out, $b_out);
+
+$result = Getopt::Long::GetOptions (
+	"print" => \$give_out,
+	"b_out" => \$b_out,
+);
+
+if ($give_out)  {
+	for ( my $i=0; $i<$lang; $i++ )
+		{ print $$item_list_ref[$i][0], ' ' }
+	exit;
+};
+
+if ($b_out) {
+	for ( my $i=0; $i<$lang; $i++ )
+		{ print 'build/modified_pod/', $$item_list_ref[$i][0], ' ' }
+	exit;
+};
+
+
+for (my $i=0; $i<$lang; $i++)
+	{ `sed -e '4,6c\=head0 $$item_list_ref[$i][1]' $$item_list_ref[$i][0] > build/modified_pod/$$item_list_ref[$i][0]` }
+
+
+
