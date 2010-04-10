1ec6718613aba2a501a3cd383b65c2704590448a
Michael H. Hind <mike.hind@gmail.com>
2010-04-10 09:37:54 +0000

    fix perlcritic failures - shebang line, use strict/warnings, add coda and remove hard tabs
    git-svn-id: https://svn.parrot.org/parrot/trunk@45505 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/docs/filename_and_chapter.pl
+++ tools/docs/filename_and_chapter.pl
@@ -1,9 +1,12 @@
-#!perl -w
+#! perl
 # Copyright (C) 2010, Parrot Foundation.
 # $Id$
 #
 # perl script that deliver all filenames and the associated chapters
 #
+
+use strict;
+use warnings;
 
 use Getopt::Long ();
 
@@ -16,25 +19,30 @@ my $lang = @$item_list_ref;
 my ( $result, $give_out, $b_out);
 
 $result = Getopt::Long::GetOptions (
-	"print" => \$give_out,
-	"b_out" => \$b_out,
+    "print" => \$give_out,
+    "b_out" => \$b_out,
 );
 
 if ($give_out)  {
-	for ( my $i=0; $i<$lang; $i++ )
-		{ print $$item_list_ref[$i][0], ' ' }
-	exit;
+    for ( my $i=0; $i<$lang; $i++ )
+        { print $$item_list_ref[$i][0], ' ' }
+    exit;
 };
 
 if ($b_out) {
-	for ( my $i=0; $i<$lang; $i++ )
-		{ print 'build/modified_pod/', $$item_list_ref[$i][0], ' ' }
-	exit;
+    for ( my $i=0; $i<$lang; $i++ )
+        { print 'build/modified_pod/', $$item_list_ref[$i][0], ' ' }
+    exit;
 };
 
 
 for (my $i=0; $i<$lang; $i++)
-	{ `sed -e '4,6c\=head0 $$item_list_ref[$i][1]' $$item_list_ref[$i][0] > build/modified_pod/$$item_list_ref[$i][0]` }
+    { `sed -e '4,6c\=head0 $$item_list_ref[$i][1]' $$item_list_ref[$i][0] > build/modified_pod/$$item_list_ref[$i][0]` }
 
 
-
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
