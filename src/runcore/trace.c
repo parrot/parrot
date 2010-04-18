2507cae9e7a3c21e8529c6a9093e0ba61fd55594
chromatic <chromatic@wgz.org>
2010-04-18 07:24:45 +0000

    [runcores] Tidied tracing runcore; no functional changes.
    git-svn-id: https://svn.parrot.org/parrot/trunk@45773 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/runcore/trace.c
+++ src/runcore/trace.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2001-2009, Parrot Foundation.
+Copyright (C) 2001-2010, Parrot Foundation.
 $Id$
 
 =head1 NAME
@@ -80,17 +80,17 @@ static STRING*
 trace_class_name(PARROT_INTERP, ARGIN(const PMC* pmc))
 {
     ASSERT_ARGS(trace_class_name)
-    STRING *class_name;
+
     if (PObj_is_class_TEST(pmc)) {
-        SLOTTYPE * const class_array = (SLOTTYPE *)PMC_data(pmc);
-        PMC * const class_name_pmc = get_attrib_num(class_array,
+        SLOTTYPE * const class_array    = PMC_data_typed(pmc, SLOTTYPE);
+        PMC      * const class_name_pmc = get_attrib_num(class_array,
                                                     PCD_CLASS_NAME);
-        class_name = VTABLE_get_string(interp, class_name_pmc);
+        return VTABLE_get_string(interp, class_name_pmc);
     }
     else
-        class_name = pmc->vtable->whoami;
-    return class_name;
+        return pmc->vtable->whoami;
 }
+
 
 /*
 
@@ -113,17 +113,21 @@ trace_pmc_dump(PARROT_INTERP, ARGIN_NULLOK(PMC *pmc))
         Parrot_io_eprintf(debugger, "(null)");
         return;
     }
+
     if (PMC_IS_NULL(pmc))  {
         Parrot_io_eprintf(debugger, "PMCNULL");
         return;
     }
+
     if (!pmc->vtable || (UINTVAL)pmc->vtable == 0xdeadbeef) {
         Parrot_io_eprintf(debugger, "<!!no vtable!!>");
         return;
     }
-    if (PObj_on_free_list_TEST(pmc)) {
-        Parrot_io_eprintf(debugger, "**************** PMC is on free list *****\n");
-    }
+
+    if (PObj_on_free_list_TEST(pmc))
+        Parrot_io_eprintf(debugger,
+            "**************** PMC is on free list *****\n");
+
     if (pmc->vtable->pmc_class == pmc) {
         STRING * const name = trace_class_name(interp, pmc);
         Parrot_io_eprintf(debugger, "Class=%Ss:PMC(%#p)", name, pmc);
@@ -134,25 +138,23 @@ trace_pmc_dump(PARROT_INTERP, ARGIN_NULLOK(PMC *pmc))
             Parrot_io_eprintf(debugger, "%S=PMC(%#p Str:(NULL))",
                     VTABLE_name(interp, pmc), pmc);
         else {
-            STRING* const escaped = Parrot_str_escape_truncate(
-                            interp, s, 20);
+            STRING * const escaped = Parrot_str_escape_truncate(interp, s, 20);
             if (escaped)
                 Parrot_io_eprintf(debugger, "%S=PMC(%#p Str:\"%Ss\")",
-                    VTABLE_name(interp, pmc), pmc,
-                    escaped);
+                    VTABLE_name(interp, pmc), pmc, escaped);
             else
                 Parrot_io_eprintf(debugger, "%S=PMC(%#p Str:\"(null)\")",
                     VTABLE_name(interp, pmc), pmc);
         }
     }
-    else if (pmc->vtable->base_type == enum_class_Boolean) {
+    else if (pmc->vtable->base_type == enum_class_Boolean)
         Parrot_io_eprintf(debugger, "Boolean=PMC(%#p: %d)",
                 pmc, VTABLE_get_integer(interp, pmc));
-    }
-    else if (pmc->vtable->base_type == enum_class_Integer) {
+
+    else if (pmc->vtable->base_type == enum_class_Integer)
         Parrot_io_eprintf(debugger, "Integer=PMC(%#p: %d)",
                 pmc, VTABLE_get_integer(interp, pmc));
-    }
+
     else if (pmc->vtable->base_type == enum_class_BigInt) {
         STRING * const s = VTABLE_get_string(interp, pmc);
         Parrot_io_eprintf(debugger, "BigInt=PMC(%#p: %Ss)",
@@ -160,8 +162,7 @@ trace_pmc_dump(PARROT_INTERP, ARGIN_NULLOK(PMC *pmc))
     }
     else if (pmc->vtable->base_type == enum_class_Complex) {
         STRING * const s = VTABLE_get_string(interp, pmc);
-        Parrot_io_eprintf(debugger, "Complex=PMC(%#p: %Ss)",
-                pmc, s);
+        Parrot_io_eprintf(debugger, "Complex=PMC(%#p: %Ss)", pmc, s);
     }
     else if (pmc->vtable->base_type == enum_class_Sub) {
         PMC_get_sub(interp, pmc, sub);
@@ -177,6 +178,7 @@ trace_pmc_dump(PARROT_INTERP, ARGIN_NULLOK(PMC *pmc))
                 VTABLE_name(interp, pmc), pmc);
     }
 }
+
 
 /*
 
@@ -199,16 +201,18 @@ trace_key_dump(PARROT_INTERP, ARGIN(PMC *key))
     while (key) {
         switch (PObj_get_FLAGS(key) & KEY_type_FLAGS) {
           case KEY_integer_FLAG:
-            len += Parrot_io_eprintf(debugger, "%vi", VTABLE_get_integer(interp, key));
+              len += Parrot_io_eprintf(debugger, "%vi",
+                 VTABLE_get_integer(interp, key));
             break;
           case KEY_number_FLAG:
-            len += Parrot_io_eprintf(debugger, "%vg", VTABLE_get_number(interp, key));
+              len += Parrot_io_eprintf(debugger, "%vg",
+                  VTABLE_get_number(interp, key));
             break;
           case KEY_string_FLAG:
             {
             const STRING * const s = key_string(interp, key);
-            STRING* const escaped = Parrot_str_escape_truncate(
-                            interp, s, 20);
+            STRING * const escaped = Parrot_str_escape_truncate(interp, s, 20);
+
             if (escaped)
                 len += Parrot_io_eprintf(debugger, "\"%Ss\"", escaped);
             else
@@ -216,33 +220,37 @@ trace_key_dump(PARROT_INTERP, ARGIN(PMC *key))
             }
             break;
           case KEY_integer_FLAG|KEY_register_FLAG:
-            len += Parrot_io_eprintf(debugger, "I%vd=%vd", VTABLE_get_integer(interp, key),
+              len += Parrot_io_eprintf(debugger, "I%vd=%vd",
+                  VTABLE_get_integer(interp, key),
                     REG_INT(interp, VTABLE_get_integer(interp, key)));
             break;
           case KEY_number_FLAG|KEY_register_FLAG:
-            len += Parrot_io_eprintf(debugger, "I%vd=%vd", VTABLE_get_integer(interp, key),
+              len += Parrot_io_eprintf(debugger, "I%vd=%vd",
+                  VTABLE_get_integer(interp, key),
                     REG_NUM(interp, VTABLE_get_integer(interp, key)));
             break;
           case KEY_string_FLAG|KEY_register_FLAG:
             {
-            const STRING * const s = REG_STR(interp, VTABLE_get_integer(interp, key));
-            STRING* const escaped = Parrot_str_escape_truncate(
-                            interp, s, 20);
+            const STRING * const s = REG_STR(interp,
+                 VTABLE_get_integer(interp, key));
+            STRING * const escaped = Parrot_str_escape_truncate(interp, s, 20);
             if (escaped)
-                len += Parrot_io_eprintf(debugger, "S%vd=\"%Ss\"", VTABLE_get_integer(interp, key),
-                        escaped);
+                len += Parrot_io_eprintf(debugger, "S%vd=\"%Ss\"",
+                    VTABLE_get_integer(interp, key), escaped);
             else
                 len += Parrot_io_eprintf(debugger, "S%vd=\"(null)\"",
                         VTABLE_get_integer(interp, key));
             }
             break;
           case KEY_pmc_FLAG|KEY_register_FLAG:
-            len += Parrot_io_eprintf(debugger, "P%vd=", VTABLE_get_integer(interp, key));
-            trace_pmc_dump(debugger, REG_PMC(interp, VTABLE_get_integer(interp, key)));
+              len += Parrot_io_eprintf(debugger, "P%vd=",
+                  VTABLE_get_integer(interp, key));
+              trace_pmc_dump(debugger, REG_PMC(interp,
+                  VTABLE_get_integer(interp, key)));
             break;
           default:
             len += Parrot_io_eprintf(debugger, "??");
-            key = NULL;
+            key  = NULL;
             break;
         }
 
@@ -251,11 +259,12 @@ trace_key_dump(PARROT_INTERP, ARGIN(PMC *key))
             if (key)
                 len += Parrot_io_eprintf(debugger, ";");
         }
-    } /* while */
+    }
 
     len += Parrot_io_eprintf(debugger, "]");
     return len;
 }
+
 
 /*
 
@@ -276,29 +285,23 @@ trace_op_dump(PARROT_INTERP,
         ARGIN(const opcode_t *pc))
 {
     ASSERT_ARGS(trace_op_dump)
-    INTVAL s, n;
-    int more = 0, var_args;
-    Interp * const debugger = debugger_or_interp(interp);
-    op_info_t * const info = &interp->op_info_table[*pc];
-    PMC *sig;
-    int type;
-    int len;
+    Interp    * const debugger = debugger_or_interp(interp);
+    op_info_t * const info     = &interp->op_info_table[*pc];
+    PMC *sig                   = PMCNULL;
+    INTVAL n                   = info->op_count;
+    INTVAL s                   = 1;
+    int more                   = 0, type;
+    int var_args               = 0;
+
+    int len = Parrot_io_eprintf(debugger, "%04vx ", (UINTVAL)(pc - code_start))
+            + Parrot_io_eprintf(debugger, "%s", info->name);
+
 #define ARGS_COLUMN 40
 
-    PARROT_ASSERT(debugger);
-    sig = NULL; /* silence compiler uninit warning */
-
-    s = 1;
-    len = Parrot_io_eprintf(debugger, "%04vx ", (UINTVAL)(pc - code_start));
-    len += Parrot_io_eprintf(debugger, "%s", info->name);
-
-    n = info->op_count;
-    var_args = 0;
-
-    if (*pc == PARROT_OP_set_args_pc ||
-            *pc == PARROT_OP_get_results_pc ||
-            *pc == PARROT_OP_get_params_pc ||
-            *pc == PARROT_OP_set_returns_pc) {
+    if (*pc == PARROT_OP_set_args_pc
+    ||  *pc == PARROT_OP_get_results_pc
+    ||  *pc == PARROT_OP_get_params_pc
+    ||  *pc == PARROT_OP_set_returns_pc) {
         sig = interp->code->const_table->constants[pc[1]]->u.key;
 
         if (!sig)
@@ -306,7 +309,7 @@ trace_op_dump(PARROT_INTERP,
                 "NULL sig PMC detected in trace_op_dump");
 
         var_args = VTABLE_elements(interp, sig);
-        n += var_args;
+        n       += var_args;
     }
 
     if (n > 1) {
@@ -315,9 +318,9 @@ trace_op_dump(PARROT_INTERP,
         /* pass 1 print arguments */
         for (i = s; i < n; i++) {
             const opcode_t o = pc[i];
-            if (i < info->op_count) {
+
+            if (i < info->op_count)
                 type = info->types[i - 1];
-            }
             else {
                 if (!sig)
                     Parrot_ex_throw_from_c_args(interp, NULL, 1,
@@ -326,21 +329,22 @@ trace_op_dump(PARROT_INTERP,
                 type = VTABLE_get_integer_keyed_int(interp, sig, i - 2) &
                     (PARROT_ARG_TYPE_MASK|PARROT_ARG_CONSTANT);
             }
-            if (i > s &&
-                    type != PARROT_ARG_KC &&
-                    type != PARROT_ARG_KIC &&
-                    type != PARROT_ARG_KI &&
-                    type != PARROT_ARG_K) {
+
+            if (i > s
+            &&  type != PARROT_ARG_KC
+            &&  type != PARROT_ARG_KIC
+            &&  type != PARROT_ARG_KI
+            &&  type != PARROT_ARG_K)
                 len += Parrot_io_eprintf(debugger, ", ");
-            }
+
             switch (type) {
               case PARROT_ARG_IC:
                 len += Parrot_io_eprintf(debugger, "%vd", o);
                 break;
               case PARROT_ARG_NC:
                 len += Parrot_io_eprintf(debugger, "%vg",
-                                         Parrot_pcc_get_num_constant(interp,
-                                                                     CURRENT_CONTEXT(interp), o));
+                        Parrot_pcc_get_num_constant(interp,
+                            CURRENT_CONTEXT(interp), o));
                 break;
               case PARROT_ARG_PC:
                 if (var_args)
@@ -351,10 +355,9 @@ trace_op_dump(PARROT_INTERP,
                 break;
               case PARROT_ARG_SC:
                 {
-                    STRING* const escaped = Parrot_str_escape_truncate(
-                            interp,
-                            Parrot_pcc_get_string_constant(interp,
-                                                           CURRENT_CONTEXT(interp), o), 20);
+                    STRING * const escaped = Parrot_str_escape_truncate(
+                            interp, Parrot_pcc_get_string_constant(interp,
+                                        CURRENT_CONTEXT(interp), o), 20);
                     if (escaped)
                         len += Parrot_io_eprintf(debugger, "\"%Ss\"", escaped);
                     else
@@ -363,8 +366,8 @@ trace_op_dump(PARROT_INTERP,
                 break;
               case PARROT_ARG_KC:
                 len += trace_key_dump(interp,
-                                      Parrot_pcc_get_pmc_constant(interp,
-                                                                  CURRENT_CONTEXT(interp), o));
+                        Parrot_pcc_get_pmc_constant(interp,
+                            CURRENT_CONTEXT(interp), o));
                 break;
               case PARROT_ARG_KIC:
                 len += Parrot_io_eprintf(debugger, "[%vd]", o);
@@ -399,17 +402,17 @@ trace_op_dump(PARROT_INTERP,
                 break;
             }
         }
+
         if (!more)
             goto done;
+
         if (len < ARGS_COLUMN)  {
             STRING * const fill = Parrot_str_repeat(debugger,
-                    Parrot_str_new_constant(debugger, " "),
-                    ARGS_COLUMN);
+                    Parrot_str_new_constant(debugger, " "), ARGS_COLUMN);
             Parrot_io_putps(debugger, Parrot_io_STDERR(debugger), fill);
         }
-        else {
+        else
             Parrot_io_eprintf(debugger, "\t");
-        }
 
         /* pass 2 print argument details if needed */
         for (i = 1; i < n; i++) {
@@ -419,9 +422,10 @@ trace_op_dump(PARROT_INTERP,
             else
                 type = VTABLE_get_integer_keyed_int(interp, sig, i - 2) &
                     (PARROT_ARG_TYPE_MASK|PARROT_ARG_CONSTANT);
-            if (i > s) {
+
+            if (i > s)
                 Parrot_io_eprintf(debugger, " ");
-            }
+
             switch (type) {
               case PARROT_ARG_I:
                 Parrot_io_eprintf(debugger, "I%vd=%vd", o, REG_INT(interp, o));
@@ -432,7 +436,7 @@ trace_op_dump(PARROT_INTERP,
               case PARROT_ARG_PC:
                 Parrot_io_eprintf(debugger, "PC%vd=", o);
                 trace_pmc_dump(interp, Parrot_pcc_get_pmc_constant(interp,
-                                                                   CURRENT_CONTEXT(interp), o));
+                                            CURRENT_CONTEXT(interp), o));
                 break;
               case PARROT_ARG_P:
                 Parrot_io_eprintf(debugger, "P%vd=", o);
@@ -440,10 +444,9 @@ trace_op_dump(PARROT_INTERP,
                 break;
               case PARROT_ARG_S:
                 if (REG_STR(interp, o)) {
-                    STRING* const escaped = Parrot_str_escape_truncate(
+                    STRING * const escaped = Parrot_str_escape_truncate(
                                 interp, REG_STR(interp, o), 20);
-                    Parrot_io_eprintf(debugger, "S%vd=\"%Ss\"", o,
-                                escaped);
+                    Parrot_io_eprintf(debugger, "S%vd=\"%Ss\"", o, escaped);
                 }
                 else
                     Parrot_io_eprintf(debugger, "S%vd=\"(null)\"", o);
@@ -463,20 +466,21 @@ trace_op_dump(PARROT_INTERP,
 done:
 
     if (interp->code->annotations) {
-        PMC * const annot = PackFile_Annotations_lookup(interp, interp->code->annotations,
-                pc - code_start + 1, NULL);
+        PMC * const annot = PackFile_Annotations_lookup(interp,
+                interp->code->annotations, pc - code_start + 1, NULL);
+
         if (!PMC_IS_NULL(annot)) {
             PMC * const pfile = VTABLE_get_pmc_keyed_str(interp, annot,
                     Parrot_str_new_constant(interp, "file"));
             PMC * const pline = VTABLE_get_pmc_keyed_str(interp, annot,
                     Parrot_str_new_constant(interp, "line"));
+
             if ((!PMC_IS_NULL(pfile)) && (!PMC_IS_NULL(pline))) {
                 /* The debugger interpreter may not be the same as
                  * the main interpreter, extract values from the
-                 * PMC instad of passing them directly
-                 */
+                 * PMC instad of passing them directly */
                 STRING * const file = VTABLE_get_string(interp, pfile);
-                const INTVAL line   = VTABLE_get_integer(interp, pline);
+                const INTVAL   line = VTABLE_get_integer(interp, pline);
                 Parrot_io_eprintf(debugger, " (%Ss:%li)", file, (long)line);
             }
         }
@@ -485,13 +489,13 @@ done:
     Parrot_io_eprintf(debugger, "\n");
 }
 
+
 /*
 
 =item C<void trace_op(PARROT_INTERP, const opcode_t *code_start, const opcode_t
 *code_end, const opcode_t *pc)>
 
-Prints the PC, OP and ARGS. Used by C<runops_trace()>. With bounds
-checking.
+Prints the PC, OP and ARGS. Used by C<runops_trace()>. With bounds checking.
 
 I<Not really part of the API.>
 
@@ -506,15 +510,17 @@ trace_op(PARROT_INTERP,
         ARGIN_NULLOK(const opcode_t *pc))
 {
     ASSERT_ARGS(trace_op)
-    if (!pc) {
+
+    if (!pc)
         return;
-    }
 
     if (pc >= code_start && pc < code_end)
         trace_op_dump(interp, code_start, pc);
     else
-        Parrot_io_eprintf(interp, "PC=%ld; OP=<err>\n", (long)(pc - code_start));
+        Parrot_io_eprintf(interp, "PC=%ld; OP=<err>\n",
+        (long)(pc - code_start));
 }
+
 
 /*
 
