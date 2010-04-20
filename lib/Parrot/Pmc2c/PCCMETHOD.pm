f32e01402b1d604668bff59a347026297162eb27
Andy Lester <andy@petdance.com>
2010-04-20 05:47:08 +0000

    Consting the _call_object arg in the generated code. Removed an unused Perl method.
    git-svn-id: https://svn.parrot.org/parrot/trunk@45817 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- lib/Parrot/Pmc2c/PCCMETHOD.pm
+++ lib/Parrot/Pmc2c/PCCMETHOD.pm
@@ -414,7 +414,7 @@ sub rewrite_pccmethod {
     $e->emit( <<"END", __FILE__, __LINE__ + 1 );
     PMC * const _ctx         = CURRENT_CONTEXT(interp);
     PMC * const _ccont       = Parrot_pcc_get_continuation(interp, _ctx);
-    PMC * _call_object       = Parrot_pcc_get_signature(interp, _ctx);
+    PMC * const _call_object = Parrot_pcc_get_signature(interp, _ctx);
     PMC * _ret_object        = PMCNULL;
 
     { /* BEGIN PARMS SCOPE */
@@ -428,11 +428,11 @@ END
             $params_varargs);
 END
     }
-    $e->emit( <<"END", __FILE__, __LINE__ + 1 );
+    $e->emit( <<'END', __FILE__, __LINE__ + 1 );
     { /* BEGIN PMETHOD BODY */
 END
 
-    $e_post->emit( <<"END", __FILE__, __LINE__ + 1 );
+    $e_post->emit( <<'END', __FILE__, __LINE__ + 1 );
 
     } /* END PMETHOD BODY */
     } /* END PARAMS SCOPE */
@@ -448,10 +448,6 @@ END
     $self->{PCCMETHOD} = 1;
 
     return 1;
-}
-
-sub isquoted {
-    1;
 }
 
 sub rewrite_pccinvoke {
