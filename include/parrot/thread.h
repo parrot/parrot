e4e0cf0247366869bdf98da6e295fde425b1a65f
Peter Lobsinger <plobsing@gmail.com>
2010-05-01 17:12:02 +0000

    headerizer
    git-svn-id: https://svn.parrot.org/parrot/trunk@46205 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/thread.h
+++ include/parrot/thread.h
@@ -194,16 +194,7 @@ PMC * pt_shared_fixup(PARROT_INTERP, ARGMOD(PMC *pmc))
 void pt_suspend_self_for_gc(PARROT_INTERP)
         __attribute__nonnull__(1);
 
-void pt_thread_detach(UINTVAL tid);
-PARROT_CAN_RETURN_NULL
-PMC* pt_thread_join(ARGIN(Parrot_Interp parent), UINTVAL tid)
-        __attribute__nonnull__(1);
-
-void pt_thread_kill(UINTVAL tid);
-void pt_thread_prepare_for_run(Parrot_Interp d, NULLOK(Parrot_Interp s));
-PMC* pt_thread_create(PARROT_INTERP,
-    INTVAL type,
-    INTVAL clone_flags)
+PMC * pt_thread_create(PARROT_INTERP, INTVAL type, INTVAL clone_flags)
         __attribute__nonnull__(1);
 
 int pt_thread_create_run(PARROT_INTERP,
@@ -212,17 +203,21 @@ int pt_thread_create_run(PARROT_INTERP,
     ARGIN(PMC *sub),
     ARGIN_NULLOK(PMC *arg))
         __attribute__nonnull__(1)
-        __attribute__nonnull__(4)
-        __attribute__nonnull__(5);
+        __attribute__nonnull__(4);
 
+void pt_thread_detach(UINTVAL tid);
+PARROT_CAN_RETURN_NULL
+PMC* pt_thread_join(ARGIN(Parrot_Interp parent), UINTVAL tid)
+        __attribute__nonnull__(1);
+
+void pt_thread_kill(UINTVAL tid);
+void pt_thread_prepare_for_run(Parrot_Interp d, NULLOK(Parrot_Interp s));
 int pt_thread_run(PARROT_INTERP,
-    ARGOUT(PMC *thread_interp_pmc),
+    PMC *thread_interp_pmc,
     ARGIN(PMC *sub),
     ARGIN_NULLOK(PMC *arg))
         __attribute__nonnull__(1)
-        __attribute__nonnull__(2)
-        __attribute__nonnull__(3)
-        FUNC_MODIFIES(*thread_interp_pmc);
+        __attribute__nonnull__(3);
 
 void pt_thread_wait_with(PARROT_INTERP, ARGMOD(Parrot_mutex *mutex))
         __attribute__nonnull__(1)
@@ -263,20 +258,18 @@ PMC * pt_transfer_sub(
     , PARROT_ASSERT_ARG(pmc))
 #define ASSERT_ARGS_pt_suspend_self_for_gc __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp))
+#define ASSERT_ARGS_pt_thread_create __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp))
+#define ASSERT_ARGS_pt_thread_create_run __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(sub))
 #define ASSERT_ARGS_pt_thread_detach __attribute__unused__ int _ASSERT_ARGS_CHECK = (0)
 #define ASSERT_ARGS_pt_thread_join __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(parent))
 #define ASSERT_ARGS_pt_thread_kill __attribute__unused__ int _ASSERT_ARGS_CHECK = (0)
 #define ASSERT_ARGS_pt_thread_prepare_for_run __attribute__unused__ int _ASSERT_ARGS_CHECK = (0)
-#define ASSERT_ARGS_pt_thread_create __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
-       PARROT_ASSERT_ARG(interp))
-#define ASSERT_ARGS_pt_thread_create_run __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
-       PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(sub) \
-    , PARROT_ASSERT_ARG(arg))
 #define ASSERT_ARGS_pt_thread_run __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(thread_interp_pmc) \
     , PARROT_ASSERT_ARG(sub))
 #define ASSERT_ARGS_pt_thread_wait_with __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
