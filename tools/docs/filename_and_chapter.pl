6bd6f7fc058223e80492d67b0b969604bbea9b33
James E Keenan (Jim) <jkeenan@cpan.org>
2010-05-02 23:47:33 +0000

    [codingstd] Parrot coding standards, FBOW, tell us to avoid global filehandles and the 2-argument form of 'open'.  Correcting this file.  This file still fails to pass perlcritic.t due to subroutine prototypes, but I don't yet know enough about what this file does to eliminate them myself.
    git-svn-id: https://svn.parrot.org/parrot/trunk@46224 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/docs/filename_and_chapter.pl
+++ tools/docs/filename_and_chapter.pl
@@ -43,9 +43,9 @@ for ( my $i=0; $i<$lang; $i++ ) {
 }
 
 
-open( TEX_FH, '>build/parrot-book.tex');
+open my $TEX_FH, '>', 'build/parrot-book.tex');
 
-print TEX_FH <<'HEADER';
+print $TEX_FH <<'HEADER';
 \documentclass[11pt,a4paper,oneside]{report}
 \usepackage{graphics,graphicx}
 \usepackage{colortbl}
@@ -56,36 +56,36 @@ HEADER
 
 for ( my $i=0; $i<$lang; $i++ ) {
     my $parser = Pod::PseudoPod::LaTeX->new();
-    $parser->output_fh( *TEX_FH );
+    $parser->output_fh( $TEX_FH );
     $parser->parse_file( "${MOD_BUILD_PATH}$$item_list_ref[$i][0]" );
 }
 
-print TEX_FH <<'FOOTER';
+print $TEX_FH <<'FOOTER';
 \end{document}
 FOOTER
 
-close( TEX_FH );
+close( $TEX_FH );
 
 
 sub write_mod( $ ) {
     my $icnt;
 
-    open( IN_FH, "<$$item_list_ref[$_[0]][0]" ) ||
+    open( my $IN_FH, '<', "$$item_list_ref[$_[0]][0]" ) ||
         die( "$0: can't open $$item_list_ref[$_[0]][0] for reading ($!)\n" );
-    open( OUT_FH, ">${MOD_BUILD_PATH}$$item_list_ref[$_[0]][0]" );
+    open( my $OUT_FH, '>', "${MOD_BUILD_PATH}$$item_list_ref[$_[0]][0]" );
 
     # do the same as: sed -e '4,6c\=head0 $$item_list_ref[$i][1]'
-    while( <IN_FH> ) {
+    while( <$IN_FH> ) {
         if ( $icnt = (4..6) ) {
             if ( $icnt =~ /E0$/ ) {
-                print( OUT_FH "=head0 $$item_list_ref[$_[0]][1]\n");
+                print( $OUT_FH "=head0 $$item_list_ref[$_[0]][1]\n");
             }
         }
-        else { print( OUT_FH ); }
+        else { print( $OUT_FH ); }
     }
 
-    close( IN_FH );
-    close( OUT_FH );
+    close( $IN_FH );
+    close( $OUT_FH );
 }
 
 
