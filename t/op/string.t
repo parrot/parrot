parrot/parrot

commit 7333fb4c696a50fadc814710f306e3f3e77f9c70

fixed version of r46564, simplify mixed_cs_index, cotto++
git-svn-id: https://svn.parrot.org/parrot/trunk@46587 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/string/charset/ascii.c
@@ -531,43 +531,34 @@ mixed_cs_index(PARROT_INTERP, ARGIN(const STRING *src), ARGIN(const STRING *sear
     UINTVAL offs)
 {
     ASSERT_ARGS(mixed_cs_index)
-    String_iter src_iter, search_iter;
-    UINTVAL len, next_pos;
-    INTVAL found_at;
 
-    ENCODING_ITER_INIT(interp, src, &src_iter);
-    src_iter.set_position(interp, &src_iter, offs);
-    ENCODING_ITER_INIT(interp, search, &search_iter);
-    len = search->strlen;
+    if (search->strlen < src->strlen) {
+        String_iter src_iter, search_iter;
+        const UINTVAL maxpos = src->strlen - search->strlen + 1;
+        const UINTVAL cfirst = Parrot_str_indexed(interp, search, 0);
 
-    found_at = -1;
-    next_pos = offs;
+        ENCODING_ITER_INIT(interp, src, &src_iter);
+        src_iter.set_position(interp, &src_iter, offs);
+        ENCODING_ITER_INIT(interp, search, &search_iter);
 
-    for (; len && offs < src->strlen ;) {
-        const UINTVAL c1 = src_iter.get_and_advance(interp, &src_iter);
-        const UINTVAL c2 = search_iter.get_and_advance(interp, &search_iter);
-
-        if (c1 == c2) {
-            --len;
-            if (found_at == -1)
-                found_at = offs;
-            ++offs;
-        }
-        else {
-            len = search->strlen;
-            ++offs;
-            ++next_pos;
-            if (offs != next_pos) {
-                src_iter.set_position(interp, &src_iter, next_pos);
-                offs = next_pos;
+        while (src_iter.charpos < maxpos) {
+            if (cfirst == src_iter.get_and_advance(interp, &src_iter)) {
+                const INTVAL next_pos = src_iter.charpos;
+                const INTVAL next_byte = src_iter.bytepos;
+                UINTVAL len;
+                search_iter.set_position(interp, &search_iter, 1);
+                for (len = search->strlen - 1; len; --len) {
+                    if ((src_iter.get_and_advance(interp, &src_iter)) !=
+                            (search_iter.get_and_advance(interp, &search_iter)))
+                        break;
+                }
+                if (len == 0)
+                    return next_pos - 1;
+                src_iter.charpos = next_pos;
+                src_iter.bytepos = next_byte;
             }
-
-            found_at = -1;
-            search_iter.set_position(interp, &search_iter, 0);
         }
     }
-    if (len == 0)
-        return found_at;
     return -1;
 }
 

commit 7d89b04fa3c44d3a4dc503fe38970f39ccb41b96

fix for bug introduced in r46587 and tests for its failure conditions
git-svn-id: https://svn.parrot.org/parrot/trunk@46588 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/string/charset/ascii.c
@@ -532,7 +532,7 @@ mixed_cs_index(PARROT_INTERP, ARGIN(const STRING *src), ARGIN(const STRING *sear
 {
     ASSERT_ARGS(mixed_cs_index)
 
-    if (search->strlen < src->strlen) {
+    if (search->strlen <= src->strlen) {
         String_iter src_iter, search_iter;
         const UINTVAL maxpos = src->strlen - search->strlen + 1;
         const UINTVAL cfirst = Parrot_str_indexed(interp, search, 0);

t/op/string.t
@@ -19,7 +19,7 @@ Tests Parrot string registers and operations.
 .sub main :main
     .include 'test_more.pir'
 
-    plan(370)
+    plan(374)
 
     set_s_s_sc()
     test_clone()
@@ -919,6 +919,14 @@ WHILE:
     index $I1, $S0, $S1
     is( $I1, "-1", 'index, 3-arg form' )
 
+    # Ascii - Non-ascii, same content
+    set $S0, "hello"
+    set $S1, unicode:"hello"
+    index $I1, $S0, $S1
+    is( $I1, "0", 'index, 3-arg form' )
+    index $I1, $S1, $S0
+    is( $I1, "0", 'index, 3-arg form' )
+
     # Non-ascii, source shorter than searched
     set $S0, unicode:"-o"
     set $S1, unicode:"@INC"
@@ -938,6 +946,14 @@ WHILE:
     set $S1, "qwx"
     index $I1, $S0, $S1, 0
     is( $I1, "-1", 'index, 4-arg form' )
+
+    # Ascii - Non-ascii, same content
+    set $S0, "hello"
+    set $S1, unicode:"hello"
+    index $I1, $S0, $S1, 0
+    is( $I1, "0", 'index, 4-arg form' )
+    index $I1, $S1, $S0, 0
+    is( $I1, "0", 'index, 4-arg form' )
 .end
 
 .sub index_four_arg_form_bug_twenty_two_thousand_seven_hundred_and_eighteen