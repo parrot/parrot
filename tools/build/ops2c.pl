f5a7df9dcb2ab68b4749065b9207b1a51b9aef84
Vasily Chekalkin <bacek@bacek.com>
2010-05-24 03:29:22 +0000

    Explicitely use PConfig{bindir} to invoke ops2c from ops2c.pl
    git-svn-id: https://svn.parrot.org/parrot/trunk@46938 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/build/ops2c.pl
+++ tools/build/ops2c.pl
@@ -5,10 +5,16 @@
 use strict;
 use warnings;
 
+use FindBin qw($Bin);
+use lib "$Bin/../lib";    # install location
+use Parrot::Config;
+use File::Spec;
+
 warn 'ops2c.pl is deprecated. Use ops2c$(EXE) instead';
 # Ignore "core"
 shift @ARGV;
-exec 'ops2c', @ARGV;
+my $ops2c = File::Spec->catfile($PConfig{bindir}, 'ops2c');
+exec $ops2c, @ARGV;
 
 # Local Variables:
 #   mode: cperl
