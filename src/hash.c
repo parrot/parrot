376488794e00885872a2bdbbe9419979d369a4f3
Nick Wellnhofer <wellnhofer@aevum.de>
2010-08-30 15:21:31 +0000

    Fix r48687
    git-svn-id: https://svn.parrot.org/parrot/trunk@48726 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/hash.c
+++ src/hash.c
@@ -1101,7 +1101,8 @@ parrot_chash_destroy_values(PARROT_INTERP, ARGMOD(Hash *hash), NOTNULL(value_fre
     UINTVAL i;
 
     parrot_hash_iterate(hash,
-        mem_gc_free(interp, _bucket->key););
+        mem_gc_free(interp, _bucket->key);
+        func(_bucket->value););
 
     parrot_hash_destroy(interp, hash);
 }
