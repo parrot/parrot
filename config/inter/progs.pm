e3c9cdfb22f48a36c33143c9cdd6e7f1f1171a98
James E Keenan (Jim) <jkeenan@cpan.org>
2010-09-03 00:23:13 +0000

    Per discussion in http://trac.parrot.org/parrot/ticket/854, revert code changes in r48758, deleting TODO item and replacing it with explanatory inline comment.
    git-svn-id: https://svn.parrot.org/parrot/trunk@48769 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/inter/progs.pm
+++ config/inter/progs.pm
@@ -103,11 +103,16 @@ sub _get_programs {
     $conf->debug("\nccflags: $ccflags\n");
 
     $linkflags = $conf->data->get('linkflags');
+    # The substitution below has no effect on most platforms but may be needed
+    # on Win32 with Strawberry Perl and other OSes.  See TT #854.
+    $linkflags =~ s/-libpath:\S+//g;
     $linkflags = integrate( $linkflags, $conf->options->get('linkflags') );
     $linkflags = prompt( "And flags for your linker?", $linkflags ) if $ask;
     $conf->data->set( linkflags => $linkflags );
 
     $ldflags = $conf->data->get('ldflags');
+    # For substitution below, see comment above.
+    $ldflags =~ s/-libpath:\S+//g;
     $ldflags = integrate( $ldflags, $conf->options->get('ldflags') );
     $ldflags = prompt( "And your $ld flags for building shared libraries?", $ldflags )
         if $ask;
