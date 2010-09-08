86db403ccf87fb14a15728e96c14b4fa0283cf32
Christoph Otto <christoph_git@mksig.org>
2010-09-08 05:10:16 -0700

    port gen_release_info.pl to git, making it about 180x faster in the process

--- tools/release/gen_release_info.pl
+++ tools/release/gen_release_info.pl
@@ -22,27 +22,28 @@ my $repo_url = 'https://svn.parrot.org/parrot/tags';
 ##  create a release information data structure
 my $r = {
     map { $_->{number} => $_ }
-    map { m{^(RELEASE_)(.*)/}
+    map { m{^(RELEASE_)([0-9_]+)}
             ? {
                 tag => "$1$2",
                 number => sub{$a = shift; $a =~ y/_/./; $a }->($2),
             }
             : ()
         }
-    qx  { svn ls $repo_url }
+    qx  { git tag -l 'RELEASE_*' }
 };
 
 ##  gather interesting release-related information from the tag
 map {
     ##  ask subversion for info about the tag
-    my $readme = $repo_url . '/' . $r->{$_}{tag};
-    warn "retrieving info on $readme\n";
-    my $info = qx{ LANG=C svn info $readme };
+    my $tag       = $r->{$_}{tag};
+    my $commit_id = qx{ git rev-parse $tag};
+    chomp $commit_id;
+    my $info      = qx{ git show $commit_id --quiet --format="Author: %an%nCommit: %H%nDate: %ai" };
 
     ##  pull the interesting items
-    $info =~ m{Author: (\S+)} and $r->{$_}{author}   = $1;
-    $info =~ m{Rev: (\S+)}    and $r->{$_}{revision} = $1;
-    $info =~ m{Date: (\S+)}   and $r->{$_}{date}     = $1;
+    $info =~ m{Author: (\S+)} and $r->{$_}{author} = $1;
+    $info =~ m{Commit: (\S+)} and $r->{$_}{commit} = $1;
+    $info =~ m{Date: (\S+)}   and $r->{$_}{date}   = $1;
 } keys %{ $r };
 
 
@@ -51,7 +52,7 @@ print
     map { "$_\n" }
     map { my $n = $_; join ',' =>
         map { $r->{$n}{$_} || '' }
-        qw{ tag number author revision date  }
+        qw{ tag number author commit date  }
     }
     sort keys %$r;
 
