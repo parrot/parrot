c4729e2d3ddc950926430168d0443a58013dc240
James E Keenan (Jim) <jkeenan@cpan.org>
2010-09-12 00:26:36 +0000

    [codingstd] No trailing whitespace.
    git-svn-id: https://svn.parrot.org/parrot/trunk@48946 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/global_setup.c
+++ src/global_setup.c
@@ -224,7 +224,7 @@ parrot_global_setup_2(PARROT_INTERP)
     }
     else {
         op_lib_t  *core_ops = PARROT_CORE_OPLIB_INIT(interp, 1);
-        interp->op_hash     = parrot_create_hash_sized(interp, enum_type_ptr, 
+        interp->op_hash     = parrot_create_hash_sized(interp, enum_type_ptr,
                                     Hash_key_type_cstring, core_ops->op_count);
         parrot_hash_oplib(interp, core_ops);
     }
