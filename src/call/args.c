214d8f07da88f9381fa733b4962f77899c29f67e
Vasily Chekalkin <bacek@bacek.com>
2010-10-13 12:02:24 +0000

    Add more write barriers when we are poking into CallContext directly
    git-svn-id: https://svn.parrot.org/parrot/branches/generational_gc@49524 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/call/args.c
+++ src/call/args.c
@@ -321,6 +321,7 @@ Parrot_pcc_build_sig_object_from_op(PARROT_INTERP, ARGIN_NULLOK(PMC *signature),
     }
 
     /* this macro is much, much faster than the VTABLE STRING comparisons */
+    Parrot_gc_write_barrier(interp, call_object);
     SETATTR_CallContext_arg_flags(interp, call_object, raw_sig);
     GETATTR_FixedIntegerArray_size(interp, raw_sig, arg_count);
     GETATTR_FixedIntegerArray_int_array(interp, raw_sig, int_array);
@@ -1539,6 +1540,7 @@ Parrot_pcc_merge_signature_for_tailcall(PARROT_INTERP,
 
         /* Store raw signature */
         GETATTR_CallContext_return_flags(interp, parent, return_flags);
+        Parrot_gc_write_barrier(interp, tailcall);
         SETATTR_CallContext_return_flags(interp, tailcall, return_flags);
 
     }
