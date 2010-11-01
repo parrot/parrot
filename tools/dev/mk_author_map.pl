07926321b61d98249afbc84a5943326f86bf3e26
Jonathan "Duke" Leto <jonathan@leto.net>
2010-11-01 23:45:19 +0000

    [tools] Make the author map code an actual file and remove it from CREDITS
    git-svn-id: https://svn.parrot.org/parrot/trunk@49766 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ tools/dev/mk_author_map.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my %n;
+while (<>) {
+    #warn "[$_]";
+    if (m/^\s*$/) {
+        printf "%-10s = %s <%s>\n", $n{U}, $n{N}, $n{E} if $n{U};
+        %n = ();
+    }
+    next unless m/^([NEDUSW]): (.*)/;
+    my ($f, $v) = ($1, $2);
+    $n{$f} = $v unless $n{$f};  # doesn't do the right thing for the multiline fields
+}
