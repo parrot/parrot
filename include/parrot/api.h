669ccd4908e9cec59d394f519bfe02656533c3d3
Whiteknight <wknight8111@gmail.com>
2010-11-08 22:02:22 -0500

    start putting together the api.h file, which will be what embedding applications, and only the embedding applications, should use

---
+++ include/parrot/api.h
@@ -0,0 +1,19 @@
+/* api.h
+ *  Copyright (C) 2001-2010, Parrot Foundation.
+ *  Overview:
+ *     Parrot's external embedding API. This functionality is only to be used
+ *     by embedding applications, and only this functionality may be used by
+ *     them.
+ */
+
+#ifdef PARROT_API_H_GUARD
+#define PARROT_API_H_GUARD
+
+#include "parrot/config.h"
+#include "parrot/core_types.h"
+
+/* HEADERIZER BEGIN src/embed_api.c */
+
+/* HEADERIZER END src/embed_api.c */
+
+#endif /* PARROT_API_H_GUARD */
--- include/parrot/interpreter.h
+++ include/parrot/interpreter.h
@@ -1,7 +1,5 @@
 /* interpreter.h
  *  Copyright (C) 2001-2010, Parrot Foundation.
- *  SVN Info
- *     $Id$
  *  Overview:
  *     The interpreter API handles running the operations
  */
