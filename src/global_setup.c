1e410d319b640eb0bf4e2f05f206ee3eb53c0738
Fernando Brito <email@fernandobrito.com>
2010-11-30 03:18:59 -0300

    'make codetest' was giving errors on that

--- src/global_setup.c
+++ src/global_setup.c
@@ -55,8 +55,8 @@ static void Parrot_gbl_set_config_hash_interpreter(PARROT_INTERP)
 
 /*
 
-=item C<void Parrot_gbl_set_config_hash_internal(const unsigned char* parrot_config,
-unsigned int parrot_config_size)>
+=item C<void Parrot_gbl_set_config_hash_internal(const unsigned char*
+parrot_config, unsigned int parrot_config_size)>
 
 Called by Parrot_set_config_hash with the serialised hash which
 will be used in subsequently created Interpreters.
