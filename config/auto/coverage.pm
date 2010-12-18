8653371342c439afa7873880ccc0b2eaa7f00d8c
jkeenan <jkeenan@cpan.org>
2010-12-17 20:59:23 -0500

    Rework config/auto/cover.pm.  Convert cover, gcov and gcov2perl to make variables in root.in.  auto::perldoc: Use lexically scoped filehandles.

--- config/auto/coverage.pm
+++ config/auto/coverage.pm
@@ -22,8 +22,7 @@ package auto::coverage;
 use strict;
 use warnings;
 
-use Data::Dumper;$Data::Dumper::Indent=1;
-#use File::Temp qw (tempfile );
+use File::Which;
 use base qw(Parrot::Configure::Step);
 use Parrot::Configure::Utils ':auto';
 
@@ -39,154 +38,23 @@ sub _init {
 sub runstep {
     my ( $self, $conf ) = @_;
 
-    my $cmd_stub =
-        $conf->data->get('scriptdirexp_provisional') . $conf->data->get('slash');
-   
-    my @devel_cover_utilities = qw| cover gcov2perl |;
-    my %utils_needed;
-    foreach my $util ( @devel_cover_utilities ) {
-        my $cmd = "$cmd_stub$util";
-        my $content = capture_output("$cmd -h") || undef;
-        $utils_needed{$util} =
-            $self->_initial_content_check($conf, $content, $util);
-    }
-    my $cmd = 'gcov';
-    my $content = capture_output("$cmd -h") || undef;
-    $utils_needed{$cmd} =
-        $self->_initial_content_check($conf, $content, $cmd);
-#    print STDERR Dumper \%utils_needed;
-    my @utils_lacking = ();
+    my %utils_needed = map { $_ => undef } qw( gcov gcov2perl cover );
     foreach my $util (keys %utils_needed) {
-        unless ($utils_needed{$util}) {
-            push @utils_lacking, $util;
-        }
+        $utils_needed{$util} = which($util);
     }
+    my @utils_lacking = grep { ! defined $utils_needed{$_} } keys %utils_needed;
     if (@utils_lacking) {
         $self->set_result("lacking @utils_lacking");
     }
     else {
-        $conf->data->set( "has_coverage_tools" => 1 );
+        $conf->data->set(
+            "has_coverage_tools" => 1,
+            %utils_needed,
+        );
         $self->set_result('yes');
     }
     return 1;
-
-#    return 1 unless defined( $self->_initial_content_check($conf, $content,
-#            $util) );
-#
-#    my $version = $self->_analyze_perldoc($cmd, $filename, $content);
-#
-#    _handle_version($conf, $version, $cmd);
-#
-#    my $TEMP_pod_build = <<'E_NOTE';
-#
-## the following part of the Makefile was built by 'config/auto/perldoc.pm'
-#
-#E_NOTE
-#
-#    opendir OPS, 'src/ops' or die "opendir ops: $!";
-#    my @ops = sort grep { !/^\./ && /\.ops$/ } readdir OPS;
-#    closedir OPS;
-#
-#    my $TEMP_pod = join q{ } =>
-#        map { my $t = $_; $t =~ s/\.ops$/.pod/; "ops/$t" } @ops;
-#
-#    my $new_perldoc = $conf->data->get('new_perldoc');
-#
-#    foreach my $ops (@ops) {
-#        my $pod = $ops;
-#        $pod =~ s/\.ops$/.pod/;
-#        if ( $new_perldoc ) {
-#            $TEMP_pod_build .= <<"END"
-#ops/$pod: ../src/ops/$ops
-#\t\$(PERLDOC_BIN) -ud ops/$pod ../src/ops/$ops
-#\t\$(CHMOD) 0644 ops/$pod
-#
-#END
-#        }
-#        else {
-#            $TEMP_pod_build .= <<"END"
-#ops/$pod: ../src/ops/$ops
-#\t\$(PERLDOC_BIN) -u ../ops/$ops > ops/$pod
-#\t\$(CHMOD) 0644 ../ops/$pod
-#
-#END
-#        }
-#    }
-#
-#    $conf->data->set(
-#        TEMP_pod             => $TEMP_pod,
-#        TEMP_pod_build       => $TEMP_pod_build,
-#    );
-
-    return 1;
 }
-
-sub _initial_content_check {
-    my $self = shift;
-    my ($conf, $content, $util) = @_;
-    if (! defined $content) {
-        $conf->data->set(
-            "TEMP_$util"            => '',
-            "TEMP_${util}_build"    => '',
-        );
-#        $self->set_result('no');
-        return;
-    }
-    else {
-        return 1;
-    }
-}
-
-#sub _analyze_perldoc {
-#    my $self = shift;
-#    my ($cmd, $tmpfile, $content) = @_;
-#    my $version;
-#    if ( $content =~ m/^Unknown option:/ ) {
-#        $content = capture_output("$cmd perldoc") || '';
-#        if ($content =~ m/perldoc/) {
-#            $version = $self->_handle_old_perldoc();
-#        }
-#        else {
-#            $version = $self->_handle_no_perldoc();
-#        }
-#    }
-#    elsif ( open my $FH, '<', $tmpfile ) {
-#        local $/;
-#        $content = <$FH>;
-#        close $FH;
-#        $version = 2;
-#        $self->set_result('yes');
-#    }
-#    else {
-#        $version = $self->_handle_no_perldoc();
-#    }
-#    unlink $tmpfile;
-#    return $version;
-#}
-#
-#sub _handle_old_perldoc {
-#    my $self = shift;
-#    $self->set_result('yes, old version');
-#    return 1;
-#}
-#
-#sub _handle_no_perldoc {
-#    my $self = shift;
-#    $self->set_result('failed');
-#    return 0;
-#}
-#
-#sub _handle_version {
-#    my ($conf, $version, $cmd) = @_;
-#    $conf->data->set(
-#        has_perldoc => $version != 0 ? 1 : 0,
-#        new_perldoc => $version == 2 ? 1 : 0
-#    );
-#
-#    $conf->data->set( perldoc => $cmd ) if $version;
-#
-#    return 1;
-#}
 
 1;
 
--- t/steps/auto/coverage-01.t
+++ t/steps/auto/coverage-01.t
@@ -1,10 +1,10 @@
 #! perl
 # Copyright (C) 2007, Parrot Foundation.
-# auto/perldoc-01.t
+# auto/coverage-01.t
 
 use strict;
 use warnings;
-use Test::More qw(no_plan); # tests => 27;
+use Test::More tests =>  7;
 use Carp;
 use lib qw( lib t/configure/testlib );
 use_ok('config::auto::coverage');
@@ -36,70 +36,13 @@ my $step = test_step_constructor_and_description($conf);
 ok($step->runstep($conf), "runstep() completed successfully");
 ok(defined($step->result), "Result was defined");
 
-$conf->replenish($serialized);
-
-########## _initial_content_check() ##########
-
-my $content = undef;
-my $util = 'gcov';
-my $rv = $step->_initial_content_check($conf, $content, $util);
-ok(! defined $rv, "Got expected return value when content was undefined");
-#ok(! $conf->data->get( "has_coverage_tools" ),
-#    "has_$util false as expected when content is empty" );
-
-########### _handle_version() ##########
-#
-#my $version;
-#$version = 0;
-#ok(auto::perldoc::_handle_version($conf, $version, 'not_a_path'),
-#    "_handle_version() returned true value");
-#is($conf->data->get('has_perldoc'), 0,
-#    "Got expected value for 'has_perldoc'");
-#is($conf->data->get('new_perldoc'), 0,
-#    "Got expected value for 'new_perldoc'");
-#is($conf->data->get('perldoc'), 'echo',
-#    "... and expected 'perldoc' path");
-#
-#$version = 1;
-#ok(auto::perldoc::_handle_version($conf, $version, 'path_to_pd'),
-#    "_handle_version() returned true value");
-#is($conf->data->get('has_perldoc'), 1,
-#    "Got expected value for 'has_perldoc'");
-#is($conf->data->get('new_perldoc'), 0,
-#    "Got expected value for 'new_perldoc'");
-#is($conf->data->get('perldoc'), 'path_to_pd',
-#    "... and expected 'perldoc' path");
-#
-#$version = 2;
-#ok(auto::perldoc::_handle_version($conf, $version, 'another_path_to_pd'),
-#    "_handle_version() returned true value");
-#is($conf->data->get('has_perldoc'), 1,
-#    "Got expected value for 'has_perldoc'");
-#is($conf->data->get('new_perldoc'), 1,
-#    "Got expected value for 'new_perldoc'");
-#is($conf->data->get('perldoc'), 'another_path_to_pd',
-#    "... and expected 'perldoc' path");
-#
-########### _handle_old_perldoc() ##########
-#
-#$version = $step->_handle_old_perldoc();
-#is($version, 1, "Got expected version setting for old perldoc");
-#is($step->result(), q{yes, old version},
-#    "Got expected result when old perldoc");
-#
-########### _handle_no_perldoc() ##########
-#
-#$version = $step->_handle_no_perldoc();
-#is($version, 0, "Got expected version setting for no perldoc");
-#is($step->result(), q{failed}, "Got expected result when no perldoc");
-
 pass("Completed all tests in $0");
 
 ################### DOCUMENTATION ###################
 
 =head1 NAME
 
-auto/coverage-01.t - test auto::perldoc
+auto/coverage-01.t - test auto::coverage
 
 =head1 SYNOPSIS
 
