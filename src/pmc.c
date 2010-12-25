96fa854f102e42758885ef8af4bff4dfb38f2a1e
Vasily Chekalkin <bacek@bacek.com>
2010-12-26 02:13:10 +1100

    Fix function argument guard

--- src/pmc.c
+++ src/pmc.c
@@ -803,7 +803,7 @@ PARROT_HOT
 PARROT_INLINE
 PARROT_CANNOT_RETURN_NULL
 PMC *
-Parrot_pmc_box_string(PARROT_INTERP, ARGIN(STRING *string))
+Parrot_pmc_box_string(PARROT_INTERP, ARGIN_NULLOK(STRING *string))
 {
     ASSERT_ARGS(Parrot_pmc_box_string)
     PMC * ret = Parrot_pmc_new(interp, enum_class_String);
