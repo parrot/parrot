a75cc9012961fd644129abcdd8de12bb9bc4c794
Nolan Lum <nol888@gmail.com>
2011-01-03 00:24:17 -0500

    Make the PMC pretty-printer ridiculous amounts cooler.

--- tools/dev/gdb-pp.py
+++ tools/dev/gdb-pp.py
@@ -2,6 +2,7 @@ from gdb.printing import PrettyPrinter, SubPrettyPrinter
 import gdb.types
 import gdb
 import re
+import itertools
 
 class ParrotPrinter(PrettyPrinter):
     """
@@ -66,18 +67,47 @@ class ParrotPrinter(PrettyPrinter):
         class Printer(object):
             def __init__(self, val):
                 self.val = val
+                self.pmc_name = _parrot_str_to_str(self.val['vtable'].dereference()['whoami'])
 
             def to_string(self):
-                pmc_name = _parrot_str_to_str(self.val['vtable'].dereference()['whoami'])
-                '''try:
-                    attr_name = 'Parrot_%s_attributes' % pmc_name
+                return 'PMC<%s>' % self.pmc_name
+
+            def children(self):
+                try:
+                    attr_name = 'Parrot_%s_attributes' % self.pmc_name
                     attr_type = gdb.lookup_type(attr_name).pointer()
 
                     attrs = self.val['data'].cast(attr_type).dereference()
-                except RuntimeError as e:
-                    pass'''
 
-                return 'PMC<%s>' % pmc_name
+                    # Python iterator magic.
+                    name_value_tuples = PMCIterator(attrs)
+                    names = itertools.imap(lambda val: ("attr-name", val[0]), name_value_tuples)
+                    values = itertools.imap(lambda val: ("attr-value", val[1]), name_value_tuples)
+
+                    # Something ridiculous happens here.
+                    # I take [0, 2, 4] and [1, 3, 5], turn it into
+                    # [(0, 1), (2, 3), (4, 5)], and then that into
+                    # [0, 1, 2, 3, 4, 5]. Only the numbers are tuples. Magic.
+                    # What we go through for 100% lazy iteration.
+                    return itertools.chain.from_iterable(itertools.izip(names, values))
+                except RuntimeError as e:
+                    return ( ( "", "" ) ).__iter__()
+
+            def display_hint(self):
+                return 'map'
+
+class PMCIterator(object):
+    def __init__(self, attrs):
+        self.attrs = attrs
+        self.iterobj = attrs.type.fields().__iter__()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        nextattr = self.iterobj.next()
+        #print "[[[%s]]]" % nextattr.name
+        return (nextattr.name, self.attrs[nextattr.name])
 
 def _parrot_str_to_str(val):
     encoding = val['encoding'].dereference()
