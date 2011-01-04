f7a044b2397456e87a3b3aab0d3f13c6f1156638
Whiteknight <wknight8111@gmail.com>
2011-01-03 21:46:45 -0500

    move error-message creation for named parameter/argument mismatch into a new function. This doesn't even make a dent in the huge fill_params functions, but it's a start

--- src/call/args.c
+++ src/call/args.c
@@ -129,6 +129,14 @@ static INTVAL* intval_param_from_op(PARROT_INTERP,
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
 
+PARROT_COLD
+PARROT_DOES_NOT_RETURN
+static void named_argument_arity_error(PARROT_INTERP,
+    int named_arg_count,
+    Hash *named_used_list,
+    PMC *named_arg_list)
+        __attribute__nonnull__(1);
+
 PARROT_WARN_UNUSED_RESULT
 static FLOATVAL numval_constant_from_op(PARROT_INTERP,
     ARGIN(const opcode_t *raw_params),
@@ -252,6 +260,8 @@ static STRING** string_param_from_op(PARROT_INTERP,
 #define ASSERT_ARGS_intval_param_from_op __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
     , PARROT_ASSERT_ARG(raw_params))
+#define ASSERT_ARGS_named_argument_arity_error __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp))
 #define ASSERT_ARGS_numval_constant_from_op __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
     , PARROT_ASSERT_ARG(raw_params))
@@ -1135,32 +1145,47 @@ fill_params(PARROT_INTERP, ARGMOD_NULLOK(PMC *call_object),
                     named_arg_count);
             }
 
-            if (named_arg_count > named_count) {
-                /* At this point we know we have named arguments that weren't
-                 * assigned to parameters. We're going to throw an exception
-                 * anyway, so spend a little extra effort to tell the user
-                 * *which* named argument is extra. */
-                INTVAL named_arg_index;
-
-                /* Named argument iteration. */
-                for (named_arg_index = 0; named_arg_index < named_arg_count; ++named_arg_index) {
-                    STRING * const name = VTABLE_get_string_keyed_int(interp,
-                            named_arg_list, named_arg_index);
-
-                    if (!parrot_hash_exists(interp, named_used_list, name)) {
-                        if (named_used_list != NULL)
-                            parrot_hash_destroy(interp, named_used_list);
-                        Parrot_ex_throw_from_c_args(interp, NULL,
-                                EXCEPTION_INVALID_OPERATION,
-                                "too many named arguments: '%S' not used",
-                                name);
-                    }
-                }
-            }
+            if (named_arg_count > named_count)
+                named_argument_arity_error(interp, named_arg_count, named_used_list, named_arg_list);
         }
     }
     if (named_used_list != NULL)
         parrot_hash_destroy(interp, named_used_list);
+}
+
+/*
+
+=item C<static void named_argument_arity_error(PARROT_INTERP, int
+named_arg_count, Hash *named_used_list, PMC *named_arg_list)>
+
+In the case of a mismatch between passed and expected named arguments, throw
+a helpful exception.
+
+=cut
+
+*/
+
+PARROT_COLD
+PARROT_DOES_NOT_RETURN
+static void
+named_argument_arity_error(PARROT_INTERP, int named_arg_count,
+        Hash *named_used_list, PMC *named_arg_list)
+{
+    ASSERT_ARGS(named_argument_arity_error)
+    INTVAL named_arg_index;
+
+    /* Named argument iteration. */
+    for (named_arg_index = 0; named_arg_index < named_arg_count; ++named_arg_index) {
+        STRING * const name = VTABLE_get_string_keyed_int(interp,
+                named_arg_list, named_arg_index);
+
+        if (!parrot_hash_exists(interp, named_used_list, name)) {
+            Parrot_ex_throw_from_c_args(interp, NULL,
+                    EXCEPTION_INVALID_OPERATION,
+                    "too many named arguments: '%S' not used",
+                    name);
+        }
+    }
 }
 
 
