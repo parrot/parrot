ee7bde16ecf5753cd93236beed5279f832fa209c
Nick Wellnhofer <wellnhofer@aevum.de>
2011-01-09 17:00:14 +0100

    [str] Introduce Parrot_str_to_encoded_cstring

--- include/parrot/string_funcs.h
+++ include/parrot/string_funcs.h
@@ -363,6 +363,15 @@ char * Parrot_str_to_cstring(PARROT_INTERP, ARGIN_NULLOK(const STRING *s))
         __attribute__nonnull__(1);
 
 PARROT_EXPORT
+PARROT_MALLOC
+PARROT_CANNOT_RETURN_NULL
+char * Parrot_str_to_encoded_cstring(PARROT_INTERP,
+    ARGIN_NULLOK(const STRING *s),
+    ARGIN(const STR_VTABLE *enc))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(3);
+
+PARROT_EXPORT
 PARROT_WARN_UNUSED_RESULT
 size_t Parrot_str_to_hashval(PARROT_INTERP, ARGIN(const STRING *s))
         __attribute__nonnull__(1)
@@ -584,6 +593,9 @@ STRING * Parrot_str_iter_substr(PARROT_INTERP,
        PARROT_ASSERT_ARG(interp))
 #define ASSERT_ARGS_Parrot_str_to_cstring __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp))
+#define ASSERT_ARGS_Parrot_str_to_encoded_cstring __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(enc))
 #define ASSERT_ARGS_Parrot_str_to_hashval __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
     , PARROT_ASSERT_ARG(s))
