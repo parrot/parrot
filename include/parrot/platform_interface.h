4ae951d7463dca99cfc9c3d199fa632044934035
Peter Lobsinger <plobsing@gmail.com>
2011-01-10 16:57:49 -0500

    remove declaration of dead memexec functions

--- include/parrot/platform_interface.h
+++ include/parrot/platform_interface.h
@@ -38,17 +38,6 @@ void Parrot_free_memalign(void *);
 #  define Parrot_memalign_if_possible(a, s) Parrot_memalign((a), (s))
 #endif
 
-#ifdef PARROT_HAS_EXEC_PROTECT
-void *mem_alloc_executable(size_t);
-void mem_free_executable(void *, size_t);
-void *mem_realloc_executable(void *, size_t, size_t);
-#else
-#  define mem_alloc_executable mem_internal_allocate
-#  define mem_free_executable(a, b) mem_internal_free(a)
-#  define mem_realloc_executable(a, b, c) mem_internal_realloc((a), (c))
-#endif
-
-
 /*
 ** Process ID
 */
