c0ddc7563fb7ed78f7235fec72f8e635894018ae
Nick Wellnhofer <wellnhofer@aevum.de>
2011-02-01 20:11:57 +0100

    Set the minimum threshold to something more reasonable

--- src/gc/gc_ms2.c
+++ src/gc/gc_ms2.c
@@ -641,7 +641,7 @@ Parrot_gc_ms2_init(PARROT_INTERP, ARGIN(Parrot_GC_Init_Args *args))
                                 : GC_DEFAULT_DYNAMIC_THRESHOLD;
         self->min_threshold     = args->min_threshold
                                 ? args->min_threshold
-                                : Parrot_sysmem_amount(interp) / 8;
+                                : 4 * 1024 * 1024;
         self->gc_threshold      = self->min_threshold;
 
         Parrot_gc_str_initialize(interp, &self->string_gc);
