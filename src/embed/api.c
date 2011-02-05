2310551f5df9e6d84d51a4241dba00b6288524ec
Nick Wellnhofer <wellnhofer@aevum.de>
2011-02-05 15:27:01 +0100

    Fix assertion failure

--- src/embed/api.c
+++ src/embed/api.c
@@ -438,8 +438,6 @@ Parrot_api_run_bytecode(Parrot_PMC interp_pmc, Parrot_PMC pbc,
 {
     ASSERT_ARGS(Parrot_api_run_bytecode)
     EMBED_API_CALLIN(interp_pmc, interp)
-    PMC * main_sub = NULL;
-
     PackFile * const pf = (PackFile *)VTABLE_get_pointer(interp, pbc);
 
     /* Debugging mode nonsense. */
@@ -451,6 +449,8 @@ Parrot_api_run_bytecode(Parrot_PMC interp_pmc, Parrot_PMC pbc,
     if (!pf)
         Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_UNEXPECTED_NULL,
             "Could not get packfile.");
+    if (!mainargs)
+        mainargs = PMCNULL;
     Parrot_pf_execute_bytecode_program(interp, pf, mainargs);
     EMBED_API_CALLOUT(interp_pmc, interp)
 }
