cac9e0eaf915bb2be35d0c8ffe561e24e0cd84b9
Peter Lobsinger <plobsing@gmail.com>
2011-02-24 23:30:25 -0500

    remove unused ITALIAS

--- compilers/imcc/instructions.h
+++ compilers/imcc/instructions.h
@@ -12,7 +12,6 @@ enum INSTYPE {    /*instruction type can be   */
     ITPCCRET   =   0x20000, /*  PCC sub return */
     ITCALL     =   0x40000, /*  function call */
     ITLABEL    =   0x80000, /*  label         */
-    ITALIAS    =  0x100000, /*  set P,P  */
     ITRESULT   =  0x400000, /*  .get_results */
     ITSAVES    =  0x800000, /*  saveall/restoreall in a bsr */
     ITPCCSUB   = 0x1000000, /*  PCC sub call */
