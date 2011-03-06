b1dd71fc25c69e439097957812f0ec78a7cb485d
Vasily Chekalkin <bacek@bacek.com>
2011-03-06 23:54:54 +1100

    Add more types

--- runtime/parrot/library/LLVM/Type.pm
+++ runtime/parrot/library/LLVM/Type.pm
@@ -1,4 +1,15 @@
 module LLVM::Type {
+    sub pointer($type, :$address_space?) {
+        %LLVM::F<LLVMPointerType>($type, $address_space);
+    }
+
+    sub int1() { %LLVM::F<LLVMInt1Type>() }
+    sub int8() { %LLVM::F<LLVMInt8Type>() }
+    sub int16() { %LLVM::F<LLVMInt16Type>() }
+    sub inti2() { %LLVM::F<LLVMInti2Type>() }
+    sub int64() { %LLVM::F<LLVMInt64Type>() }
+    sub int($radix) { %LLVM::F<LLVMIntType>($radix) }
+
     sub void() { %LLVM::F<LLVMVoidType>() };
 };
 
