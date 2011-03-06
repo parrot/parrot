a4d354f73748499acd3e9db093984b732bffbee4
Vasily Chekalkin <bacek@bacek.com>
2011-03-06 21:11:57 +1100

    Add LLVM::Module.add_function

--- runtime/parrot/library/LLVM/Module.pm
+++ runtime/parrot/library/LLVM/Module.pm
@@ -16,6 +16,18 @@ class LLVM::Module {
         %LLVM::F<LLVMDumpModule>($!ref);
     }
 
+    method add_function ($name, $signature) {
+        # FIXME. Total hack for now.
+        my $type := %LLVM::F<LLVMFunctionType>(
+            LLVM::Type::void(), # return
+            undef,              # parameters
+            0,                  # number of parameters
+            0,                  # is var args
+        );
+
+        %LLVM::F<LLVMAddFunction>($!ref, $name, $type);
+    }
+
     method _get_ptr() { $!ref };
 };
 
