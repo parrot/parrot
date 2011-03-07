47027d4dfd2baedce0de85990451b881703d222f
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 08:11:07 +1100

    Add more Types.

--- runtime/parrot/library/LLVM/Type.pm
+++ runtime/parrot/library/LLVM/Type.pm
@@ -1,20 +1,22 @@
 module LLVM::Type {
-    sub pointer($type, :$address_space?) {
-        %LLVM::F<PointerType>($type, $address_space);
-    }
-
-    sub int1() { %LLVM::F<Int1Type>() }
-    sub int8() { %LLVM::F<Int8Type>() }
-    sub int16() { %LLVM::F<Int16Type>() }
-    sub int32() { %LLVM::F<Int32Type>() }
-    sub int64() { %LLVM::F<Int64Type>() }
-    sub int($radix) { %LLVM::F<IntType>($radix) }
-
-    sub void() { %LLVM::F<VoidType>() };
+    multi sub int1() { %LLVM::F<Int1Type>() }
+    multi sub int8() { %LLVM::F<Int8Type>() }
+    multi sub int16() { %LLVM::F<Int16Type>() }
+    multi sub int32() { %LLVM::F<Int32Type>() }
+    multi sub int64() { %LLVM::F<Int64Type>() }
+    multi sub int($radix) { %LLVM::F<IntType>($radix) }
 
     sub struct(*@parts, :$packed?) {
         %LLVM::F<StructType>(LLVM::to_array(@parts), +@parts, $packed);
     }
+
+    sub pointer($type, :$address_space?) {
+        %LLVM::F<PointerType>($type, $address_space);
+    }
+
+    sub void() { %LLVM::F<VoidType>() };
+    sub label() { %LLVM::F<LabelType>() };
+    sub opaque() { %LLVM::F<OpaqueType>() };
 };
 
 # vim: ft=perl6
