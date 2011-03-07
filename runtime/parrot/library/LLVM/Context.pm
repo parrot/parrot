3ff23a7725f214e530c492a36e4bbd22bf68c28e
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 07:40:57 +1100

    Add more stuff to LLVM::Context

--- runtime/parrot/library/LLVM/Context.pm
+++ runtime/parrot/library/LLVM/Context.pm
@@ -11,6 +11,13 @@ class LLVM::Context is LLVM::Opaque {
         LLVM::Context.new.wrap(%LLVM::F<GetGlobalContext>());
     }
 
+    method int1() { %LLVM::F<Int1TypeInContext>(self) }
+    method int8() { %LLVM::F<Int8TypeInContext>(self) }
+    method int16() { %LLVM::F<Int16TypeInContext>(self) }
+    method int32() { %LLVM::F<Int32TypeInContext>(self) }
+    method int64() { %LLVM::F<Int64TypeInContext>(self) }
+    method int($radix) { %LLVM::F<IntTypeInContext>(self, $radix) }
+
 }
 
 # vim: ft=perl6
