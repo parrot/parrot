64f28957166224698734827c5bd2ad70c1e85a07
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 07:36:15 +1100

    Add some guts to LLVM::Context

--- runtime/parrot/library/LLVM/Context.pm
+++ runtime/parrot/library/LLVM/Context.pm
@@ -1,4 +1,16 @@
 class LLVM::Context is LLVM::Opaque {
+    method BUILD() {
+        self.wrap(%LLVM::F<ContextCreate>());
+    }
+
+    method DESTROY() {
+        %LLVM::F<ContextDispose>(self);
+    }
+
+    sub global() {
+        LLVM::Context.new.wrap(%LLVM::F<GetGlobalContext>());
+    }
+
 }
 
 # vim: ft=perl6
