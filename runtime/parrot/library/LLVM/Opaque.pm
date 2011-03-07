28a5f590c5de8f639782e9c6f377675cd70d78da
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 10:28:53 +1100

    Add stub for Opaque.new. I do need way to call SUPER.new here

--- runtime/parrot/library/LLVM/Opaque.pm
+++ runtime/parrot/library/LLVM/Opaque.pm
@@ -1,8 +1,11 @@
 class LLVM::Opaque {
     has $!ptr;
 
-    # Default implementation of BUILD.
+    # Default implementation of BUILD/new
     method BUILD($ptr) { self.wrap($ptr) }
+
+    #multi method new()     { self }
+    #multi method new($ptr) { self.wrap($ptr) }
 
     method wrap($ptr) { $!ptr := $ptr; self };
     method unwrap()   { $!ptr };
