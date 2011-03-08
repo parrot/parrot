113f93086113e33e7624441bb1397fba1eefaf13
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 13:21:45 +1100

    Add fetching params from Function

--- runtime/parrot/library/LLVM/Function.pm
+++ runtime/parrot/library/LLVM/Function.pm
@@ -14,6 +14,52 @@ class LLVM::Function is LLVM::Opaque {
     method set_linkage($linkage) {
         %LLVM::F<SetLinkage>(self, $linkage);
     }
+
+
+    =item *
+    CountParams => "ip",
+
+    method params_count() {
+        %LLVM::F<CountParams>(self);
+    }
+
+    =item *
+    GetParams => "vpp",
+
+    =item *
+    GetParam => "ppi",
+
+    method param($idx) {
+        LLVM::Value.create(%LLVM::F<GetParam>(self, $idx));
+    }
+
+    =item *
+    GetParamParent => "pp",
+
+    =item *
+    GetFirstParam => "pp",
+
+    =item *
+    GetLastParam => "pp",
+
+    =item *
+    GetNextParam => "pp",
+
+    =item *
+    GetPreviousParam => "pp",
+
+    =item *
+    AddAttribute => "vpi",
+
+    =item *
+    RemoveAttribute => "vpi",
+
+    =item *
+    GetAttribute => "ip",
+
+    =item *
+    SetParamAlignment => "vpi",
+
 };
 
 # vim: ft=perl6
