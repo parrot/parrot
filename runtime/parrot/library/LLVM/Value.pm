4ebee5133742c6627c4033f75e026e03521ee753
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 22:01:03 +1100

    Add more functions to Value.

--- runtime/parrot/library/LLVM/Value.pm
+++ runtime/parrot/library/LLVM/Value.pm
@@ -1,10 +1,20 @@
 class LLVM::Value is LLVM::Opaque {
+    method typeof() {
+        LLVM::Type.create(
+            LLVM::Call("TypeOf", self)
+        );
+    }
+
     multi method name() {
         LLVM::call("GetValueName", self);
     }
 
     multi method name($name) {
         LLVM::call("SetValueName", self, $name);
+    }
+
+    method dump() {
+        LLVM::call("DumpValue", self);
     }
 }
 
