ed86c6af20bc1fafe77e2e7c7a5d1b24f10a03aa
Vasily Chekalkin <bacek@bacek.com>
2011-03-09 08:14:43 +1100

    Add float types

--- runtime/parrot/library/LLVM/Type.pm
+++ runtime/parrot/library/LLVM/Type.pm
@@ -1,11 +1,17 @@
 class LLVM::Type is LLVM::Opaque {
 
-    multi sub int1() { LLVM::Type.create(LLVM::call("Int1Type", )) }
-    multi sub int8() { LLVM::Type.create(LLVM::call("Int8Type", )) }
-    multi sub int16() { LLVM::Type.create(LLVM::call("Int16Type", )) }
-    multi sub int32() { LLVM::Type.create(LLVM::call("Int32Type", )) }
-    multi sub int64() { LLVM::Type.create(LLVM::call("Int64Type", )) }
+    multi sub int1()      { LLVM::Type.create(LLVM::call("Int1Type", )) }
+    multi sub int8()      { LLVM::Type.create(LLVM::call("Int8Type", )) }
+    multi sub int16()     { LLVM::Type.create(LLVM::call("Int16Type", )) }
+    multi sub int32()     { LLVM::Type.create(LLVM::call("Int32Type", )) }
+    multi sub int64()     { LLVM::Type.create(LLVM::call("Int64Type", )) }
     multi sub int($radix) { LLVM::Type.create(LLVM::call("IntType", $radix)) }
+
+    sub float()    { LLVM::Type.create(LLVM::call("FloatType")) }
+    sub double()   { LLVM::Type.create(LLVM::call("DoubleType")) }
+    sub x86fp80()  { LLVM::Type.create(LLVM::call("X86FP80Type")) }
+    sub fp128()    { LLVM::Type.create(LLVM::call("FP128Type")) }
+    sub ppcfp128() { LLVM::Type.create(LLVM::call("PPCFP128Type")) }
 
     sub struct(*@parts, :$packed?) {
         LLVM::Type.create(LLVM::call("StructType", LLVM::to_array(@parts), +@parts, $packed));
