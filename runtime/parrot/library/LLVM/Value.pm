ed5ee1cbbef00db588d8aac00c2f599b44fa4303
Vasily Chekalkin <bacek@bacek.com>
2011-03-08 12:49:29 +1100

    Add Value.name

--- runtime/parrot/library/LLVM/Value.pm
+++ runtime/parrot/library/LLVM/Value.pm
@@ -1,4 +1,11 @@
 class LLVM::Value is LLVM::Opaque {
+    multi method name() {
+        %LLVM::F<GetValueName>(self);
+    }
+
+    multi method name($name) {
+        %LLVM::F<SetValueName>(self, $name);
+    }
 }
 
 # vim: ft=perl6
