805a4101e62b89184eb6ab9b665f05c9e0225e62
Vasily Chekalkin <bacek@bacek.com>
2011-03-09 10:35:33 +1100

    Wrap constants into Value and add test for it

--- runtime/parrot/library/LLVM/Constant.pm
+++ runtime/parrot/library/LLVM/Constant.pm
@@ -1,15 +1,15 @@
 module LLVM::Constant {
 
     sub null($type) {
-        LLVM::call("ConstNull", $type);
+        LLVM::Value.create(LLVM::call("ConstNull", $type));
     }
 
     sub string($str) {
-        LLVM::call("ConstString", ~$str, pir::length($str), 0);
+        LLVM::Value.create(LLVM::call("ConstString", ~$str, pir::length($str), 0));
     }
 
     sub integer($int, $radix?) {
-        LLVM::call("ConstInt", LLVM::Type::int($radix // 32), +$int, 1);
+        LLVM::Value.create(LLVM::call("ConstInt", LLVM::Type::int($radix // 32), +$int, 1));
     }
 };
 
