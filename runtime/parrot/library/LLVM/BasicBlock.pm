ecbda7c836293fc63b4403f4c63c7e112460c593
Jonathan "Duke" Leto <jonathan@leto.net>
2011-03-17 15:42:03 -0700

    Add the next method to LLVM::BasicBlock

--- runtime/parrot/library/LLVM/BasicBlock.pm
+++ runtime/parrot/library/LLVM/BasicBlock.pm
@@ -1,4 +1,9 @@
 class LLVM::BasicBlock is LLVM::Value {
+    method next () {
+        LLVM::BasicBlock.create(
+            LLVM::call("GetNextBasicBlock), self)
+        );
+    }
 };
 
 # vim: ft=perl6
