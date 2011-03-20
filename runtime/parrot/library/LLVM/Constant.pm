a96f28a39632cd8304a3e98b54bad4d1f93f36a0
luben karavelov <luben@debian.mail.bg>
2011-03-20 23:49:43 +0200

    Flesh out LLVM::Constant

--- runtime/parrot/library/LLVM/Constant.pm
+++ runtime/parrot/library/LLVM/Constant.pm
@@ -4,12 +4,29 @@ module LLVM::Constant {
         LLVM::Value.create(LLVM::call("ConstNull", $type));
     }
 
+    sub all_ones($type) {
+        LLVM::Value.create(LLVM::call("ConstAllOnes", $type));
+    }
+
+    sub undef($type) {
+        LLVM::Value.create(LLVM::call("GetUndef", $type));
+    }
+
+    sub pointer_null($type) {
+        LLVM::Value.create(LLVM::call("ConstPointerNull", $type));
+    }
+
     sub string($str) {
         LLVM::Value.create(LLVM::call("ConstString", ~$str, pir::length($str), 0));
     }
 
     sub integer($int, $radix?) {
         LLVM::Value.create(LLVM::call("ConstInt", LLVM::Type::int($radix // 32), +$int, 1));
+    }
+
+    sub real($double) {
+        LLVM::Value.create(LLVM::call("ConstReal", LLVM::Type::double(), +$double));
+
     }
 };
 
