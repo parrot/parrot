1c76a45b93db818763c719a8a58d2a53a04e653e
Vasily Chekalkin <bacek@bacek.com>
2011-03-21 10:15:42 +1100

    Use bit of static typing in LLVM bindings.

--- runtime/parrot/library/LLVM/Constant.pm
+++ runtime/parrot/library/LLVM/Constant.pm
@@ -1,30 +1,30 @@
 module LLVM::Constant {
 
-    sub null($type) {
+    sub null(LLVM::Type $type) {
         LLVM::Value.create(LLVM::call("ConstNull", $type));
     }
 
-    sub all_ones($type) {
+    sub all_ones(LLVM::Type $type) {
         LLVM::Value.create(LLVM::call("ConstAllOnes", $type));
     }
 
-    sub undef($type) {
+    sub undef(LLVM::Type $type) {
         LLVM::Value.create(LLVM::call("GetUndef", $type));
     }
 
-    sub pointer_null($type) {
+    sub pointer_null(LLVM::Type $type) {
         LLVM::Value.create(LLVM::call("ConstPointerNull", $type));
     }
 
-    sub string($str) {
+    sub string(LLVM::Type $str) {
         LLVM::Value.create(LLVM::call("ConstString", ~$str, pir::length($str), 0));
     }
 
-    sub integer($int, $radix?) {
+    sub integer(LLVM::Type $int, $radix?) {
         LLVM::Value.create(LLVM::call("ConstInt", LLVM::Type::int($radix // 32), +$int, 1));
     }
 
-    sub real($double) {
+    sub real(LLVM::Type $double) {
         LLVM::Value.create(LLVM::call("ConstReal", LLVM::Type::double(), +$double));
 
     }
