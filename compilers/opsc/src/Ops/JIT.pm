f376e0a8b07e3ca68f989ee0fc02f20f18605e9e
Vasily Chekalkin <bacek@bacek.com>
2011-03-26 13:43:05 +1100

    Load PBC in JIT.new

--- compilers/opsc/src/Ops/JIT.pm
+++ compilers/opsc/src/Ops/JIT.pm
@@ -2,14 +2,70 @@
 
 =begin Description
 
-JITter?
+LLVM JITter?
 
 =end Description
 
 class Ops::JIT;
 
+# Ops::OpsFile
+has $!ops_file;
 
-method new() {
+# OpLib
+has $!oplib;
+
+# Packfile used.
+has $!packfile;
+has $!constants;
+has $!bytecode;
+has $!opmap;
+
+=item new
+Create new JITter for given PBC and OpsFile.
+
+method new(Str $pbc, Ops::File $ops_file, OpLib $oplib) {
+    $!ops_file := $ops_file;
+    $!oplib    := $oplib;
+
+    self._load_pbc($pbc);
+}
+
+=item load_pbc
+Load PBC file.
+
+method _load_pbc(Str $file) {
+    # Load PBC into memory
+    my $handle   := open($file, :r, :bin);
+    my $contents := $handle.readall;
+    $handle.close();
+
+    $!packfile := pir::new('Packfile');
+    $!packfile.unpack($contents);
+
+    # Find Bytecode and Constants segments.
+    my $dir := $!packfile.get_directory();
+    die unless pir::defined($dir);
+
+    # Types:
+    # 2 Constants.
+    # 3 Bytecode.
+    # 4 DEBUG
+    for $dir -> $it {
+        say("Segment { $it.key } => { $it.value.type }");
+        my $segment := $it.value;
+        if $segment.type == 2 {
+            $!constants := $segment;
+        }
+        elsif $segment.type == 3 {
+            $!bytecode := $segment;
+        }
+    }
+
+    # Sanity check
+    $!bytecode // die("Couldn't find Bytecode");
+    $!constants // die("Couldn't find Constants");
+
+    $!opmap := $!bytecode.opmap() // die("Couldn't load OpMap");
 }
 
 method process(Ops::Op $op, %c) {
