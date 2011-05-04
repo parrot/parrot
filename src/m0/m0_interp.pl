29165dca7bc17c0ef66e119f2f34d9b52748a5e3
Christoph Otto <christoph_git@mksig.org>
2011-05-04 00:53:20 -0700

    stub out bytecode loading in the M0 interp

--- src/m0/m0_interp.pl
+++ src/m0/m0_interp.pl
@@ -36,11 +36,31 @@ Load and run an M0 bytecode file.
 sub run_m0b {
     my ($filename) = @_;
 
-    my $interp_data = load_m0b($filename);
-
-    #start running byte 0 of the bytecode segment of chunk 
+    my $interp = new_interp();
+    load_m0b($interp, $filename);
+    #$ctx = new_context($interp);
+    #run_ops($ctx);
 }
 
+
+=item C<initialize_interp>
+
+Initialize an M0 interpreter.  Note that this is different from a context,
+where most of the interesting work takes place.
+
+=cut
+
+sub new_interp {
+    my $interp;
+
+    $interp->{opfuncs} = [
+        \&m0_opfunc_noop,
+        \&m0_opfunc_say_i,
+    ];
+    $interp->{config} = [];
+    $interp->{contexts} = [];
+    return $interp;
+}
 
 
 =item C<load_m0b>
@@ -50,23 +70,63 @@ Load an M0 bytecode file into memory.
 =cut
 
 sub load_m0b {
-    my ($filename} = @_;
+    my ($interp, $filename) = @_;
 
-    # parse header
-    
-    # parse directory segment
+    my $m0b = slurp($filename);
+    parse_m0b_header($interp, $m0b);
+    parse_m0b_dirseg($interp, $m0b);
+    parse_m0b_chunks($interp, $m0b);
+}
 
-    # foreach chunnk
-        # parse variables segment
-        # parse metadata segment
-        # parse bytecode segment
+sub m0_opfunc_noop { }
 
+sub m0_opfunc_say_i {
+    my ($ctx, $a1, $a2, $a3) = @_;
+    print $a1 $a2;
 }
 
 
+=item C<parse_m0b_header>
 
+Verify that an M0 bytecode header is valid.
 
+=cut
 
+sub parse_m0b_header {
+    my ($interp, $m0b) = @_;
+    # verify magic number
+    # verify that M0 version matches what's expected
+    # ignore everything else
+}
 
+sub parse_m0b_dirseg {
+    my ($interp, $m0b) = @_;
 
+    # start at byte 0x10
+    # verify the segment identifier
+    # store the number of bytes in this segment
+    # current byte is 24
+    # while current_byte <= (16 + size)
+        # store the offset of the variables chunk
+        # store the size of the chunk name
+        # get the bytes of the chunk name
+        # store the name of the chunk in $interp->{chunks}[$n]{name}
+}
 
+sub parse_m0b_chunks {
+    my ($interp, $m0b) = @_;
+
+    # for each chunk
+        # read the variables segment
+        # for each variable
+            # stick the variable into the next slot
+
+        # read the metadata segment
+        # for each metadata entry
+            # add the mapping to the right offset (use offsets into the variables table, not direct values)
+
+        # read the bytecode segment
+        # for each op
+            # break the op into 4 ints
+            # store the ints in the next slot
+}
