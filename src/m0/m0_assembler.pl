fa4122aabda7b37c86c92fe5a9153fdd200ff6b6
Jonathan "Duke" Leto <jonathan@leto.net>
2011-05-06 22:37:30 -0700

    Steal directory bytecode generation code from m0 interp tests

--- src/m0/m0_assembler.pl
+++ src/m0/m0_assembler.pl
@@ -21,12 +21,17 @@ use feature 'say';
 use autodie qw/:all/;
 use File::Slurp qw/slurp write_file/;
 use Data::Dumper;
+#use Carp::Always;
 
 my $file = shift || die "Usage: $0 foo.m0";
 my $file_metadata = {
     total_chunks => 0,
 };
 
+my $M0_DIR_SEG      = 0x01;
+my $M0_VARS_SEG     = 0x02;
+my $M0_META_SEG     = 0x03;
+my $M0_BC_SEG       = 0x04;
 
 assemble($file);
 
@@ -39,7 +44,7 @@ sub assemble {
     my $version  = parse_version($source);
     my $chunk    = parse_next_chunk($source);
     my $header   = m0b_header();
-    my $bytecode = $header . generate_bytecode_for_chunk($chunk);
+    my $bytecode = $header . generate_bytecode_for_chunk($ops, $chunk);
 
     my $bytecode_file = $file . 'b';
     say "Writing bytecode to $bytecode_file";
@@ -81,20 +86,153 @@ sub parse_op_data {
 }
 
 sub generate_bytecode_for_chunk {
-    my ($chunk) = @_;
+    my ($ops, $chunk) = @_;
 
-    my $b = $chunk->{bytecode};
+    # textual bytecode
+    my $tb       = $chunk->{bytecode};
+    my $bytecode = '';
 
     # iterate over textual representation of bytecode
     # use variable table to generate binary bytecode
-    my @lines = split /\n/, $b;
+    my @lines = split /\n/, $tb;
     for my $line (@lines) {
         if ($line =~ m/^(?<opname>[A-z_]+)\s+(?<arg1>\w+)\s*,\s*(?<arg2>\w+)\s*,\s*(?<arg3>\w+)\s*$/) {
+            # warn $+{opname} . '=>' . opname_to_num($ops, $+{opname});
+            $bytecode = m0b_dir_seg([ $chunk ]);
         } else {
             say "Invalid M0 bytecode segment: $line";
             exit 1;
         }
     }
+    return $bytecode;
+}
+
+sub m0b_header_length {
+    return 8  # magic number
+         + 8; # config data
+}
+
+=item C<m0b_dir_seg>
+
+Generate a directory for the given set of chunks, assuming that the directory
+segment's first byte is at $offset.
+
+=cut
+
+sub m0b_dir_seg {
+    my ($chunks) = @_;
+
+    my $offset     = m0b_header_length();
+    my $seg_bytes  = pack('L', $M0_DIR_SEG);
+    $seg_bytes    .= pack('L', scalar @$chunks);
+    $seg_bytes    .= pack('L', m0b_dir_seg_length($chunks));
+    $offset       += m0b_dir_seg_length($chunks);
+
+    for (@$chunks) {
+        $seg_bytes .= pack('L', $offset);
+        $seg_bytes .= pack('L', length($_->{name}));
+        $seg_bytes .= $_->{name};
+        $offset    += m0b_chunk_length($_);
+        #say "offset of next chunk is $offset";
+    }
+
+    return $seg_bytes;
+}
+
+=item C<m0b_dir_seg_length>
+
+Calculate the number of bytes in a directory segment, using the chunk names in
+$chunks.
+
+=cut
+
+sub m0b_dir_seg_length {
+    my ($chunks) = @_;
+
+    my $seg_length = 12; # 4 bytes for identifier, 4 for count, 4 for length
+
+    for (@$chunks) {
+        $seg_length += 4; # offset into the m0b file
+        $seg_length += 4; # size of name
+        $seg_length += length($_->{name});
+    }
+
+    return $seg_length;
+}
+
+=item C<m0b_chunk_length>
+
+Calculate the number of bytes in a chunk.
+
+=cut
+
+sub m0b_chunk_length {
+    my ($chunk) = @_;
+    my $chunk_length =
+           m0b_bc_seg_length($chunk->{bc})
+         + m0b_vars_seg_length($chunk->{vars})
+         + m0b_meta_seg_length($chunk->{meta});
+    #say "chunk length is $chunk_length";
+    return $chunk_length;
+}
+
+=item C<m0b_bc_seg_length>
+
+Calculate the size of an M0 bytecode segment.
+
+=cut
+
+sub m0b_bc_seg_length {
+    my ($size) = @_;
+
+    my $seg_length = 12; # 4 for segment identifier, 4 for count, 4 for size
+    $seg_length += $size * 4;
+
+    #say "bytecode seg length is $seg_length";
+    return $seg_length;
+}
+
+=item C<m0b_vars_seg_length>
+
+Calculate the number of bytes that a variables segment will occupy.
+
+=cut
+
+sub m0b_vars_seg_length {
+    my ($vars) = @_;
+
+    my $seg_length = 12; # 4 for segment identifier, 4 for count, 4 for size
+
+    for (@$vars) {
+        my $var_length = length($_);
+        $seg_length += 4; # storage of size of variable
+        $seg_length += length($_);
+        #say "after adding var '$_', length is $seg_length";
+    }
+
+    #say "vars seg length is $seg_length";
+    return $seg_length;
+}
+
+=item C<m0b_meta_seg_length>
+
+Calculate the number of bytes that a metadata segment will occupy.
+
+=cut
+
+sub m0b_meta_seg_length {
+    my ($metadata) = @_;
+    
+    my $seg_length = 12; # 4 for segment identifier, 4 for count, 4 for size
+
+    for my $offset (keys %$metadata) {
+        for (keys %{$metadata->{$offset}}) {
+            $seg_length += 12;
+        }
+    }
+
+    #say "metadata seg length is $seg_length";
+    return $seg_length;
 }
 
 sub opname_to_num {
