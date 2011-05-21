4b4b726d794c540c4c8f096a541234097098d939
soh_cah_toa <kpolulak@gmail.com>
2011-05-21 16:56:19 -0400

    Removed unnecessary #includes

--- frontend/pdb/main.c
+++ frontend/pdb/main.c
@@ -1,17 +1,12 @@
 /* Copyright (C) 2001-2010, Parrot Foundation. */
 
-#define PARROT_IN_EXTENSION
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "parrot/api.h"
-#include "parrot/parrot.h"
-#include "parrot/embed.h"
-#include "parrot/runcore_api.h"
 
 int
 main(void) {
-    Parrot_PMC       interp;
+    Parrot_PMC    *interp;
     Parrot_Init_Args *initargs;
 
     /* Setup default initialization parameters */
@@ -22,23 +17,22 @@ main(void) {
         fprintf(stderr, "[ERROR] Failed to allocate new interpreter\n");
 
         if (interp != NULL)
-            show_last_error_and_exit(interp);
+            exit(EXIT_FAILURE);
         else
             fprintf(stderr, "[ERROR] Failed to get error details\n");
-
-        exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE);
     }
 
     /* Register pdb runcore */
     if (!Parrot_api_set_runcore(interp, "pdb", 0)) {
-        show_last_error_and_exit(interp);
+        exit(EXIT_FAILURE);
     }
 
     /* DEBUG */
     printf("Reached line %d\n", __LINE__);
     /* DEBUG */
 
-    Parrot_x_exit(interp, 0);
+    return (0);
 }
 
 /*
