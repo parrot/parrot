4ccfe231965b9ab3ec495bc388ce74ba27607b1f
soh_cah_toa <kpolulak@gmail.com>
2011-05-30 14:57:25 -0400

    Use Parrot_api_get_result() on error

--- frontend/hbdb/main.c
+++ frontend/hbdb/main.c
@@ -8,8 +8,9 @@
 
 /* TODO Check for command line arguments */
 /* TODO Define usage() function          */
+/* TODO Add perldoc                      */
 
-static void fail(const char *msg);
+static void fail(Parrot_PMC interp);
 
 int
 main(int argc, char *argv[])
@@ -25,37 +26,37 @@ main(int argc, char *argv[])
 
     /* Create new interpreter */
     if (!Parrot_api_make_interpreter(NULL, 0, initargs, &interp)) {
-        fail("Failed to allocate new interpreter");
+        fail(NULL);
     }
 
     /* TODO Check for argv[1] here and call usage() */
     /* Get bytecode filename */
     if (!Parrot_api_string_import_ascii(interp, argv[1], &file)) {
-        fail("Bad filename");
+        fail(interp);
     }
 
     /* Register hbdb runcore */
     if (!Parrot_api_set_runcore(interp, "hbdb", 0)) {
         Parrot_api_destroy_interpreter(interp);
-        fail("Failed to register runcore");
+        fail(interp);
     }
 
     /* Load bytecode file from command line */
     if (!Parrot_api_load_bytecode_file(interp, file, &pbc)) {
         Parrot_api_destroy_interpreter(interp);
-        fail("Failed to load bytecode");
+        fail(interp);
     }
 
     /* Ready bytecode */
     if (!Parrot_api_ready_bytecode(interp, pbc, &main_sub)) {
         Parrot_api_destroy_interpreter(interp);
-        fail("Failed to prepare bytecode");
+        fail(interp);
     }
 
     /* Run bytecode */
     if (!Parrot_api_run_bytecode(interp, pbc, NULL)) {
         Parrot_api_destroy_interpreter(interp);
-        fail("Failed to run bytecode");
+        fail(interp);
     }
 
     /* DEBUG */
@@ -67,10 +68,25 @@ main(int argc, char *argv[])
 }
 
 static void
-fail(const char *msg)
+fail(Parrot_PMC interp)
 {
-    fprintf(stderr, "[ERROR] %s\n", msg);
-    exit(EXIT_FAILURE);
+    char          *msg;
+
+    Parrot_Int    is_error,
+                  exit_code;
+    Parrot_String errmsg;
+    Parrot_PMC    exception;
+
+    if (!Parrot_api_get_result(interp, &is_error, &exception, &exit_code, &errmsg))
+        exit(EXIT_FAILURE);
+
+    if (is_error) {
+        Parrot_api_string_export_ascii(interp, errmsg, &msg);
+        fprintf(stderr, "ERROR: %s\n", msg);
+        Parrot_api_string_free_exported_ascii(interp, msg);
+    }
+
+    exit(exit_code);
 }
 
 /*
