2d30dac7f18d16241c3ea7b1ae0170937543404f
soh_cah_toa <kpolulak@gmail.com>
2011-05-30 17:24:06 -0400

    Test function in src/hbdb.c

---
+++ src/hbdb.c
@@ -0,0 +1,19 @@
+/* Copyright (C) 2001-2010, Parrot Foundation. */
+
+#include <stdio.h>
+
+/* DEBUG */
+void
+test(void)
+{
+    printf("Hello world!\n");
+}
+/* DEBUG */
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4 cinoptions='\:2=2' :
+ */
+
