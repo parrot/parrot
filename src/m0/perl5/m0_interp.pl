579e3b20e6d65dccb17456afbe1cea773d191f65
Christoph Otto <christoph_github@mksig.org>
2011-05-30 22:30:50 -0700

    update m0 interp code to be less inconsistent with recent spec changes

--- src/m0/perl5/m0_interp.pl
+++ src/m0/perl5/m0_interp.pl
@@ -27,34 +27,35 @@ my $file = shift || die "Usage: $0 foo.m0b";
 my $debugging = 0;
 
 use constant {
-    M0_DIR_SEG  => 0x01,
-    M0_VARS_SEG => 0x02,
-    M0_META_SEG => 0x03,
-    M0_BC_SEG   => 0x04,
+    M0_DIR_SEG   => 0x01,
+    M0_CONST_SEG => 0x02,
+    M0_META_SEG  => 0x03,
+    M0_BC_SEG    => 0x04,
 };
 
-# context mnemonics
+# call frame mnemonics
 use constant {
-    INTERP => 0,
-    PC     => 1,
-    EH     => 2,
-    EX     => 3,
-    PCX    => 4,
-    CHUNK  => 5,
-    VAR    => 6,
-    MDS    => 7,
-    BCS    => 8,
+    INTERP  => 0,
+    PC      => 1,
+    EH      => 2,
+    PCF     => 3,
+    CHUNK   => 4,
+    CONST   => 5,
+    MDS     => 6,
+    BCS     => 7,
+    REGSZ   => 8,
+    SPILLCF => 11,
 };
 
 # interp mnemonics
 
 use constant {
-  OP_FUNCS   => 0,
-  CHUNKS     => 1,
-  CHUNK_INFO => 2,
-  CHUNK_MAP  => 3,
-  CONTEXTS   => 4,
-  CONFIG     => 5,
+  OP_FUNCS    => 0,
+  CHUNKS      => 1,
+  CHUNK_INFO  => 2,
+  CHUNK_MAP   => 3,
+  CALL_FRAMES => 4,
+  CONFIG      => 5,
 };
 
 
@@ -82,14 +83,14 @@ sub run_m0b {
 
     my $interp = new_interp();
     load_m0b($interp, $filename);
-    my $ctx = new_context($interp);
-    run_ops($ctx);
+    my $cf = new_call_frame($interp);
+    run_ops($cf);
 }
 
 
 =item C<initialize_interp>
 
-Initialize an M0 interpreter.  Note that this is different from a context,
+Initialize an M0 interpreter.  Note that this is different from a call frame,
 where most of the interesting work takes place.
 
 =cut
@@ -136,37 +137,38 @@ sub new_interp {
         \&m0_opfunc_exit,
     ];
     $interp->[CONFIG] = {};
-    $interp->[CONTEXTS] = [];
+    $interp->[CALL_FRAMES] = [];
     $interp->[CHUNK_INFO] = [];
     $interp->[CHUNK_MAP] = {};
     $interp->[CHUNKS] = [];
     return $interp;
 }
 
-=item C<new_context>
+=item C<new_call_frame>
 
-Create a new context and perform any initializiation necessary to start running
+Create a new call frame and perform any initializiation necessary to start running
 bytecode.
 
 =cut
 
-sub new_context {
+sub new_call_frame{
     my ($interp) = @_;
-    my $ctx = [];
+    my $cf = [];
 
-    $ctx->[$_] = 0 for (0 .. 255);
+    $cf->[$_] = 0 for (0 .. 255);
 
-    $ctx->[INTERP] = $interp;
-    $ctx->[PC]     = 0;
-    $ctx->[EH]     = {};
-    $ctx->[EX]     = {};
-    $ctx->[PCX]    = {};
-    $ctx->[CHUNK]  = $interp->[CHUNKS][0]{name};
-    $ctx->[VAR]    = $interp->[CHUNKS][0]{vars};
-    $ctx->[MDS]    = $interp->[CHUNKS][0]{meta};
-    $ctx->[BCS]    = $interp->[CHUNKS][0]{bc};
+    $cf->[INTERP]  = $interp;
+    $cf->[PC]      = 0;
+    $cf->[EH]      = {};
+    $cf->[PCF]     = {};
+    $cf->[CHUNK]   = $interp->[CHUNKS][0]{name};
+    $cf->[CONSTS]  = $interp->[CHUNKS][0]{consts};
+    $cf->[MDS]     = $interp->[CHUNKS][0]{meta};
+    $cf->[BCS]     = $interp->[CHUNKS][0]{bc};
+    $cf->[REGSZ]   = 4;
+    $cf->[SPILLCF] = {};
 
-    return $ctx;
+    return $cf;
 }
 
 
@@ -177,25 +179,25 @@ Run ops until there aren't ops left to run.
 =cut
 
 sub run_ops {
-    my ($ctx) = @_;
+    my ($cf) = @_;
     
     while (1) {
-        my $init_pc = $ctx->[PC];
-        my $instr_count = scalar(@{$ctx->[BCS]});
-        if ($ctx->[PC] >= $instr_count){
+        my $init_pc = $cf->[PC];
+        my $instr_count = scalar(@{$cf->[BCS]});
+        if ($cf->[PC] >= $instr_count){
             exit;
         }
-        my $op_num  = $ctx->[BCS][$init_pc][0];
-        my $op_func = $ctx->[INTERP][OP_FUNCS][$op_num];
-        my $a1 = $ctx->[BCS][$init_pc][1];
-        my $a2 = $ctx->[BCS][$init_pc][2];
-        my $a3 = $ctx->[BCS][$init_pc][3];
-        &$op_func($ctx, $a1, $a2, $a3);
-        my $final_pc = $ctx->[PC];
+        my $op_num  = $cf->[BCS][$init_pc][0];
+        my $op_func = $cf->[INTERP][OP_FUNCS][$op_num];
+        my $a1 = $cf->[BCS][$init_pc][1];
+        my $a2 = $cf->[BCS][$init_pc][2];
+        my $a3 = $cf->[BCS][$init_pc][3];
+        &$op_func($cf, $a1, $a2, $a3);
+        my $final_pc = $cf->[PC];
 
         # allow ops to change control flow
         if ($init_pc == $final_pc) {
-            $ctx->[PC]++;
+            $cf->[PC]++;
         }
     }
 }
@@ -218,267 +220,267 @@ sub load_m0b {
 }
 
 sub m0_opfunc_noop {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "noop $a1, $a2, $a3";
 }
 
 sub m0_opfunc_goto {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "goto $a1, $a2, $a3";
 
     my $offset = 256 * $a1 + $a2;
-    $ctx->[PC] = $offset;
+    $cf->[PC] = $offset;
 }
 
 sub m0_opfunc_goto_if {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "goto_if $a1, $a2, $a3";
 
     my $offset = 256 * $a1 + $a2;
-    my $cond   = $ctx->[$a3];
-    $ctx->[PC] = $offset if ($cond);
+    my $cond   = $cf->[$a3];
+    $cf->[PC] = $offset if ($cond);
 }
 
 sub m0_opfunc_goto_chunk {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "goto_chunk $a1, $a2, $a3";
 
     my $new_pc      = 256 * $a1 + $a2;
-    my $chunk_name  = $ctx->[$a3];
-    my $interp      = $ctx->[INTERP];
+    my $chunk_name  = $cf->[$a3];
+    my $interp      = $cf->[INTERP];
 
     die "invalid chunk name '$chunk_name' in goto_chunk"
       unless exists $interp->[CHUNK_MAP]{$chunk_name};
 
     my $chunk_num  = $interp->[CHUNK_MAP]{$chunk_name};
-    $ctx->[CHUNK]  = $interp->[CHUNKS][$chunk_num]{name};
-    $ctx->[VAR]    = $interp->[CHUNKS][$chunk_num]{vars};
-    $ctx->[MDS]    = $interp->[CHUNKS][$chunk_num]{meta};
-    $ctx->[BCS]    = $interp->[CHUNKS][$chunk_num]{bc};
-    $ctx->[PC]     = $new_pc;
-    $ctx->[CHUNK]  = $chunk_name;
+    $cf->[CHUNK]  = $interp->[CHUNKS][$chunk_num]{name};
+    $cf->[CONST]  = $interp->[CHUNKS][$chunk_num]{consts};
+    $cf->[MDS]    = $interp->[CHUNKS][$chunk_num]{meta};
+    $cf->[BCS]    = $interp->[CHUNKS][$chunk_num]{bc};
+    $cf->[PC]     = $new_pc;
+    $cf->[CHUNK]  = $chunk_name;
 }
 
 sub m0_opfunc_add_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "add_i $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] + $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] + $cf->[$a3];
 }
 
 sub m0_opfunc_add_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "add_n $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] + $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] + $cf->[$a3];
 }
 
 sub m0_opfunc_sub_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "sub_i $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] - $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] - $cf->[$a3];
 }
 
 sub m0_opfunc_sub_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "sub_n $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] - $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] - $cf->[$a3];
 }
 
 sub m0_opfunc_mult_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "mult_i $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] * $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] * $cf->[$a3];
 }
 
 sub m0_opfunc_mult_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "mult_n $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] * $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] * $cf->[$a3];
 }
 
 sub m0_opfunc_div_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "div_i $a1, $a2, $a3";
 
-    $ctx->[$a1] = int($ctx->[$a2] / $ctx->[$a3]);
+    $cf->[$a1] = int($cf->[$a2] / $cf->[$a3]);
 }
 
 sub m0_opfunc_div_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "div_n $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] / $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] / $cf->[$a3];
 }
 
 sub m0_opfunc_mod_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "mod_i $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] % $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] % $cf->[$a3];
 }
 
 sub m0_opfunc_mod_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "mod_n $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] % $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] % $cf->[$a3];
 }
 
 sub m0_opfunc_iton {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "iton $a1, $a2, $a3";
 
-    $ctx->[$a1] = int($ctx->[$a2]);
+    $cf->[$a1] = int($cf->[$a2]);
 }
 
 sub m0_opfunc_ntoi {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "ntoi $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2];
+    $cf->[$a1] = $cf->[$a2];
 }
 
 sub m0_opfunc_ashr {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "ashr $a1, $a2, $a3";
 
     {
         # shift right with sign extension
         use integer;
-        $ctx->[$a1] = $ctx->[$a2] >> $ctx->[$a3];
+        $cf->[$a1] = $cf->[$a2] >> $cf->[$a3];
     }
 }
 
 sub m0_opfunc_lshr {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "lshr $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] >> $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] >> $cf->[$a3];
 }
 
 sub m0_opfunc_shl {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "shl $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] << $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] << $cf->[$a3];
 }
 
 sub m0_opfunc_and {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "and $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] & $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] & $cf->[$a3];
 }
 
 sub m0_opfunc_or {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "or $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] | $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] | $cf->[$a3];
 }
 
 sub m0_opfunc_xor {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "xor $a1, $a2, $a3";
 
-    $ctx->[$a1] = $ctx->[$a2] ^ $ctx->[$a3];
+    $cf->[$a1] = $cf->[$a2] ^ $cf->[$a3];
 }
 
 sub m0_opfunc_set {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "set $a1, $a2, $a3";
     
-    $ctx->[$a1] = $ctx->[$a2];
+    $cf->[$a1] = $cf->[$a2];
 }
 
 sub m0_opfunc_copy_byte {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "copy_byte $a1, $a2, $a3";
 }
 
 sub m0_opfunc_copy_mem {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "copy_mem $a1, $a2, $a3";
 }
 
 sub m0_opfunc_set_var {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     my $idx     = $a2 * 256 + $a3;
-    my $var     = $ctx->[VAR][$idx];
-    $ctx->[$a1] = $var;
-    m0_say "set_var $a1, $a2, $a3 (var = $var)";
+    my $const   = $cf->[CONST][$idx];
+    $cf->[$a1]  = $const;
+    m0_say "set_var $a1, $a2, $a3 (const = $const)";
 }
 
 sub m0_opfunc_csym {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "csym $a1, $a2, $a3";
 }
 
 sub m0_opfunc_ccall_arg {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "ccall_arg $a1, $a2, $a3";
 }
 
 sub m0_opfunc_ccall_ret {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "ccall_ret $a1, $a2, $a3";
 }
 
 sub m0_opfunc_ccall {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "ccall $a1, $a2, $a3";
 }
 
 sub m0_opfunc_print_s {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "print_s $a1, $a2, $a3";
 
-    my $handle = $ctx->[$a1];
-    my $var    = $ctx->[$a2];
+    my $handle = $cf->[$a1];
+    my $var    = $cf->[$a2];
     # TODO: print to $handle instead of stdout
     say $var;
 }
 
 sub m0_opfunc_print_i {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "print_i $a1, $a2, $a3";
 
-    my $handle = $ctx->[$a1];
-    my $var    = $ctx->[$a2];
+    my $handle = $cf->[$a1];
+    my $var    = $cf->[$a2];
     # TODO: print to $handle instead of stdout
     say $var;
 }
 
 sub m0_opfunc_print_n {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "print_n $a1, $a2, $a3";
 
-    my $handle = $ctx->[$a1];
-    my $var    = $ctx->[$a2];
+    my $handle = $cf->[$a1];
+    my $var    = $cf->[$a2];
     # TODO: print to $handle instead of stdout
     say $var;
 }
 
 sub m0_opfunc_alloc {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "alloc $a1, $a2, $a3";
 }
 
 sub m0_opfunc_free {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "free $a1, $a2, $a3";
 }
 
 sub m0_opfunc_exit {
-    my ($ctx, $a1, $a2, $a3) = @_;
+    my ($cf, $a1, $a2, $a3) = @_;
     m0_say "exit $a1, $a2, $a3";
 
-    exit($ctx->[$a1]);
+    exit($cf->[$a1]);
 }
 
 
@@ -529,7 +531,7 @@ sub parse_m0b_dirseg {
     my $seg_byte_count  = unpack("L", get_bytes($m0b, $cursor, 4));
     my $chunks_found = 0;
     while ($chunks_found < $seg_entry_count) {
-        my $vars_seg_offset   = unpack("L", get_bytes($m0b, $cursor, 4));
+        my $consts_seg_offset   = unpack("L", get_bytes($m0b, $cursor, 4));
         my $chunk_name_length = unpack("L", get_bytes($m0b, $cursor, 4));
         my $chunk_name        = unpack("a[$chunk_name_length]", get_bytes($m0b, $cursor, $chunk_name_length));
         $interp->[CHUNK_INFO][$chunks_found]{name} = $chunk_name;
@@ -543,33 +545,34 @@ sub parse_m0b_chunks {
 
     for my $chunk_name (@{$interp->[CHUNK_INFO]}) {
         my $chunk;
-        $chunk->{vars} = m0b_parse_vars_seg($interp, $m0b, $cursor);
-        $chunk->{meta} = m0b_parse_meta_seg($interp, $m0b, $cursor);
-        $chunk->{bc}   = m0b_parse_bc_seg(  $interp, $m0b, $cursor);
-        $chunk->{name} = $chunk_name;
+        $chunk->{consts} = m0b_parse_const_seg($interp, $m0b, $cursor);
+        $chunk->{meta}   = m0b_parse_meta_seg($interp, $m0b, $cursor);
+        $chunk->{bc}     = m0b_parse_bc_seg(  $interp, $m0b, $cursor);
+        $chunk->{name}   = $chunk_name;
         push @{$interp->[CHUNKS]}, $chunk;
     }
 }
 
 
-sub m0b_parse_vars_seg {
+sub m0b_parse_const_seg {
     my ($interp, $m0b, $cursor) = @_;
 
-    my $vars = [];
+    my $consts = [];
     # verify the segment identifier
     my $seg_ident = unpack("L", get_bytes($m0b, $cursor, 4));
-    if ($seg_ident != M0_VARS_SEG) {
-        die "didn't find M0 variables segment";
+    if ($seg_ident != M0_CONST_SEG) {
+        die "didn't find M0 constants segment";
     }
 
-    my $var_count  = unpack("L", get_bytes($m0b, $cursor, 4));
-    my $byte_count = unpack("L", get_bytes($m0b, $cursor, 4));
-    while (scalar(@$vars) < $var_count) {
-        my $var_length = unpack("L", get_bytes($m0b, $cursor, 4));
-        my $var        = unpack("a[$var_length]", get_bytes($m0b, $cursor, $var_length));
-        push @$vars, $var;
+    my $const_count = unpack("L", get_bytes($m0b, $cursor, 4));
+    my $byte_count  = unpack("L", get_bytes($m0b, $cursor, 4));
+    while (scalar(@$consts < $const_count) {
+        my $const_length = unpack("L", get_bytes($m0b, $cursor, 4));
+        my $const        = unpack("a[$const_length]", get_bytes($m0b, $cursor, $const_length));
+        say "found constant of length $const_length: '$const'";
+        push @$consts, $const;
     }
-    return $vars;
+    return $consts;
 }
 
 
