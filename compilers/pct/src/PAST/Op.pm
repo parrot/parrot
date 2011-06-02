36c321ecad5a061a1a38598716628b387676a7be
Brian Gernhardt <brian.gernhardt@mail.rit.edu>
2011-06-02 13:07:32 -0400

    PAST::Op - No more Q:PIR
    That's the last bit of PIR in PAST::Node and children.

--- compilers/pct/src/PAST/Op.pm
+++ compilers/pct/src/PAST/Op.pm
@@ -136,22 +136,11 @@ precedence table, and the attributes being set correspond
 to traits in the grammar.
 
 method opattr(%hash) {
-    Q:PIR {
-        .local pmc hash
-        find_lex hash, '%hash'
-
-        $P0 = split ' ', "pasttype pirop inline lvalue"
-        $P1 = iter $P0
-      iter_loop:
-        unless $P1 goto iter_end
-        $S0 = shift $P1
-        $P2 = hash[$S0]
-        if null $P2 goto iter_loop
-        $P3 = find_method self, $S0
-        self.$P3($P2)
-        goto iter_loop
-      iter_end:
-    }
+	for <pasttype pirop inline lvalue> {
+		my $arg := %hash{$_};
+		next unless pir::defined($arg);
+		self."$_"($arg);
+	}
 }
 
 =begin head1
