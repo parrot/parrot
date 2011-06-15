2178eb0ebe102a1b609eb0fdbb5003d6fd0a050d
soh_cah_toa <kpolulak@gmail.com>
2011-06-15 19:39:24 -0400

    Removed hbdb_get_line_number() and its API wrapper because thanks to IMCC, I'll probably never use it

--- src/embed/hbdb.c
+++ src/embed/hbdb.c
@@ -53,30 +53,6 @@ Parrot_api_hbdb_get_command(Parrot_PMC interp_pmc)
 
 /*
 
-=item C<Parrot_Int Parrot_api_hbdb_get_line_number(Parrot_PMC interp_pmc,
-Parrot_PMC *context_pmc)>
-
-Wrapper function for C<hbdb_get_line_number()>. Returns the line number for the current context.
-
-=cut
-
-*/
-
-PARROT_API
-Parrot_Int
-Parrot_api_hbdb_get_line_number(Parrot_PMC interp_pmc, ARGIN(Parrot_PMC *context_pmc))
-{
-    ASSERT_ARGS(Parrot_api_hbdb_get_line_number)
-
-    EMBED_API_CALLIN(interp_pmc, interp)
-
-    hbdb_get_line_number(interp, context_pmc);
-
-    EMBED_API_CALLOUT(interp_pmc, interp)
-}
-
-/*
-
 =item C<Parrot_Int Parrot_api_hbdb_init(Parrot_PMC interp_pmc)>
 
 Wrapper function for C<hbdb_init()>. Performs general initialization operations.
--- src/hbdb.c
+++ src/hbdb.c
@@ -276,31 +276,6 @@ hbdb_get_command(PARROT_INTERP)
 
 /*
 
-=item C<INTVAL hbdb_get_line_number(PARROT_INTERP, PMC *context_pmc)>
-
-Returns the line number for the current context.
-
-=cut
-
-*/
-
-INTVAL
-hbdb_get_line_number(PARROT_INTERP, ARGIN(PMC *context_pmc))
-{
-    ASSERT_ARGS(hbdb_get_line_number)
-
-    INTVAL line_num;
-    Parrot_Context * const context = PMC_data_typed(context_pmc, Parrot_Context *);
-
-    /*line_num = Parrot_sub_get_line_from_pc(interp,
-                                           Parrot_pcc_get_sub(interp, context_pmc),
-                                           context->current_pc);*/
-
-    return line_num;
-}
-
-/*
-
 =item C<void hbdb_init(PARROT_INTERP)>
 
 Performs general initialization operations.
