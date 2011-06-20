cb9eaf0dea7bb1934327c995b3e48cff6d7f2c18
soh_cah_toa <kpolulak@gmail.com>
2011-06-19 22:32:57 -0400

    Updated links to Perl 6 synopsis and apocalypse 12. Also deleted a few duplicate blank lines

--- src/multidispatch.c
+++ src/multidispatch.c
@@ -206,9 +206,7 @@ static PMC * Parrot_mmd_sort_candidates(PARROT_INTERP,
 /* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
 /* HEADERIZER END: static */
 
-
 #define MMD_DEBUG 0
-
 
 /*
 
@@ -305,7 +303,6 @@ Parrot_mmd_multi_dispatch_from_c_args(PARROT_INTERP,
     va_end(args);
 }
 
-
 /*
 
 =item C<PMC * Parrot_mmd_find_multi_from_long_sig(PARROT_INTERP, STRING *name,
@@ -341,7 +338,6 @@ Parrot_mmd_find_multi_from_long_sig(PARROT_INTERP, ARGIN(STRING *name),
         return Parrot_mmd_sort_candidates(interp, type_tuple, multi_sub);
     }
 }
-
 
 /*
 
@@ -417,7 +413,6 @@ mmd_build_type_tuple_from_type_list(PARROT_INTERP, ARGIN(PMC *type_list))
     return multi_sig;
 }
 
-
 /*
 
 =item C<static PMC* mmd_build_type_tuple_from_long_sig(PARROT_INTERP, STRING
@@ -441,7 +436,6 @@ mmd_build_type_tuple_from_long_sig(PARROT_INTERP, ARGIN(STRING *long_sig))
     return mmd_build_type_tuple_from_type_list(interp, type_list);
 }
 
-
 /*
 
 =item C<PMC* Parrot_mmd_build_type_tuple_from_sig_obj(PARROT_INTERP, PMC
@@ -463,7 +457,6 @@ Parrot_mmd_build_type_tuple_from_sig_obj(PARROT_INTERP, ARGIN(PMC *sig_obj))
     ASSERT_ARGS(Parrot_mmd_build_type_tuple_from_sig_obj)
     return VTABLE_get_pmc(interp, sig_obj);
 }
-
 
 /*
 
@@ -526,7 +519,6 @@ mmd_cvt_to_types(PARROT_INTERP, ARGIN(PMC *multi_sig))
     return ar;
 }
 
-
 /*
 
 =item C<static PMC * Parrot_mmd_get_cached_multi_sig(PARROT_INTERP, PMC
@@ -568,7 +560,6 @@ Parrot_mmd_get_cached_multi_sig(PARROT_INTERP, ARGIN(PMC *sub_pmc))
 
     return PMCNULL;
 }
-
 
 #define MMD_BIG_DISTANCE 0x7fff
 
@@ -747,7 +738,6 @@ mmd_distance(PARROT_INTERP, ARGIN(PMC *pmc), ARGIN(PMC *arg_tuple))
     return dist;
 }
 
-
 /*
 
 =item C<static PMC * Parrot_mmd_sort_candidates(PARROT_INTERP, PMC *arg_tuple,
@@ -781,7 +771,6 @@ Parrot_mmd_sort_candidates(PARROT_INTERP, ARGIN(PMC *arg_tuple), ARGIN(PMC *cl))
 
     return best_candidate;
 }
-
 
 /*
 
@@ -827,7 +816,6 @@ Parrot_mmd_maybe_candidate(PARROT_INTERP, ARGIN(PMC *pmc), ARGIN(PMC *cl))
     return 0;
 }
 
-
 /*
 
 =item C<static void mmd_search_by_sig_obj(PARROT_INTERP, STRING *name, PMC
@@ -864,7 +852,6 @@ mmd_search_by_sig_obj(PARROT_INTERP, ARGIN(STRING *name),
     Parrot_mmd_maybe_candidate(interp, multi_sub, candidates);
 }
 
-
 /*
 
 =item C<static void mmd_search_global(PARROT_INTERP, STRING *name, PMC *cl)>
@@ -889,7 +876,6 @@ mmd_search_global(PARROT_INTERP, ARGIN(STRING *name), ARGIN(PMC *cl))
 
     Parrot_mmd_maybe_candidate(interp, multi_sub, cl);
 }
-
 
 /*
 
@@ -920,7 +906,6 @@ mmd_add_multi_global(PARROT_INTERP, ARGIN(STRING *sub_name), ARGIN(PMC *sub_obj)
     PARROT_ASSERT(multi_sub->vtable->base_type == enum_class_MultiSub);
     VTABLE_push_pmc(interp, multi_sub, sub_obj);
 }
-
 
 /*
 
@@ -953,7 +938,6 @@ mmd_add_multi_to_namespace(PARROT_INTERP, ARGIN(STRING *ns_name),
     PARROT_ASSERT(multi_sub->vtable->base_type == enum_class_MultiSub);
     VTABLE_push_pmc(interp, multi_sub, sub_obj);
 }
-
 
 /*
 
@@ -997,7 +981,6 @@ Parrot_mmd_add_multi_from_long_sig(PARROT_INTERP,
     mmd_add_multi_to_namespace(interp, ns_name, sub_name, sub_obj);
     mmd_add_multi_global(interp, sub_name, sub_obj);
 }
-
 
 /*
 
@@ -1091,7 +1074,6 @@ Parrot_mmd_add_multi_list_from_c_args(PARROT_INTERP,
     }
 }
 
-
 /*
 
 =item C<MMD_Cache * Parrot_mmd_cache_create(PARROT_INTERP)>
@@ -1112,7 +1094,6 @@ Parrot_mmd_cache_create(PARROT_INTERP)
     PMC *cache = Parrot_pmc_new(interp, enum_class_Hash);
     return cache;
 }
-
 
 /*
 
@@ -1161,7 +1142,6 @@ mmd_cache_key_from_values(PARROT_INTERP, ARGIN(const char *name),
     return key;
 }
 
-
 /*
 
 =item C<PMC * Parrot_mmd_cache_lookup_by_values(PARROT_INTERP, MMD_Cache *cache,
@@ -1189,7 +1169,6 @@ Parrot_mmd_cache_lookup_by_values(PARROT_INTERP, ARGMOD(MMD_Cache *cache),
     return PMCNULL;
 }
 
-
 /*
 
 =item C<void Parrot_mmd_cache_store_by_values(PARROT_INTERP, MMD_Cache *cache,
@@ -1213,7 +1192,6 @@ Parrot_mmd_cache_store_by_values(PARROT_INTERP, ARGMOD(MMD_Cache *cache),
     if (key)
         VTABLE_set_pmc_keyed_str(interp, cache, key, chosen);
 }
-
 
 /*
 
@@ -1264,7 +1242,6 @@ mmd_cache_key_from_types(PARROT_INTERP, ARGIN(const char *name),
     return key;
 }
 
-
 /*
 
 =item C<PMC * Parrot_mmd_cache_lookup_by_types(PARROT_INTERP, MMD_Cache *cache,
@@ -1292,7 +1269,6 @@ Parrot_mmd_cache_lookup_by_types(PARROT_INTERP, ARGMOD(MMD_Cache *cache),
     return PMCNULL;
 }
 
-
 /*
 
 =item C<void Parrot_mmd_cache_store_by_types(PARROT_INTERP, MMD_Cache *cache,
@@ -1318,7 +1294,6 @@ Parrot_mmd_cache_store_by_types(PARROT_INTERP, ARGMOD(MMD_Cache *cache),
         VTABLE_set_pmc_keyed_str(interp, cache, key, chosen);
 }
 
-
 /*
 
 =item C<void Parrot_mmd_cache_mark(PARROT_INTERP, MMD_Cache *cache)>
@@ -1340,7 +1315,6 @@ Parrot_mmd_cache_mark(PARROT_INTERP, ARGMOD(MMD_Cache *cache))
     Parrot_gc_mark_PMC_alive(interp, cache);
 }
 
-
 /*
 
 =back
@@ -1348,13 +1322,12 @@ Parrot_mmd_cache_mark(PARROT_INTERP, ARGMOD(MMD_Cache *cache))
 =head1 SEE ALSO
 
 F<include/parrot/multidispatch.h>,
-L<http://svn.perl.org/perl6/doc/trunk/design/apo/A12.pod>,
-L<http://svn.perl.org/perl6/doc/trunk/design/syn/S12.pod>
+L<http://perlcabal.org/syn/S12.html>,
+L<http://dev.perl.org/perl6/doc/design/apo/A12.html>
 
 =cut
 
 */
-
 
 /*
  * Local variables:
