ca2a5d45909aecfd450129ce2a86f734b8b1fc8e
soh_cah_toa <kpolulak@gmail.com>
2011-06-21 20:29:11 -0400

    Created the Parrot::Test::HBDB module so tests can be run from multiple files

---
+++ lib/Parrot/Test/HBDB.pm
@@ -0,0 +1,101 @@
+# Copyright (C) 2001-2011, Parrot Foundation.
+
+=head1 NAME
+
+Parrot::Test::HBDB - testing routines for the HBDB debugger
+
+=head1 SYNOPSIS
+
+Set the number of tests to be run like this:
+
+    use Parrot::Test::HBDB tests => 8;
+
+=head1 DESCRIPTION
+
+The C<Parrot::Test::HBDB> module provides various test functions specific to HBDB.
+
+=head1 REQUIREMENTS
+
+This module requires you to build HBDB, by running "make hbdb". If this
+requirement has not been met, all tests will be skipped.
+
+=head2 Subroutines
+
+=over 4
+
+=cut
+
+package Parrot::Test::HBDB;
+
+use strict;
+use warnings;
+use lib qw(lib);
+
+use File::Spec;
+use Test::Builder;
+use Test::More ();
+use Parrot::Config;
+
+require Exporter;
+
+use base qw(Exporter);
+
+our @EXPORT = qw(hbdb_output_like);
+
+my $exec;
+
+# Check that HBDB has been built first
+BEGIN {
+    my $path = File::Spec->join(".", "hbdb");
+    $exec = $path . $PConfig{'exe'};
+
+    # Skip tests if executable doesn't exist
+    unless (-f $exec) {
+        Test::Builder->plan(skip_all => "HBDB hasn't been built. Please run \"make hbdb\"");
+        exit(0);
+    }
+}
+
+my $builder = Test::Builder->new();
+
+sub import {
+    my ($self, $plan, $args) = @_;
+
+    $builder->plan($plan, $args);
+
+    __PACKAGE__->export_to_level(2, __PACKAGE__);
+}
+
+=item C<hbdb_output_like($opt, qr/rx/, $desc)>
+
+Similar to C<Test::More::like()>. C<$opt> is the command-line switch to pass
+to hbdb, C<$rx> is a regular expression to match, and C<$desc> is a short
+description. Passes the test if the output matches the given regular
+expression.
+
+=cut
+
+sub hbdb_output_like {
+    my ($opt, $rx, $desc) = @_;
+    my $out = `$exec $opt`;
+
+    Test::More::like($out, $rx, $desc);
+}
+
+1;
+
+=back
+
+=head1 SEE ALSO
+
+F<docs/tests.pod>, F<docs/hbdb.pod>
+
+=cut
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
+
--- t/tools/hbdb.t
+++ t/tools/hbdb.t
@@ -12,12 +12,12 @@ t/tools/hbdb.t - tests for the HBDB debugger
 
 =head1 DESCRIPTION
 
-Tests the C<hbdb> debugger by providing it with a number of source
+Tests the HBDB debugger by providing it with a number of source
 files and running through it with various commands.
 
 =head1 REQUIREMENTS
 
-This test script requires you to build hbdb, by running "make hbdb". If this
+This test script requires you to build HBDB, by running "make hbdb". If this
 requirement has not been met, all tests will be skipped.
 
 =cut
@@ -28,25 +28,9 @@ use strict;
 use lib qw/lib/;
 
 use Test::More;
-use Parrot::Config;
-use Parrot::Test;
+use Parrot::Test::HBDB tests => 2;
 
-my ($path, $exec);
-
-# Check that HBDB has been built first
-BEGIN {
-    $path = File::Spec->join(".", "hbdb");
-    $exec = $path . $PConfig{'exe'};
-
-    # Skip tests if executable doesn't exist
-    unless (-f $exec) {
-        plan(skip_all => "HBDB hasn't been built. Please run \"make hbdb\"");
-        exit(0);
-    }
-
-    plan(tests => 2);
-}
-
+# Output to match
 my $help_regex = <<OUTPUT;
 /Usage: hbdb [[]options[]] [[]file[]]
 
@@ -63,29 +47,6 @@ hbdb_output_like("-h", $help_regex, "HBDB: Help message");
 
 # Test that unknown switches fail correctly
 hbdb_output_like("--foobar", $help_regex, "HBDB: Unknown options");
-
-=head1 SUBROUTINES
-
-=over 4
-
-=item C<hbdb_output_like($opt, $rx, $desc)>
-
-Acts as a wrapper around C<like()> to reduce duplicate code. C<$opt> is the
-command-line switch to test, C<$rx> is a regular expression to match, and
-C<$desc> is a short description.
-
-=cut
-
-sub hbdb_output_like {
-    my ($opt, $rx, $desc) = @_;
-    my $out = `$exec $opt`;
-
-    like($out, $rx, $desc);
-}
-
-=back
-
-=cut
 
 # Local Variables:
 #   mode: cperl
