80907fea191ef842df99bef31150e3c4f28f8333
chromatic <chromatic@wgz.org>
2011-06-29 21:02:08 -0700

    Added m0b file magic number verification.

--- src/m0/c/m0_interp.c
+++ src/m0/c/m0_interp.c
@@ -14,6 +14,10 @@ void call_frame_free( M0_Interp *interp, M0_CallFrame *cf );
 
 void interp_free( M0_Interp *interp );
 
+int parse_mob_header( M0_Interp *interp, FILE *stream );
+
+void * read_from_stream( FILE *stream, size_t bytes );
+
 int main( int argc, const char *argv[]) {
     M0_Interp *interp = new_interp();
 
@@ -59,6 +63,8 @@ load_m0b(M0_Interp *interp, const char *filename) {
     if (!mob)
         return 0;
 
+    parse_mob_header( interp, mob );
+
     return fclose( mob );
 }
 
@@ -72,4 +78,34 @@ void call_frame_free( M0_Interp *interp, M0_CallFrame *cf) {
 
 void interp_free( M0_Interp *interp ) {
     free( interp );
+}
+
+int
+parse_mob_header( M0_Interp *interp, FILE *stream ) {
+    char      *magic     = (char *)read_from_stream( stream, 8 );
+    const char header[8] = { 254, 77, 48, 66, 13, 10, 26, 10 };
+
+    if (strncmp( magic, header, 8 ) != 0) {
+        fprintf( stderr, "Invalid M0B header\n" );
+        free( magic );
+        return( 0 );
+    }
+
+    free( magic );
+}
+
+void *
+read_from_stream( FILE *stream, size_t bytes ) {
+    if (feof(stream))
+        return NULL;
+    else {
+        char *data = malloc( bytes );
+
+        if (fread(data, 1, bytes, stream) != bytes) {
+            free( data );
+            return NULL;
+        }
+
+        return data;
+    }
 }
