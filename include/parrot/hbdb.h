770adeca0763e6e1041198eb41daf638a0fded8a
Christoph Otto <christoph_github@mksig.org>
2011-07-01 05:30:44 -0400

    make flag macros less noisy

--- include/parrot/hbdb.h
+++ include/parrot/hbdb.h
@@ -21,9 +21,9 @@
 #define INTERP_ATTR(x) ((Parrot_ParrotInterpreter_attributes *)PMC_data(x))->interp
 
 /* Convience macros for manipulating the HBDB bitmask in Parrot_Interp */
-#define HBDB_FLAG_SET(interp, flag)   ((interp)->hbdb->state |= (flag))
-#define HBDB_FLAG_CLEAR(interp, flag) ((interp)->hbdb->state &= ~(flag))
-#define HBDB_FLAG_TEST(interp, flag)  ((interp)->hbdb->state &= (flag))
+#define HBDB_FLAG_SET(interp, flag)   ((interp)->hbdb->state = (hbdb_state_t)((interp)->hbdb->state | (flag)))
+#define HBDB_FLAG_CLEAR(interp, flag) ((interp)->hbdb->state = (hbdb_state_t)((interp)->hbdb->state & ~(flag)))
+#define HBDB_FLAG_TEST(interp, flag)  ((interp)->hbdb->state & (flag))
 
 /* Flags used to alter and identify the current state of the debugger   */
 typedef enum {
