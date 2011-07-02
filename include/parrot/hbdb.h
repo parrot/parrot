7ead92302f6221c72cd595e8113745f752b233c2
soh_cah_toa <kpolulak@gmail.com>
2011-07-02 18:02:12 -0400

    Fixed typo in inline comment thanks to kid51++

--- include/parrot/hbdb.h
+++ include/parrot/hbdb.h
@@ -20,7 +20,7 @@
 /* Abstract access to fields in Parrot_Interp */
 #define INTERP_ATTR(x) ((Parrot_ParrotInterpreter_attributes *)PMC_data(x))->interp
 
-/* Convience macros for manipulating the HBDB bitmask in Parrot_Interp */
+/* Convenience macros for manipulating the HBDB bitmask in Parrot_Interp */
 #define HBDB_FLAG_SET(interp, flag)   ((interp)->hbdb->state = (hbdb_state_t)((interp)->hbdb->state | (flag)))
 #define HBDB_FLAG_CLEAR(interp, flag) ((interp)->hbdb->state = (hbdb_state_t)((interp)->hbdb->state & ~(flag)))
 #define HBDB_FLAG_TEST(interp, flag)  ((interp)->hbdb->state & (flag))
