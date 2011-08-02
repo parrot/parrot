5f2c12ac87a0f6ebac886a6bf92f56b05954acaf
Brian Gernhardt <brian.gernhardt@mail.rit.edu>
2011-08-02 17:41:43 -0400

    POST::PBCCompiler - use old method to compile
    Use the code from bacek's PIR::Compiler to generate a PBC file.

--- compilers/pct/src/POST/PBCCompiler.pm
+++ compilers/pct/src/POST/PBCCompiler.pm
@@ -69,6 +69,63 @@ method mainpmc($packfile, *%adverbs) {
     $view.main_sub();
 }
 
+=begin
+Emit PBC file.
+
+Long explanation about it:
+Currently there is no way in Parrot to generate Packfile and attach it
+to Interp via existing API. This function is partially hack to generate
+serialized PBC, load it back and execute it (modulus fix for TT#1685).
+
+Best way to deal with such issues is:
+1. Switch Interp to use Packfile* PMC internally.
+2. Add API calls to attach freshly generated Packfile to current Interp.
+
+Quick "fix" can be:
+1. Add "PackFile_unpack_string" function which will accept STRING.
+2. Expose this function via Interp (or Packfile PMC method).
+
+Kind of wishful thinking, but we can fix it.
+
+=end
+
+method pbc($post, *%adverbs) {
+    #pir::trace(4);
+    my $packfile := self.packfile($post, |%adverbs);
+
+    my $main_sub := $post<main_sub>;
+
+    my $unlink;
+    my $filename := ~%adverbs<output>;
+    if !$filename {
+        # TODO Add mkstemp into OS PMC.
+        $filename := "/tmp/temp.pbc";
+        $unlink   := 1;
+    }
+
+    my $handle := pir::new__Ps('FileHandle');
+    $handle.open($filename, 'w');
+    $handle.print(~$packfile);
+    $handle.close();
+
+    return sub() {
+        #pir::trace(1);
+        pir::load_bytecode($filename);
+
+        #if $unlink {
+        #    my $os := pir::new__PS("OS");
+        #    $os.rm($filename);
+        #}
+
+        Q:PIR<
+            %r = find_lex '$main_sub'
+            $S99 = %r
+            %r = find_sub_not_null $S99
+            %r()
+        >;
+    };
+}
+
 
 ##########################################
 # Emiting pbc
