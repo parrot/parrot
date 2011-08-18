9e6888e4111b82f23fab8fc835aa22ebd53e6744
nbrown <nbrown04@gmail.com>
2011-08-18 00:13:03 -0400

    Add div_i op and integration test.

--- src/m0/c/m0_ops.c
+++ src/m0/c/m0_ops.c
@@ -73,6 +73,13 @@ m0_op_mult_i( M0_CallFrame *frame, const unsigned char *ops )
         frame->registers[ops[3]];
 }
 
+static void
+m0_op_div_i( M0_CallFrame *frame, const unsigned char *ops )
+{
+    frame->registers[ops[1]] = frame->registers[ops[2]] /
+        frame->registers[ops[3]];
+}
+
 int
 run_ops( M0_Interp *interp, M0_CallFrame *cf ) {
     UNUSED(interp);
@@ -124,6 +131,10 @@ run_ops( M0_Interp *interp, M0_CallFrame *cf ) {
 
                 case (M0_MULT_I):
                     m0_op_mult_i( cf, &ops[pc] );
+                break;
+
+                case (M0_DIV_I):
+                    m0_op_div_i( cf, &ops[pc] );
                 break;
 
                 default:
