9aea1cffe81eb0828f45c22983324f4f282b8503
Christoph Otto <christoph_github@mksig.org>
2011-08-30 14:33:55 -0700

    factor out build commonalities, start using System::Command

--- tools/dev/all_hll_test.pl
+++ tools/dev/all_hll_test.pl
@@ -4,6 +4,7 @@
 use strict;
 use warnings;
 use autodie;
+use System::Command;
 
 =head1 NAME
 
@@ -23,56 +24,104 @@ mkdir $tmp_dir;
 mkdir $install_dir;
 
 print "running Configure.pl on Parrot in cwd\n";
-system "perl Configure.pl --prefix=$install_dir --optimize";
+my $parrot_configure = System::Command->new(qq<perl Configure.pl --prefix=$install_dir --optimize>);
+waitpid($parrot_configure->pid(), 0);
+if (!waitpid($parrot_configure->pid(), 0)) {
+    die "OH NOES TEH PARROT CONFIGUER DIED";
+}
+my $configure_stdout = join('', readline($parrot_configure->stdout()));
+my $configure_stderr = join('', readline($parrot_configure->stderr()));
+
+print "building Parrot\n";
+my $parrot_build = System::Command->new(qw<make>);
+print while readline($parrot_build->stdout());
+waitpid($parrot_build->pid(), 0);
 print "installing Parrot into tmp dir\n";
-system qw<make install>;
+my $parrot_install = System::Command->new(qw<make install>);
+print while readline($parrot_install->stdout());
+waitpid($parrot_install->pid(), 0);
 
 {
     local %ENV = %ENV;
     $ENV{"PATH"} = "$install_dir/bin:" . $ENV{"PATH"};
 
-    chdir $tmp_dir;
-    print "cloning winxed\n";
-    system qw<git clone https://github.com/NotFound/winxed.git winxed_test>;
-    chdir "winxed_test";
-    system qq<winxed setup.winxed>;
-    print "testing winxed";
-    system qq<winxed setup.winxed test>;
+    test_hll({
+        "name" => "Winxed",
+        "clone" => [qw<git clone https://github.com/NotFound/winxed.git winxed_test>],
+        "build" => [qq<winxed setup.winxed>],
+        "test" =>  [qq<winxed setup.winxed test>],
+        "tmp"  => $tmp_dir,
+    });
 
-    chdir $tmp_dir;
-    print "cloning rosella\n";
-    system qw<git clone https://github.com/Whiteknight/Rosella.git rosella_test>;
-    chdir "rosella_test";
-    print "building rosella\n";
-    system qq<winxed setup.winxed>;
-    print "testing rosella";
-    system qq<winxed setup.winxed test>;
+    test_hll({
+        "name" => "Rosella",
+        "clone" => [qw<git clone https://github.com/Whiteknight/Rosella.git rosella_test>],
+        "build" => [qq<winxed setup.winxed>],
+        "test" =>  [qq<winxed setup.winxed test>],
+        "tmp"  => $tmp_dir,
+    });
 
-    chdir $tmp_dir;
-    print "cloning nqp\n";
-    system qw<git clone git://github.com/perl6/nqp.git nqp_test>;
-    chdir "nqp_test";
-    system "perl Configure.pl --with_parrot=$install_dir/bin/parrot";
-    print "installing nqp to tmp dir\n";
-    system qw<make install>;
+    test_hll({
+        "name"      => "nqp",
+        "clone"     => [qw<git clone https://github.com/perl6/nqp.git nqp_test>],
+        "configure" => [qq<perl Configure.pl --with_parrot=$install_dir/bin/parrot>],
+        "install"   => [qq<make install>],
+        "test"      => [qq<make test>],
+        "tmp"       => $tmp_dir,
+    });
 
-    chdir $tmp_dir;
-    system qw<git clone -b nom https://github.com/rakudo/rakudo.git rakudo_test>;
-    chdir "rakudo_test";
-    system "perl Configure.pl --with-parrot=$install_dir/bin/parrot";
-    print "building Rakudo\n";
-    system qw<make spectest_regression>;
-    print "running Rakudo's spectest_regression\n";
-    system qw<make spectest_regression>;
+    test_hll({
+        "name"      => "Rakudo",
+        "clone"     => [qw<git clone -b nom https://github.com/rakudo/rakudo.git rakudo_test>],
+        "configure" => [qq<perl Configure.pl --with_parrot=$install_dir/bin/parrot>],
+        #XXX: should be spectest_regression; using test to make this easier to test
+        "test"      => [qq<make test>],
+        "tmp"       => $tmp_dir,
+    });
 
-    chdir $tmp_dir;
-    print "cloning lua\n";
-    system qw<git clone https://github.com/fperrad/lua.git lua_test>;
-    chdir "lua_test";
-    system qw<parrot setup.pir>;
-    print "running lua's tests\n";
-    system qw<parrot setup.pir test>;
+    test_hll({
+        "name"  => "Lua",
+        "clone" => [qw<git clone https://github.com/fperrad/lua.git lua_test>],
+        "build" => [qq<parrot setup.pir>],
+        "test"  => [qq<parrot setup.pir test>],
+        "tmp"   => $tmp_dir,
+    });
 
+}
+
+sub test_hll {
+    my %opts = %{$_[0]};
+    my $proj_name     = $opts{name};
+    my $clone_cmd     = $opts{clone};
+    my $configure_cmd = $opts{configure};
+    my $build_cmd     = $opts{build};
+    my $install_cmd   = $opts{install};
+    my $test_cmd      = $opts{test};
+    my $tmp_dir       = $opts{tmp};
+
+    my $proj_dir = lc($proj_name) . "_test";
+    chdir $tmp_dir;
+    qx<rm -rf $proj_dir>;
+    print "cloning $proj_name\n";
+    system @$clone_cmd;
+    chdir $proj_dir;
+
+    if ($configure_cmd) {
+        print "configuring $proj_name";
+        system @$configure_cmd;
+    }
+    if ($build_cmd) {
+        print "building $proj_name\n";
+        system @$build_cmd;
+    }
+    if ($install_cmd) {
+        print "installing $proj_name";
+        system @$install_cmd;
+    }
+    if ($test_cmd) {
+        print "testing $proj_name";
+        system @$test_cmd;
+    }
 }
 
 # Local Variables:
