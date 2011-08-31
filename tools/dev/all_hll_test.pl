eb7a585d7a660e7d3b8f743474800ee3c8cc5850
Christoph Otto <christoph_github@mksig.org>
2011-08-31 11:40:36 -0700

    better status tracking, less verbose output

--- tools/dev/all_hll_test.pl
+++ tools/dev/all_hll_test.pl
@@ -35,17 +35,18 @@ my $install_dir = "$tmp_dir/parrot_install";
 qx<rm -rf $tmp_dir>;
 mkdir $tmp_dir;
 mkdir $install_dir;
-my %status;
+my $status = [];
+my $verbose = 0;
 
 build_project({
     "name"      => "Parrot",
-    "configure" => [qq<perl Configure.pl --prefix = $install_dir --optimize>],
+    "configure" => [qq<perl Configure.pl --prefix=$install_dir --optimize>],
     "build"     => [qq<make>],
     "install"   => [qq<make install>],
-    "test"      =>  [qq<make test>],
+    #"test"      => [qq<make test>],
     "tmp"       => $tmp_dir,
     "use_cwd"   => 1,
-}, \%status);
+}, $status);
 
 {
     local %ENV = %ENV;
@@ -57,7 +58,7 @@ build_project({
         "build" => [qq<winxed setup.winxed>],
         "test"  => [qq<winxed setup.winxed test>],
         "tmp"   => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"    => "Rosella",
@@ -67,7 +68,7 @@ build_project({
         # needed by parrot-linear-algebra
         "install" => [qq<winxed setup.winxed install>],
         "tmp"     => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"  => "parrot-linear-algebra",
@@ -75,7 +76,7 @@ build_project({
         "build" => [qq<parrot-nqp setup.nqp>],
         "test"  => [qq<parrot-nqp setup.nqp test>],
         "tmp"   => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"  => "ohm-eta-wink-kzd",
@@ -83,7 +84,7 @@ build_project({
         "build" => [qq<make>],
         "test"  => [qq<make test>],
         "tmp"   => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"  => "Plumage",
@@ -91,7 +92,7 @@ build_project({
         "build" => [qq<parrot setup.pir build>],
         "test"  => [qq<parrot setup.pir test>],
         "tmp"   => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"      => "nqp",
@@ -102,7 +103,7 @@ build_project({
         # needed by Rakudo
         "install"   => [qq<make install>],
         "tmp"       => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"      => "Rakudo",
@@ -111,7 +112,7 @@ build_project({
         "build"     => [qq<make>],
         "test"      => [qq<make spectest_regression>],
         "tmp"       => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"  => "Lua",
@@ -119,7 +120,7 @@ build_project({
         "build" => [qq<parrot setup.pir>],
         "test"  => [qq<parrot setup.pir test>],
         "tmp"   => $tmp_dir,
-    }, \%status);
+    }, $status);
 
     build_project({
         "name"      => "Partcl-nqp",
@@ -128,29 +129,50 @@ build_project({
         "build"     => [qq<make>],
         "test"      => [qq<make test>],
         "tmp"       => $tmp_dir,
-    }, \%status);
+    }, $status);
 }
 
-say "finished all everything";
-say "status for all projects and stages:";
-say Dumper(%status);
+foreach my $proj_status (@$status) {
+    my $proj_name = (keys %$proj_status)[0];
+    print "status for '$proj_name':";
+    my $stage_num = 0;
+    my @bad_stages;
+    my @good_stages;
+    foreach my $stage (@{$proj_status->{$proj_name}}) {
+        if ($stage->{exit}) {
+            push @bad_stages, $stage->{stage};
+        }
+        else {
+            push @good_stages, $stage->{stage};
+        }
+    }
+    if (@bad_stages) {
+        say "$bad_stages[0] failed";
+    }
+    else {
+        say "ok";
+    }
+}
 
 
 sub build_project {
     my %opts = %{$_[0]};
-    my $status = %{$_[1]};
+    my $global_status = $_[1];
     my $proj_name = $opts{name};
     my $tmp_dir   = $opts{tmp};
     my $use_cwd   = exists $opts{use_cwd} ? 1 : 0;
     my $stage_num = 0;
+    my @proj_status;
 
     my $proj_dir = lc($proj_name) . "_test";
     mkdir "$tmp_dir/logs" unless -d "$tmp_dir/logs";
+    print "running $proj_name: ";
 
     foreach my $stage (qw<clone configure build install test>){
+
         if ($stage eq "clone" && exists $opts{$stage} && !$use_cwd) {
 
-            say "running stage '$stage' for $proj_name: ";
+            print "$stage ";
             chdir $tmp_dir;
             qx<rm -rf $proj_dir>;
 
@@ -159,7 +181,7 @@ sub build_project {
             my $cmd_stdout = '';
             while (readline($cmd->stdout())) {
                 $cmd_stdout .= $_;
-                print $_;
+                print $_ if $verbose;
             }
 
             write_file("$tmp_dir/logs/${proj_name}_stage${stage_num}_${stage}_stdout.log", $cmd_stdout);
@@ -167,12 +189,13 @@ sub build_project {
             write_file("$tmp_dir/logs/${proj_name}_stage${stage_num}_${stage}_cmdline.log", join(' ', $cmd->cmdline()));
 
             $cmd->close();
-            $status{$proj_name}[$stage_num] = {
+            push @proj_status, {
                 stage => $stage,
                 exit  => $cmd->exit(),
             };
 
             if ($cmd->exit()) {
+                say "\n$stage failed";
                 return;
             }
             chdir "$tmp_dir/$proj_dir";
@@ -180,14 +203,14 @@ sub build_project {
         }
         elsif (exists $opts{$stage}) {
 
-            say "running stage '$stage' for $proj_name: " . join(' ', @{$opts{$stage}});
+            print "$stage ";
             $use_cwd || chdir "$tmp_dir/$proj_dir";
             my $cmd = System::Command->new(@{$opts{$stage}});
             my $cmd_stdout = '';
 
             while (readline($cmd->stdout())) {
                 $cmd_stdout .= $_;
-                print $_;
+                print $_ if $verbose;
             }
             my $cmd_stderr = join('', readline($cmd->stderr()));
 
@@ -196,17 +219,20 @@ sub build_project {
             write_file("$tmp_dir/logs/${proj_name}_stage${stage_num}_${stage}_cmdline.log", join(' ', $cmd->cmdline()));
 
             $cmd->close();
-            $status{$proj_name}[$stage_num] = {
+            push @proj_status, {
                 stage => $stage,
                 exit  => $cmd->exit(),
             };
 
             if ($cmd->exit()) {
+                say "$stage FAILED";
                 return;
             }
             $stage_num++;
         }
     }
+    say "ok!";
+    push @$global_status, { $proj_name => [ @proj_status ] };
 }
 
 # Local Variables:
