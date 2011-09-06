6f2c5c1dfeff57c2716f3c4d1f2d442ebe005ae2
jkeenan <jkeenan@cpan.org>
2011-09-05 21:22:04 -0400

    On Linux, use settings appropriate for gcc or g++ as defaults.  In effect, account for possibility that $cc symlinks to gcc or g++.

--- config/init/hints/linux.pm
+++ config/init/hints/linux.pm
@@ -26,9 +26,12 @@ sub runstep {
     if ( $libs !~ /-lrt\b/ ) {
         $libs .= ' -lrt';
     }
-    my $cc_shared      = $conf->data->get('cc_shared');
-    my $ld_share_flags = $conf->data->get('ld_share_flags');
-    my $ld_load_flags  = $ld_share_flags;;
+
+    # On Linux, we'll start by assuming that 'cc' is a symlink to 'gcc'
+    my $cc_shared      = $conf->option_or_data('cc_shared') || '-fPIC';
+    my $ld_share_flags = $conf->option_or_data('ld_share_flags') ||
+        '-shared -fPIC';
+    my $ld_load_flags  = $ld_share_flags;
 
     if ( $cc =~ /icc/ ) {
 
@@ -46,11 +49,6 @@ sub runstep {
         if ( $cc_shared !~ /-KPIC/ ) {
             $cc_shared = '-KPIC';
         }
-    }
-    elsif ( $cc =~ /g(c|\+){2}/ ) {
-        $cc_shared      = '-fPIC',
-        $ld_share_flags = '-shared -fPIC',
-        $ld_load_flags  = '-shared -fPIC',
     }
 
     if ( $ccflags !~ /-D_GNU_SOURCE/ ) {
