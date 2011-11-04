df69781380ede51268343350529c46e9bb85a3ae
Gerhard R <gerd.r.devel@googlemail.com>
2011-11-04 17:47:36 +0200

    cleanup compiler flags, file generation, ignored files

--- config/init/hints/msys.pm
+++ config/init/hints/msys.pm
@@ -5,8 +5,6 @@ package init::hints::msys;
 use strict;
 use warnings;
 
-# TODO: update to gerdr/msys
-
 sub _real_path {
     my ( $path ) = @_;
     $path = `mkdir -p '$path' && cd '$path' && pwd -W`;
@@ -15,43 +13,47 @@ sub _real_path {
 }
 
 sub runstep {
-    my ( $self, $conf ) = @_;
+    my (undef, $conf) = @_;
 
-    # Translate absolute paths accessed by Parrot tools
-    # from UNIX-style to Windows-style
+    # Translate absolute paths from UNIX-style to Windows-style
     my @keys = qw{bindir build_dir tempdir libdir includedir};
     my %dirs;
     @dirs{@keys} = map { _real_path $conf->data->get($_) } @keys;
     $conf->data->set(%dirs);
 
-    # Assume Windows 2000 or above
-    $conf->data->set(ccflags => "-DWIN32 -DWINVER=0x0500 ");
-
-    # Identify as msys as setting win32 => 1 breaks the build
+    # Identify as msys instead of win32
     $conf->data->set(msys => 1);
+
+    # Assume Windows 2000 or above
+    $conf->data->set(ccflags => '-DWINVER=0x0500 ');
 
     # Create Parrot as shared library
     $conf->data->set(
         parrot_is_shared    => 1,
         has_dynamic_linking => 1,
+        has_static_linking  => 0,
         ld_share_flags      => '-shared',
         ld_load_flags       => '-shared',
         sym_export          => '__declspec(dllexport)',
         sym_import          => '__declspec(dllimport)'
     );
 
-    # Create libparrot.dll in same directory as parrot.exe
-    # Generates unnecessary clutter in build directory
-    $conf->data->set(blib_dir => '.');
+    # Link non-default Windows libs and setup path for MSYS libs
+    # Use /bin instead of /lib to avoid incorrectly pulling in other MSYS libs
+    $conf->data->set(libs => '-lws2_32 -L/bin');
 
-    # Setup dynamic linking
+    # Setup built Parrot
     $conf->data->set(
-        libparrot_ldflags        => '-L' . $dirs{build_dir} . ' -lparrot',
-        libparrot_linkflags      => '-L' . $dirs{build_dir} . ' -lparrot',
-        inst_libparrot_ldflags   => '-L' . $dirs{bindir} . ' -lparrot',
-        inst_libparrot_linkflags => '-L' . $dirs{bindir} . ' -lparrot',
-        libs =>
-'-lmsvcrt -lmoldname -lkernel32 -luser32 -lgdi32 -lwinspool -lcomdlg32 -ladvapi32 -lshell32 -lole32 -loleaut32 -lnetapi32 -luuid -lws2_32 -lmpr -lwinmm -lversion '
+        libparrot_ldflags   => '-L. -lparrot',
+        libparrot_linkflags => '-L. -lparrot',
+    );
+
+    # Setup installed Parrot
+    my $bindir = $conf->data->get('bindir');
+    $bindir =~ s{ }{\\ }g;
+    $conf->data->set(
+        inst_libparrot_ldflags   => "-L$bindir -lparrot",
+        inst_libparrot_linkflags => "-L$bindir -lparrot"
     );
 
     # NCI testing
