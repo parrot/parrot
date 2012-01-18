02324c95e3107050fd216616a042273bbc9cf042
Whiteknight <wknight8111@gmail.com>
2012-01-17 19:01:16 -0500

    Get all the metadata in place for Parrot 4.0, 'Hyperstasis'

--- src/ops/core_ops.c
+++ src/ops/core_ops.c
@@ -24094,8 +24094,8 @@ op_lib_t core_op_lib = {
   "",                  /* suffix */
   PARROT_FUNCTION_CORE,                       /* core_type = PARROT_XX_CORE */
   0,                                /* flags */
-  3,    /* major_version */
-  11,    /* minor_version */
+  4,    /* major_version */
+  0,    /* minor_version */
   0,    /* patch_version */
   1104,             /* op_count */
   core_op_info_table,       /* op_info_table */
@@ -24223,7 +24223,7 @@ static void hop_deinit(PARROT_INTERP)
     hop_buckets = NULL;
 }PARROT_EXPORT
 op_lib_t *
-Parrot_DynOp_core_3_11_0(PARROT_INTERP, long init) {
+Parrot_DynOp_core_4_0_0(PARROT_INTERP, long init) {
     /* initialize and return op_lib ptr */
     if (init == 1) {
 
@@ -24252,7 +24252,7 @@ Parrot_lib_core_ops_load(PARROT_INTERP)
 
 {
     PMC *const lib = Parrot_pmc_new(interp, enum_class_ParrotLibrary);
-    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_3_11_0;
+    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_4_0_0;
     dynop_register(interp, lib);
     return lib;
 }
