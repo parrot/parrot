c74304257245875e43859e56762b60392e869826
Jonathan "Duke" Leto <jonathan@leto.net>
2012-03-20 08:31:54 -0700

    Run perl tools/release/auto_release.pl 4.2.0

--- src/ops/core_ops.c
+++ src/ops/core_ops.c
@@ -24325,7 +24325,7 @@ op_lib_t core_op_lib = {
   PARROT_FUNCTION_CORE,                       /* core_type = PARROT_XX_CORE */
   0,                                /* flags */
   4,    /* major_version */
-  1,    /* minor_version */
+  2,    /* minor_version */
   0,    /* patch_version */
   1113,             /* op_count */
   core_op_info_table,       /* op_info_table */
@@ -24454,7 +24454,7 @@ static void hop_deinit(PARROT_INTERP)
     hop_buckets = NULL;
 }PARROT_EXPORT
 op_lib_t *
-Parrot_DynOp_core_4_1_0(PARROT_INTERP, long init) {
+Parrot_DynOp_core_4_2_0(PARROT_INTERP, long init) {
     /* initialize and return op_lib ptr */
     if (init == 1) {
 
@@ -24483,7 +24483,7 @@ Parrot_lib_core_ops_load(PARROT_INTERP)
 
 {
     PMC *const lib = Parrot_pmc_new(interp, enum_class_ParrotLibrary);
-    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_4_1_0;
+    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_4_2_0;
     dynop_register(interp, lib);
     return lib;
 }
