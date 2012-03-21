16d0081a1a3ea350708ab88186b984b239c2b05e
Andy Lester <andy@petdance.com>
2012-03-21 09:48:23 -0500

    remove nested comments

--- frontend/parrot2/main.c
+++ frontend/parrot2/main.c
@@ -637,8 +637,7 @@ parseflags(Parrot_PMC interp, int argc, ARGIN(const char *argv[]),
           case OPT_GC_DEBUG:
           /*
 #if DISABLE_GC_DEBUG
-            /* Parrot_warn(interp, PARROT_WARNINGS_ALL_FLAG, */
-            Parrot_warn(interp, 0xFFFF,
+            Parrot_warn(interp, 0xFFFF, // PARROT_WARNINGS_ALL_FLAG,
                 "PARROT_GC_DEBUG is set but the binary was compiled "
                 "with DISABLE_GC_DEBUG.");
 #endif
