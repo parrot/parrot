67cec30db7971e4cd8ae3a632629f84b395bfc27
Andy Lester <andy@petdance.com>
2012-03-25 20:18:41 -0500

    inlined allocate_more_chunks() which is called in only one place

--- include/parrot/pointer_array.h
+++ include/parrot/pointer_array.h
@@ -67,38 +67,6 @@ Inline functions for faster access.
 
 */
 
-#define ASSERT_ARGS_allocate_more_chunks __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
-       PARROT_ASSERT_ARG(interp) \
-     , PARROT_ASSERT_ARG(self))
-
-/*
-
-=over 4
-
-=item C<static void allocate_more_chunks(PARROT_INTERP, Parrot_Pointer_Array
-*self)>
-
-allocate more chunks
-
-=cut
-
-*/
-
-static
-PARROT_INLINE
-void
-allocate_more_chunks(PARROT_INTERP, ARGIN(Parrot_Pointer_Array *self))
-{
-    ASSERT_ARGS(allocate_more_chunks)
-
-    self->current_chunk = self->total_chunks++;
-    mem_internal_realloc_n_typed(self->chunks,
-            self->total_chunks,
-            Parrot_Pointer_Array_Chunk*);
-    self->chunks[self->current_chunk] = mem_internal_allocate_typed(Parrot_Pointer_Array_Chunk);
-    self->chunks[self->current_chunk]->num_free  = CELL_PER_CHUNK;
-    self->chunks[self->current_chunk]->next_free = 0;
-}
 
 /*
 
@@ -133,8 +101,15 @@ Parrot_pa_insert(PARROT_INTERP, ARGIN(Parrot_Pointer_Array *self), ARGIN(void *p
 
     /* If there is no free chunks */
     if (self->current_chunk >= self->total_chunks
-            || !self->chunks[self->current_chunk]->num_free)
-        allocate_more_chunks(interp, self);
+            || !self->chunks[self->current_chunk]->num_free) {
+        self->current_chunk = self->total_chunks++;
+        mem_internal_realloc_n_typed(self->chunks,
+                self->total_chunks,
+                Parrot_Pointer_Array_Chunk*);
+        self->chunks[self->current_chunk] = mem_internal_allocate_typed(Parrot_Pointer_Array_Chunk);
+        self->chunks[self->current_chunk]->num_free  = CELL_PER_CHUNK;
+        self->chunks[self->current_chunk]->next_free = 0;
+    }
 
     chunk = self->chunks[self->current_chunk];
     --chunk->num_free;
