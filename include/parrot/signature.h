2281fc51207f5463541b057eaa8f2596560f7451
Vasily Chekalkin <bacek@bacek.com>
2012-03-27 22:40:51 +1100

    Implement fetching of named parameters and fix compilation errors

--- include/parrot/signature.h
+++ include/parrot/signature.h
@@ -35,30 +35,60 @@ void Parrot_pcc_signature_reset(PARROT_INTERP,
         __attribute__nonnull__(2);
 
 INTVAL Parrot_pcc_signature_get_integer(PARROT_INTERP,
-    ARGIN(Parrot_Signature *),
+    ARGIN(Parrot_Signature *self),
     INTVAL key)
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
 
+INTVAL Parrot_pcc_signature_get_integer_named(PARROT_INTERP,
+    ARGIN(Parrot_Signature *self),
+    ARGIN(STRING *key))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(2)
+        __attribute__nonnull__(3);
+
 FLOATVAL Parrot_pcc_signature_get_number(PARROT_INTERP,
-    ARGIN(Parrot_Signature *),
+    ARGIN(Parrot_Signature *self),
     INTVAL key)
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
+
+FLOATVAL Parrot_pcc_signature_get_number_named(PARROT_INTERP,
+    ARGIN(Parrot_Signature *self),
+    ARGIN(STRING *key))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(2)
+        __attribute__nonnull__(3);
 
 PARROT_CAN_RETURN_NULL
 PMC* Parrot_pcc_signature_get_pmc(PARROT_INTERP,
-    ARGIN(Parrot_Signature *),
+    ARGIN(Parrot_Signature *self),
     INTVAL key)
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
 
 PARROT_CAN_RETURN_NULL
+PMC * Parrot_pcc_signature_get_pmc_named(PARROT_INTERP,
+    ARGIN(Parrot_Signature *self),
+    ARGIN(STRING *key))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(2)
+        __attribute__nonnull__(3);
+
+PARROT_CAN_RETURN_NULL
 STRING* Parrot_pcc_signature_get_string(PARROT_INTERP,
-    ARGIN(Parrot_Signature *),
+    ARGIN(Parrot_Signature *self),
     INTVAL key)
         __attribute__nonnull__(1)
         __attribute__nonnull__(2);
+
+PARROT_CAN_RETURN_NULL
+STRING * Parrot_pcc_signature_get_string_named(PARROT_INTERP,
+    ARGIN(Parrot_Signature *),
+    ARGIN(STRING *key))
+        __attribute__nonnull__(1)
+        __attribute__nonnull__(2)
+        __attribute__nonnull__(3);
 
 void Parrot_pcc_signature_push_float(PARROT_INTERP,
     ARGIN(Parrot_Signature *self),
@@ -126,18 +156,38 @@ void Parrot_pcc_signature_push_string_named(PARROT_INTERP,
 #define ASSERT_ARGS_Parrot_pcc_signature_get_integer \
      __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(Parrot_Signature *))
+    , PARROT_ASSERT_ARG(self))
+#define ASSERT_ARGS_Parrot_pcc_signature_get_integer_named \
+     __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(self) \
+    , PARROT_ASSERT_ARG(key))
 #define ASSERT_ARGS_Parrot_pcc_signature_get_number \
      __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(Parrot_Signature *))
+    , PARROT_ASSERT_ARG(self))
+#define ASSERT_ARGS_Parrot_pcc_signature_get_number_named \
+     __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(self) \
+    , PARROT_ASSERT_ARG(key))
 #define ASSERT_ARGS_Parrot_pcc_signature_get_pmc __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(Parrot_Signature *))
+    , PARROT_ASSERT_ARG(self))
+#define ASSERT_ARGS_Parrot_pcc_signature_get_pmc_named \
+     __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(self) \
+    , PARROT_ASSERT_ARG(key))
 #define ASSERT_ARGS_Parrot_pcc_signature_get_string \
      __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
-    , PARROT_ASSERT_ARG(Parrot_Signature *))
+    , PARROT_ASSERT_ARG(self))
+#define ASSERT_ARGS_Parrot_pcc_signature_get_string_named \
+     __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
+       PARROT_ASSERT_ARG(interp) \
+    , PARROT_ASSERT_ARG(Parrot_Signature *) \
+    , PARROT_ASSERT_ARG(key))
 #define ASSERT_ARGS_Parrot_pcc_signature_push_float \
      __attribute__unused__ int _ASSERT_ARGS_CHECK = (\
        PARROT_ASSERT_ARG(interp) \
