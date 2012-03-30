parrot/parrot

commit 6050332f3afd125ed606e0e94f85e945888934e6

Cast a malloc to make g++ happy, #363

src/platform/generic/dl.c
@@ -44,7 +44,7 @@ push_handle_entry(void *handle)
 {
     struct handle_entry *e;
 
-    e = malloc(sizeof(struct handle_entry));
+    e = (handle_entry *) malloc(sizeof(struct handle_entry));
     if (!e) { return; }
     e->handle = handle;
     e->next = handle_list;

commit 0488f0afde22c5d04070ff0dc3884d298ba2b16a

Unbreak the build on gcc broken by 6050332f3afd125ed606e0e94f85e945888934e6

src/platform/generic/dl.c
@@ -44,7 +44,7 @@ push_handle_entry(void *handle)
 {
     struct handle_entry *e;
 
-    e = (handle_entry *) malloc(sizeof(struct handle_entry));
+    e = (struct handle_entry *) malloc(sizeof(struct handle_entry));
     if (!e) { return; }
     e->handle = handle;
     e->next = handle_list;