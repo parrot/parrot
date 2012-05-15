210ad264c4e3e2da03209f71c11ce2817b895f91
Whiteknight <wknight8111@gmail.com>
2012-05-15 19:31:10 -0400

    Commit metadata for 4.4.0, Parrot Banana Fanna Fo Ferret.

--- src/ops/core_ops.c
+++ src/ops/core_ops.c
@@ -24444,7 +24444,7 @@ op_lib_t core_op_lib = {
   PARROT_FUNCTION_CORE,                       /* core_type = PARROT_XX_CORE */
   0,                                /* flags */
   4,    /* major_version */
-  3,    /* minor_version */
+  4,    /* minor_version */
   0,    /* patch_version */
   1125,             /* op_count */
   core_op_info_table,       /* op_info_table */
@@ -24573,7 +24573,7 @@ static void hop_deinit(PARROT_INTERP)
     hop_buckets = NULL;
 }PARROT_EXPORT
 op_lib_t *
-Parrot_DynOp_core_4_3_0(PARROT_INTERP, long init) {
+Parrot_DynOp_core_4_4_0(PARROT_INTERP, long init) {
     /* initialize and return op_lib ptr */
     if (init == 1) {
 
@@ -24602,7 +24602,7 @@ Parrot_lib_core_ops_load(PARROT_INTERP)
 
 {
     PMC *const lib = Parrot_pmc_new(interp, enum_class_ParrotLibrary);
-    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_4_3_0;
+    ((Parrot_ParrotLibrary_attributes*)PMC_data(lib))->oplib_init = (void *) Parrot_DynOp_core_4_4_0;
     dynop_register(interp, lib);
     return lib;
 }
