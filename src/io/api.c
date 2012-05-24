5b0c2c6e657279f314b84c49974a143669ca302f
Whiteknight <wknight8111@gmail.com>
2012-05-23 21:13:42 -0400

    Move API functions out of core.c into api.c where they belong. If the function is public and IO-general-purpose, it belongs in api.c

--- src/io/api.c
+++ src/io/api.c
@@ -8,19 +8,7 @@ src/io/api.c - Parrot I/O API
 =head1 DESCRIPTION
 
 The Parrot I/O subsystem provides the core I/O functionality for all parts of
-Parrot. This file implements the public interface to the I/O subsystem. Common
-utility functions that aren't part of the public interface are defined in
-F<src/io/core.c> and utility functions for a specific platform are in
-F<src/io/unix.c>, F<src/io/win32.c>, or F<src/io/portable.c>. The platform is
-selected in F<include/parrot/io.h>, based on #defines from the configuration
-process.
-
-The C<FileHandle> PMC provides the class-based interface for filehandles that
-is used in Parrot ops.
-
-TODO: Where possible, extract some of the filehandle-related details into
-F<src/io/filehandle.c>, and extract the stringhandle details into
-a new F<src/io/io_string.c>.
+Parrot. This file implements the public interface to the I/O subsystem.
 
 =cut
 
@@ -37,6 +25,8 @@ a new F<src/io/io_string.c>.
 
 #include <stdarg.h>
 
+PIOOFF_T piooffsetzero;
+
 /* HEADERIZER HFILE: include/parrot/io.h */
 /* HEADERIZER BEGIN: static */
 /* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
@@ -51,6 +41,120 @@ static const STR_VTABLE * get_encoding(PARROT_INTERP, ARGIN(PMC *pmc))
     , PARROT_ASSERT_ARG(pmc))
 /* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
 /* HEADERIZER END: static */
+
+
+
+/* OLD FUNCTIONS
+    These functions need to be up-converted to the new architecture.
+*/
+
+/*
+
+=item C<void Parrot_io_init(PARROT_INTERP)>
+
+Sets up the interpreter's I/O storage and creates the C<STD*> handles.
+
+Called when creating an interpreter.
+
+=cut
+
+*/
+
+PARROT_EXPORT
+void
+Parrot_io_init(PARROT_INTERP)
+{
+    ASSERT_ARGS(Parrot_io_init)
+    /* Has interp been initialized already? */
+    if (interp->piodata) {
+        /* memsub system is up and running: */
+        /* Init IO stacks and handles for interp instance.  */
+        PIOHANDLE os_handle;
+
+        os_handle           = PIO_STDHANDLE(interp, PIO_STDIN_FILENO);
+        _PIO_STDIN(interp)  = Parrot_io_fdopen_flags(interp, PMCNULL,
+                                os_handle, PIO_F_READ);
+
+        os_handle           = PIO_STDHANDLE(interp, PIO_STDOUT_FILENO);
+        _PIO_STDOUT(interp) = Parrot_io_fdopen_flags(interp, PMCNULL,
+                                os_handle, PIO_F_WRITE);
+
+        os_handle           = PIO_STDHANDLE(interp, PIO_STDERR_FILENO);
+        _PIO_STDERR(interp) = Parrot_io_fdopen_flags(interp, PMCNULL,
+                                os_handle, PIO_F_WRITE);
+
+        if (Interp_debug_TEST(interp, PARROT_START_DEBUG_FLAG)) {
+            Parrot_io_eprintf(NULL, "I/O system initialized.\n");
+        }
+
+        return;
+    }
+
+
+    interp->piodata = mem_gc_allocate_zeroed_typed(interp, ParrotIOData);
+    if (interp->piodata == NULL)
+        Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_PIO_ERROR,
+            "PIO alloc piodata failure.");
+    interp->piodata->table         =
+            mem_gc_allocate_n_zeroed_typed(interp, PIO_NR_OPEN, PMC *);
+
+    if (!interp->piodata->table)
+        Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_PIO_ERROR,
+            "PIO alloc table failure.");
+}
+
+/*
+
+=item C<void Parrot_io_finish(PARROT_INTERP)>
+
+Closes the interpreter's IO resources.  Called during its interpreter
+destruction.
+
+=cut
+
+*/
+
+PARROT_EXPORT
+void
+Parrot_io_finish(PARROT_INTERP)
+{
+    ASSERT_ARGS(Parrot_io_finish)
+    /*
+     * TODO free IO of std-handles
+     */
+    mem_gc_free(interp, interp->piodata->table);
+    interp->piodata->table = NULL;
+    mem_gc_free(interp, interp->piodata);
+    interp->piodata = NULL;
+
+}
+
+
+/*
+
+=item C<void Parrot_IOData_mark(PARROT_INTERP, ParrotIOData *piodata)>
+
+Called from C<Parrot_gc_trace_root()> to mark the IO data live.
+
+=cut
+
+*/
+
+PARROT_EXPORT
+void
+Parrot_IOData_mark(PARROT_INTERP, ARGIN(ParrotIOData *piodata))
+{
+    ASSERT_ARGS(Parrot_IOData_mark)
+    INTVAL i;
+    ParrotIOTable table = piodata->table;
+
+    /* this was i < PIO_NR_OPEN, but only standard handles 0..2 need
+     * to be kept alive AFAIK -leo
+     */
+    for (i = 0; i < 3; ++i) {
+        Parrot_gc_mark_PMC_alive(interp, table[i]);
+    }
+}
 
 
 /*
