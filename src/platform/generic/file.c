parrot/parrot

commit 0ae1d2b85fd9af2e1368380efd6d87b7e0501921

[GH #967] Add readlink() and Parrot_file_readlink(), with tests.

src/platform/generic/file.c
@@ -576,6 +576,48 @@ Parrot_file_symlink(PARROT_INTERP, ARGIN(STRING *from), ARGIN(STRING *to))
 
 /*
 
+=item C<STRING *Parrot_file_readlink(PARROT_INTERP, STRING *path)>
+
+Reads a symlink.
+
+=cut
+
+XXX Throws a exception even for EINVAL (The named file is not a symbolic link).
+Is this the best behavior?
+
+*/
+
+PARROT_CANNOT_RETURN_NULL
+STRING *
+Parrot_file_readlink(PARROT_INTERP, ARGIN(STRING *path))
+{
+    char * const c_path = Parrot_str_to_platform_cstring(interp, path);
+    STRING      *str    = STRINGNULL;
+
+#if defined (PATH_MAX) && PATH_MAX > 0
+    const int buf_size = PATH_MAX;
+#else
+    const int buf_size = 1024;
+#endif
+
+    char * const buf = mem_internal_allocate_n_zeroed_typed(buf_size, char);
+
+    const ssize_t length_result = readlink(c_path, buf, buf_size);
+
+    if (length_result <= 0)
+        THROW("readlink");
+
+    Parrot_str_free_cstring(c_path);
+
+    str = Parrot_str_new_init(interp, buf, length_result,
+            Parrot_platform_encoding_ptr, 0);
+
+    mem_sys_free(buf);
+    return str;
+}
+
+/*
+
 =item C<void Parrot_file_link(PARROT_INTERP, STRING *from, STRING *to)>
 
 Creates a hard link

src/platform/win32/file.c
@@ -767,6 +767,40 @@ Parrot_file_symlink(PARROT_INTERP, ARGIN(STRING *from), ARGIN(STRING *to))
 
 /*
 
+=item C<STRING *Parrot_file_readlink(PARROT_INTERP, STRING *path)>
+
+Reads a symlink.
+
+=cut
+
+Early research towards a Win32 implementation; it looks quite involved:
+http://us.generation-nt.com/answer/how-read-symbolic-link-there-posix-readlink-equivalent-help-118000931.html
+    Contains example code in the 2nd comment!
+    From 1st comment:
+        Read about "Hard Links", "Junctions",...also have a look at IOCTL's
+        like FSCTL_GET_REPARSE_POINT and familiar for more information.
+        You should also pass the symbolic link handle opened with CreateFile(...)
+        to GetFinalPathNameByHandle(...) to get the target file, that works Vista
+        and upper only. AFAIK this also works for UNC Paths (local only!),...
+http://msdn.microsoft.com/en-us/library/aa365682(VS.85).aspx    Symbolic Link Effects on File Systems Functions
+http://msdn.microsoft.com/en-us/library/aa365680(VS.85).aspx    Symbolic Links
+http://msdn.microsoft.com/en-us/library/windows/desktop/aa365006(v=vs.85).aspx  Hard Links and Junctions
+http://msdn.microsoft.com/en-us/library/windows/desktop/aa365503(v=vs.85).aspx  Reparse Points
+http://msdn.microsoft.com/en-us/library/ff552012.aspx   REPARSE_DATA_BUFFER structure (Windows Drivers)
+
+*/
+
+PARROT_CANNOT_RETURN_NULL
+STRING *
+Parrot_file_readlink(PARROT_INTERP, ARGIN(STRING *path))
+{
+        Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_UNIMPLEMENTED,
+                "readlink error: Not yet implemented on Win32");
+}
+
+
+/*
+
 =item C<void Parrot_file_link(PARROT_INTERP, STRING *from, STRING *to)>
 
 Creates a symlink

include/parrot/platform_interface.h
@@ -203,6 +203,9 @@ PARROT_EXPORT
 void Parrot_file_symlink(PARROT_INTERP, ARGIN(STRING *from), ARGIN(STRING *to));
 
 PARROT_EXPORT
+STRING *Parrot_file_readlink(PARROT_INTERP, ARGIN(STRING *path));
+
+PARROT_EXPORT
 void Parrot_file_link(PARROT_INTERP, ARGIN(STRING *from), ARGIN(STRING *to));
 
 PARROT_EXPORT

src/pmc/os.pmc
@@ -254,6 +254,21 @@ Creates a symlink
 
 /*
 
+=item C<STRING *readlink(STRING *path)>
+
+Reads a symlink.
+
+=cut
+
+*/
+
+METHOD readlink(STRING *path) {
+    STRING * const link_path = Parrot_file_readlink(INTERP, path);
+    RETURN(STRING *link_path);
+}
+
+/*
+
 =item C<void link(STRING *from, STRING *to)>
 
 Creates a hard link

t/pmc/os.t
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use lib qw( . lib ../lib ../../lib );
 use Test::More;
-use Parrot::Test tests => 35;
+use Parrot::Test tests => 36;
 use Parrot::Config;
 use Cwd;
 use File::Spec;
@@ -541,6 +541,29 @@ OUT
 
     ok( -l "xpto", "symlink was really created" );
     unlink "xpto" if -f "xpto";
+}
+
+# Test readlink
+SKIP: {
+    skip "Admin rights and Vista needed for symlinks on Windows", 1 if $MSWin32;
+
+    my $real_path    = 'MANIFEST';
+    my $symlink_path = 'xpto';
+    symlink $real_path, $symlink_path
+        or warn "Failed to symlink: $!";
+
+    pir_output_is( <<'CODE', <<"OUT", 'Test readlink' );
+.sub main :main
+        $P1 = new ['OS']
+        $S1 = $P1."readlink"("xpto")
+        say $S1
+        end
+.end
+CODE
+$real_path
+OUT
+
+    unlink $symlink_path if -f $symlink_path;
 }
 
 # Test link to file. May require root permissions

commit e315aae09dedb8108b59cbd04d609cdb694f115a

[codingstd] Fix c_parens.t failures caused by 0ae1d2b .

src/platform/generic/file.c
@@ -594,7 +594,7 @@ Parrot_file_readlink(PARROT_INTERP, ARGIN(STRING *path))
     char * const c_path = Parrot_str_to_platform_cstring(interp, path);
     STRING      *str    = STRINGNULL;
 
-#if defined (PATH_MAX) && PATH_MAX > 0
+#if defined(PATH_MAX) && PATH_MAX > 0
     const int buf_size = PATH_MAX;
 #else
     const int buf_size = 1024;