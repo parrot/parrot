parrot/parrot

commit 4d0deaf4459045b13e6373d6947ae59a2a8b8ae9

[imcc] Fix and add -Wlogical-op test in imcparser.c
The disallow bareword method names check in sub_label_op
was bailing out on ' only, but skipped ".
Thanks to dcb314. Closing #1015

compilers/imcc/imcc.y
@@ -2124,10 +2124,10 @@ the_sub:
             /* disallow bareword method names; SREG name constants are fine */
             const char * const name = $3->name;
             if (!($3->type & VTREG)) {
-                if (*name != '\'' || *name != '\"')
+                if (*name != '\'' && *name != '\"')
                     IMCC_fataly(imcc, EXCEPTION_SYNTAX_ERROR,
                         "Bareword method name '%s' not allowed in PIR",
-                        $3->name);
+                        $name);
             }
 
             imcc->cur_obj = $1;

config/auto/warnings.pm
@@ -233,9 +233,6 @@ sub _init {
         '-Wcast-qual' => [ qw(
             compilers/imcc/imcparser.c
         ) ],
-        '-Wlogical-op' => [ qw(
-            compilers/imcc/imcparser.c
-        ) ],
     };
     # add at the end
     $gcc->{'override'} = $gpp->{'override'} = {

commit 453560a7ee2741d1a165f54ca0be6b83d89ec7e2

Fix compile failure introduced by 4d0deaf .
Failure not spotted because you have to already know to run
`perl Configure.pl --maintainer` and
`rm compilers/imcc/imcparser.{c,h}` to regenerate the IMCC grammar;
otherwise `make` will silently not even try to re-compile imcc.{y,l} .

Needs ticket to be opened on this silent issue.

compilers/imcc/imcc.y
@@ -2127,7 +2127,7 @@ the_sub:
                 if (*name != '\'' && *name != '\"')
                     IMCC_fataly(imcc, EXCEPTION_SYNTAX_ERROR,
                         "Bareword method name '%s' not allowed in PIR",
-                        $name);
+                        name);
             }
 
             imcc->cur_obj = $1;