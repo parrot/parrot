acdcfa7b41d11f535f4dcc700ef8a561d184e60d
Kent Fredric <kentfredric@gmail.com>
2013-11-28 23:01:28 +1300

    Add utility <podextract.pl>, which uses only the internals of perldoc required to extract pod from a source file

---
+++ tools/build/podextract.pl
@@ -0,0 +1,75 @@
+#! perl
+
+=head1 NAME
+
+tools/build/podextract.pl
+
+=head1 DESCRIPTION
+
+Simple POD Extraction
+
+=head1 SYNOPSIS
+
+Previously:
+
+    perldoc -ud $target $source
+
+or
+
+    perldoc -u $source > $target
+
+Now:
+
+    perl podextract.pl $source $target
+
+=head1 RATIONALE
+
+C<perldoc> does many things, and is somewhat complex.
+
+For instance, normally,
+
+    perldoc perldoc
+
+Inspects various paths, and paths that are controlled by C<%ENV> to find relevant documentation for "perldoc".
+
+And because of this, it is a little security concious, and pre-emptively drops root to UID=nobody
+
+But Parrot is not using this feature of C<perldoc>
+
+Parrot is simply using C<perldoc> as a content filter to extract C<POD> from source files.
+
+And as such, it only needs the alternative function
+
+    perldoc $PATH
+
+To work, which doesn't require C<%ENV>, and additionally, dropping root to C<UID=nobody> makes it impossible
+for some parrot to invoke C<perldoc -ud $target $source> on some systems, because it drops privelages and can then no longer read C<$source>, and can no longer write C<$target>.
+
+However, some means vendor tooling that B<MUST> execute C<make install> as root, by proxy, C<MUST> invoke C<perldoc> as root, and as such, invokes the security problem, which is impossible to work around.
+
+Given a simple C<chmod> is not enough, because parent directories also are not readable or writeable by C<UID=nobody>, and additionally, vendor tooling causes fatal access violations when a process running as C<UID=nobody> even attempts to do directory lookaround ( which C<perldoc> does much of as part of C<%ENV> handling )
+
+So this tool is simple:
+
+It takes the very core utility in C<PerlDoc> that parrot needed to utilize, and calls it directly.
+
+And this avoids the misguided attempts of increasing security, which simply does nothing useful.
+
+=cut
+
+use strict;
+use warnings;
+
+if ( not $ARGV[1] ) {
+    die "pod_extract <src> <target>";
+}
+if ( not -e $ARGV[0] ) {
+    die "<src> $ARGV[0] does not exist";
+}
+require Pod::Perldoc::ToPod;
+my $parser = Pod::Perldoc::ToPod->new();
+open my $output, '>', $ARGV[1] or die "Cant write to $ARGV[1] $! $?";
+$parser->parse_from_file( $ARGV[0], $output );
+
+1
+
