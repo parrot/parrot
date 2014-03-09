f8a98057e157a9be78557ba6a4dc0fd967e3695c
Paul Cochrane <paul@liekut.de>
2014-03-09 11:51:52 +0100

    [io] wrapping lines to within 100 characters

--- src/io/io_private.h
+++ src/io/io_private.h
@@ -148,19 +148,23 @@ struct _ParrotIOData {
 #define BUFFER_USED_SIZE(b) ((size_t)((b)->buffer_end - (b)->buffer_start))
 #define BUFFER_FREE_HEAD_SPACE(b) ((b)->buffer_start - (b)->buffer_ptr)
 #define BUFFER_FREE_END_SPACE(b) ((b)->buffer_size - ((b)->buffer_end - (b)->buffer_ptr))
-#define BUFFER_CAN_BE_NORMALIZED(b) (((size_t)BUFFER_FREE_HEAD_SPACE(b)) > ((size_t)BUFFER_USED_SIZE(b)))
+#define BUFFER_CAN_BE_NORMALIZED(b) \
+    (((size_t)BUFFER_FREE_HEAD_SPACE(b)) > ((size_t)BUFFER_USED_SIZE(b)))
 
 #define BUFFER_ASSERT_SANITY(b) do { \
         PARROT_ASSERT((b)->buffer_ptr <= (b)->buffer_start); \
         PARROT_ASSERT((b)->buffer_start <= (b)->buffer_end); \
         PARROT_ASSERT((b)->buffer_end <= (b)->buffer_ptr + (b)->buffer_size); \
-        PARROT_ASSERT(BUFFER_FREE_HEAD_SPACE(b) + BUFFER_USED_SIZE(b) + BUFFER_FREE_END_SPACE(b) == (b)->buffer_size); \
+        PARROT_ASSERT(BUFFER_FREE_HEAD_SPACE(b) + BUFFER_USED_SIZE(b) + BUFFER_FREE_END_SPACE(b) \
+                == (b)->buffer_size); \
     } while (0);
 
 #define BUFFER_DBG_PRINT(b) do { \
         fprintf(stderr, "\t%x [%x - %x]\n", (b)->buffer_ptr, (b)->buffer_start, (b)->buffer_end); \
-        fprintf(stderr, "\t\t(%d + %d + %d)\n", BUFFER_FREE_HEAD_SPACE(b), BUFFER_USED_SIZE(b), BUFFER_FREE_END_SPACE(b)); \
-        fprintf(stderr, "\t\t\t = %d\n", BUFFER_FREE_HEAD_SPACE(b) + BUFFER_USED_SIZE(b) + BUFFER_FREE_END_SPACE(b)); \
+        fprintf(stderr, "\t\t(%d + %d + %d)\n", \
+                BUFFER_FREE_HEAD_SPACE(b), BUFFER_USED_SIZE(b), BUFFER_FREE_END_SPACE(b)); \
+        fprintf(stderr, "\t\t\t = %d\n", \
+                BUFFER_FREE_HEAD_SPACE(b) + BUFFER_USED_SIZE(b) + BUFFER_FREE_END_SPACE(b)); \
     } while (0);
 
 /* Get an editable version of the IO_VTABLE structure, typically used during
