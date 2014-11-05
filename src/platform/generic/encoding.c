202c7f8fd79bdc964854acc27ab476cdafbf8d5e
Reini Urban <rurban@cpanel.net>
2014-11-05 22:50:37 +0100

    unify platform encoding.c, support all
    Closes GH #1120.
    Get support for other platform encodings for free.

--- src/platform/generic/encoding.c
+++ src/platform/generic/encoding.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, Parrot Foundation.
+ * Copyright (C) 2012-2014, Parrot Foundation.
  */
 
 /*
@@ -22,6 +22,11 @@ Platform C string encoding management
 
 #include "parrot/parrot.h"
 
+#if defined(PARROT_HAS_HEADER_LOCALE) && defined(PARROT_HAS_HEADER_LANGINFO)
+#  include <locale.h>
+#  include <langinfo.h>
+#endif
+
 /* HEADERIZER HFILE: none */
 
 /*
@@ -38,7 +43,32 @@ the OS, to an appropriate value.
 void
 Parrot_init_platform_encoding(SHIM_INTERP)
 {
+#if defined(PARROT_HAS_HEADER_LOCALE) && defined(PARROT_HAS_HEADER_LANGINFO)
+    const char *orig  = setlocale(LC_CTYPE, NULL); /* store original locale */
+    setlocale(LC_CTYPE, "");                       /* set locale to environment specification */
+    {
+        const char *codeset = nl_langinfo(CODESET);
+        if (STREQ(codeset, "UTF-8")
+         || STREQ(codeset, "utf-8"))          /* darwin 10.4 ppc */
+            Parrot_platform_encoding_ptr = Parrot_utf8_encoding_ptr;
+        else if (STREQ(codeset, "ISO-8859-1") /* linux */
+              || STREQ(codeset, "ISO8859-1")) /* bsd */
+            Parrot_platform_encoding_ptr = Parrot_latin1_encoding_ptr;
+        else if (STREQ(codeset, "ANSI_X3.4-1968") /* linux */
+              || STREQ(codeset, "US-ASCII")   /* freebsd */
+              || STREQ(codeset, "646"))       /* bsd */
+            Parrot_platform_encoding_ptr = Parrot_ascii_encoding_ptr;
+        else {
+            /* Can't use Parrot_warn here, the interpreter is not ready */
+            fprintf(stderr,
+                "Unknown codeset `%s', defaulting to ASCII\n", codeset);
+            Parrot_platform_encoding_ptr = Parrot_ascii_encoding_ptr;
+        }
+    }
+    setlocale(LC_CTYPE, orig); /* restore original locale */
+#else
     Parrot_platform_encoding_ptr = Parrot_ascii_encoding_ptr;
+#endif
 }
 
 
