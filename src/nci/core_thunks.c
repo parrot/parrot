c4ec76d414887b8c0c2e4f82647dd457706faeaf
Reini Urban <rurban@cpanel.net>
2015-01-23 01:59:38 -0600

    Release 7.0.1

--- src/nci/core_thunks.c
+++ src/nci/core_thunks.c
@@ -9,9 +9,9 @@
 
 /* src/nci/core_thunks.c
  *  Copyright (C) 2010-2015, Parrot Foundation.
- *  Overview:
- *     Native Call Interface routines.
- *     Code to call C from parrot without libffi.
+ *
+ * Native Call Interface routines.
+ * Code to call C from parrot without libffi.
  */
 
 
