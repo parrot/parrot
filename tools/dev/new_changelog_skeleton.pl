0c5754ff3fa5c8b8215077a5af2d2e4d44b20cd1
Bruce Gray <bruce.gray@acm.org>
2015-09-15 22:15:04 -0500

    Refactored new_changelog_skeleton.pl, and removed obsolete code that incremented the date.

--- tools/dev/new_changelog_skeleton.pl
+++ tools/dev/new_changelog_skeleton.pl
@@ -29,10 +29,16 @@ and then "git status" should show a modification to the ChangeLog file.
 
 =cut
 
-my $c = 1;
-my @months   = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
-my %months   = map { uc($_) => $c++ } @months;
-my $month_re = join '|', @months;
+sub military_to_iso_date {
+    my ($in_date) = @_;
+    my $c = 1;
+    my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+    my %months = map { uc($_) => sprintf('%02d', $c++) } @months;
+    my $months = join '|', @months;    
+    $in_date =~ m{ \A \s* (\d{2}) \s ($months) \s (\d{4}) \s* \z }msix
+        or die;
+    return sprintf '%04d-%02d-%02d', $3, $months{uc $2}, $1;
+}
 
 my $original_lines = Parrot::BuildUtil::slurp_file($changelog_path);
 my $json           = Parrot::BuildUtil::slurp_file($release_info_path);
@@ -49,25 +55,13 @@ my ( $version, $next_date ) =
 my ( $major, $minor, $patch ) = split /\./, $version;
 die unless defined $patch;
 
-my $re = qr{ \A (\d{1,2}) [ ] ($month_re) [ ] (\d{4}) \z }msx;
-my ( $day, $month_name, $year ) = ( $next_date =~ /$re/ )
-    or die;
-
-my $month = $months{ uc $month_name } or die;
-
-$month++;
-if ( $month == 13 ) {
-    $year++;
-    $month = 1;
-}
-
 $minor++;
 if ( $minor == 12 ) {
     $major++;
     $minor = 0;
 }
 
-my $date_ymd = sprintf '%04d-%02d-%02d', $year, $month, $day;
+my $date_ymd = military_to_iso_date($next_date);
 
 my $skeleton =<<"SKELETON";
 $date_ymd    release $major.$minor.0
