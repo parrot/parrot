74402a188a66e7150de854a0dd41d2b3486630d9
Bruce Gray <bruce.gray@acm.org>
2015-09-15 21:07:56 -0500

    Rewrote new_changelog_skeleton.pl to use JSON release data instead of parsing the Changelog, which allows us to put the complete future date (instead of leaving out the Day) into the skeleton. Fixed bugs too.

--- tools/dev/new_changelog_skeleton.pl
+++ tools/dev/new_changelog_skeleton.pl
@@ -1,8 +1,15 @@
 #! perl
-# Copyright (C) 2012-2013, Parrot Foundation.
+# Copyright (C) 2012-2013, 2015, Parrot Foundation.
 
 use strict;
 use warnings;
+use autodie;
+use JSON;
+use lib 'lib';
+use Parrot::BuildUtil;
+
+my $changelog_path    = './Changelog';
+my $release_info_path = './tools/release/release.json';
 
 =head1 NAME
 
@@ -20,41 +27,50 @@ Run this command
 
 and then "git status" should show a modification to the ChangeLog file.
 
-=head1 AUTHOR
-
-Jonathan "Duke" Leto
-
 =cut
 
-sub read_changelog {
-    my ($x,$y,$z);
-    my ($year, $month, $day);
+my $c = 1;
+my @months   = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+my %months   = map { uc($_) => $c++ } @months;
+my $month_re = join '|', @months;
 
-    open (my $fh, '<', 'ChangeLog') or die $!;
-    for my $line (<$fh>) {
-        if ($line =~ m/(\d+)-(\d+)-(\d+).*release (\d+)\.(\d+)\.(\d+)/) {
-            ($year, $month, $day, $x,$y,$z) = ($1,$2,$3,$4,$5,$6);
-            return ($year, $month, $day, $x,$y,$z);
-        }
-    }
-    close $fh;
-    return;
+my $original_lines = Parrot::BuildUtil::slurp_file($changelog_path);
+my $json           = Parrot::BuildUtil::slurp_file($release_info_path);
+
+my $href = decode_json($json)
+    or die "Failed to decode json";
+
+my ( $version, $next_date ) =
+    map { $href->{$_} or die "'$_' missing from $release_info_path" }
+    qw( release.version release.nextdate );
+    # "release.version"  : "7.7.0",
+    # "release.nextdate" : "15 Sep 2015",
+
+my ( $major, $minor, $patch ) = split /\./, $version;
+die unless defined $patch;
+
+my $re = qr{ \A (\d{1,2}) [ ] ($month_re) [ ] (\d{4}) \z }msx;
+my ( $day, $month_name, $year ) = ( $next_date =~ /$re/ )
+    or die;
+
+my $month = $months{ uc $month_name } or die;
+
+$month++;
+if ( $month == 13 ) {
+    $year++;
+    $month = 1;
 }
 
-sub run {
-    my ($year, $month, $day, $x, $y, $z) = read_changelog();
+$minor++;
+if ( $minor == 12 ) {
+    $major++;
+    $minor = 0;
+}
 
-    return unless $year && $x;
+my $date_ymd = sprintf '%04d-%02d-%02d', $year, $month, $day;
 
-    $month eq '12' ? ($month=1, $year++) : $month++;
-
-    $y     eq '11' ? ($x++, $y = 0, $z =0 ) : $y++;
-
-    local $/;
-    open (my $fh, '<', 'ChangeLog') or die $!;
-    my $changelog = <$fh>;
-    my $skeleton =<<SKELETON;
-$year-$month-XX    release $x.$y.$z
+my $skeleton =<<"SKELETON";
+$date_ymd    release $major.$minor.0
     - Core
     - Build
     - Documentation
@@ -63,12 +79,9 @@ $year-$month-XX    release $x.$y.$z
 
 SKELETON
 
-    open (my $wfh, '>', 'ChangeLog') or die $!;
-    print $wfh $skeleton, $changelog;
-    close $wfh;
-}
-
-run();
+open my $wfh, '>', $changelog_path;
+print $wfh $skeleton, $original_lines;
+close $wfh;
 
 # Local Variables:
 #   mode: cperl
